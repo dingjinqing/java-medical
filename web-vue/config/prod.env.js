f2ef58e052f7cb6f1f7c749458d7fddce08314f8
lixinguo <lixinguo@wangdian.cn>
2019-06-24 03:40:17 +0000

    图片域名

--- web-vue/config/prod.env.js
+++ web-vue/config/prod.env.js
@@ -1,7 +1,7 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  FILE_NAME: '"dev"',
+  FILE_NAME: '"prod"',
   API_DOMAIN: '"localhost:8086"',
-  IMAGE_DOMAIN: '"localhost:8080"'
+  IMAGE_DOMAIN: '"mpdevimg2.weipubao.cn"'
 }
