6753bb6ac26d0d1c70c1d61f516f099429abfba2
huangzhuangzhuang <huangzhuangzhuang@wangdian.cn>
2019-06-27 10:55:19 +0000

    system用户申请列表

---
+++ jmp/src/main/java/com/vpu/mp/service/saas/official/FreeExperienceService.java
@@ -0,0 +1,129 @@
+package com.vpu.mp.service.saas.official;
+
+import org.apache.commons.dbcp2.Utils;
+import org.apache.commons.lang3.StringUtils;
+import org.jooq.Record;
+import org.jooq.SelectWhereStep;
+
+import com.vpu.mp.db.main.tables.pojos.ShopFreeExperience;
+import com.vpu.mp.service.foundation.BaseService;
+import com.vpu.mp.service.foundation.Page;
+import com.vpu.mp.service.foundation.PageResult;
+import com.vpu.mp.service.foundation.Util;
+import com.vpu.mp.service.saas.official.FreeExperienceService.FreeExperiencePageListParam;
+
+import lombok.Data;
+
+import static com.vpu.mp.db.main.tables.ShopFreeExperience.SHOP_FREE_EXPERIENCE;
+
+import java.sql.Timestamp;
+/**
+ * 
+ * System中官方申请试用服务
+ * @author 黄壮壮
+ * 2019-06-27 15:45
+ */
+public class FreeExperienceService extends BaseService{
+	
+	/**
+	 * 
+	 * 接收客户端参数类
+	 */
+	@Data
+	final public static class FreeExperiencePageListParam{
+		public String company;
+		public String contact;
+		public String startTime;
+		public String endTime;
+		public Integer provinceId;
+		public Integer searchShopId;
+		public Page page;
+		public Byte isDeal;   // 已处理： 1 ； 未处理： 0
+	}
+	
+	/**
+	 * 分页查询
+	 * @param param
+	 * @return PageResult
+	 */
+	public PageResult getPageList(FreeExperiencePageListParam param) {
+		SelectWhereStep<Record> select = db().select().from(SHOP_FREE_EXPERIENCE);
+		//多条件选择
+		select = this.buildOptions(select,param);
+		//升序
+		select.orderBy(SHOP_FREE_EXPERIENCE.FE_ID.asc());
+		
+		if(param.page.pageRows != null && param.page.pageRows>0) {
+			return this.getPageResult(select,param.page.currentPage,param.page.pageRows);
+		}else {
+			return this.getPageResult(select, param.page.currentPage);
+		}
+	}
+	
+	/**
+	 * 多条件查询
+	 * @param select
+	 * @param param
+	 * @return
+	 */
+	private SelectWhereStep<Record> buildOptions(SelectWhereStep<Record> select,FreeExperiencePageListParam param) {
+		if(param == null) {
+			return select;
+		}
+		
+		//公司
+		if(!StringUtils.isEmpty(param.company)) {
+			select.where(SHOP_FREE_EXPERIENCE.COMPANY.eq(param.getCompany()));
+		}
+		
+		
+		//省份编号为int
+		if(param.provinceId != null) {
+			select.where(SHOP_FREE_EXPERIENCE.PROVINCE_ID.eq(param.getProvinceId()));
+		}
+		
+		//开始时间
+		if(param.getStartTime() != null) {
+			select.where(SHOP_FREE_EXPERIENCE.ASK_TIME.ge(Util.convertToTimestamp(param.getStartTime())));
+		}
+		//最终时间
+		if(param.getEndTime() != null) {
+			select.where(SHOP_FREE_EXPERIENCE.ASK_TIME.le(Util.convertToTimestamp(param.getEndTime())));
+		}
+		//店铺ID
+		if(param.getSearchShopId() != null) {
+			select.where(SHOP_FREE_EXPERIENCE.SHOP_ID.eq(param.getSearchShopId()));
+		}
+		
+		//是否处理
+		if(param.isDeal != null) {
+			select.where(SHOP_FREE_EXPERIENCE.IS_DEAL.eq(param.getIsDeal()));
+		}
+		return select;
+	}
+	
+	
+	/**
+	 * 查询手机号是否注册成功
+	 * @param mobile
+	 * @return boolean
+	 */
+	public boolean verifyIsExist(String mobile) {
+		return db().fetchCount(SHOP_FREE_EXPERIENCE, SHOP_FREE_EXPERIENCE.MOBILE.eq(mobile))>0;		 
+	}
+	
+	/**
+	 * 将用户信息持久化
+	 * @param usernamedbzz
+	 * @param mobile
+	 */
+	public int insertUserInfo(ShopFreeExperience shopFreeExperience) {
+		ShopFreeExperience shop = shopFreeExperience;
+		int i = this.db()
+			.insertInto(SHOP_FREE_EXPERIENCE,SHOP_FREE_EXPERIENCE.CONTACT, SHOP_FREE_EXPERIENCE.MOBILE,SHOP_FREE_EXPERIENCE.SOURCE)
+			.values(shop.getContact(), shop.getMobile(),shop.getSource())
+			.execute();
+		return i;
+	}
+	
+}
