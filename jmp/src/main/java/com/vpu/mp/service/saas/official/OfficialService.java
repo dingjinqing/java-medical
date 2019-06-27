f6c20497f69ec357ff9c1b065519a0284ac374b8
huangzhuangzhuang <huangzhuangzhuang@wangdian.cn>
2019-06-27 10:37:08 +0000

    更新

--- jmp/src/main/java/com/vpu/mp/service/saas/official/OfficialService.java
+++ jmp/src/main/java/com/vpu/mp/service/saas/official/OfficialService.java
@@ -12,26 +12,7 @@ import com.vpu.mp.service.foundation.BaseService;
  * 首页处理逻辑
  */
 public class OfficialService extends BaseService{
-	/**
-	 * 查询手机号是否注册成功
-	 * @param mobile
-	 * @return boolean
-	 */
-	public boolean verifyIsExist(String mobile) {
-		return db().fetchCount(SHOP_FREE_EXPERIENCE, SHOP_FREE_EXPERIENCE.MOBILE.eq(mobile))>0;		 
-	}
 	
-	/**
-	 * 将用户信息持久化
-	 * @param usernamedbzz
-	 * @param mobile
-	 */
-	public int insertUserInfo(ShopFreeExperience shopFreeExperience) {
-		ShopFreeExperience shop = shopFreeExperience;
-		int i = this.db()
-			.insertInto(SHOP_FREE_EXPERIENCE,SHOP_FREE_EXPERIENCE.CONTACT, SHOP_FREE_EXPERIENCE.MOBILE,SHOP_FREE_EXPERIENCE.SOURCE)
-			.values(shop.getContact(), shop.getMobile(),shop.getSource())
-			.execute();
-		return i;
-	}
+	public FreeExperienceService freeExperienceService;
+	
 }
