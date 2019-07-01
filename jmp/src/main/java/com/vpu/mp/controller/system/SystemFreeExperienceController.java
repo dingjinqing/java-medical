b41b122a199544622de40fcdc67200398d746bb3
huangzhuangzhuang <huangzhuangzhuang@wangdian.cn>
2019-07-01 10:12:23 +0000

    添加更新功能

--- jmp/src/main/java/com/vpu/mp/controller/system/SystemFreeExperienceController.java
+++ jmp/src/main/java/com/vpu/mp/controller/system/SystemFreeExperienceController.java
@@ -2,19 +2,21 @@ package com.vpu.mp.controller.system;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.vpu.mp.service.foundation.JsonResult;
+import com.vpu.mp.service.foundation.JsonResultCode;
 import com.vpu.mp.service.foundation.PageResult;
 import com.vpu.mp.service.saas.official.FreeExperienceService.FreeExperiencePageListParam;
 import com.vpu.mp.support.LineConvertHump;
-
+import com.vpu.mp.service.pojo.saas.offical.FreeExperienceInfo;
 
 /**
- * 
+ * 处理system中用户申请试用列表
  * @author 黄壮壮
  * 2019-06-26 17:27
  */
@@ -33,5 +35,25 @@ public class SystemFreeExperienceController extends SystemBaseController{
 		PageResult pageResult = this.saas.official.freeExperienceService.getPageList(param);
 		
 		return this.success(pageResult);
+	}
+	
+	
+	/**
+	 * 
+	 * @param freeExperienceInfo
+	 * @return
+	 */
+	@PostMapping("/system/free/set/info")
+	@ResponseBody
+	public JsonResult setFreeExperienceInfo(
+			@LineConvertHump @RequestBody FreeExperienceInfo freeExperienceInfo) {
+
+		int result = this.saas.official.freeExperienceService.updateFreeExperience(freeExperienceInfo);
+	
+		if(result == 0) {
+			return this.fail();
+		}else {
+			return this.success();
+		} 
 	}
 }
