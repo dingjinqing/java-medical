58d8323647c8fae495fdd79cd500cba369ff7a4d
huangzhuangzhuang <huangzhuangzhuang@wangdian.cn>
2019-07-01 01:09:36 +0000

    更新

--- jmp/src/main/java/com/vpu/mp/service/saas/official/OfficialService.java
+++ jmp/src/main/java/com/vpu/mp/service/saas/official/OfficialService.java
@@ -1,18 +1,12 @@
 package com.vpu.mp.service.saas.official;
 
-import static com.vpu.mp.db.main.tables.ShopFreeExperience.SHOP_FREE_EXPERIENCE;
-
-import com.vpu.mp.db.main.tables.pojos.ShopFreeExperience;
 import com.vpu.mp.service.foundation.BaseService;
 
 /**
  * 
  * @author 黄壮壮
- * 2019-06-25 
- * 首页处理逻辑
+ * 2019-07-01 09:09
  */
 public class OfficialService extends BaseService{
-	
 	public FreeExperienceService freeExperienceService;
-	
 }
