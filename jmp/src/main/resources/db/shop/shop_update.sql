81f639e589519bb53ec960c111d614cf4a8ad79f
lixiaobing <lixiaobing@wangdian.cn>
2019-07-01 03:20:24 +0000

    mini_shop库:b2c_sort表添加新字段，修改一些字段类型

--- jmp/src/main/resources/db/shop/shop_update.sql
+++ jmp/src/main/resources/db/shop/shop_update.sql
@@ -1,3 +1,10 @@
--- b2c_goods_brand 将记录创建时间添加默认值为当前时间戳，记录修改时间设置为当前记录变化时间戳
+-- 李晓冰 b2c_goods_brand 将记录创建时间添加默认值为当前时间戳，记录修改时间设置为当前记录变化时间戳
 ALTER TABLE b2c_goods_brand MODIFY COLUMN add_time DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '记录创建时间';
 ALTER TABLE b2c_goods_brand MODIFY COLUMN update_time DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '记录修改时间';
+
+-- b2c_sort 添加update_time 字段
+ALTER TABLE b2c_sort ADD COLUMN update_time TIMESTAMP  DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '记录修改时间' AFTER create_time;
+-- b2c_sort 删除字段keywords
+ALTER TABLE b2c_sort DROP COLUMN keywords;
+-- b2c_sort 修改parent_id字段类型，使其和sort_id类型一致
+ALTER TABLE b2c_sort MODIFY COLUMN parent_id INT NOT NULL;
