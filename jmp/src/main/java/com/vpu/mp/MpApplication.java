a5ab0720fab5ae7a4fdcf73736f58e6f810ee496
lixinguo <lixinguo@wangdian.cn>
2019-07-03 04:17:37 +0000

    时区问题，默认北京时区

--- jmp/src/main/java/com/vpu/mp/MpApplication.java
+++ jmp/src/main/java/com/vpu/mp/MpApplication.java
@@ -1,5 +1,7 @@
 package com.vpu.mp;
 
+import java.util.TimeZone;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
@@ -14,6 +16,7 @@ import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfigurat
 public class MpApplication {
 	
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Shanghai"));
 		SpringApplication app = new SpringApplication(MpApplication.class);
 		app.addListeners(new MpRunListener());
 		app.run(args);
