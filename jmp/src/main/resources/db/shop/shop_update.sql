38e5825414a8a945d4c0293099a6776b0ce9d9c9
lixiaobing <lixiaobing@wangdian.cn>
2019-07-03 10:34:04 +0000

    修改shop库goods_brand,goods_sort表

--- jmp/src/main/resources/db/shop/shop_update.sql
+++ jmp/src/main/resources/db/shop/shop_update.sql
@@ -8,3 +8,12 @@ ALTER TABLE b2c_sort ADD COLUMN update_time TIMESTAMP  DEFAULT CURRENT_TIMESTAMP
 ALTER TABLE b2c_sort DROP COLUMN keywords;
 -- b2c_sort 修改parent_id字段类型，使其和sort_id类型一致
 ALTER TABLE b2c_sort MODIFY COLUMN parent_id INT NOT NULL;
+
+-- 7月3号 李晓冰 设置goods_brand goods_sort 部分字段默认值和非空约束
+ALTER TABLE b2c_goods_brand MODIFY COLUMN is_recommend TINYINT NOT NULL DEFAULT 0 COMMENT '是否为推荐品牌 0否 1是';
+ALTER TABLE b2c_goods_brand MODIFY COLUMN classify_id TINYINT NOT NULL DEFAULT 0 COMMENT '品牌所属分类 0未分类 否则是分类id';
+ALTER TABLE b2c_goods_brand MODIFY COLUMN update_time DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '记录修改时间';
+
+ALTER TABLE b2c_sort MODIFY COLUMN sort_name VARCHAR(90) NOT NULL;
+ALTER TABLE b2c_sort MODIFY COLUMN parent_id INT(11) NOT NULL DEFAULT 0 COMMENT '分类父节点，0表示一级';
+ALTER TABLE b2c_sort MODIFY COLUMN sort_name VARCHAR(90) NOT NULL;
