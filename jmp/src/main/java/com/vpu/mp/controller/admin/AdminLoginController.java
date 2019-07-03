37f5c4d2058cf4b8253ae80aeeb367170c23fb49
zhaojianqiang <zhaojianqiang@wangdian.cn>
2019-07-03 11:09:16 +0000

    增加前缀api

--- jmp/src/main/java/com/vpu/mp/controller/admin/AdminLoginController.java
+++ jmp/src/main/java/com/vpu/mp/controller/admin/AdminLoginController.java
@@ -5,6 +5,7 @@ import javax.validation.Valid;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.vpu.mp.service.foundation.JsonResult;
@@ -18,6 +19,7 @@ import com.vpu.mp.service.pojo.shop.auth.ShopLoginParam;
  *
  */
 @RestController
+@RequestMapping("/api")
 public class AdminLoginController extends AdminBaseController {
 
 	@PostMapping(value = "/admin/login")
