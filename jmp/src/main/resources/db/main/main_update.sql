f573dd887be9b31067ac758ef7050a74f92ae54a
wangbingbing <wangbingbing@wangdian.cn>
2019-07-05 09:31:11 +0000

    b2c_shop结构

--- jmp/src/main/resources/db/main/main_update.sql
+++ jmp/src/main/resources/db/main/main_update.sql
@@ -16,23 +16,10 @@ MODIFY COLUMN `pub_time`  timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' COMME
 --表中店铺续费金额字段变为decimal
 alter table b2c_shop_renew modify column renew_money decimal(12,2);
 
---删除店铺表中share_config、shop_style字段，该项配置转移到店铺库shop_cfg表中
+--07-05王兵兵 删除店铺表中share_config、shop_style字段，该项配置转移到店铺库shop_cfg表中
 alter table b2c_shop drop column share_config;
 alter table b2c_shop drop column shop_style;
 
---店铺表多余字段
-alter table b2c_shop drop column shop_bank_no;
-alter table b2c_shop drop column shop_license;
-alter table b2c_shop drop column shop_tax_credential;
-alter table b2c_shop drop column organization_code_certificate;
-alter table b2c_shop drop column permit_for_opening_bank_account;
-alter table b2c_shop drop column financial_registration;
-alter table b2c_shop drop column shop_bank_name;
-alter table b2c_shop drop column shop_bank_branch;
-alter table b2c_shop drop column reject_reason;
-alter table b2c_shop drop column shop_stat_code;
-alter table b2c_shop drop column shop_icp;
-alter table b2c_shop drop column shop_copyright;
-
+--07-05王兵兵 shop表缺少的字段
 alter table b2c_shop ADD COLUMN   `expire_time` date DEFAULT NULL COMMENT '到期时间';
 
