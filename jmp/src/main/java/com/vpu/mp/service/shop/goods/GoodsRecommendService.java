0662efe019f323a05acc266d14f0662f16958957
huangronggang <huangronggang@wangdian.cn>
2019-07-09 11:53:04 +0000

    新增商品推荐 查询接口 （之前漏提交的）

--- jmp/src/main/java/com/vpu/mp/service/shop/goods/GoodsRecommendService.java
+++ jmp/src/main/java/com/vpu/mp/service/shop/goods/GoodsRecommendService.java
@@ -1,11 +1,16 @@
 package com.vpu.mp.service.shop.goods;
 
 import static com.vpu.mp.db.shop.Tables.RECOMMEND_GOODS;
+import static com.vpu.mp.db.shop.Tables.GOODS;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import org.jooq.Record;
 import org.jooq.RecordMapper;
 import org.jooq.SelectConditionStep;
 import org.jooq.SelectJoinStep;
+import org.jooq.SelectWhereStep;
 import org.jooq.tools.StringUtils;
 
 import com.vpu.mp.db.shop.tables.RecommendGoods;
@@ -13,6 +18,9 @@ import com.vpu.mp.db.shop.tables.records.RecommendGoodsRecord;
 import com.vpu.mp.service.foundation.BaseService;
 import com.vpu.mp.service.foundation.DelFlag;
 import com.vpu.mp.service.foundation.PageResult;
+import com.vpu.mp.service.pojo.shop.goods.Goods;
+import com.vpu.mp.service.pojo.shop.goods.recommend.GoodsRecommend;
+import com.vpu.mp.service.pojo.shop.goods.recommend.GoodsRecommendPageListParam;
 
 /**
  * @author 黄荣刚
@@ -21,6 +29,30 @@ import com.vpu.mp.service.foundation.PageResult;
  */
 public class GoodsRecommendService extends BaseService {
 
+	
+	public PageResult<RecommendGoodsRecord> getPageList(GoodsRecommendPageListParam param){
+		SelectWhereStep<RecommendGoodsRecord> selectFrom = db().selectFrom(RECOMMEND_GOODS);
+		SelectConditionStep<?> select = this.buildOptions(selectFrom,param);
+		PageResult<RecommendGoodsRecord> pageResult = this.getPageResult(select,param.getCurrentPage(),param.getPageRows(),RecommendGoodsRecord.class);
+		
+		return pageResult;
+	}
+
+	/**
+	 * @param selectFrom
+	 * @param param
+	 * @return
+	 */
+	private SelectConditionStep<?> buildOptions(SelectWhereStep<RecommendGoodsRecord> selectFrom,
+			GoodsRecommendPageListParam param) {
+		SelectConditionStep<?> scs = selectFrom.where(RECOMMEND_GOODS.DEL_FLAG.eq(DelFlag.NORMAL.getCode()));
+		if(!StringUtils.isBlank(param.getRecommendName())) {
+			scs = scs.and(RECOMMEND_GOODS.RECOMMEND_NAME.like(this.likeValue(param.getRecommendName())));
+		}
+			
+		return scs;
+	}
+	
 	
 
 }
