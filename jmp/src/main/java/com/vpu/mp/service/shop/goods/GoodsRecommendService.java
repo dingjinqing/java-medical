ed33372c8470b8c2cb5a0daee1a957eaaba20199
huangronggang <huangronggang@wangdian.cn>
2019-07-09 05:45:18 +0000

    新增  商品推荐  增删改查

---
+++ jmp/src/main/java/com/vpu/mp/service/shop/goods/GoodsRecommendService.java
@@ -0,0 +1,27 @@
+package com.vpu.mp.service.shop.goods;
+
+import static com.vpu.mp.db.shop.Tables.RECOMMEND_GOODS;
+
+import org.jooq.Record;
+import org.jooq.RecordMapper;
+import org.jooq.SelectConditionStep;
+import org.jooq.SelectJoinStep;
+import org.jooq.tools.StringUtils;
+
+import com.vpu.mp.db.shop.tables.RecommendGoods;
+import com.vpu.mp.db.shop.tables.records.RecommendGoodsRecord;
+import com.vpu.mp.service.foundation.BaseService;
+import com.vpu.mp.service.foundation.DelFlag;
+import com.vpu.mp.service.foundation.PageResult;
+import com.vpu.mp.service.pojo.shop.goods.recommend.GoodsRecommend;
+import com.vpu.mp.service.pojo.shop.goods.recommend.GoodsRecommendPageListParam;
+/**
+ * @author 黄荣刚
+ * @date 2019年7月9日
+ *
+ */
+public class GoodsRecommendService extends BaseService {
+
+	
+
+}
