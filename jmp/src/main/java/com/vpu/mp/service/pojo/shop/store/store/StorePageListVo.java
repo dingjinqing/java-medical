a071acf21d66480c0ead61124b87985bcbabbab1
wangbingbing <wangbingbing@wangdian.cn>
2019-07-10 08:50:02 +0000

    出入参命名标准

---
+++ jmp/src/main/java/com/vpu/mp/service/pojo/shop/store/store/StorePageListVo.java
@@ -0,0 +1,27 @@
+package com.vpu.mp.service.pojo.shop.store.store;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author 王兵兵
+ *
+ * 2019年7月4日
+ */
+@Data
+@NoArgsConstructor
+public class StorePageListVo {
+
+	public String storeName;
+	public Integer posShopId;//门店编码
+	public String groupName;
+	public String provinceCode;
+	public String cityCode;
+	public String districtCode;
+	public String address;
+	public String manager;
+	public String mobile;
+	public String openingTime;
+	public String closeTime;
+	public Byte businessState;
+}
---
+++ jmp/src/main/java/com/vpu/mp/service/pojo/shop/store/store/StoreParam.java
@@ -0,0 +1,16 @@
+package com.vpu.mp.service.pojo.shop.store.store;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author 王兵兵
+ *
+ * 2019年7月9日
+ */
+@Data
+@NoArgsConstructor
+public class StoreParam {
+	private Integer   storeId;
+	private Integer   posShopId;
+}
