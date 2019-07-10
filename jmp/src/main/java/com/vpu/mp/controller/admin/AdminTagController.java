6e420e788fe86ed313d278ace426b9ed7f228106
huangzhuangzhuang <huangzhuangzhuang@wangdian.cn>
2019-07-10 11:40:35 +0000

    添加标签功能

--- jmp/src/main/java/com/vpu/mp/controller/admin/AdminTagController.java
+++ jmp/src/main/java/com/vpu/mp/controller/admin/AdminTagController.java
@@ -1,5 +1,8 @@
 package com.vpu.mp.controller.admin;
 
+import javax.validation.Valid;
+
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +20,11 @@ import com.vpu.mp.service.pojo.shop.member.TagPageListParam;
 @RestController
 public class AdminTagController extends AdminBaseController {
 
+	/**
+	 * 分页获取全部标签列表
+	 * @param param
+	 * @return
+	 */
 	@PostMapping(value = "/api/admin/tag/list")
 	public JsonResult getTagList(@RequestBody TagPageListParam param) {
 		//TODO test
@@ -30,4 +38,16 @@ public class AdminTagController extends AdminBaseController {
 		PageResult<TagInfoVo> pageResult = this.saas.getShopApp(4891645).tag.getPageList(param);
 		return this.success(pageResult);
 	}
+	
+	
+	@PostMapping(value = "/api/admin/tag/add")
+	public JsonResult addTag(@RequestBody @Valid TagPageListParam param,BindingResult result) {
+		//TODO test
+		System.out.println(param);
+		if(result.hasErrors()) {
+			return this.fail(result.getFieldError().getDefaultMessage());
+		}
+		return this.success();
+	}
+	
 }
