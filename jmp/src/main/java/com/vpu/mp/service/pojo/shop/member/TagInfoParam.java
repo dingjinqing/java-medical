1e1cf75cd063b2943765bf915c8a0deed668330a
huangzhuangzhuang <huangzhuangzhuang@wangdian.cn>
2019-07-11 10:21:36 +0000

    阿里规范

--- jmp/src/main/java/com/vpu/mp/service/pojo/shop/member/TagInfoParam.java
+++ jmp/src/main/java/com/vpu/mp/service/pojo/shop/member/TagInfoParam.java
@@ -5,11 +5,17 @@ import javax.validation.constraints.Size;
 
 import com.vpu.mp.service.foundation.JsonResultMessage;
 
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
-@Data
+/**
+ * 
+ * @author 黄壮壮 2019-07-11 18:20
+ */
+@Getter
+@Setter
 public class TagInfoParam extends BaseMemberPojo {
 	@NotBlank(message = JsonResultMessage.MSG_MEMBER_TAG_NOT_NULL)
-	@Size(max=15,message = JsonResultMessage.MSG_MEMBER_TAG_LENGTH_LIMIT)
+	@Size(max = 15, message = JsonResultMessage.MSG_MEMBER_TAG_LENGTH_LIMIT)
 	private String tagName;
 }
