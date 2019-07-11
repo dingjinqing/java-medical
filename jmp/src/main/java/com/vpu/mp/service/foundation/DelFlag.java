8cedd8708cc68aaf0509ded9d8402db7f5538cb7
lixiaobing <lixiaobing@wangdian.cn>
2019-07-11 09:33:40 +0000

    添加分割符号_

--- jmp/src/main/java/com/vpu/mp/service/foundation/DelFlag.java
+++ jmp/src/main/java/com/vpu/mp/service/foundation/DelFlag.java
@@ -23,7 +23,8 @@ public enum DelFlag {
      *  数据库记录假删除时，在记录唯一字段添加的前缀值
      * @author 李晓冰
      */
-	public static final String DEL_ITEM_PREFIX="del_";
+    public static final String DEL_ITEM_SPLITER="_";
+	public static final String DEL_ITEM_PREFIX="del"+DEL_ITEM_SPLITER;
     /**
      *  数据库假删除标志，jooq中该字段设置值的时候使用枚举会报错。
      * @author 李晓冰
