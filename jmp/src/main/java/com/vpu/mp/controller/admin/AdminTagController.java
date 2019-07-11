ddc12f8760a6c67c338cd85097b9741f022f160c
huangzhuangzhuang <huangzhuangzhuang@wangdian.cn>
2019-07-11 04:00:18 +0000

    提交

--- jmp/src/main/java/com/vpu/mp/controller/admin/AdminTagController.java
+++ jmp/src/main/java/com/vpu/mp/controller/admin/AdminTagController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.vpu.mp.service.foundation.JsonResult;
+import com.vpu.mp.service.foundation.JsonResultMessage;
 import com.vpu.mp.service.foundation.PageResult;
 import com.vpu.mp.service.pojo.shop.member.TagInfoVo;
 import com.vpu.mp.service.pojo.shop.member.TagPageListParam;
@@ -27,26 +28,32 @@ public class AdminTagController extends AdminBaseController {
 	 */
 	@PostMapping(value = "/api/admin/tag/list")
 	public JsonResult getTagList(@RequestBody TagPageListParam param) {
-		//TODO test
-//		String name = new Object(){}.getClass().getEnclosingMethod().getName();
-//		System.out.println("1. Tag管理  in "+name);
-//		System.out.println(param);
-		
-		
-		//this.shop().tag.getPageList(param);
-		//TODO 测试用的shopid
-		PageResult<TagInfoVo> pageResult = this.saas.getShopApp(4891645).tag.getPageList(param);
+		System.out.println(param);
+		PageResult<TagInfoVo> pageResult = shop().tag.getPageList(param);
 		return this.success(pageResult);
 	}
 	
 	
 	@PostMapping(value = "/api/admin/tag/add")
 	public JsonResult addTag(@RequestBody @Valid TagPageListParam param,BindingResult result) {
-		//TODO test
-		System.out.println(param);
+
+		//测试参数
 		if(result.hasErrors()) {
 			return this.fail(result.getFieldError().getDefaultMessage());
 		}
+		
+		//check is the tagName is exits
+		boolean tagNameExists = shop().tag.tagNameExists(param.getTagName());
+		// if tag name exists then return
+		if(tagNameExists) {
+			JsonResult r = this.fail(JsonResultMessage.MSG_MEMBER_TAG_NAME_EXIST);
+			r.setMessage(param.getTagName()+r.getMessage());
+			return r;
+		}
+		
+		// insert into database
+		shop().tag.addTagName(param.getTagName());
+	
 		return this.success();
 	}
 	
