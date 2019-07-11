8b87e92c56b9f9e876842cab9ffeaf8bc0683292
huangzhuangzhuang <huangzhuangzhuang@wangdian.cn>
2019-07-11 10:09:54 +0000

    符合阿里扫描规范

--- jmp/src/main/java/com/vpu/mp/service/pojo/shop/member/MemberPageListParam.java
+++ jmp/src/main/java/com/vpu/mp/service/pojo/shop/member/MemberPageListParam.java
@@ -1,25 +1,34 @@
 package com.vpu.mp.service.pojo.shop.member;
 
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 /**
- *  admin系统店铺会员入参参数
- * TODO
- * @author 黄壮壮
- * 2019-07-05 18:06
+ * admin系统店铺会员入参参数 TODO
+ * 
+ * @author 黄壮壮 2019-07-05 18:06
  */
-@Data
-public class MemberPageListParam extends BaseMemberPojo{
+@Getter
+@Setter
+public class MemberPageListParam extends BaseMemberPojo {
 
 	private String mobile;
 	private String username;
-	//来源 门店来源-1未录入0后台>0为门店
+	/**
+	 * 来源 门店来源-1未录入0后台>0为门店
+	 */
 	private Integer source;
-	//邀请人
+	/**
+	 * 邀请人
+	 */
 	private String inviteUserName;
-	//创建时间
+	/**
+	 * 创建时间
+	 */
 	private String createTime;
-	//结束时间
+	/**
+	 * 结束时间
+	 */
 	private String endTime;
-	
+
 }
