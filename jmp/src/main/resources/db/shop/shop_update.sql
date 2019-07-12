a41895434c84b5c14f7e5833e8f3feaf093ac0bb
liangchen <liangchen@wangdian.cn>
2019-07-12 01:13:09 +0000

    对7.10在shop_update.sql的改动做了补充修改

--- jmp/src/main/resources/db/shop/shop_update.sql
+++ jmp/src/main/resources/db/shop/shop_update.sql
@@ -37,7 +37,7 @@ ADD UNIQUE INDEX unique_spec_id_spec_val_name (spec_id,spec_val_name);
 
 
 --7月10日 梁晨 b2c_comment_goods 添加is_shop_add  bogus_username  bogus_user_avatar字段
-ALTER TABLE b2c_comment_goods ADD COLUMN is_shop_add tinyint(1)  not null DEFAULT '' comment '是否商家增加：0不是，1是',
+ALTER TABLE b2c_comment_goods ADD COLUMN is_shop_add tinyint(1)  not null DEFAULT '0' comment '是否商家增加：0不是，1是',
 ALTER TABLE b2c_comment_goods ADD COLUMN bogus_username  varchar(32)  not null default '' comment '用户名称：商家添加时使用',
 ALTER TABLE b2c_comment_goods ADD COLUMN bogus_user_avatar varchar(100)  not null default '' comment '用户头像：商家添加时使用';
 
