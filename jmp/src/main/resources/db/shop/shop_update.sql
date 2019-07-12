97deb65c7b8596945ca63553fac60e49b4dd5622
changle <changle@wangdian.cn>
2019-07-12 00:34:38 +0000

    添加测评活动相关表数据结构

--- jmp/src/main/resources/db/shop/shop_update.sql
+++ jmp/src/main/resources/db/shop/shop_update.sql
@@ -54,4 +54,120 @@ ALTER TABLE b2c_tag ADD COLUMN update_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP O
 -- 修改标签组关系表
 ALTER TABLE b2c_goods_label_couple MODIFY COLUMN label_id INT(11) NOT NULL;
 
+--7月11日 常乐 创建测评活动相关表结构
+##测评活动表
+##drop table if exists `b2c_assess_activity`;
+create table `b2c_assess_activity` (
+  `id`                mediumint(8) unsigned not null auto_increment,
+  `shop_id`           int(11)               null     default 0 comment '店铺ID',
+  `act_code`          varchar(32)           null     default '' comment '活动编码',
+  `act_name`          varchar(120)          not null default '' comment '活动名称',
+  `start_time`        datetime              not null comment '活动起始时间',
+  `end_time`          datetime              not null comment '活动截止时间',
+  `due_time_type`     tinyint(1)            null     default 0 comment '是否永久有效：0否，1是',
+  `part_times_type`   tinyint(1)            null     default 0 comment '单用户参与次数类型：0不限制，1限制次数',
+  `part_times_day`    int(4)                null     default 1 comment '每天最多参与次数：默认为1,0表示不限制',
+  `part_times_total`  int(4)                null     default 1 comment '累计最多参与次数：默认为1,0表示不限制',
+  `feedback_total`    int(4)                null     default 0 comment '活动总反馈数量：默认为0,0表示不限制',
+  `assess_judge_type` tinyint(1)            null     default 0 comment '测评结果判断条件：0根据选项判断，1根据得分判断',
+  `cover_style_type`  tinyint(1)            null     default 0 comment '封面样式类型：0默认样式，1自定义',
+  `cover_style`       text comment '封面样式内容，json串',
+  `assess_desc`       varchar(500)          not null default '' comment '测评介绍',
+  `create_time`       timestamp       default current_timestamp,
+  `update_time`       timestamp       default current_timestamp on update current_timestamp comment '最后修改时间',
+  `is_block`          tinyint(1)            null     default 0 comment '活动状态：0未停用，1已停用',
+  `del_flag`          tinyint(1)            null     default 0 comment '删除标识：0未删除，1已删除',
+  `pub_flag`          tinyint(1)            null     default 0 comment '发布标识：0未发布，1已发布',
+  primary key (`id`),
+  key `act_name` (`act_name`),
+  key (`shop_id`)
+);
+
+##测评活动题目表
+##drop table if exists `b2c_assess_topic`;
+create table `b2c_assess_topic` (
+  `id`                mediumint(8) unsigned not null auto_increment,
+  `shop_id`           int(11)               null     default 0 comment '店铺ID',
+  `assess_id`         int(11)               null     default 0 comment '测评活动ID',
+  `topic_type`        tinyint(1)            null     default 0 comment '题目格式：0文本，1图片，2视频',
+  `topic_type_path`   varchar(200)          null     default '' comment '题目图片、视频路径',
+  `topic_title`       varchar(200)          null     default '' comment '题目标题',
+  `topic_level`       tinyint(2)            null     default 1 comment '题目优先级，越小优先级越大，从1开始',
+  `bg_img_type`       tinyint(1)            null     default 0 comment '题目背景类型：0默认，1自定义',
+  `bg_img_path`       varchar(200)          null     default '' comment '题目背景图片路径',
+  `option_type`       tinyint(1)            null     default 0 comment '选项类型：0单选，1多选',
+  `option_skip_type`  tinyint(1)            null     default 0 comment '多选时跳转类型：1跳转到指定题目，2跳转导致指定结果',
+  `option_skip_value` int(8)                null     default 0 comment '多选时跳转到指定题目ID或者结果ID',
+  `option_content`    text comment '选项内容，json串，包括选项描述、图片和跳转逻辑或者分值',
+  `result_id`         int(11)               null     default 0 comment '对应ID',
+  `create_time`       timestamp       default current_timestamp,
+  `update_time`       timestamp       default current_timestamp on update current_timestamp comment '最后修改时间',
+  `del_flag`          tinyint(1)            null     default 0 comment '删除标识：0未删除，1已删除',
+  primary key (`id`),
+  key `assess_id` (`assess_id`),
+  key (`shop_id`)
+);
+
+##测评活动结果表
+#drop table if exists `b2c_assess_result`;
+create table `b2c_assess_result` (
+  `id`                     mediumint(8) unsigned not null auto_increment,
+  `shop_id`                int(11)               null     default 0 comment '店铺ID',
+  `assess_id`              int(11)               null     default 0 comment '测评活动ID',
+  `result`                 varchar(200)          null     default '' comment '测试结果',
+  `min_point`              int(11)               null     default 0 comment '结果分值区间起始值',
+  `max_point`              int(11)               null     default 0 comment '结果分值区间终值',
+  `result_img_path`        varchar(200)          null     default '' comment '测试结果图片路径',
+  `result_desc`            varchar(500)          null     default '' comment '测试结果描述',
+  `reward_type`            tinyint(1)            null     default 0 comment '奖励类型：0不设置，1优惠券，2奖品，3积分，4余额，5自定义',
+  `reward_info`            varchar(300)          null     default '' comment '奖励内容',
+  `reward_limit_type`      tinyint(1)            null     default 0 comment '领奖限制类型：0无限制，1分享好友领奖品，2填写信息领奖品',
+  `reward_limit_condition` varchar(32)           null     default '' comment '领奖限制条件,多选字符串逗号隔开：1真实姓名，2手机号，3身份证号码，4性别，5生日，6婚姻状况，7教育程度，8所在行业，9所在地',
+  `bg_type`                tinyint(1)            null     default 0 comment '结果页背景类型：0默认，1自定义',
+  `bg_img_path`            varchar(132)          null     default '' comment '结果页背景图片路径',
+  `create_time`            timestamp       default current_timestamp,
+  `update_time`            timestamp       default current_timestamp on update current_timestamp comment '最后修改时间',
+  `del_flag`               tinyint(1)            null     default 0 comment '删除标识：0未删除，1已删除',
+  primary key (`id`),
+  key (`assess_id`),
+  key (`shop_id`)
+);
+
+##测评活动记录表
+#drop table if exists `b2c_assess_record`;
+create table `b2c_assess_record` (
+  `id`           mediumint(8) unsigned not null  auto_increment,
+  `shop_id`      int(11)               not null  default 0 comment '店铺ID',
+  `user_id`      int(11)               not null  default 0 comment '会员ID',
+  `assess_id`    int(11)               not null  default 0 comment '测评活动ID',
+  `topic_id`     int(11)               not null  default 0 comment '测评得出结果的题目的ID',
+  `result_id`    int(11)               not null  default 0 comment '测评结果ID',
+  `assess_point` varchar(11)           null      default '' comment '测评总得分或者题目的选项编号',
+  `create_time`   timestamp            default current_timestamp,
+  `update_time`   timestamp            default current_timestamp on update current_timestamp comment '最后修改时间',
+  `finish_time`  datetime              not null comment '测评完成时间',
+  `del_flag`     tinyint(1)            not null  default 0 comment '删除标识：0未删除，1已删除',
+  `state_status` tinyint(1)            not null  default 0 comment '测评状态：0测评中，1测评完成待领取奖励，2测评完成已领取，3领取失效，4测评失败',
+  primary key (`id`),
+  key (`assess_id`),
+  key (`shop_id`),
+  key (`user_id`)
+);
+
+##测评活动答题记录表
+#drop table if exists `b2c_assess_record`;
+create table `b2c_assess_topic_record` (
+  `id`           mediumint(8) unsigned not null auto_increment,
+  `record_id`    int(11)               not null default 0 comment '测评活动记录表关联ID',
+  `topic_id`     int(11)               not null default 0 comment '测评题目的ID',
+  `topic_option` varchar(11)           not null default '' comment '测评题目选项编号,多选的逗号隔开',
+  `topic_point`  int(8)                not null default 0 comment '测评得分',
+  `result_id`    int(11)               not null default 0 comment '测评结果ID',
+  `create_time`  timestamp       default current_timestamp,
+  `update_time`  timestamp       default current_timestamp on update current_timestamp comment '最后修改时间',
+  primary key (`id`),
+  key (`record_id`),
+  key (`result_id`)
+);
+
 
