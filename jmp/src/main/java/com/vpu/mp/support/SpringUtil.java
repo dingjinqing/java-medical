66b94237fbc2b4d9b39e410319982cadf260b28f
lixinguo <lixinguo@wangdian.cn>
2019-07-12 03:54:34 +0000

    SpringUtil

--- jmp/src/main/java/com/vpu/mp/support/SpringUtil.java
+++ jmp/src/main/java/com/vpu/mp/support/SpringUtil.java
@@ -18,22 +18,12 @@ public class SpringUtil implements ApplicationContextAware {
 		applicationContext = applicationContextParam;
 	}
 
-	public static Object getObject(String id) {
-		Object object = null;
-		object = applicationContext.getBean(id);
-		return object;
-	}
-
-	public static <T> T getObject(Class<T> tClass) {
+	public static <T> T getBean(Class<T> tClass) {
 		return applicationContext.getBean(tClass);
 	}
-
-	public static Object getBean(String tClass) {
-		return applicationContext.getBean(tClass);
-	}
-
-	public <T> T getBean(Class<T> tClass) {
-		return applicationContext.getBean(tClass);
+	
+	public static Object getBean(String id) {
+		return applicationContext.getBean(id);
 	}
 
 }
