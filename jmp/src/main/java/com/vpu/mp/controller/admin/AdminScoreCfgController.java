d4d6a86604afc599299e405bbeb50332340076e1
huangzhuangzhuang <huangzhuangzhuang@wangdian.cn>
2019-07-15 12:11:36 +0000

    添加

---
+++ jmp/src/main/java/com/vpu/mp/controller/admin/AdminScoreCfgController.java
@@ -0,0 +1,31 @@
+package com.vpu.mp.controller.admin;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.vpu.mp.service.foundation.JsonResult;
+import com.vpu.mp.service.pojo.shop.member.ShopCfgParam;
+import com.vpu.mp.service.shop.ShopApplication;
+
+/**
+ * 积分管理controller
+ * @author 黄壮壮
+ * 2019-07-15 14:21
+ */
+@RestController
+public class AdminScoreCfgController extends AdminBaseController {
+
+	@PostMapping(value="/api/admin/user/score/update")
+	public JsonResult userScoreConfig(@RequestBody ShopCfgParam param) {
+		
+		System.out.println("userScoreConfig method: "+param);
+		//判断积分有效期类型
+		int r = this.shop().score.setShopCfg(param);
+		if(r == -1) {
+			return this.fail();
+		}
+
+		return this.success();
+	}
+}
