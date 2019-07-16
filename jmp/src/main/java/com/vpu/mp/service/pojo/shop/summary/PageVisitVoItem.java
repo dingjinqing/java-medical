6fbae0aef914e4bff046e03515912e57fa5be711
zhengbaole <zhengbaole@wangdian.cn>
2019-07-16 03:04:46 +0000

    页面统计 VO 单项

---
+++ jmp/src/main/java/com/vpu/mp/service/pojo/shop/summary/PageVisitVoItem.java
@@ -0,0 +1,57 @@
+package com.vpu.mp.service.pojo.shop.summary;
+
+import lombok.Data;
+
+/**
+ * 访问页面统计出参单项
+ *
+ * @author 郑保乐
+ */
+@Data
+public class PageVisitVoItem {
+
+    /**
+     * 页面路径
+     */
+    private String pagePath;
+
+    /**
+     * 页面名称
+     */
+    private String pageName;
+
+    /**
+     * 入口页面次数
+     */
+    private String entryPagePv;
+
+    /**
+     * 退出页面次数
+     */
+    private String exitPagePv;
+
+    /**
+     * 分享次数
+     */
+    private String pageSharePv;
+
+    /**
+     * 分享人数
+     */
+    private String pageShareUv;
+
+    /**
+     * 次均访问时长
+     */
+    private Double pageStayTimePv;
+
+    /**
+     * 访问次数
+     */
+    private String pageVisitPv;
+
+    /**
+     * 访问人数
+     */
+    private String pageVisitUv;
+}
