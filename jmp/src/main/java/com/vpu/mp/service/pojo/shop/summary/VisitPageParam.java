11c8edc83d4fed3959b46f4dce0bc191370fb82c
zhengbaole <zhengbaole@wangdian.cn>
2019-07-16 05:55:03 +0000

    访问页面统计

--- jmp/src/main/java/com/vpu/mp/service/pojo/shop/summary/VisitPageParam.java
+++ jmp/src/main/java/com/vpu/mp/service/pojo/shop/summary/VisitPageParam.java
@@ -1,11 +1,16 @@
 package com.vpu.mp.service.pojo.shop.summary;
 
+import com.vpu.mp.db.shop.tables.records.MpVisitPageRecord;
 import com.vpu.mp.service.foundation.JsonResultMessage;
 import lombok.Data;
+import org.jooq.SortField;
+import org.jooq.TableField;
 
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
+
+import static com.vpu.mp.db.shop.tables.MpVisitPage.MP_VISIT_PAGE;
 
 /**
  * 访问页面统计入参
@@ -14,6 +19,9 @@ import javax.validation.constraints.NotBlank;
  */
 @Data
 public class VisitPageParam {
+
+    public static final int ASC = 1;
+    public static final int DESC = 2;
 
     @NotBlank(message = JsonResultMessage.MSG_PARAM_ERROR)
     private String startDate;
@@ -24,13 +32,61 @@ public class VisitPageParam {
      * 按第几个字段排序
      */
     @NotBlank(message = JsonResultMessage.MSG_PARAM_ERROR)
-    @Min(1) @Max(8)
+    @Min(1)
+    @Max(8)
     private Integer action;
 
     /**
      * 排序方式（1: ASC 2: DESC）
      */
     @NotBlank(message = JsonResultMessage.MSG_PARAM_ERROR)
-    @Min(1) @Max(2)
+    @Min(1)
+    @Max(2)
     private Integer asc;
+
+    /**
+     * action 和字段的对应关系
+     */
+    public enum Actions {
+
+        PV(MP_VISIT_PAGE.PAGE_VISIT_PV),
+        UV(MP_VISIT_PAGE.PAGE_VISIT_UV),
+        STAY_TIME(MP_VISIT_PAGE.PAGE_STAYTIME_PV),
+        ENTRY_PV(MP_VISIT_PAGE.ENTRYPAGE_PV),
+        EXIT_PV(MP_VISIT_PAGE.EXITPAGE_PV),
+        SHARE_PV(MP_VISIT_PAGE.PAGE_SHARE_PV),
+        SHARE_UV(MP_VISIT_PAGE.PAGE_SHARE_UV);
+
+        private TableField<MpVisitPageRecord, ?> field;
+
+        Actions(TableField<MpVisitPageRecord, ?> field) {
+            this.field = field;
+        }
+
+        public TableField<MpVisitPageRecord, ?> getField() {
+            return field;
+        }
+    }
+
+    /**
+     * 取字段及排序方式
+     */
+    public SortField<?> getSortField() {
+        Integer action = getAction();
+        Integer asc = getAsc();
+        Actions[] fields = Actions.values();
+        int length = fields.length;
+        if (0 > action || length < action) {
+            throw new IllegalStateException("Unexpected action: " + action);
+        }
+        TableField<MpVisitPageRecord, ?> field = fields[action - 1].getField();
+        switch (asc) {
+            case ASC:
+                return field.asc();
+            case DESC:
+                return field.desc();
+            default:
+                throw new IllegalStateException("Unexpected asc: " + asc);
+        }
+    }
 }
--- jmp/src/main/java/com/vpu/mp/service/shop/summary/visit/PageService.java
+++ jmp/src/main/java/com/vpu/mp/service/shop/summary/visit/PageService.java
@@ -2,9 +2,14 @@ package com.vpu.mp.service.shop.summary.visit;
 
 import com.vpu.mp.db.shop.tables.records.MpVisitPageRecord;
 import com.vpu.mp.service.pojo.shop.summary.PageVisitVo;
+import com.vpu.mp.service.pojo.shop.summary.PageVisitVoItem;
 import com.vpu.mp.service.pojo.shop.summary.VisitPageParam;
 import org.jooq.Result;
+import org.jooq.SortField;
 import org.jooq.impl.DSL;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static com.vpu.mp.db.shop.tables.MpVisitPage.MP_VISIT_PAGE;
 
@@ -18,11 +23,28 @@ public class PageService extends BaseVisitService {
     public PageVisitVo getPageVisit(VisitPageParam param) {
         String startDate = param.getStartDate();
         String endDate = param.getEndDate();
-        Result<MpVisitPageRecord> result = getPageVisitResult(startDate, endDate);
-        return null;
+        SortField<?> sortField = param.getSortField();
+        Result<MpVisitPageRecord> result = getPageVisitResult(startDate, endDate, sortField);
+        List<MpVisitPageRecord> records = result.into(MpVisitPageRecord.class);
+        PageVisitVo vo = new PageVisitVo();
+        List<PageVisitVoItem> items = records.parallelStream().map(r -> {
+            PageVisitVoItem item = new PageVisitVoItem();
+            item.setEntryPagePv(String.valueOf(r.getEntrypagePv()));
+            item.setExitPagePv(String.valueOf(r.getExitpagePv()));
+            item.setPagePath(r.getPagePath());
+            item.setPageSharePv(String.valueOf(r.getPageSharePv()));
+            item.setPageShareUv(String.valueOf(r.getPageShareUv()));
+            item.setPageVisitPv(String.valueOf(r.getPageVisitPv()));
+            item.setPageVisitUv(String.valueOf(r.getPageVisitUv()));
+            // TODO 页面名称
+            return item;
+        }).collect(Collectors.toList());
+        vo.setList(items);
+        return vo;
     }
 
-    private Result<MpVisitPageRecord> getPageVisitResult(String startDate, String endDate) {
+    private Result<MpVisitPageRecord> getPageVisitResult(
+            String startDate, String endDate, SortField<?> orderBy) {
         return db().select(
                 MP_VISIT_PAGE.PAGE_PATH,
                 DSL.sum(MP_VISIT_PAGE.PAGE_VISIT_PV),
@@ -35,6 +57,7 @@ public class PageService extends BaseVisitService {
                 .from(MP_VISIT_PAGE)
                 .where(MP_VISIT_PAGE.REF_DATE.between(startDate).and(endDate))
                 .groupBy(MP_VISIT_PAGE.REF_DATE, MP_VISIT_PAGE.PAGE_PATH)
+                .orderBy(orderBy)
                 .fetch().into(MP_VISIT_PAGE);
     }
 }
