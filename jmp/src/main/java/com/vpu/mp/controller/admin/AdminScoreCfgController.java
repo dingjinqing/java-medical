8ba29dc484342425c9eaeccc20abc597cb284690
huangzhuangzhuang <huangzhuangzhuang@wangdian.cn>
2019-07-16 11:13:12 +0000

    更新

--- jmp/src/main/java/com/vpu/mp/controller/admin/AdminScoreCfgController.java
+++ jmp/src/main/java/com/vpu/mp/controller/admin/AdminScoreCfgController.java
@@ -1,10 +1,13 @@
 package com.vpu.mp.controller.admin;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.vpu.mp.service.foundation.JsonResult;
+import com.vpu.mp.service.pojo.shop.member.ScoreCfgVo;
 import com.vpu.mp.service.pojo.shop.member.ShopCfgParam;
 
 /**
@@ -13,9 +16,16 @@ import com.vpu.mp.service.pojo.shop.member.ShopCfgParam;
  * 2019-07-15 14:21
  */
 @RestController
+@RequestMapping(value="/api/admin/user/score")
 public class AdminScoreCfgController extends AdminBaseController {
 
-	@PostMapping(value="/api/admin/user/score/update")
+	@GetMapping(value="/get")
+	public JsonResult getScoreConfig() {
+		
+		ScoreCfgVo vo = shop().score.getShopScoreCfg();
+		return this.success(vo);
+	}
+	@PostMapping(value="/update")
 	public JsonResult userScoreConfig(@RequestBody ShopCfgParam param) {
 		
 		//判断积分有效期类型
@@ -23,7 +33,6 @@ public class AdminScoreCfgController extends AdminBaseController {
 		if(r == -1) {
 			return this.fail();
 		}
-
 		return this.success();
 	}
 }
