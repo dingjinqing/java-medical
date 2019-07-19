138728022f22c72c8142bf467516cbafc5eee401
lixinguo <lixinguo@wangdian.cn>
2019-07-19 07:57:52 +0000

    getBean 优化

--- jmp/src/main/java/com/vpu/mp/support/SpringUtil.java
+++ jmp/src/main/java/com/vpu/mp/support/SpringUtil.java
@@ -1,17 +1,18 @@
 package com.vpu.mp.support;
 
 import org.springframework.beans.BeansException;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.beans.factory.support.GenericBeanDefinition;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
-import org.springframework.stereotype.Component;
+import org.springframework.context.annotation.Configuration;
 
 /**
  * 
  * @author 新国
  *
  */
-@Component
+@Configuration
 public class SpringUtil implements ApplicationContextAware {
 
 	private static ApplicationContext applicationContext;
@@ -22,18 +23,24 @@ public class SpringUtil implements ApplicationContextAware {
 	}
 
 	public static <T> T getBean(Class<T> tClass) {
-		String name =tClass.getSimpleName();
-		String className = name.substring(0,1).toLowerCase()+name.substring(1,name.length());
-		if( applicationContext.containsBean(className) ){
+		String name = tClass.getSimpleName();
+		String className = name.substring(0, 1).toLowerCase() + name.substring(1, name.length());
+		if(applicationContext !=null && applicationContext.containsBean(className)) {
 			return applicationContext.getBean(tClass);
-		}else {
-			System.out.println("#########################"+tClass.getName());
 		}
-		return null;
+		DefaultListableBeanFactory factory = new DefaultListableBeanFactory();
+		if (!factory.containsBean(className)) {
+			GenericBeanDefinition gbd = new GenericBeanDefinition();
+			gbd.setBeanClass(tClass);
+			factory.registerBeanDefinition(className, gbd);
+		}
+		return factory.getBean(tClass);
 	}
-	public static boolean inited(){
+
+	public static boolean inited() {
 		return applicationContext != null;
 	}
+
 	public static Object getBean(String id) {
 		return applicationContext.getBean(id);
 	}
