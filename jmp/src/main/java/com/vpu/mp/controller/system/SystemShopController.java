49ab8478941df0a092bf46b5b1f0683cd3a69685
zhaojianqiang <zhaojianqiang@wangdian.cn>
2019-07-19 01:47:32 +0000

    增加版本权限

--- jmp/src/main/java/com/vpu/mp/controller/system/SystemShopController.java
+++ jmp/src/main/java/com/vpu/mp/controller/system/SystemShopController.java
@@ -11,6 +11,10 @@ import com.vpu.mp.db.main.tables.records.ShopRecord;
 import com.vpu.mp.service.foundation.JsonResult;
 import com.vpu.mp.service.foundation.JsonResultCode;
 import com.vpu.mp.service.pojo.saas.shop.ShopListQueryParam;
+import com.vpu.mp.service.pojo.saas.shop.VersionEditParam;
+import com.vpu.mp.service.pojo.saas.shop.VersionListQueryParam;
+import com.vpu.mp.service.pojo.saas.shop.VersionShowParam;
+import com.vpu.mp.service.pojo.saas.shop.version.VersionConfig;
 import com.vpu.mp.service.pojo.shop.auth.ShopMobileReq;
 import com.vpu.mp.service.pojo.shop.auth.ShopRenewReq;
 import com.vpu.mp.service.pojo.shop.auth.ShopReq;
@@ -55,7 +59,7 @@ public class SystemShopController extends SystemBaseController {
 		if (accountInfo == null) {
 			return fail();
 		}
-		if (saas.shop.addShop(shopReq)) {
+		if (saas.shop.addShop(shopReq, sysAuth.user(), request)) {
 			return success(JsonResultCode.CODE_SUCCESS);
 		}
 		return fail();
@@ -79,5 +83,43 @@ public class SystemShopController extends SystemBaseController {
 	@PostMapping(value = "/system/shop/list")
 	public JsonResult shopList(@RequestBody ShopListQueryParam param) {
 		return success(saas.shop.getPageList(param));
+	}
+
+	/**
+	 * 版本权限查询
+	 * 
+	 * @return
+	 */
+	@PostMapping("/system/version/list")
+	public JsonResult versionList(@RequestBody VersionListQueryParam vParam) {
+		return success(saas.shop.version.getPageList(vParam));
+	}
+
+	/**
+	 * 版本功能展示
+	 * 
+	 * @return
+	 */
+	@PostMapping("/system/version/show")
+	public JsonResult showVersion(@RequestBody VersionShowParam vParam) {
+		VersionConfig vConfig = saas.shop.version.mergeVersion(vParam.shopId);
+		if (vConfig == null) {
+			return fail(JsonResultCode.CODE_FAIL);
+		}
+		return success(vConfig);
+	}
+	
+
+	/**
+	 * 店铺列表--版本权限
+	 * @param vParam
+	 * @return
+	 */
+	@PostMapping("/system/version/edit")
+	public JsonResult editVersion(@RequestBody VersionEditParam vParam) {
+		if(saas.shop.version.editVersion(vParam)==1) {
+			return success(JsonResultCode.CODE_SUCCESS);
+		}
+		return fail(JsonResultCode.CODE_FAIL);
 	}
 }
