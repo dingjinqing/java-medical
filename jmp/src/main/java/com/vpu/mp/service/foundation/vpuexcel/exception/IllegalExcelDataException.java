5ef61cd519ed5f1a2ecfe87853703ffb6c1514ed
huangzhuangzhuang <huangzhuangzhuang@wangdian.cn>
2019-07-22 01:44:32 +0000

    帮助删除vpuexcel无用包

--- jmp/src/main/java/com/vpu/mp/service/foundation/vpuexcel/exception/IllegalExcelDataException.java
+++
@@ -1,8 +0,0 @@
-package com.vpu.mp.service.foundation.vpuexcel.exception;
-
-/**
- * @author 李晓冰
- * @date 2019年07月18日
- */
-public class IllegalExcelDataException extends Exception {
-}
--- jmp/src/main/java/com/vpu/mp/service/foundation/vpuexcel/exception/IllegalSheetPositionException.java
+++
@@ -1,9 +0,0 @@
-package com.vpu.mp.service.foundation.vpuexcel.exception;
-
-/**
- * sheet位置错误
- * @author 李晓冰
- * @date 2019年07月18日
- */
-public class IllegalSheetPositionException extends Exception {
-}
