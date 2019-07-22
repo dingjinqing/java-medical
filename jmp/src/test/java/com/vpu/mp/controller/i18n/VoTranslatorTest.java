b8937532c2ca52cebf3f6dbb4cc91f853d8bf89e
zhengbaole <zhengbaole@wangdian.cn>
2019-07-22 03:38:37 +0000

    添加默认语言以及 包含 List<String> 属性出参的国际化

--- jmp/src/test/java/com/vpu/mp/controller/i18n/VoTranslatorTest.java
+++ jmp/src/test/java/com/vpu/mp/controller/i18n/VoTranslatorTest.java
@@ -6,6 +6,8 @@ import org.junit.Test;
 
 import javax.servlet.http.HttpServletRequest;
 
+import java.util.Arrays;
+
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
@@ -19,23 +21,48 @@ public class VoTranslatorTest {
 
     private CategoryVo[] voes = new CategoryVo[5];
 
+    private CategoryListVo listVo = new CategoryListVo();
+
     private VoTranslator translator;
+
+    private HttpServletRequest reqMock;
+
+    private static final String HEADER_LANG = "V-Lang";
 
     @Before
     public void setUp() {
 
-        HttpServletRequest reqMock = mock(HttpServletRequest.class);
-        when(reqMock.getHeader("V-Lang")).thenReturn("zh_CN");
-        translator = new VoTranslator(reqMock);
         voes[0] = new CategoryVo("food");
         voes[1] = new CategoryVo("digital");
         voes[2] = new CategoryVo("clean");
         voes[3] = new CategoryVo("cook");
         voes[4] = new CategoryVo("drink");
+        reqMock = mock(HttpServletRequest.class);
+        when(reqMock.getHeader(HEADER_LANG)).thenReturn("zh_CN");
+        translator = new VoTranslator(reqMock);
     }
 
+    /**
+     * 测试请求头中不包含语言字段时的翻译
+     */
     @Test
-    public void assertPropertyTranslated() {
+    public void assertPropertyTranslatedWhenDefaultLanguage() {
+        when(reqMock.getHeader(HEADER_LANG)).thenReturn(null);
+        translateCategoryArray();
+    }
+
+    /**
+     * 测试请求头中包含语言字段时的翻译
+     */
+    @Test
+    public void assertPropertyTranslatedZhCN() {
+        translateCategoryArray();
+    }
+
+    /**
+     * 翻译数组中的对象
+     */
+    private void translateCategoryArray() {
 
         for (CategoryVo vo : voes) {
             translator.translateFields(vo);
@@ -45,5 +72,19 @@ public class VoTranslatorTest {
         assertEquals(voes[2].getName(), "清洁");
         assertEquals(voes[3].getName(), "厨具");
         assertEquals(voes[4].getName(), "饮品");
+    }
+
+    /**
+     * 测试出参类中包含 {@code List<String>} 属性的翻译
+     */
+    @Test
+    public void assertListPropertyTranslated() {
+        listVo.setList(Arrays.asList("food", "clean", "cook", "digital", "drink"));
+        translator.translateFields(listVo);
+        assertEquals(listVo.getList().get(0), "食品");
+        assertEquals(listVo.getList().get(1), "清洁");
+        assertEquals(listVo.getList().get(2), "厨具");
+        assertEquals(listVo.getList().get(3), "数码");
+        assertEquals(listVo.getList().get(4), "饮品");
     }
 }
