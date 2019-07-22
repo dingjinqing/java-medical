49b098efaa048bb37ce0d8ed3243f617548a4ac1
huangzhuangzhuang <huangzhuangzhuang@wangdian.cn>
2019-07-22 11:45:02 +0000

    添加验证规则

--- jmp/src/main/java/com/vpu/mp/service/pojo/shop/member/account/ScoreParam.java
+++ jmp/src/main/java/com/vpu/mp/service/pojo/shop/member/account/ScoreParam.java
@@ -1,5 +1,10 @@
 package com.vpu.mp.service.pojo.shop.member.account;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+
+import com.vpu.mp.service.foundation.JsonResultMessage;
+
 import lombok.Data;
 
 /**
@@ -9,8 +14,17 @@ import lombok.Data;
 @Data
 public class ScoreParam {
 	private String remark;
+	
+	@NotNull(message=JsonResultMessage.MSG_MEMBER_SCORE_NOT_NULL)
 	private Integer[] userId;
+	
+	@Min(message=JsonResultMessage.MSG_MEMBER_SCORE_NOT_BE_NEGATIVE, value = 0)
+	@NotNull(message=JsonResultMessage.MSG_MEMBER_SCORE_NOT_NULL)
 	private Integer score;
+	
+	@Min(message=JsonResultMessage.MSG_MEMBER_SCORE_NOT_BE_NEGATIVE, value = 0)
+	@NotNull(message=JsonResultMessage.MSG_MEMBER_SCORE_NOT_NULL)
 	private Integer scoreDis;
+	private String orderSn; 
 	
 }
