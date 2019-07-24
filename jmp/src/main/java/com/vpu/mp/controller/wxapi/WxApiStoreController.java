edbacb602c53b65e2236e7cb6059b2daf20075ee
wangbingbing <wangbingbing@wangdian.cn>
2019-07-24 03:20:08 +0000

    空的小程序端接口controller 暂时不写功能

---
+++ jmp/src/main/java/com/vpu/mp/controller/wxapi/WxApiStoreController.java
@@ -0,0 +1,13 @@
+package com.vpu.mp.controller.wxapi;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author 王兵兵
+ *
+ * 2019年7月24日
+ */
+@RestController
+public class WxApiStoreController extends WxApiBaseController{
+
+}
