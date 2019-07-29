2154b4994566610e2c6f5c88e5e2a1cd173e9c75
huangzhuangzhuang <huangzhuangzhuang@wangdian.cn>
2019-07-29 11:16:54 +0000

    创建新文件

--- jmp/src/main/java/com/vpu/mp/service/pojo/shop/member/card/NormalMemberCardParam.java
+++ jmp/src/main/java/com/vpu/mp/service/pojo/shop/member/card/NormalMemberCardParam.java
@@ -1,5 +1,8 @@
 package com.vpu.mp.service.pojo.shop.member.card;
 
+import java.math.BigDecimal;
+import java.sql.Timestamp;
+
 import lombok.Data;
 
 /**
@@ -15,17 +18,52 @@ public class NormalMemberCardParam {
 	private String cardName;
 	private Byte bgType;
 	private String bgColor;
-	private Byte expiredType;
-	private Float disCount;
+	private String bgImg;
+
+	private BigDecimal disCount;
 	private Byte discountIsAll;
-	private Byte isPay;
-	private String payOwnGood;
-	private String endTime;
+
 	private Integer[] goodsId;
 	private Integer[] shopCategoryIds;
 	private Integer[] platformCategoryIds;
-	private Byte storeListType;
-	private Integer[] storeList;
+
+	private Byte powerScore;
+	private Byte powerCount;
+	private String powerPayOwnGood;
+
+	private Integer score;
+	private BigDecimal[] goodsMoney;
+	private BigDecimal[] getScores;
+	private Byte offset;
+	private BigDecimal perGoodsMoney;
+	private BigDecimal perGetScores;
 	
+	private Byte powerCard;
+	private Integer sendMoney;
+	private Byte offsetMoney;
+	private BigDecimal[] money;
+	private BigDecimal[] getMoney;
+	private BigDecimal perMoney;
+	private BigDecimal perGetMoney;
 	
+	private Byte expiredType;
+	private Timestamp startTime;
+	private Timestamp endTime;
+	private Integer receiveDay;
+	private Byte dateType;
+	
+	private String storeListType;
+	private String[] storeList;
+	
+	private String desc;
+	private String mobile;
+	
+	private Byte isPay;
+	private Byte payType;
+	private BigDecimal payMoney;
+	private BigDecimal payScore;
+	
+	private Byte activation;
+	private String[] activationCfgBox;
+	private Byte examine;
 }
