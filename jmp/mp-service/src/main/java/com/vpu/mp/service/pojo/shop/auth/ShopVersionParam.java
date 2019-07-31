3512d28386cb15f99ccfde26712500602128a134
zhaojianqiang <zhaojianqiang@wangdian.cn>
2019-07-31 07:50:57 +0000

    改成解析成对象的

---
+++ jmp/mp-service/src/main/java/com/vpu/mp/service/pojo/shop/auth/ShopVersionParam.java
@@ -0,0 +1,21 @@
+package com.vpu.mp.service.pojo.shop.auth;
+
+import java.util.List;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * 
+ * @author zhaojianqiang
+ *
+ * 2019年7月31日 下午3:10:22
+ */
+@Data
+@NoArgsConstructor
+public class ShopVersionParam {
+	public String vsName;
+	public String name;
+	public String enName;
+	public List<String> includeApi;
+}
