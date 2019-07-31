a061d6f100601fd1f802308e92c4f37a593d1691
zhaoxin <zhaoxin@wangdian.cn>
2019-07-31 01:00:03 +0000

    accountList.js提交

---
+++ web-vue/src/api/system/accountList.js
@@ -0,0 +1,43 @@
+import service from '@/util/request.js'
+
+// 商家账号信息
+export function accountInfoRequest () {
+  return service({
+    url: '/api/system/shop/account/edit',
+    method: 'post'
+  })
+}
+
+// 添加店铺时验证手机号是否已经存在
+export function confirmMobileRequest () {
+  return service({
+    url: '/api/system/shop/check/mobile',
+    method: 'post'
+  })
+}
+
+// 添加商家账户
+export function addCoountRequest (data) {
+  return service({
+    url: '/api/system/shop/account/add',
+    method: 'post',
+    data: data
+  })
+}
+
+// 商家账户列表查询
+export function searchAccountRequest (data) {
+  return service({
+    url: '/api/system/shop/account/list',
+    method: 'post',
+    data: data
+  })
+}
+
+// 添加店铺
+export function addShopRequest () {
+  return service({
+    url: '/api/system/shop/add',
+    method: 'post'
+  })
+}
