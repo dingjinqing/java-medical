4f8f09eeeadddfe51ee49278ee16a0f103fcc3dc
lixinguo <lixinguo@wangdian.cn>
2019-08-01 09:17:20 +0000

    wechat

--- jmp/mp-service/src/main/java/com/vpu/mp/service/saas/shop/MpOperateLogService.java
+++ jmp/mp-service/src/main/java/com/vpu/mp/service/saas/shop/MpOperateLogService.java
@@ -1,9 +1,10 @@
 package com.vpu.mp.service.saas.shop;
 
+import static com.vpu.mp.db.main.tables.MpOperateLog.MP_OPERATE_LOG;
+
 import org.springframework.stereotype.Service;
 
 import com.vpu.mp.service.foundation.service.MainBaseService;
-import static com.vpu.mp.db.main.tables.MpOperateLog.MP_OPERATE_LOG;
 
 @Service
 public class MpOperateLogService extends MainBaseService {
@@ -12,62 +13,62 @@ public class MpOperateLogService extends MainBaseService {
 	 * 设置服务器域名
 	 */
 	public static final Byte OP_TYPE_MODIFY_DOMAIN = 1;
-	
+
 	/**
 	 * 上传代码
 	 */
 	public static final Byte OP_TYPE_UPLOAD_CODE = 2;
-	
+
 	/**
 	 * 添加体验者
 	 */
 	public static final Byte OP_TYPE_ADD_TESTER = 3;
-	
+
 	/**
 	 * 删除体验者
 	 */
 	public static final Byte OP_TYPE_DEL_TESTER = 4;
-	
+
 	/**
 	 * 获取体验者二维码
 	 */
 	public static final Byte OP_TYPE_GET_TESTER_QR = 5;
-	
+
 	/**
 	 * 获取可选类目
 	 */
 	public static final Byte OP_TYPE_GET_CATEGORY = 6;
-	
+
 	/**
 	 * 获取页面配置
 	 */
 	public static final Byte OP_TYPE_GET_PAGE_CFG = 7;
-	
+
 	/**
 	 * 提交审核
 	 */
 	public static final Byte OP_TYPE_SUBMIT_AUDIT = 8;
-	
+
 	/**
 	 * 发布代码
 	 */
 	public static final Byte OP_TYPE_PUBLISH_CODE = 9;
-	
+
 	/**
 	 * 审核成功通知
 	 */
 	public static final Byte OP_TYPE_AUDIT_SUCCESS = 10;
-	
+
 	/**
 	 * 审核失败通知
 	 */
 	public static final Byte OP_TYPE_AUDIT_FAILED = 11;
-	
+
 	/**
 	 * 授权通知
 	 */
 	public static final Byte OP_TYPE_AUTH_OK = 12;
-	
+
 	/**
 	 * 取消授权通知
 	 */
@@ -88,16 +89,4 @@ public class MpOperateLogService extends MainBaseService {
 						MP_OPERATE_LOG.OPERATE_TYPE, MP_OPERATE_LOG.MEMO)
 				.values(appId, templateId.intValue(), operateType, memo).execute();
 	}
-	
-	
-	/**
-     * 发布小程序申请
-     *
-     * @param string $appId
-     * @param int $templateId
-     */
-    public void deployApply($appId, $templateId = 0)
-    {
-        $templateId = $templateId ?: $this->shop->mpVersion->getCurrentUseTemplateId($appId);
-    }
 }
