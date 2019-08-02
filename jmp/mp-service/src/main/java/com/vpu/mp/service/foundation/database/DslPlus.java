b383c8dfa49c6be03eae8ab1897a8e3893032cef
kongdecheng <kongdecheng@wangdian.cn>
2019-08-02 07:19:51 +0000

    补充Dsl缺少的mysql函数

--- jmp/mp-service/src/main/java/com/vpu/mp/service/foundation/database/DslPlus.java
+++ jmp/mp-service/src/main/java/com/vpu/mp/service/foundation/database/DslPlus.java
@@ -12,43 +12,49 @@ import java.sql.Timestamp;
 
 /**
  * 补充Dsl缺少的mysql函数
+ *
  * @author 孔德成
  * @date 2019/8/1 18:06
  */
-public  class   DslPlus  {
+public class DslPlus {
 
     private static final Logger log = LoggerFactory.getLogger(DslPlus.class);
 
 
-    private static final String FORMAT="format";
+    private static final String FORMAT = "format";
 
     /**
+     * 格式化时间到日
      *
      * @param field
-     * @param format %Y-%m-%d
      * @return
      */
-    public static Field<String> dateFormatDay(Field<Timestamp> field, String format) {
-        log.debug(format);
-        return DSL.field("date_format({0}, {1})", SQLDataType.VARCHAR, field, DSL.inline(format));
+    public static Field<String> dateFormatDay(Field<Timestamp> field) {
+        return DSL.field("date_format({0}, {1})", SQLDataType.VARCHAR, field, DSL.inline(FORMAT));
     }
 
 
     /**
+     * 格式化时间
      *
      * @param field
      * @param format %Y-%m-%d
      * @return
      */
     public static Field<String> dateFormat(Field<Timestamp> field, String format) {
-        log.debug(format);
+        log.debug("dateFormat" + format);
         return DSL.field("date_format({0}, {1})", SQLDataType.VARCHAR, field, DSL.inline(format));
 
     }
 
 
-
-    public static Condition findInSet(String format,Field<String> field) {
+    /**
+     *  FIND_IN_SET 函数
+     * @param format
+     * @param field
+     * @return
+     */
+    public static Condition findInSet(String format, Field<String> field) {
         return DSL.condition("FIND_IN_SET({0}, {1})", field, DSL.inline(format));
     }
 }
