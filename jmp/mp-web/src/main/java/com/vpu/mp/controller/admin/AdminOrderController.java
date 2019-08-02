8ebdd5aaea954c9f1d368f92c526c7f677b9c7c1
wangshuai <wangshuai@wangdian.cn>
2019-08-02 10:38:44 +0000

    订单优化、操作建立统一流程

--- jmp/mp-web/src/main/java/com/vpu/mp/controller/admin/AdminOrderController.java
+++ jmp/mp-web/src/main/java/com/vpu/mp/controller/admin/AdminOrderController.java
@@ -1,31 +1,37 @@
 package com.vpu.mp.controller.admin;
 
-import org.jooq.tools.StringUtils;
+import javax.validation.Valid;
+
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.vpu.mp.service.foundation.data.JsonResult;
+import com.vpu.mp.service.foundation.data.JsonResultCode;
 import com.vpu.mp.service.foundation.util.PageResult;
 import com.vpu.mp.service.pojo.shop.order.OrderListInfoVo;
 import com.vpu.mp.service.pojo.shop.order.OrderPageListQueryParam;
 import com.vpu.mp.service.pojo.shop.order.OrderParam;
 import com.vpu.mp.service.pojo.shop.order.store.StoreOrderListInfoVo;
 import com.vpu.mp.service.pojo.shop.order.store.StoreOrderPageListQueryParam;
+import com.vpu.mp.service.pojo.shop.order.write.operate.refund.money.RefundMoneyParam;
+import com.vpu.mp.service.pojo.shop.order.write.remark.SellerRemarkParam;
+import com.vpu.mp.service.pojo.shop.order.write.ship.ShipListParam;
+import com.vpu.mp.service.pojo.shop.order.write.ship.ShipParam;
 import com.vpu.mp.service.pojo.shop.order.write.star.StarParam;
 
 /**
- * 订单模块
+ * 	订单模块
  * 
- * @author 常乐,王帅
- * 2019年6月27日
+ * @author 常乐,王帅 2019年6月27日
  */
 @RestController
 @RequestMapping("/api/admin/order")
-public class AdminOrderController extends AdminBaseController{
+public class AdminOrderController extends AdminBaseController {
 	/**
-	 * 订单综合查询（不包括买单订单、虚拟商品订单）
+	 * 	订单综合查询（不包括买单订单、虚拟商品订单）
+	 * 
 	 * @param param
 	 * @return
 	 */
@@ -34,37 +40,67 @@ public class AdminOrderController extends AdminBaseController{
 		PageResult<? extends OrderListInfoVo> result = shop().readOrder.getPageList(param);
 		return success(result);
 	}
-	
+
 	/**
-	 * 订单详情（不包括退款货、买单订单、虚拟商品订单）
-	 * @param order
-	 * @return
-	 */
-	@PostMapping("/get")
-	public JsonResult get(@RequestBody OrderParam order) {
-		if(StringUtils.isEmpty(order.getMainOrderSn())) {
-			//TODO 
-			return fail();
-		}
-		return success(shop().readOrder.get(order.getMainOrderSn()));
-	}
-	
-	/**
-	   * 买单订单列表查询
+	 * 	买单订单列表查询
 	 */
 	@PostMapping("/store/list")
 	public JsonResult storyOrderList(@RequestBody StoreOrderPageListQueryParam param) {
 		PageResult<StoreOrderListInfoVo> result = shop().readOrder.getPageList(param);
 		return success(result);
 	}
-	
+
 	/**
-	   *  订单标星切换
+	 * 	订单详情（不包括退款货、买单订单、虚拟商品订单）
+	 * 
+	 * @param order
+	 * @return
+	 */
+	@PostMapping("/get")
+	public JsonResult get(@RequestBody OrderParam order) {
+		return success(shop().readOrder.get(order.getMainOrderSn()));
+	}
+
+	/**
+	 * 	订单标星切换
 	 */
 	@PostMapping("/star")
 	public JsonResult switchStar(@RequestBody StarParam param) {
 		shop().writeOrder.switchStar(param);
 		return success();
 	}
+	
+	/**
+	 * 	更新卖家备注
+	 */
+	@PostMapping("/sellerRemark")
+	public JsonResult sellerRemark(@RequestBody @Valid SellerRemarkParam param) {
+		return success(shop().writeOrder.sellerRemark(param));
+	}
+	
+	/**
+	 * 	发货_查询可发货商品
+	 */
+	@PostMapping("/shipGoods")
+	public JsonResult shipGoodsList(@RequestBody @Valid ShipListParam param) {
+		return success(shop().readOrder.shipGoodsList(param));
+	}
+	
+	/**
+	 * 	发货
+	 */
+	@PostMapping("/ship")
+	public JsonResult ship(@RequestBody @Valid ShipParam param) {
+		JsonResultCode code = shop().writeOrder.ship(param);
+		return code == null ? success() : fail(code);
+	}
+	
+	/**
+	 * 	mp退款
+	 */
+	@PostMapping("/ship")
+	public JsonResult refundMoney(@RequestBody RefundMoneyParam param) {
+		JsonResultCode code = shop().orderActionFactory.orderOperate(param);
+		return code == null ? success() : fail(code);
+	}
 }
-
