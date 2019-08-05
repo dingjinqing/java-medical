6bea2b9de1c090b2807c9402f5a92734ddedcfd4
zhengbaole <zhengbaole@wangdian.cn>
2019-08-05 11:13:59 +0000

    拼团抽奖参与用户 pojo

---
+++ jmp/mp-service/src/main/java/com/vpu/mp/service/pojo/shop/market/groupdraw/join/JoinUserListVo.java
@@ -0,0 +1,34 @@
+package com.vpu.mp.service.pojo.shop.market.groupdraw.join;
+
+import lombok.Data;
+
+import java.time.LocalDate;
+
+/**
+ * 拼团抽奖 - 参与用户 列表出参
+ *
+ * @author 郑保乐
+ */
+@Data
+public class JoinUserListVo {
+
+    /** 昵称 **/
+    private String username;
+    /** 手机号 **/
+    private String mobile;
+    /** 参与时间 **/
+    private LocalDate createTime;
+    /** 订单号 **/
+    private String orderSn;
+    /** 是否团长 **/
+    private Boolean isGrouper;
+    /** 团id **/
+    private Integer groupId;
+    /** 成团时间 **/
+    private LocalDate endTime;
+    /** 邀请用户数量 **/
+    private Short inviteUserNum;
+
+    /** 抽奖码数量 **/
+    private Short codeCount;
+}
