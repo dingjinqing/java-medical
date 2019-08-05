9aa1cc16f9b79ebeafc38c8a03d6660acaa5e6f8
zhengbaole <zhengbaole@wangdian.cn>
2019-08-05 02:00:51 +0000

    找回重构时删除的国际化单元测试代码

---
+++ jmp/mp-service/src/test/java/com/vpu/mp/controller/i18n/NestedPureVo.java
@@ -0,0 +1,14 @@
+package com.vpu.mp.controller.i18n;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+/**
+ * @author 郑保乐
+ */
+@Data
+@AllArgsConstructor
+class NestedPureVo {
+
+    private PureVo nestedVo;
+}
