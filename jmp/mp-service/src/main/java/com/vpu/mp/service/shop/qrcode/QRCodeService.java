1ef13145869dd0153485a3e07df603e683f7eb58
lixiaobing <lixiaobing@wangdian.cn>
2019-08-08 07:10:54 +0000

    小程序活动分享码获取功能修改

--- jmp/mp-service/src/main/java/com/vpu/mp/service/shop/qrcode/QRCodeService.java
+++ jmp/mp-service/src/main/java/com/vpu/mp/service/shop/qrcode/QRCodeService.java
@@ -3,14 +3,14 @@ package com.vpu.mp.service.shop.qrcode;
 import com.vpu.mp.db.shop.tables.records.CodeRecord;
 import com.vpu.mp.service.foundation.service.ShopBaseService;
 import com.vpu.mp.service.shop.image.ImageService;
-import org.apache.commons.io.FileUtils;
+import me.chanjar.weixin.common.error.WxErrorException;
 import org.jooq.Record1;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
-import java.io.File;
 import java.text.SimpleDateFormat;
 
+import static com.vpu.mp.db.main.Tables.MP_AUTH_SHOP;
 import static com.vpu.mp.db.shop.tables.Code.CODE;
 import static java.lang.String.format;
 
@@ -24,6 +24,8 @@ public class QRCodeService extends ShopBaseService {
 
     private final ImageService imageService;
 
+    private final int qrcodWidth=430;
+
     @Value("http://${domain.image}/")
     private String imageDomain;
 
@@ -33,7 +35,7 @@ public class QRCodeService extends ShopBaseService {
 
 
     /**
-     * 获取小程序码
+     * 获取小程分享码
      *
      * @param pageUrl 带参数的小程序页面url
      * @param typeId  类型id
@@ -41,23 +43,38 @@ public class QRCodeService extends ShopBaseService {
      *
      * @return 小程序码图片url
      */
-    public String getMpQRCode(String pageUrl, Short typeId, Integer paramId) throws Exception {
+    public String getMpQRCode(String pageUrl, Short typeId, Integer paramId){
         Record1<String> record = shopDb().select(CODE.QRCODE_IMG).from(CODE).where(CODE.TYPE_URL.eq(pageUrl))
             .or(CODE.TYPE.eq(typeId).and(CODE.PARAM_ID.eq(String.valueOf(paramId))))
             .fetchAny();
+
         if (null == record) {
-            String buffer = open().getMpQRCodeBuffer(pageUrl);
-            File tempFile = File.createTempFile("tmp", "");
-            FileUtils.writeByteArrayToFile(tempFile, buffer.getBytes());
+
+            Integer shopId = getShopId();
+            String appId = mainDb().select(MP_AUTH_SHOP.APP_ID).from(MP_AUTH_SHOP)
+                .where(MP_AUTH_SHOP.SHOP_ID.eq(shopId)).fetchAny(MP_AUTH_SHOP.APP_ID);
+
             String relativePath = format("upload/%s/qrcode/%s/T%sP%s_%s.jpg", typeId, getShopId(), typeId, paramId,
                 new SimpleDateFormat("yyyyMMddHHmmss").format(new java.util.Date()));
-            imageService.uploadToUpYun(relativePath, tempFile);
-            CodeRecord codeRecord = new CodeRecord(null, typeId, typeId.toString(), pageUrl,
+
+            try {
+                byte[] qrcodeBytes = open().getWxOpenComponentService().getWxMaServiceByAppid(appId).getQrcodeService().createQrcodeBytes(pageUrl, qrcodWidth);
+                this.imageService.getUpYunClient().writeFile(relativePath,qrcodeBytes,true);
+            } catch (WxErrorException e) {
+                logger().warn("获取小程序分享码错误："+e.getMessage());
+                return null;
+            } catch (Exception e) {
+                logger().warn("upYun上传文件错误："+e.getMessage());
+                return null;
+            }
+
+            CodeRecord codeRecord = new CodeRecord(null, typeId, paramId.toString(), pageUrl,
                 relativePath, (byte) 0, "0", null, null);
             shopDb().insertInto(CODE).set(codeRecord).execute();
-            return relativePath;
+
+            return this.imageService.imageUrl(relativePath);
         }
         String imageUrl = record.get(CODE.QRCODE_IMG);
-        return imageDomain + imageUrl;
+        return this.imageService.imageUrl(imageUrl);
     }
 }
