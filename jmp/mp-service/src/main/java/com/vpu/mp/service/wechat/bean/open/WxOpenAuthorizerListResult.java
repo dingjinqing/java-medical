017003fc03e0cc8777d24121532521d1bfbdeaca
lixinguo <lixinguo@wangdian.cn>
2019-08-08 05:12:52 +0000

    微信扩展接口

--- jmp/mp-service/src/main/java/com/vpu/mp/service/wechat/bean/open/WxOpenAuthorizerListResult.java
+++ jmp/mp-service/src/main/java/com/vpu/mp/service/wechat/bean/open/WxOpenAuthorizerListResult.java
@@ -1,38 +1,35 @@
 package com.vpu.mp.service.wechat.bean.open;
 
-import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
-import com.google.gson.annotations.SerializedName;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.vpu.mp.service.foundation.util.Util;
 
 import lombok.Data;
-import me.chanjar.weixin.common.util.json.WxGsonBuilder;
 
 @Data
-public class WxOpenAuthorizerListResult implements Serializable {
-	private static final long serialVersionUID = -5891226046698581644L;
+public class WxOpenAuthorizerListResult {
 
-	@SerializedName("total_count")
+	@JsonProperty("total_count")
 	private Integer totalCount;
 
-	@SerializedName("list")
+	@JsonProperty("list")
 	private List<WxMpAppInfo> list = new ArrayList<>();
 
-	static final class WxMpAppInfo implements Serializable{
-		private static final long serialVersionUID = -726195037493295418L;
+	static final class WxMpAppInfo{
 
-		@SerializedName("authorizer_appid")
+		@JsonProperty("authorizer_appid")
 		private String authorizerAppId;
 
-		@SerializedName("refresh_token")
+		@JsonProperty("refresh_token")
 		private String refreshToken;
 
-		@SerializedName("auth_time")
+		@JsonProperty("auth_time")
 		private Integer authTime;
 	}
 
 	public static WxOpenAuthorizerListResult fromJson(String json) {
-		return WxGsonBuilder.create().fromJson(json, WxOpenAuthorizerListResult.class);
+		return Util.parseJson(json, WxOpenAuthorizerListResult.class);
 	}
 }
