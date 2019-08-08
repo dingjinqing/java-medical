b9a8d78f24f6269025912b8efb33a46344a30f60
huangronggang <huangronggang@wangdian.cn>
2019-08-08 03:50:42 +0000

    拼团瓜分积分活动 相关的service

---
+++ jmp/mp-service/src/main/java/com/vpu/mp/service/shop/market/integration/GroupIntegrationListService.java
@@ -0,0 +1,386 @@
+package com.vpu.mp.service.shop.market.integration;
+
+import static com.vpu.mp.db.shop.tables.GroupIntegrationDefine.GROUP_INTEGRATION_DEFINE;
+import static com.vpu.mp.db.shop.tables.GroupIntegrationList.GROUP_INTEGRATION_LIST;
+import static com.vpu.mp.db.shop.tables.User.USER;
+
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.util.List;
+
+import org.jooq.SelectConditionStep;
+import org.jooq.SelectJoinStep;
+import org.jooq.SelectOnConditionStep;
+import org.jooq.impl.DSL;
+import org.jooq.tools.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+
+import com.vpu.mp.db.shop.tables.records.GroupIntegrationDefineRecord;
+import com.vpu.mp.db.shop.tables.records.GroupIntegrationListRecord;
+import com.vpu.mp.service.foundation.data.DelFlag;
+import com.vpu.mp.service.foundation.service.ShopBaseService;
+import com.vpu.mp.service.foundation.util.PageResult;
+import com.vpu.mp.service.pojo.shop.market.integration.GroupIntegrationDefineEnums;
+import com.vpu.mp.service.pojo.shop.market.integration.GroupIntegrationDefineEnums.IsGroupper;
+import com.vpu.mp.service.pojo.shop.market.integration.GroupIntegrationListDetailParam;
+import com.vpu.mp.service.pojo.shop.market.integration.GroupIntegrationListEnums;
+import com.vpu.mp.service.pojo.shop.market.integration.GroupIntegrationListEnums.IsGrouper;
+import com.vpu.mp.service.pojo.shop.market.integration.GroupIntegrationListEnums.IsNew;
+import com.vpu.mp.service.pojo.shop.market.integration.GroupIntegrationListEnums.Status;
+import com.vpu.mp.service.pojo.shop.market.integration.GroupIntegrationListParticipationVo;
+import com.vpu.mp.service.pojo.shop.market.integration.GroupIntegrationSuccessParam;
+import com.vpu.mp.service.pojo.shop.market.integration.GroupIntegrationSuccessVo;
+import com.vpu.mp.service.pojo.shop.market.integration.GroupperInfoPojo;
+import com.vpu.mp.service.pojo.shop.member.account.ScoreParam;
+import com.vpu.mp.service.shop.member.ScoreService;
+
+/**
+ * @author huangronggang
+ * @date 2019年8月6日
+ */
+@Service
+public class GroupIntegrationListService extends ShopBaseService {
+	
+	public static final byte TRADE_TYPE=10;
+	public static final byte TRADE_FLOW=1;
+	
+	public static final String REMARK = "积分瓜分";
+	
+	@Autowired private GroupIntegrationService groupIntegration;
+	@Autowired private GroupIntegrationCalculatorHandler calculatorHandler;
+	@Autowired private ScoreService scoreService;
+
+	/** 
+	 * 参团明细
+	 * @param param
+	 * @return
+	 */
+	public PageResult<GroupIntegrationListParticipationVo> getDetailPageList(GroupIntegrationListDetailParam param){
+		SelectOnConditionStep<?> select = db().select(GROUP_INTEGRATION_LIST.ID,GROUP_INTEGRATION_LIST.USER_ID,USER.USERNAME,USER.MOBILE,GROUP_INTEGRATION_LIST.IS_NEW,GROUP_INTEGRATION_LIST.START_TIME,GROUP_INTEGRATION_LIST.GROUP_ID,GROUP_INTEGRATION_LIST.INVITE_NUM,GROUP_INTEGRATION_LIST.INTEGRATION,GROUP_INTEGRATION_LIST.IS_GROUPER,GROUP_INTEGRATION_LIST.CAN_INTEGRATION)
+			.from(GROUP_INTEGRATION_LIST)
+			.leftJoin(USER)
+			.on(GROUP_INTEGRATION_LIST.USER_ID.eq(USER.USER_ID))
+			.leftJoin(GROUP_INTEGRATION_DEFINE)
+			.on(GROUP_INTEGRATION_LIST.INTE_ACTIVITY_ID.eq(GROUP_INTEGRATION_DEFINE.ID));
+		SelectConditionStep<?> step = buildOptionDetail(select,param);
+		return getPageResult(step, param.getCurrentPage(), param.getPageRows(), GroupIntegrationListParticipationVo.class);
+		
+	}
+	/**
+	 * 成团明细
+	 * @param param
+	 * @return
+	 */
+	public PageResult<GroupIntegrationSuccessVo> getSuccessPageList(GroupIntegrationSuccessParam param){
+		SelectJoinStep<?> selectFrom = db().select(GROUP_INTEGRATION_LIST.GROUP_ID,DSL.count(GROUP_INTEGRATION_LIST.ID).as("participantNum"),DSL.sum(GROUP_INTEGRATION_LIST.INTEGRATION).as("useIntegration"))
+			.from(GROUP_INTEGRATION_LIST);
+		SelectConditionStep<?> select = buildOptionSuccess(selectFrom,param);
+		PageResult<GroupIntegrationSuccessVo> result = getPageResult(select, GroupIntegrationSuccessVo.class);
+		List<GroupIntegrationSuccessVo> list = result.dataList;
+		if(list == null ||list.isEmpty()) {
+			return result;
+		}
+		GroupIntegrationDefineRecord defineRecord = groupIntegration.selectDefineById(param.getActId());
+		for(GroupIntegrationSuccessVo vo :list) {
+			GroupperInfoPojo grouperInfo = getGrouperInfo(param.getActId(),vo.getGroupId());
+			if(grouperInfo == null) {
+				continue;
+			}
+			vo.setGrouperName(grouperInfo.getUsername());
+			vo.setMobile(grouperInfo.getMobile());
+			vo.setStartTime(grouperInfo.getStartTime());
+			vo.setEndTime(grouperInfo.getEndTime());
+			vo.setActId(grouperInfo.getInteActivityId());
+			vo.setStatus(grouperInfo.getStatus());
+			vo.setLimitAmount(defineRecord.getLimitAmount());
+		}
+		return result;
+	}
+	
+	/**
+	 * 查某一个团长信息 
+	 * @param actId
+	 * @param groupId
+	 * @return
+	 */
+	private GroupperInfoPojo getGrouperInfo(Integer actId,Integer groupId) {
+		GroupperInfoPojo pojo = db().select().from(GROUP_INTEGRATION_LIST)
+			.leftJoin(USER).on(GROUP_INTEGRATION_LIST.USER_ID.eq(USER.USER_ID))
+			.where(GROUP_INTEGRATION_LIST.INTE_ACTIVITY_ID.eq(actId))
+			.and(GROUP_INTEGRATION_LIST.GROUP_ID.eq(groupId))
+			.and(GROUP_INTEGRATION_LIST.IS_GROUPER.eq(IsGrouper.TRUE.value()))
+			.fetchOneInto(GroupperInfoPojo.class);
+			
+		return pojo;	
+	}
+
+	/**
+	 * 获取某一个活动 所有团 的团长
+	 * @param id
+	 * @return
+	 */
+	public List<GroupIntegrationListRecord> getOnGoingGrouperInfo(Integer id) {
+		List<GroupIntegrationListRecord> fetch = db().selectFrom(GROUP_INTEGRATION_LIST)
+			.where(GROUP_INTEGRATION_LIST.INTE_ACTIVITY_ID.eq(id))
+			.and(GROUP_INTEGRATION_LIST.IS_GROUPER.eq(GroupIntegrationListEnums.IsGrouper.TRUE.value()))
+			.and(GROUP_INTEGRATION_LIST.STATUS.eq(GroupIntegrationListEnums.Status.UNDERWAY.value()))
+			.fetch();
+		return fetch;
+	}
+	
+	/**
+	 * 记录拼团结果
+	 * @param actId
+	 * @param groupId
+	 * @param status 取值见 GroupIntegrationListEnums.Status
+	 * @return
+	 */
+	public int setIntegrationListResult(Integer actId,Integer groupId, Byte status) {
+		return db().update(GROUP_INTEGRATION_LIST)
+				.set(GROUP_INTEGRATION_LIST.END_TIME,Timestamp.valueOf(LocalDateTime.now()))
+				.set(GROUP_INTEGRATION_LIST.STATUS,status)
+				.where(GROUP_INTEGRATION_LIST.INTE_ACTIVITY_ID.eq(actId))
+				.and(GROUP_INTEGRATION_LIST.GROUP_ID.eq(groupId))
+				.execute();
+	}
+	/** 
+	 * 更新某一个团员参团获取的积分 
+	 * @param id 该团员对应记录的ID
+	 * @param integration
+	 * @return
+	 */
+	public int updateIntegration(Integer id,Integer integration) {
+		return db().update(GROUP_INTEGRATION_LIST)
+					.set(GROUP_INTEGRATION_LIST.INTEGRATION,integration)
+					.where(GROUP_INTEGRATION_LIST.ID.eq(id))
+					.execute();
+	}
+	/** 更新参加某一个活动的某一个团全部成员获取的积分*/
+	public int batchUpdateIntegeration(Integer actId,Integer groupId,Integer integration) {
+		return db().update(GROUP_INTEGRATION_LIST)
+			.set(GROUP_INTEGRATION_LIST.INTEGRATION,integration)
+			.where(GROUP_INTEGRATION_LIST.INTE_ACTIVITY_ID.eq(actId))
+			.and(GROUP_INTEGRATION_LIST.GROUP_ID.eq(groupId))
+			.execute();
+	}
+	/**
+	 * 更新团长获取的积分 
+	 * @param actId
+	 * @param groupId
+	 * @param integration
+	 * @return
+	 */
+	public int updateGroupperIntegration(Integer actId,Integer groupId,Integer integration) {
+		return db().update(GROUP_INTEGRATION_LIST)
+				.set(GROUP_INTEGRATION_LIST.INTEGRATION,integration)
+				.where(GROUP_INTEGRATION_LIST.INTE_ACTIVITY_ID.eq(actId))
+				.and(GROUP_INTEGRATION_LIST.GROUP_ID.eq(groupId))
+				.and(GROUP_INTEGRATION_LIST.IS_GROUPER.eq(IsGroupper.YES.value()))
+				.execute();
+	}
+	/**
+	 * 邀请的的人数
+	 * @param groupId
+	 * @param userId
+	 * @return
+	 */
+	public int getInviteNum(Integer groupId,Integer userId) {
+		return db().select(DSL.count())
+			.from(GROUP_INTEGRATION_LIST)
+			.where(GROUP_INTEGRATION_LIST.GROUP_ID.eq(groupId))
+			.and(GROUP_INTEGRATION_LIST.INVITE_USER.eq(userId))
+			.fetchOneInto(Integer.class);
+	}
+	/**
+	 * 邀请的新用户人数
+	 * @param groupId
+	 * @param userId
+	 * @return
+	 */
+	public int getInviteNewNum(Integer groupId,Integer userId) {
+		return db().select(DSL.count())
+			.from(GROUP_INTEGRATION_LIST)
+			.where(GROUP_INTEGRATION_LIST.GROUP_ID.eq(groupId))
+			.and(GROUP_INTEGRATION_LIST.INVITE_USER.eq(userId))
+			.and(GROUP_INTEGRATION_LIST.IS_NEW.eq(IsNew.YES.value()))
+			.fetchOneInto(Integer.class);
+	}
+	/**
+	 * 获取参加某活动 某一个团的全部成员参与信息 
+	 * @param actId 拼团活动ID
+	 * @param groupId 团ID
+	 * @return
+	 */
+	public List<GroupIntegrationListParticipationVo> getGroupIntegrationListParticipation(Integer actId,Integer groupId){
+		List<GroupIntegrationListParticipationVo> fetch = db().select(GROUP_INTEGRATION_LIST.ID,GROUP_INTEGRATION_LIST.USER_ID,USER.USERNAME,USER.MOBILE,GROUP_INTEGRATION_LIST.IS_NEW,GROUP_INTEGRATION_LIST.START_TIME,GROUP_INTEGRATION_LIST.GROUP_ID,GROUP_INTEGRATION_LIST.INVITE_NUM,GROUP_INTEGRATION_LIST.INTEGRATION,GROUP_INTEGRATION_LIST.IS_GROUPER,GROUP_INTEGRATION_LIST.CAN_INTEGRATION)
+			.from(GROUP_INTEGRATION_LIST)
+			.leftJoin(USER)
+			.on(GROUP_INTEGRATION_LIST.USER_ID.eq(USER.USER_ID))
+			.leftJoin(GROUP_INTEGRATION_DEFINE)
+			.on(GROUP_INTEGRATION_LIST.INTE_ACTIVITY_ID.eq(GROUP_INTEGRATION_DEFINE.ID))
+			.where(GROUP_INTEGRATION_DEFINE.DEL_FLAG.eq(DelFlag.NORMAL_VALUE))
+			.and(GROUP_INTEGRATION_LIST.GROUP_ID.eq(groupId))
+			.and(GROUP_INTEGRATION_LIST.INTE_ACTIVITY_ID.eq(actId))
+			.orderBy(GROUP_INTEGRATION_LIST.INTEGRATION)
+			.fetchInto(GroupIntegrationListParticipationVo.class);
+		return fetch;
+	}
+	/**
+	 * 给参加某活动的某一个团进行结算 
+	 * @param groupId
+	 * @param actId
+	 */
+//		TODO 待指定线程池
+	@Async
+	public void asyncSuccessGroupIntegration(Integer groupId,Integer actId) {
+//		活动内容 
+		GroupIntegrationDefineRecord defineRecord = groupIntegration.selectDefineById(actId);
+//		参与情况 
+		List<GroupIntegrationListParticipationVo> listParticipation = getGroupIntegrationListParticipation(actId, groupId);
+		if(listParticipation == null||listParticipation.size()==0) {
+			return ;
+		}
+		if(isFail(defineRecord,listParticipation)) {
+			this.setIntegrationListResult(actId, groupId,GroupIntegrationListEnums.Status.FAIL.value());
+//			TODO 通知用户拼团失败
+		}else {
+			calculatorHandler.handle(defineRecord, listParticipation);
+			this.setIntegrationListResult(actId, groupId, GroupIntegrationListEnums.Status.SUCCESS.value());
+			if(defineRecord.getIsDayDivide().equals(GroupIntegrationDefineEnums.IsDayDivide.NO.value()) && defineRecord.getInteTotal() > 0) {
+				groupIntegration.refreshRemainInte(actId);
+			}
+			listParticipation = this.getGroupIntegrationListParticipation(actId, groupId);
+			listParticipation.forEach(item->{
+				ScoreParam scoreParam = new ScoreParam();
+				scoreParam.setOrderSn("");
+				scoreParam.setRemark(REMARK);
+				scoreParam.setScore(item.getIntegration());
+				scoreParam.setScoreDis(0);
+				scoreService.updateMemberScore(scoreParam, null, item.getUserId(),TRADE_TYPE, TRADE_FLOW);
+			});
+//			TODO 通知用户拼团成功
+		}
+		defineRecord = groupIntegration.selectDefineById(actId);
+		if(!canContinue(defineRecord)) {
+			defineRecord.setIsContinue(GroupIntegrationDefineEnums.IsContinue.FALSE.value());
+			groupIntegration.updateDefine(defineRecord);
+		}
+		
+		
+	}
+
+	/**
+	 * 拼团活动是否能继续开团
+	 * @param defineRecord
+	 * @return
+	 */
+	private boolean canContinue(GroupIntegrationDefineRecord defineRecord) {
+		boolean canDayDivide = defineRecord.getIsDayDivide().equals(GroupIntegrationDefineEnums.IsDayDivide.YES.value());
+		int remainInte = defineRecord.getInteRemain();
+		int groupInte = defineRecord.getInteGroup();
+		int totalInte = defineRecord.getInteTotal();
+		if(canDayDivide&&remainInte == 0 && totalInte >0 ) {
+			return false;
+		}
+		if(!canDayDivide && remainInte<groupInte && totalInte >0) {
+			return false;
+		}
+		return true;
+	}
+
+	/**
+	 *组团失败条件，积分不足时失败，
+	 *用户开团24小时后,拼团未满员不可以瓜分积分，且用户数量不满足成团条件 返回失败
+	 *失败 返回false
+	 * @param defineRecord
+	 * @param listParticipation
+	 * @return
+	 */
+	private boolean isFail(GroupIntegrationDefineRecord defineRecord,
+			List<GroupIntegrationListParticipationVo> listParticipation) {
+//		剩余积分 
+		Integer canIntegration = listParticipation.get(0).getCanIntegration();
+//		积分不足时失败
+		if(canIntegration == 0) {
+			return true;
+		}
+		int userNum = listParticipation.size();
+//		用户开团24小时后,拼团未满员不可以瓜分积分，且用户数量不满足成团条件 返回失败
+		if(GroupIntegrationDefineEnums.IsDayDivide.NO.value().equals(defineRecord.getIsDayDivide())&&defineRecord.getLimitAmount()>userNum) {
+			return true;
+		}
+		return false;
+	}
+	
+	/**
+	 * @param select
+	 * @param param 
+	 * @return 
+	 */
+	private SelectConditionStep<?> buildOptionDetail(SelectOnConditionStep<?> select, GroupIntegrationListDetailParam param) {
+		SelectConditionStep<?> step = select.where(GROUP_INTEGRATION_DEFINE.DEL_FLAG.eq(DelFlag.NORMAL_VALUE))
+			.and(GROUP_INTEGRATION_LIST.INTE_ACTIVITY_ID.eq(param.getActId()));
+		if(!StringUtils.isBlank(param.getMobile())) {
+			step.and(USER.MOBILE.like(this.likeValue(param.getMobile())));
+		}
+		if(!StringUtils.isBlank(param.getUsername())) {
+			step.and(USER.USERNAME.like(this.likeValue(param.getUsername())));
+		}
+		if(param.getStartTime() != null) {
+			step.and(GROUP_INTEGRATION_LIST.END_TIME.ge(param.getStartTime()));
+		}
+		if(param.getEndTime() != null) {
+			step.and(GROUP_INTEGRATION_LIST.END_TIME.le(param.getEndTime()));
+		}
+		if(param.getIsGrouper() != null) {
+			step.and(GROUP_INTEGRATION_LIST.IS_GROUPER.eq(param.getIsGrouper()));
+		}
+		if(param.getInviteNum() != null) {
+			step.and(GROUP_INTEGRATION_LIST.INVITE_NUM.eq(param.getInviteNum()));
+		}
+		if(param.getMinIntegration() != null) {
+			step.and(GROUP_INTEGRATION_LIST.INTEGRATION.ge(param.getMinIntegration()));
+		}
+		if(param.getMaxIntegration() != null) {
+			step.and(GROUP_INTEGRATION_LIST.INTEGRATION.le(param.getMaxIntegration()));
+		}
+		if(param.getIsNew() != null) {
+			step.and(GROUP_INTEGRATION_LIST.IS_NEW.eq(param.getIsNew()));
+		}
+		if(param.getGroupId() != null) {
+			step.and(GROUP_INTEGRATION_LIST.GROUP_ID.eq(param.getGroupId()));
+		}
+		return step;
+			
+	}
+	
+
+	/**
+	 * @param selectFrom
+	 * @param param
+	 * @return 
+	 */
+	private SelectConditionStep<?> buildOptionSuccess(SelectJoinStep<?> selectFrom, GroupIntegrationSuccessParam param) {
+		SelectConditionStep<?> select = selectFrom.where(GROUP_INTEGRATION_LIST.STATUS.gt(Status.UNDERWAY.value()));
+		select.and(GROUP_INTEGRATION_LIST.INTE_ACTIVITY_ID.eq(param.getActId()));
+		select.groupBy(GROUP_INTEGRATION_LIST.GROUP_ID);
+		if(param.getGroupId() != null) {
+			select.and(GROUP_INTEGRATION_LIST.GROUP_ID.eq(param.getGroupId()));
+		}
+		if(param.getStatus() != null) {
+			select.and(GROUP_INTEGRATION_LIST.STATUS.eq(param.getStatus()));
+		}
+		if(param.getStartTime() != null) {
+			select.and(GROUP_INTEGRATION_LIST.END_TIME.ge(param.getStartTime()));
+		}
+		if(param.getEndTime() != null) {
+			select.and(GROUP_INTEGRATION_LIST.END_TIME.le(param.getEndTime()));
+		}
+		return select;
+	}	
+	
+	
+}
+
---
+++ jmp/mp-service/src/main/java/com/vpu/mp/service/shop/market/integration/GroupIntegrationService.java
@@ -0,0 +1,356 @@
+package com.vpu.mp.service.shop.market.integration;
+
+import static com.vpu.mp.db.shop.tables.GroupIntegrationDefine.GROUP_INTEGRATION_DEFINE;
+import static com.vpu.mp.db.shop.tables.GroupIntegrationList.GROUP_INTEGRATION_LIST;
+
+import java.math.RoundingMode;
+import java.sql.Timestamp;
+import java.text.NumberFormat;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.jooq.SelectConditionStep;
+import org.jooq.SelectWhereStep;
+import org.jooq.impl.DSL;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.vpu.mp.db.shop.tables.records.GroupIntegrationDefineRecord;
+import com.vpu.mp.db.shop.tables.records.GroupIntegrationListRecord;
+import com.vpu.mp.service.foundation.data.DelFlag;
+import com.vpu.mp.service.foundation.service.ShopBaseService;
+import com.vpu.mp.service.foundation.util.PageResult;
+import com.vpu.mp.service.pojo.shop.market.integration.GroupIntegrationDefineEnums;
+import com.vpu.mp.service.pojo.shop.market.integration.GroupIntegrationDefinePageParam;
+import com.vpu.mp.service.pojo.shop.market.integration.GroupIntegrationDefineParam;
+import com.vpu.mp.service.pojo.shop.market.integration.GroupIntegrationDefineVo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+/**
+ * @author huangronggang
+ * @date 2019年8月5日
+ * 组团瓜分积分
+ */
+@Service
+public class GroupIntegrationService extends ShopBaseService {
+	
+	@Autowired public GroupIntegrationListService groupIntegrationList;
+	
+	/**
+	 * 分页查询瓜分积分活动列表
+	 * @param pageParam
+	 * @return
+	 */
+	public PageResult<GroupIntegrationDefineVo> getPageList(GroupIntegrationDefinePageParam pageParam){
+		SelectWhereStep<?> selectFrom = db().selectFrom(GROUP_INTEGRATION_DEFINE);
+		SelectConditionStep<?> step = buildOptions(selectFrom,pageParam);
+		PageResult<GroupIntegrationDefineVo> pageResult = getPageResult(step, pageParam.getCurrentPage(), pageParam.getPageRows(), GroupIntegrationDefineVo.class);
+		List<GroupIntegrationDefineVo> dataList = pageResult.getDataList();
+		if(dataList == null) {
+			return pageResult;
+		}
+		//填充 团数量、参与人数 、消耗积分 
+		Map<Integer, ActivityInfo> activictyInfoMap = getGroupIntegrationActivictyInfo(dataList);
+		passInfo(dataList,activictyInfoMap);
+		
+		return pageResult;
+	}
+	/**
+	 * 根据iD 查询指定的瓜分积分活动
+	 * @param id
+	 * @return
+	 */
+	public GroupIntegrationDefineVo selectGroupIntegrationDefineById(Integer id) {
+		if(id == null) {
+			return null;
+		}
+		GroupIntegrationDefineVo fetchOneInto = db().selectFrom(GROUP_INTEGRATION_DEFINE)
+			.where(GROUP_INTEGRATION_DEFINE.DEL_FLAG.eq(DelFlag.NORMAL_VALUE))
+			.and(GROUP_INTEGRATION_DEFINE.ID.eq(id))
+			.fetchOneInto(GroupIntegrationDefineVo.class);
+		if(fetchOneInto == null) {
+			return null;
+		}
+		
+		ActivityInfo info = getGroupIntegrationActivictyInfo(fetchOneInto);
+		passInfo(fetchOneInto, info);
+		
+		return fetchOneInto;
+		
+	}
+	/**
+	 * 根据iD 查询指定的瓜分积分活动
+	 * @param id
+	 * @return
+	 */
+	public GroupIntegrationDefineRecord selectDefineById(Integer id) {
+		if(id == null) {
+			return null;
+		}
+		GroupIntegrationDefineRecord fetchOne = db().selectFrom(GROUP_INTEGRATION_DEFINE)
+			.where(GROUP_INTEGRATION_DEFINE.ID.eq(id))
+			.and(GROUP_INTEGRATION_DEFINE.DEL_FLAG.eq(DelFlag.NORMAL_VALUE))
+			.fetchOne();
+		
+		return fetchOne;
+		
+	}
+	
+	/**
+	 *  新增一个瓜分积分活动 
+	 * @param param
+	 * @return
+	 */
+	public int insertDefine(GroupIntegrationDefineParam param) {
+		Double paramN = calculateParamN(param.getInteGroup(),param.getLimitAmount());
+		
+		GroupIntegrationDefineRecord record = new GroupIntegrationDefineRecord();
+		record.setId(param.getId());
+		record.setShopId(getShopId());
+		record.setName(param.getName());
+		record.setInteTotal(param.getInteTotal());
+		record.setInteGroup(param.getInteGroup());
+		record.setLimitAmount(param.getLimitAmount());
+		record.setJoinLimit(param.getJoinLimit());
+		record.setDivideType(param.getDivideType());
+		record.setStartTime(param.getStartTime());
+		record.setEndTime(param.getEndTime());
+		record.setStatus(GroupIntegrationDefineEnums.Status.NORMAL.value());
+		record.setDelFlag(DelFlag.NORMAL_VALUE);
+		record.setInteRemain(param.getInteTotal());
+		record.setIsDayDivide(param.getIsDayDivide());
+		record.setIsContinue(GroupIntegrationDefineEnums.IsContinue.TRUE.value());
+		record.setParamN(paramN);
+		record.setAdvertise(param.getAdvertise());
+		db().executeInsert(record);
+		return 0;
+		
+	}
+	/**
+	 * 根据ID删除一个瓜分积分活动
+	 * @param id
+	 * @return
+	 */
+	public int deleteDefine(Integer id) {
+		int execute = db().update(GROUP_INTEGRATION_DEFINE)
+			.set(GROUP_INTEGRATION_DEFINE.DEL_FLAG, DelFlag.DISABLE_VALUE)
+			.set(GROUP_INTEGRATION_DEFINE.DEL_TIME, Timestamp.valueOf(LocalDateTime.now()))
+			.where(GROUP_INTEGRATION_DEFINE.ID.eq(id))
+			.execute();
+		return execute;
+	}
+	/**
+	 * 更新指定ID的瓜分积分活动
+	 * @param param
+	 * @return
+	 */
+	public int updateDefine(GroupIntegrationDefineParam param) {
+		int execute = db().update(GROUP_INTEGRATION_DEFINE)
+			.set(GROUP_INTEGRATION_DEFINE.NAME,param.getName())
+			.set(GROUP_INTEGRATION_DEFINE.ADVERTISE,param.getAdvertise())
+			.set(GROUP_INTEGRATION_DEFINE.START_TIME,param.getStartTime())
+			.set(GROUP_INTEGRATION_DEFINE.END_TIME,param.getEndTime())
+			.set(GROUP_INTEGRATION_DEFINE.INTE_TOTAL,param.getInteTotal())
+			.set(GROUP_INTEGRATION_DEFINE.INTE_GROUP,param.getInteGroup())
+			.set(GROUP_INTEGRATION_DEFINE.LIMIT_AMOUNT,param.getLimitAmount())
+			.set(GROUP_INTEGRATION_DEFINE.JOIN_LIMIT,param.getJoinLimit())
+			.set(GROUP_INTEGRATION_DEFINE.DIVIDE_TYPE,param.getDivideType())
+			.set(GROUP_INTEGRATION_DEFINE.IS_DAY_DIVIDE,param.getIsDayDivide())
+			.where(GROUP_INTEGRATION_DEFINE.ID.eq(param.getId()))
+			.and(GROUP_INTEGRATION_DEFINE.DEL_FLAG.eq(DelFlag.NORMAL_VALUE))
+			.execute();
+		return execute;	
+	}
+	/**
+	 * 更新指定ID的瓜分积分活动
+	 * @param param
+	 * @return
+	 */
+	public int updateDefine(GroupIntegrationDefineRecord param) {
+		return db().executeUpdate(param);
+	}
+	/**
+	 * 启用活动，或者停止活动
+	 * @param id
+	 * @param status
+	 * @return
+	 */
+	public int changDefineStatus(Integer id,Byte status) {
+		GroupIntegrationDefineRecord record = selectDefineById(id);
+		if(record.getStatus().equals(status)) {
+			return 0;
+		}
+		if(GroupIntegrationDefineEnums.Status.NORMAL.value().equals(status)) {
+			if(record.getEndTime().before(Timestamp.valueOf(LocalDateTime.now()))){
+//				这个活动已经过期了不能再启用了
+				return 0;
+			}
+		}
+		record.setStatus(status);
+		db().executeUpdate(record);
+		if(GroupIntegrationDefineEnums.Status.NORMAL.value().equals(status)) {
+			return 1;
+		}
+		//停用操作需要进行奖池分配
+		List<GroupIntegrationListRecord> list = groupIntegrationList.getOnGoingGrouperInfo(id);
+		list.forEach(item->groupIntegrationList.asyncSuccessGroupIntegration(item.getGroupId(), item.getInteActivityId()));
+		return 0;
+	}
+
+	/**
+	 * 刷新剩余积分 ：剩余积分 = 当前剩余积分 - 团的积分
+	 * @param actId
+	 */
+	public void refreshRemainInte(Integer actId) {
+		db().update(GROUP_INTEGRATION_DEFINE)
+			.set(GROUP_INTEGRATION_DEFINE.INTE_REMAIN,GROUP_INTEGRATION_DEFINE.INTE_REMAIN.minus(GROUP_INTEGRATION_DEFINE.INTE_GROUP))
+			.where(GROUP_INTEGRATION_DEFINE.ID.eq(actId))
+			.execute();
+	}
+	/**
+	 * 
+	 * @param inteGroup  每一个团的总积分
+	 * @param limitAmount    成团人数
+	 * @return
+	 */
+	private Double calculateParamN(Integer inteGroup, double limitAmount) {
+		NumberFormat nf = NumberFormat.getNumberInstance();
+		nf.setMaximumFractionDigits(2);
+		nf.setRoundingMode(RoundingMode.HALF_UP);
+		double temp = Math.pow(inteGroup,1/limitAmount );
+		String formatParamN = nf.format(temp);
+		return Double.parseDouble(formatParamN);
+	}
+
+	/**
+	 * 将团数量、参与人数 、消耗积分 信息填充到传入的对象中
+	 * @param dataList
+	 * @param activictyInfoMap
+	 */
+	private void passInfo(List<GroupIntegrationDefineVo> dataList, Map<Integer, ActivityInfo> activictyInfoMap) {
+		for (GroupIntegrationDefineVo defineVo : dataList) {
+			ActivityInfo info = activictyInfoMap.get(defineVo.getId());
+			if(info == null) {
+				continue;
+			}
+			defineVo.setUseIntegration(info.getUseIntegration());
+			defineVo.setInteUserSum(info.getInteUserSum());
+			defineVo.setInteGroupSum(info.getInteGroupSum());
+		}
+	}
+	
+	private void passInfo(GroupIntegrationDefineVo defineVo, ActivityInfo info) {
+		if(info == null) {
+			return;
+		}
+		defineVo.setUseIntegration(info.getUseIntegration());
+		defineVo.setInteUserSum(info.getInteUserSum());
+		defineVo.setInteGroupSum(info.getInteGroupSum());
+	}
+
+	/**
+	 * 查找 某些活动 的团数量、参与人数 、消耗积分信息 
+	 * @param dataList
+	 * @return key为活动ID，vlaue 为活动信息
+	 */
+	private Map<Integer, ActivityInfo> getGroupIntegrationActivictyInfo(List<GroupIntegrationDefineVo> dataList) {
+		if(dataList == null||dataList.size()==0) {
+			return new HashMap<Integer,ActivityInfo>(0);
+		}
+		List<Integer> groupIntegrationDefineIdList = new ArrayList<>(dataList.size());
+		dataList.forEach(item->groupIntegrationDefineIdList.add(item.getId()));
+		List<ActivityInfo> result = db().select(GROUP_INTEGRATION_LIST.INTE_ACTIVITY_ID.as("inteActivityId"), DSL.sum(GROUP_INTEGRATION_LIST.INTEGRATION).as("useIntegration"), DSL.count(GROUP_INTEGRATION_LIST.ID).as("inteUserSum"), DSL.countDistinct(GROUP_INTEGRATION_LIST.GROUP_ID).as("inteGroupSum"))
+			.from(GROUP_INTEGRATION_LIST)
+			.where(GROUP_INTEGRATION_LIST.INTE_ACTIVITY_ID.in(groupIntegrationDefineIdList))
+			.groupBy(GROUP_INTEGRATION_LIST.INTE_ACTIVITY_ID)
+			.fetchInto(ActivityInfo.class);
+		
+		HashMap<Integer,ActivityInfo> fetchMap =new HashMap<Integer, ActivityInfo>(result.size());
+		result.forEach(item->fetchMap.put(item.getInteActivityId(),item));
+		return fetchMap;
+	}
+	
+	/**
+	 * 查找 团数量、参与人数 、消耗积分 
+	 * @param dataList
+	 * @return 
+	 */
+	private ActivityInfo getGroupIntegrationActivictyInfo(GroupIntegrationDefineVo vo) {
+		if(vo == null) {
+			return null;
+		}
+		ActivityInfo fetchOne = db().select(DSL.sum(GROUP_INTEGRATION_LIST.INTEGRATION).as("useIntegration"), DSL.count(GROUP_INTEGRATION_LIST.ID).as("inteUserSum"), DSL.countDistinct(GROUP_INTEGRATION_LIST.GROUP_ID).as("inteGroupSum"))
+			.from(GROUP_INTEGRATION_LIST)
+			.where(GROUP_INTEGRATION_LIST.INTE_ACTIVITY_ID.eq(vo.getId()))
+			.groupBy(GROUP_INTEGRATION_LIST.INTE_ACTIVITY_ID)
+			.fetchOneInto(ActivityInfo.class);
+		return fetchOne;
+	}
+
+
+	/**
+	 * @param selectFrom
+	 * @param pageParam
+	 * @return
+	 */
+	private SelectConditionStep<?> buildOptions(SelectWhereStep<?> selectFrom, GroupIntegrationDefinePageParam pageParam) {
+		SelectConditionStep<?> step = selectFrom.where(GROUP_INTEGRATION_DEFINE.DEL_FLAG.eq(DelFlag.NORMAL_VALUE));
+		switch(pageParam.getType()) {
+			case GroupIntegrationDefineEnums.QueryType.UNSTARTED :
+				step.and(GROUP_INTEGRATION_DEFINE.START_TIME.gt(Timestamp.valueOf(LocalDateTime.now())));
+				break;
+			case GroupIntegrationDefineEnums.QueryType.OVERDUE:
+				step.and(GROUP_INTEGRATION_DEFINE.END_TIME.lt(Timestamp.valueOf(LocalDateTime.now())));
+				break;
+			case GroupIntegrationDefineEnums.QueryType.STOPPED:
+				step.and(GROUP_INTEGRATION_DEFINE.STATUS.eq(GroupIntegrationDefineEnums.Status.STOPPED.value()));
+				break;
+			case GroupIntegrationDefineEnums.QueryType.UNDER_WAY:
+				step.and(GROUP_INTEGRATION_DEFINE.START_TIME.le(Timestamp.valueOf(LocalDateTime.now())))
+					.and(GROUP_INTEGRATION_DEFINE.END_TIME.ge(Timestamp.valueOf(LocalDateTime.now())));
+				break;
+		}
+		return step;
+	}
+	
+	
+}
+@Data
+class ActivityInfo{
+	/**活动Id*/
+	private Integer inteActivityId;
+	/** 消耗积分 */
+	private Integer useIntegration;
+	/**参与人数 */
+	private Integer inteUserSum;
+	/** 	团数量 */
+	private Integer inteGroupSum;
+	/**
+	 * @param inteActivityId
+	 * @param useIntegration
+	 * @param inteUserSum
+	 * @param inteGroupSum
+	 */
+	public ActivityInfo(Integer inteActivityId, Integer useIntegration, Integer inteUserSum, Integer inteGroupSum) {
+		super();
+		this.inteActivityId = inteActivityId;
+		this.useIntegration = useIntegration;
+		this.inteUserSum = inteUserSum;
+		this.inteGroupSum = inteGroupSum;
+	}
+	/**
+	 * 
+	 */
+	public ActivityInfo() {
+		super();
+	}
+	
+}
+
