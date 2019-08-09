13a8da0155867a881cf65684bd671c4cb4bf83bd
huangronggang <huangronggang@wangdian.cn>
2019-08-09 11:12:02 +0000

    消除p3c告警

--- jmp/mp-web/src/main/java/com/vpu/mp/controller/admin/AdminGroupIntegrationController.java
+++ jmp/mp-web/src/main/java/com/vpu/mp/controller/admin/AdminGroupIntegrationController.java
@@ -124,9 +124,9 @@ public class AdminGroupIntegrationController extends AdminBaseController {
 	 */
 	@PostMapping("/getqrcode/{actId}")
 	public JsonResult getQrcode(@PathVariable Integer actId) {
-		GroupIntegrationShareQRCodeVo maQRCode = shop().groupIntegration.getMaQRCode(actId);
+		GroupIntegrationShareQRCodeVo maQrCode = shop().groupIntegration.getMaQRCode(actId);
 		if(maQRCode != null) {
-			return success(maQRCode);
+			return success(maQrCode);
 		}
 		return fail();
 	}
