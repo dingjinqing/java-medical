39f55f9c93a5f33a958a634a7809597f8bb98e16
liangchen <liangchen@wangdian.cn>
2019-08-09 01:48:55 +0000

    默认运费模板配置

--- jmp/mp-service/src/main/java/com/vpu/mp/service/pojo/shop/config/DeliverTemplateConfig.java
+++ jmp/mp-service/src/main/java/com/vpu/mp/service/pojo/shop/config/DeliverTemplateConfig.java
@@ -2,7 +2,6 @@ package com.vpu.mp.service.pojo.shop.config;
 
 import java.math.BigDecimal;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 
 import lombok.Data;
 
@@ -17,19 +16,16 @@ public class DeliverTemplateConfig {
 	/**
 	 *  0：统一运费，1：满额包邮，
 	 */
-	@JsonProperty(value = "template_name")
 	public Integer templateName;
 	
 	/**
 	 * 包邮限制最低金额
 	 */
-	@JsonProperty(value = "fee_limit")
 	public BigDecimal feeLimit;
 	
 	/**
 	 * 运费
 	 */
-	@JsonProperty(value = "price")
 	public BigDecimal price;
 	
 	
