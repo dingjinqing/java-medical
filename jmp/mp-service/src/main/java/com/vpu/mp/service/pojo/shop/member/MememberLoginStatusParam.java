3f9aff97223dc643cb9a9c733b9dcdca0c04348a
huangzhuangzhuang <huangzhuangzhuang@wangdian.cn>
2019-08-13 11:33:26 +0000

    禁止登录入参

---
+++ jmp/mp-service/src/main/java/com/vpu/mp/service/pojo/shop/member/MememberLoginStatusParam.java
@@ -0,0 +1,23 @@
+package com.vpu.mp.service.pojo.shop.member;
+
+import java.util.List;
+
+import javax.validation.constraints.NotNull;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * 
+ * @author 黄壮壮
+ * @Date:  2019年8月13日
+ * @Description: 会员用户批量设置禁止登陆-恢复登录-入参
+ */
+@Getter
+@Setter
+public class MememberLoginStatusParam {
+	/** 会员用户ID列表  */
+	@NotNull private List<Integer> userIdList;
+	/** 用户登录设置状态 1-禁止登录，0-恢复登录 */
+	@NotNull private Byte isDelete;
+}
