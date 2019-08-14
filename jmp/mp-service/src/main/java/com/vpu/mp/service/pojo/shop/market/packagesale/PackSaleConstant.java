d93aa0ee10cfa3a5b9921744473662143448522f
huangronggang <huangronggang@wangdian.cn>
2019-08-14 03:12:45 +0000

    一口价 POJO

---
+++ jmp/mp-service/src/main/java/com/vpu/mp/service/pojo/shop/market/packagesale/PackSaleConstant.java
@@ -0,0 +1,31 @@
+package com.vpu.mp.service.pojo.shop.market.packagesale;
+
+/**
+ * @author huangronggang
+ * @date 2019年8月12日
+ */
+public class PackSaleConstant {
+	
+	public static final String ID_DELIMITER =",";
+	/** 活动状态 */
+	public class ActivityStatus{
+		/** 全部 */
+		public final static  int ALL=0;
+		/** 进行中 */
+		public final static int UNDER_WAY=1;
+		/** 未开始 */
+		public final static int UNSTARTED=2;
+		/** 已过期 */
+		public final static int OVERDUE=3;
+		/** 已停用 */
+		public final static int STOPPED=4;
+	}
+	/**活动状态 （启用/停用）*/
+	public class Status{
+		/** 启用 */
+		public static final byte NORMAL=1;
+		/** 停用 */
+		public final static byte STOPED=0;
+	}
+}
+
---
+++ jmp/mp-service/src/main/java/com/vpu/mp/service/pojo/shop/market/packagesale/PackSaleParam.java
@@ -0,0 +1,128 @@
+package com.vpu.mp.service.pojo.shop.market.packagesale;
+
+import java.math.BigDecimal;
+import java.sql.Timestamp;
+import java.util.List;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+
+import org.apache.commons.lang3.StringUtils;
+
+import com.vpu.mp.db.shop.tables.records.PackageSaleRecord;
+import com.vpu.mp.service.pojo.shop.market.packagesale.PackSaleConstant.Status;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author huangronggang
+ * @date 2019年8月12日
+ */
+@Data
+@NoArgsConstructor
+public class PackSaleParam {
+	
+	/** 活动ID */
+	private Integer id;
+	/** 活动名称 */
+	@NotBlank
+	private String packageName;
+	/** 开始时间 */
+	@NotNull
+	private Timestamp startTime;
+	/** 结束时间 */
+	@NotNull
+	private Timestamp endTime;
+	/** 结算总价格 */
+	@Positive
+	private BigDecimal totalMoney;
+	/** 商品组1 */
+	@NotNull
+	private GoodsGroup group1;
+	/** 商品组2 */
+	private GoodsGroup group2;
+	/** 商品组3 */
+	private GoodsGroup group3;
+	
+	@Data
+	@NoArgsConstructor
+	public class GoodsGroup{
+		
+		/** 商品组1名称 */
+		@NotBlank
+		private String groupName;
+		/** 至少需要选择件数 */
+		@NotNull
+		private Integer goodsNumber;
+		/** 商品ID列表 */
+		private List<Integer> goodsIdList;
+		/** 平台分类ID列表 */
+		private List<Integer> catIdList;
+		/** 商家分类ID列表 */
+		private List<Integer> sortIdList;
+	}
+	public PackageSaleRecord convert2Record() {
+		PackageSaleRecord record = new PackageSaleRecord();
+		record.setId(this.getId());
+		record.setPackageName(this.getPackageName());
+		record.setStartTime(this.getStartTime());
+		record.setEndTime(this.getEndTime());
+		record.setTotalMoney(getTotalMoney());
+		
+		record.setGoodsGroup_1(Status.NORMAL);
+		record.setGroupName_1(group1.getGroupName());
+		record.setGoodsNumber_1(group1.getGoodsNumber());
+		if(group1.getGoodsIdList() != null) {
+			String goodsIds = StringUtils.join(group1.getGoodsIdList(), PackSaleConstant.ID_DELIMITER);
+			record.setGoodsIds_1(goodsIds);
+		}
+		if(group1.getCatIdList() != null) {
+			String catIds = StringUtils.join(group1.getCatIdList(), PackSaleConstant.ID_DELIMITER);
+			record.setCatIds_1(catIds);
+		}
+		if(group1.getSortIdList() != null) {
+			String sortIds = StringUtils.join(group1.getSortIdList(), PackSaleConstant.ID_DELIMITER);
+			record.setSortIds_1(sortIds);
+		}
+		if(group2 == null) {
+			return record;
+		}
+		record.setGroupName_2(group2.getGroupName());
+		record.setGoodsGroup_2(Status.NORMAL);
+		record.setGoodsNumber_2(group2.getGoodsNumber());
+		if(group2.getGoodsIdList() != null) {
+			String goodsIds = StringUtils.join(group2.getGoodsIdList(), PackSaleConstant.ID_DELIMITER);
+			record.setGoodsIds_2(goodsIds);
+		}
+		if(group2.getCatIdList() != null) {
+			String catIds = StringUtils.join(group2.getCatIdList(), PackSaleConstant.ID_DELIMITER);
+			record.setCatIds_2(catIds);
+		}
+		if(group2.getSortIdList() != null) {
+			String sortIds = StringUtils.join(group2.getSortIdList(), PackSaleConstant.ID_DELIMITER);
+			record.setSortIds_2(sortIds);
+		}
+		if(group3 == null) {
+			return record;
+		}
+
+		record.setGoodsGroup_3(Status.NORMAL);
+		record.setGroupName_3(group3.getGroupName());
+		record.setGoodsNumber_3(group3.getGoodsNumber());
+		if(group3.getGoodsIdList() != null) {
+			String goodsIds = StringUtils.join(group3.getGoodsIdList(), PackSaleConstant.ID_DELIMITER);
+			record.setGoodsIds_3(goodsIds);
+		}
+		if(group3.getCatIdList() != null) {
+			String catIds = StringUtils.join(group3.getCatIdList(), PackSaleConstant.ID_DELIMITER);
+			record.setCatIds_3(catIds);
+		}
+		if(group3.getSortIdList() != null) {
+			String sortIds = StringUtils.join(group3.getSortIdList(), PackSaleConstant.ID_DELIMITER);
+			record.setSortIds_3(sortIds);
+		}
+		return record;
+	}
+}
