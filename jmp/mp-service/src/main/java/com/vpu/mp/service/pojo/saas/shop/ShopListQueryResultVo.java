63c11f85b1b581b0d629c7ebccfc88c68b1ac89e
zhaojianqiang <zhaojianqiang@wangdian.cn>
2019-08-15 07:57:02 +0000

    加个构造器

--- jmp/mp-service/src/main/java/com/vpu/mp/service/pojo/saas/shop/ShopListQueryResultVo.java
+++ jmp/mp-service/src/main/java/com/vpu/mp/service/pojo/saas/shop/ShopListQueryResultVo.java
@@ -6,6 +6,7 @@ import java.sql.Timestamp;
 import com.vpu.mp.service.foundation.util.Page;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 /**
  * 
@@ -13,6 +14,7 @@ import lombok.Data;
  *
  */
 @Data
+@NoArgsConstructor
 public class ShopListQueryResultVo {
 	public Page page;
 	public Integer sysId;
