48a0417ae0bbe11f50c122b93ff90432b87b56e7
huangronggang <huangronggang@wangdian.cn>
2019-08-15 03:46:29 +0000

    渠道页面分析 pojo

---
+++ jmp/mp-service/src/main/java/com/vpu/mp/service/pojo/shop/market/channel/ChannelConstant.java
@@ -0,0 +1,14 @@
+package com.vpu.mp.service.pojo.shop.market.channel;
+
+/**
+ * @author huangronggang
+ * @date 2019年8月14日
+ * 渠道分析相关常量
+ */
+public class ChannelConstant {
+	/** 来源类型 自定义 */
+	public final static Byte SOURCETYPE_CUSTOMIZE =0;
+	/** 来源类型 自商品 */
+	public final static Byte SOURCETYPE_GOODS =1;
+}
+
