80f75dd9fc50aaa359a44911ff1c366035cd7253
lixiaobing <lixiaobing@wangdian.cn>
2019-08-19 02:59:29 +0000

    添加店铺未绑定小程序国际化信息

--- jmp/mp-service/src/main/java/com/vpu/mp/service/foundation/data/JsonResultMessage.java
+++ jmp/mp-service/src/main/java/com/vpu/mp/service/foundation/data/JsonResultMessage.java
@@ -163,6 +163,7 @@ public class JsonResultMessage {
 	public static final String WX_ERROR_EXCEPTION = "wx.error.exception";
 	public static final String WX_MA_NEED_UPLOADCODE = "wx.ma.need.uploadcode";
 	public static final String WX_MA_NEED_AUDITING_CODE_SUCCESS = "wx.ma.need.auditing.success";
+	public static final String WX_MA_SHOP_HAS_NO_APP="wx.ma.shop.has.no.app";
 	
 	/**
 	 * 门店管理
