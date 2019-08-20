f83d899c20d4afc8fbc9e6dfa638dbe786fb4346
lixinguo <lixinguo@wangdian.cn>
2019-08-20 10:29:44 +0000

    wechat 模板消息

--- jmp/mp-service/src/main/java/com/vpu/mp/service/shop/user/message/BusinessActivityTemplateService.java
+++ jmp/mp-service/src/main/java/com/vpu/mp/service/shop/user/message/BusinessActivityTemplateService.java
@@ -7,12 +7,11 @@ import com.vpu.mp.service.pojo.shop.user.message.MaTemplateConfig;
 @Service
 public class BusinessActivityTemplateService extends MaTemplateService {
 	
-	public static final Integer[] BUSINESS_IDS = {1, 2};
 	public static final MaTemplateConfig activityConfig= new MaTemplateConfig(
 			 "AT0654",
 		        "申请进度通知",
 		         "业务名称{{keyword1.DATA}}业务状态{{keyword2.DATA}}",
-		         BusinessActivityTemplateService.BUSINESS_IDS,
+		         new Integer[] {1,2},
 		        1
 			);
 		
