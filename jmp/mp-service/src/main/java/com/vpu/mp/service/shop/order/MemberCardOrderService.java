6ddb0ee1c54afbbefc6415fd958d2cbf2034b231
zhengbaole <zhengbaole@wangdian.cn>
2019-08-20 10:17:47 +0000

    虚拟订单表改名

--- jmp/mp-service/src/main/java/com/vpu/mp/service/shop/order/MemberCardOrderService.java
+++ jmp/mp-service/src/main/java/com/vpu/mp/service/shop/order/MemberCardOrderService.java
@@ -1,37 +1,31 @@
 package com.vpu.mp.service.shop.order;
 
-import static com.vpu.mp.db.shop.tables.CardOrder.CARD_ORDER;
-import static com.vpu.mp.db.shop.tables.MemberCard.MEMBER_CARD;
-import static com.vpu.mp.db.shop.tables.PaymentRecord.PAYMENT_RECORD;
-import static com.vpu.mp.db.shop.tables.RefundCardRecord.REFUND_CARD_RECORD;
-import static com.vpu.mp.db.shop.tables.User.USER;
-import static com.vpu.mp.db.shop.tables.UserCard.USER_CARD;
-import static com.vpu.mp.service.foundation.data.JsonResultMessage.INVALID_ACCOUNT_OR_SCORE;
-import static com.vpu.mp.service.foundation.data.JsonResultMessage.INVALID_MONEY_AMOUNT;
-import static com.vpu.mp.service.foundation.data.JsonResultMessage.INVALID_REFUND_AMOUNT;
-import static com.vpu.mp.service.foundation.data.JsonResultMessage.REFUND_ACCOUNT_LARGER_THAN_ACCOUNT_PAID;
-import static com.vpu.mp.service.foundation.data.JsonResultMessage.REFUND_SCORE_LARGER_THAN_SCORE_PAID;
-import static com.vpu.mp.service.pojo.shop.order.virtual.MemberCardParam.SUCCESS;
-import static java.lang.String.format;
-import static org.apache.commons.lang3.StringUtils.isNotEmpty;
-
-import java.math.BigDecimal;
-import java.sql.Timestamp;
-import java.util.Arrays;
-
-import org.jooq.Record10;
-import org.jooq.Record16;
-import org.jooq.SelectOnConditionStep;
-import org.springframework.stereotype.Service;
-
 import com.vpu.mp.db.shop.tables.records.PaymentRecordRecord;
 import com.vpu.mp.service.foundation.service.ShopBaseService;
 import com.vpu.mp.service.foundation.util.PageResult;
 import com.vpu.mp.service.pojo.shop.order.virtual.MemberCardParam;
 import com.vpu.mp.service.pojo.shop.order.virtual.MemberCardRefundParam;
 import com.vpu.mp.service.pojo.shop.order.virtual.MemberCardVo;
-
 import lombok.extern.slf4j.Slf4j;
+import org.jooq.Record10;
+import org.jooq.Record16;
+import org.jooq.SelectOnConditionStep;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.sql.Timestamp;
+import java.util.Arrays;
+
+import static com.vpu.mp.db.shop.tables.MemberCard.MEMBER_CARD;
+import static com.vpu.mp.db.shop.tables.PaymentRecord.PAYMENT_RECORD;
+import static com.vpu.mp.db.shop.tables.RefundCardRecord.REFUND_CARD_RECORD;
+import static com.vpu.mp.db.shop.tables.User.USER;
+import static com.vpu.mp.db.shop.tables.UserCard.USER_CARD;
+import static com.vpu.mp.db.shop.tables.VirtualOrder.VIRTUAL_ORDER;
+import static com.vpu.mp.service.foundation.data.JsonResultMessage.*;
+import static com.vpu.mp.service.pojo.shop.order.virtual.MemberCardParam.SUCCESS;
+import static java.lang.String.format;
+import static org.apache.commons.lang3.StringUtils.isNotEmpty;
 
 /**
  * 虚拟商品订单 - 会员卡
@@ -50,15 +44,15 @@ public class MemberCardOrderService extends ShopBaseService {
     public PageResult<MemberCardVo> getMemberCardOrderList(MemberCardParam param) {
         SelectOnConditionStep<Record16<Integer, String, Integer, Byte, Timestamp, BigDecimal, BigDecimal, BigDecimal,
             Timestamp, String, String, String, Byte, BigDecimal, Byte, String>> select =
-            db().select(CARD_ORDER.ORDER_ID, CARD_ORDER.ORDER_SN,
-                CARD_ORDER.VIRTUAL_GOODS_ID, CARD_ORDER.RETURN_FLAG, CARD_ORDER.PAY_TIME, CARD_ORDER.MONEY_PAID,
-                CARD_ORDER.USE_ACCOUNT, CARD_ORDER.USE_SCORE, CARD_ORDER.RETURN_TIME, USER.USERNAME, USER.MOBILE,
+            db().select(VIRTUAL_ORDER.ORDER_ID, VIRTUAL_ORDER.ORDER_SN,
+                VIRTUAL_ORDER.VIRTUAL_GOODS_ID, VIRTUAL_ORDER.RETURN_FLAG, VIRTUAL_ORDER.PAY_TIME, VIRTUAL_ORDER.MONEY_PAID,
+                VIRTUAL_ORDER.USE_ACCOUNT, VIRTUAL_ORDER.USE_SCORE, VIRTUAL_ORDER.RETURN_TIME, USER.USERNAME, USER.MOBILE,
                 MEMBER_CARD.CARD_NAME, MEMBER_CARD.CARD_TYPE, MEMBER_CARD.PAY_FEE, MEMBER_CARD.PAY_TYPE,
                 USER_CARD.CARD_NO)
-                .from(CARD_ORDER)
-                .leftJoin(MEMBER_CARD).on(MEMBER_CARD.ID.eq(CARD_ORDER.VIRTUAL_GOODS_ID))
-                .leftJoin(USER).on(CARD_ORDER.USER_ID.eq(USER.USER_ID))
-                .leftJoin(USER_CARD).on(CARD_ORDER.VIRTUAL_GOODS_ID.eq(USER_CARD.CARD_ID));
+                .from(VIRTUAL_ORDER)
+                .leftJoin(MEMBER_CARD).on(MEMBER_CARD.ID.eq(VIRTUAL_ORDER.VIRTUAL_GOODS_ID))
+                .leftJoin(USER).on(VIRTUAL_ORDER.USER_ID.eq(USER.USER_ID))
+                .leftJoin(USER_CARD).on(VIRTUAL_ORDER.VIRTUAL_GOODS_ID.eq(USER_CARD.CARD_ID));
         buildOptions(select, param);
         return getPageResult(select, param, MemberCardVo.class);
     }
@@ -76,9 +70,9 @@ public class MemberCardOrderService extends ShopBaseService {
         Timestamp startTime = param.getStartTime();
         Timestamp endTime = param.getEndTime();
         Boolean isRefund = param.getRefund();
-        select.where(CARD_ORDER.GOODS_TYPE.eq(GOODS_MEMBER_CARD));
+        select.where(VIRTUAL_ORDER.GOODS_TYPE.eq(GOODS_MEMBER_CARD));
         if (isNotEmpty(orderSn)) {
-            select.and(CARD_ORDER.ORDER_SN.like(format("%s%%", orderSn)));
+            select.and(VIRTUAL_ORDER.ORDER_SN.like(format("%s%%", orderSn)));
         }
         if (isNotEmpty(userInfo)) {
             select.and(USER.USERNAME.like(format("%s%%", userInfo)).or(USER.MOBILE.like(format("%s%%", userInfo))));
@@ -90,20 +84,20 @@ public class MemberCardOrderService extends ShopBaseService {
             select.and(MEMBER_CARD.CARD_TYPE.eq(cardType));
         }
         if (null != startTime) {
-            select.and(CARD_ORDER.PAY_TIME.ge(startTime));
+            select.and(VIRTUAL_ORDER.PAY_TIME.ge(startTime));
         }
         if (null != endTime) {
-            select.and(CARD_ORDER.PAY_TIME.le(endTime));
+            select.and(VIRTUAL_ORDER.PAY_TIME.le(endTime));
         }
         if (null != isRefund) {
             if (isRefund) {
-                select.and(CARD_ORDER.RETURN_FLAG.eq(SUCCESS).or(CARD_ORDER.RETURN_FLAG.eq(MemberCardParam.FAILED)));
+                select.and(VIRTUAL_ORDER.RETURN_FLAG.eq(SUCCESS).or(VIRTUAL_ORDER.RETURN_FLAG.eq(MemberCardParam.FAILED)));
             } else {
-                select.and(CARD_ORDER.RETURN_FLAG.eq(MemberCardParam.NOT_REFUND));
+                select.and(VIRTUAL_ORDER.RETURN_FLAG.eq(MemberCardParam.NOT_REFUND));
             }
         }
-        select.and(CARD_ORDER.DEL_FLAG.eq((byte) 0));
-        select.orderBy(CARD_ORDER.ORDER_ID.desc());
+        select.and(VIRTUAL_ORDER.DEL_FLAG.eq((byte) 0));
+        select.orderBy(VIRTUAL_ORDER.ORDER_ID.desc());
     }
 
     /**
@@ -116,15 +110,15 @@ public class MemberCardOrderService extends ShopBaseService {
         Integer orderId = param.getOrderId();
         Record10<String, BigDecimal, BigDecimal, BigDecimal, String, Integer, Byte, BigDecimal, BigDecimal, BigDecimal>
             payInfo = getPayInfo(orderId);
-        String payCode = payInfo.get(CARD_ORDER.PAY_CODE);
-        BigDecimal useAccount = payInfo.get(CARD_ORDER.USE_ACCOUNT);
-        BigDecimal useMoney = payInfo.get(CARD_ORDER.MONEY_PAID);
-        BigDecimal useScore = payInfo.get(CARD_ORDER.USE_SCORE);
-        String orderSn = payInfo.get(CARD_ORDER.ORDER_SN);
-        Integer userId = payInfo.get(CARD_ORDER.USER_ID);
-        BigDecimal returnAccount = payInfo.get(CARD_ORDER.RETURN_ACCOUNT);
-        BigDecimal returnMoney = payInfo.get(CARD_ORDER.RETURN_MONEY);
-        BigDecimal returnScore = payInfo.get(CARD_ORDER.RETURN_SCORE);
+        String payCode = payInfo.get(VIRTUAL_ORDER.PAY_CODE);
+        BigDecimal useAccount = payInfo.get(VIRTUAL_ORDER.USE_ACCOUNT);
+        BigDecimal useMoney = payInfo.get(VIRTUAL_ORDER.MONEY_PAID);
+        BigDecimal useScore = payInfo.get(VIRTUAL_ORDER.USE_SCORE);
+        String orderSn = payInfo.get(VIRTUAL_ORDER.ORDER_SN);
+        Integer userId = payInfo.get(VIRTUAL_ORDER.USER_ID);
+        BigDecimal returnAccount = payInfo.get(VIRTUAL_ORDER.RETURN_ACCOUNT);
+        BigDecimal returnMoney = payInfo.get(VIRTUAL_ORDER.RETURN_MONEY);
+        BigDecimal returnScore = payInfo.get(VIRTUAL_ORDER.RETURN_SCORE);
         BigDecimal availableReturnMoney = useMoney.subtract(returnMoney);
         BigDecimal availableReturnAccount = useAccount.subtract(returnAccount);
         BigDecimal availableReturnScore = useScore.subtract(returnScore);
@@ -183,9 +177,9 @@ public class MemberCardOrderService extends ShopBaseService {
             REFUND_CARD_RECORD.IS_SUCCESS)
             .values(Arrays.asList(orderSn, userId, money, account, score, 1)).execute();
         // 更新订单状态
-        db().update(CARD_ORDER).set(CARD_ORDER.RETURN_FLAG, SUCCESS).set(CARD_ORDER.RETURN_MONEY,
-            finalReturnMoney).set(CARD_ORDER.RETURN_ACCOUNT, finalReturnAccount)
-            .set(CARD_ORDER.RETURN_SCORE, finalReturnScore).execute();
+        db().update(VIRTUAL_ORDER).set(VIRTUAL_ORDER.RETURN_FLAG, SUCCESS).set(VIRTUAL_ORDER.RETURN_MONEY,
+            finalReturnMoney).set(VIRTUAL_ORDER.RETURN_ACCOUNT, finalReturnAccount)
+            .set(VIRTUAL_ORDER.RETURN_SCORE, finalReturnScore).execute();
     }
 
     /**
@@ -195,11 +189,11 @@ public class MemberCardOrderService extends ShopBaseService {
         BigDecimal> getPayInfo(Integer orderId) {
         Record10<String, BigDecimal, BigDecimal, BigDecimal, String, Integer, Byte, BigDecimal, BigDecimal
             , BigDecimal> order =
-            db().select(CARD_ORDER.PAY_CODE, CARD_ORDER.MONEY_PAID, CARD_ORDER.USE_SCORE, CARD_ORDER.USE_ACCOUNT,
-                CARD_ORDER.ORDER_SN, CARD_ORDER.USER_ID, CARD_ORDER.RETURN_FLAG, CARD_ORDER.RETURN_MONEY,
-                CARD_ORDER.RETURN_ACCOUNT, CARD_ORDER.RETURN_SCORE)
-                .from(CARD_ORDER)
-                .where(CARD_ORDER.ORDER_ID.eq(orderId)).fetchOne();
+            db().select(VIRTUAL_ORDER.PAY_CODE, VIRTUAL_ORDER.MONEY_PAID, VIRTUAL_ORDER.USE_SCORE,
+                VIRTUAL_ORDER.USE_ACCOUNT, VIRTUAL_ORDER.ORDER_SN, VIRTUAL_ORDER.USER_ID, VIRTUAL_ORDER.RETURN_FLAG,
+                VIRTUAL_ORDER.RETURN_MONEY, VIRTUAL_ORDER.RETURN_ACCOUNT, VIRTUAL_ORDER.RETURN_SCORE)
+                .from(VIRTUAL_ORDER)
+                .where(VIRTUAL_ORDER.ORDER_ID.eq(orderId)).fetchOne();
         if (null == order) {
             throw new IllegalArgumentException("Invalid orderId: " + orderId);
         }
