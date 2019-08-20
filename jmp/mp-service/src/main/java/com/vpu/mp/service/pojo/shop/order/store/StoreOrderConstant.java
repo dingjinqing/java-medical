27b8e0e62d68124a016d5d6c98335779b456c396
huangzhuangzhuang <huangzhuangzhuang@wangdian.cn>
2019-08-20 09:44:55 +0000

    创建常量

---
+++ jmp/mp-service/src/main/java/com/vpu/mp/service/pojo/shop/order/store/StoreOrderConstant.java
@@ -0,0 +1,11 @@
+package com.vpu.mp.service.pojo.shop.order.store;
+/**
+* @author 黄壮壮
+* @Date: 2019年8月16日
+* @Description: 门店订单表常量
+*/
+public class StoreOrderConstant {
+	/**订单状态:0未支付,1已支付,2已退款*/
+	public final static byte STORE_ORDER_UNPAID = 0;
+	public final static byte STORE_ORDER_PAID = 1;
+}
