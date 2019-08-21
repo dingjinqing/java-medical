c466ec49eaab2594ba123f9317b32ce620512bf3
zhaojianqiang <zhaojianqiang@wangdian.cn>
2019-08-21 09:46:19 +0000

    公众号查询类

---
+++ jmp/mp-service/src/main/java/com/vpu/mp/service/pojo/saas/shop/officeAccount/MpOfficeAccountListVo.java
@@ -0,0 +1,34 @@
+package com.vpu.mp.service.pojo.saas.shop.officeAccount;
+
+import java.sql.Timestamp;
+
+import lombok.Data;
+
+@Data
+public class MpOfficeAccountListVo {
+	private String appId;
+	private String nickName;
+	private String userName;
+	private String alias;
+	private String verifyTypeInfo;
+	private String headImg;
+	private String funcInfo;
+	private Integer isAuthOk;
+	private Timestamp lastAuthTime;
+	private Timestamp lastCancelAuthTime;
+	private Timestamp createTime;
+	private Integer openPay;
+	private Integer openCard;
+	private String authorizerInfo;
+	private String authorizationInfo;
+	private String payMchId;
+	private String payKey;
+	private String payCertContent;
+	private String payKeyContent;
+	private String principalName;
+	private Integer accountType;
+	private String bindOpenAppId;
+	private Integer sysId;
+	private String qrcodeUrl;
+
+}
