117e232cbfb82446f0813d5f4600d2297f20cca0
lixinguo <lixinguo@wangdian.cn>
2019-08-22 03:31:24 +0000

    公众号

---
+++ jmp/mp-service/src/main/java/com/vpu/mp/service/saas/shop/official/MpOfficialAccountService.java
@@ -0,0 +1,36 @@
+package com.vpu.mp.service.saas.shop.official;
+
+import org.springframework.stereotype.Service;
+
+import static com.vpu.mp.db.main.tables.MpOfficialAccount.MP_OFFICIAL_ACCOUNT;
+
+import com.vpu.mp.db.main.tables.records.MpOfficialAccountRecord;
+import com.vpu.mp.service.foundation.service.MainBaseService;
+
+import me.chanjar.weixin.mp.api.WxMpService;
+
+@Service
+public class MpOfficialAccountService extends MainBaseService {
+
+
+	/**
+	 * 得到公众号的服务
+	 * @param appId
+	 * @return
+	 */
+	public WxMpService getOfficialAccountClient(String appId) {
+		MpOfficialAccountRecord account = getOfficialAccountByAppid(appId);
+		assert(account !=null &&account.getIsAuthOk() == (byte)1);
+		return open.getWxOpenComponentService().getWxMpServiceByAppid(appId);
+	}
+
+	/**
+	 * 
+	 * @param appId
+	 * @return
+	 */
+	public MpOfficialAccountRecord  getOfficialAccountByAppid(String appId){
+		return db().fetchAny(MP_OFFICIAL_ACCOUNT,MP_OFFICIAL_ACCOUNT.APP_ID.eq(appId));
+	}
+	   
+}
---
+++ jmp/mp-service/src/main/java/com/vpu/mp/service/saas/shop/official/MpOfficialAccountUserService.java
@@ -0,0 +1,7 @@
+package com.vpu.mp.service.saas.shop.official;
+
+import com.vpu.mp.service.foundation.service.MainBaseService;
+
+public class MpOfficialAccountUserService extends MainBaseService {
+
+}
