64d70732b3cd888ef841e3346dc407b8d58b6c71
zhengbaole <zhengbaole@wangdian.cn>
2019-08-22 10:32:26 +0000

    export function -> export 箭头函数

--- web-vue/src/api/admin/marketManage/gift.js
+++ web-vue/src/api/admin/marketManage/gift.js
@@ -1,75 +1,59 @@
 import service from '@/util/request.js'
 
 // 赠品活动列表
-export const giftList = (data) => service({
+export const giftList = data => service({
   url: '/api/admin/market/gift/list',
   method: 'post',
   data
 })
 
 // 创建赠品活动
-export function addGift (data) {
-  return service({
-    url: '/api/admin/market/gift/add',
-    method: 'post',
-    data
-  })
-}
+export const addGift = data => service({
+  url: '/api/admin/market/gift/add',
+  method: 'post',
+  data
+})
 
 // 删除赠品活动
-export function deleteGift (id) {
-  return service({
-    url: `/api/admin/market/gift/delete/${id}`,
-    method: 'post'
-  })
-}
+export const deleteGift = id => service({
+  url: `/api/admin/market/gift/delete/${id}`,
+  method: 'post'
+})
 
 // 停用赠品活动
-export function disableGift (id) {
-  return service({
-    url: `/api/admin/market/gift/delete/${id}`,
-    method: 'post'
-  })
-}
+export const disableGift = id => service({
+  url: `/api/admin/market/gift/delete/${id}`,
+  method: 'post'
+})
 
 // 修改赠品活动优先级
-export function updateGiftLevel (data) {
-  return service({
-    url: '/api/admin/market/gift/level/update',
-    method: 'post',
-    data
-  })
-}
+export const updateGiftLevel = data => service({
+  url: '/api/admin/market/gift/level/update',
+  method: 'post',
+  data
+})
 
 // 修改赠品活动
-export function updateGift (data) {
-  return service({
-    url: '/api/admin/market/gift/update',
-    method: 'post',
-    data
-  })
-}
+export const updateGift = data => service({
+  url: '/api/admin/market/gift/update',
+  method: 'post',
+  data
+})
 
 // 查询赠品活动明细
-export function getGiftDetail (id) {
-  return service({
-    url: `/api/admin/market/gift/detail/${id}`,
-    method: 'post'
-  })
-}
+export const getGiftDetail = id => service({
+  url: `/api/admin/market/gift/detail/${id}`,
+  method: 'post'
+})
 
 // 查询会员卡列表
-export function getMemberCardList () {
-  return service({
-    url: `/api/admin/market/gift/member_card/list`,
-    method: 'post'
-  })
-}
+export const getMemberCardList = () => service({
+  url: `/api/admin/market/gift/member_card/list`,
+  method: 'post'
+})
 
 // 查询用户标签列表
-export function getTagList () {
-  return service({
-    url: `/api/admin/market/gift/tag/list`,
-    method: 'post'
-  })
-}
+export const getTagList = () => service({
+  url: `/api/admin/market/gift/tag/list`,
+  method: 'post'
+})
