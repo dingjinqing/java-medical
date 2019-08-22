bc9d8a74ccc6acec97b42a3193bf07f248ed8d58
huangzhuangzhuang <huangzhuangzhuang@wangdian.cn>
2019-08-22 11:16:49 +0000

    优化代码

--- jmp/mp-web/src/main/java/com/vpu/mp/controller/admin/AdminMemberController.java
+++ jmp/mp-web/src/main/java/com/vpu/mp/controller/admin/AdminMemberController.java
@@ -137,36 +137,37 @@ public class AdminMemberController extends AdminBaseController{
 	}
 	
 	
-	/**
-	 * 会员列表更新积分
-	 * @param param
-	 * @return
-	 */
-	@PostMapping(value="/score/update")
-	public JsonResult updateMemberScore(@RequestBody @Valid ScoreParam param) {
-		
-		// 判断修改后的值不能小于零、
-		Integer score = param.getScore();
-		//最小值
-		Integer scoreDis = param.getScoreDis();
-		if((score+scoreDis)<0) {
-			return fail(JsonResultMessage.MSG_MEMBER_SCORE_NOT_ENOUGH);
-		}
-		
-		Integer subAccountId = this.adminAuth.user().getSubAccountId();
-		
-		if(param.getUserId()!=null) {
-			Integer[] arrayUserId = param.getUserId();
-			int userNumber = arrayUserId.length;
-			Byte tradeType = 4;
-			Byte tradeFlow = 1;
-			for(int i=0;i<userNumber;i++) {
-				Integer userId = arrayUserId[i];
-				shop().member.score.updateMemberScore(param,subAccountId,userId,tradeType,tradeFlow);
-			}
-		}else {
-			return fail(JsonResultMessage.MSG_MEMBER_NOT_EXIST);
-		}
-		return success(param.toString());
-	}
+//---------------------------------正在做代码迁移--------------------------------------
+//	/**
+//	 * 会员列表更新积分
+//	 * @param param
+//	 * @return
+//	 */
+//	@PostMapping(value="/score/update")
+//	public JsonResult updateMemberScore(@RequestBody @Valid ScoreParam param) {
+//		
+//		// 判断修改后的值不能小于零
+//		Integer score = param.getScore();
+//		//最小值
+//		Integer scoreDis = param.getScoreDis();
+//		if((score+scoreDis)<0) {
+//			return fail(JsonResultMessage.MSG_MEMBER_SCORE_NOT_ENOUGH);
+//		}
+//		
+//		Integer subAccountId = this.adminAuth.user().getSubAccountId();
+//		
+//		if(param.getUserId()!=null) {
+//			Integer[] arrayUserId = param.getUserId();
+//			int userNumber = arrayUserId.length;
+//			Byte tradeType = 4;
+//			Byte tradeFlow = 1;
+//			for(int i=0;i<userNumber;i++) {
+//				Integer userId = arrayUserId[i];
+//				shop().member.score.updateMemberScore(param,subAccountId,userId,tradeType,tradeFlow);
+//			}
+//		}else {
+//			return fail(JsonResultMessage.MSG_MEMBER_NOT_EXIST);
+//		}
+//		return success(param.toString());
+//	}
 }
--- jmp/mp-web/src/main/java/com/vpu/mp/controller/admin/AdminMemberScoreController.java
+++ jmp/mp-web/src/main/java/com/vpu/mp/controller/admin/AdminMemberScoreController.java
@@ -7,7 +7,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.vpu.mp.service.foundation.data.JsonResult;
+import com.vpu.mp.service.foundation.data.JsonResultMessage;
 import com.vpu.mp.service.foundation.util.PageResult;
+import com.vpu.mp.service.pojo.shop.member.account.ScoreParam;
 import com.vpu.mp.service.pojo.shop.member.score.ScorePageListParam;
 import com.vpu.mp.service.pojo.shop.member.score.ScorePageListVo;
 
@@ -23,5 +25,38 @@ public class AdminMemberScoreController extends AdminBaseController {
 		logger().info("正在会员用户积分明细");
 		PageResult<ScorePageListVo> pageList = shop().member.score.getPageListOfScoreDetails(param);
 		return success(pageList);
+	}
+	
+	
+	/**
+	 * 更新积分
+	 * @param param
+	 * @return
+	 */
+	@PostMapping("/api/admin/member/score/update")
+	public JsonResult updateMemberScore(@RequestBody @Valid ScoreParam param) {
+		// 判断修改后的值不能小于零
+		Integer score = param.getScore();
+		//最小值
+		Integer scoreDis = param.getScoreDis();
+		if((score+scoreDis)<0) {
+			return fail(JsonResultMessage.MSG_MEMBER_SCORE_NOT_ENOUGH);
+		}
+		
+		Integer subAccountId = this.adminAuth.user().getSubAccountId();
+		
+		if(param.getUserId()!=null) {
+			Integer[] arrayUserId = param.getUserId();
+			int userNumber = arrayUserId.length;
+			Byte tradeType = 4;
+			Byte tradeFlow = 1;
+			for(int i=0;i<userNumber;i++) {
+				Integer userId = arrayUserId[i];
+				shop().member.score.updateMemberScore(param,subAccountId,userId,tradeType,tradeFlow);
+			}
+		}else {
+			return fail(JsonResultMessage.MSG_MEMBER_NOT_EXIST);
+		}
+		return success(param.toString());
 	}
 }
