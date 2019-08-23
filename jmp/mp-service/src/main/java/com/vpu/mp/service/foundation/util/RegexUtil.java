0c36e0a1bed50e770473e844e0d51098bcd35f6f
luguangyao <luguangyao@wangdian.cn>
2019-08-23 03:09:42 +0000

    fix:操作记录取system账号的用户名，不取昵称

---
+++ jmp/mp-service/src/main/java/com/vpu/mp/service/foundation/util/RegexUtil.java
@@ -0,0 +1,38 @@
+package com.vpu.mp.service.foundation.util;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class RegexUtil {
+
+    private static final List<String> str = Arrays.asList(
+       new String[] { "\\", "$", "(", ")", "*", "+", ".", "[", "]", "?", "^", "{", "}", "|" });
+
+
+
+    public static List<String> getSubStrList(String first,String lastStr,String content){
+        List<String> result = new ArrayList<>();
+        for(String key:str){
+            if(first.contains(key)){
+                first = first.replace(key, "\\" + key);
+            }
+            if( lastStr.contains(key) ){
+                lastStr = lastStr.replace(key, "\\" + key);
+            }
+        }
+        StringBuilder pStr = new StringBuilder("(?<=")
+            .append(first)
+            .append(")\\.*?(?=")
+            .append(lastStr)
+            .append(")");
+        Pattern p = Pattern.compile(pStr.toString());
+        Matcher m = p.matcher(content);
+        while (m.find()){
+            result.add(m.group());
+        }
+        return result;
+    }
+}
