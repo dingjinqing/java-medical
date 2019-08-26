2ee6e9348f8122010e8fba96f73eefcf5576900f
kongdecheng <kongdecheng@wangdian.cn>
2019-08-26 05:58:29 +0000

    多人拼团

--- web-vue/src/view/admin/index/leftNavComponents/first_market_manage/spellGroup/groupList.vue
+++ web-vue/src/view/admin/index/leftNavComponents/first_market_manage/spellGroup/groupList.vue
@@ -1,208 +1,267 @@
 <template>
-  <div class="content">
-    <div class="main">
-      <el-tabs
-        v-model="tabSwitch"
-        @tab-click="handleClick"
-      >
-        <el-tab-pane
-          v-for="(item) in tabInfo"
-          :key="item.name"
-          :label="item.title"
-          :name="item.name"
-        >
-          <el-button type="primary">添加好友助力活动</el-button>
-        </el-tab-pane>
-      </el-tabs>
+    <div class="content">
+        <div class="main">
+            <el-tabs
+                    v-model="tabSwitch"
+                    @tab-click="handleClick"
+            >
+                <el-tab-pane
+                        v-for="(item) in tabInfo"
+                        :key="item.name"
+                        :label="item.title"
+                        :name="item.name"
+                >
+                    <el-button type="primary">添加好友助力活动</el-button>
+                </el-tab-pane>
+            </el-tabs>
+        </div>
+        <div class="table_list">
+            <el-table
+                    class="version-manage-table"
+                    header-row-class-name="tableClss"
+                    :data="tableData"
+                    border
+                    style="width: 100%"
+            >
+                <el-table-column
+                        prop="name"
+                        label="活动名称"
+                        align="center"
+                >
+                </el-table-column>
+
+                <el-table-column
+                        prop="activityType"
+                        label="活动类型"
+                        align="center"
+                >
+                </el-table-column>
+
+                <el-table-column
+                        prop="goodsName"
+                        label="商品名称"
+                        align="center"
+                >
+                </el-table-column>
+
+                <el-table-column
+                        prop="vaildDate"
+                        label="有效期"
+                        align="center"
+                >
+                </el-table-column>
+
+                <el-table-column
+                        prop="status"
+                        label="活动状态"
+                        align="center"
+                >
+                </el-table-column>
+
+                <el-table-column
+                        prop="limitAmount"
+                        label="成团人数"
+                        align="center"
+                >
+                </el-table-column>
+
+                <el-table-column
+                        prop="groupOrderNum"
+                        label="成团订单数"
+                        align="center"
+                >
+                </el-table-column>
+
+                <el-table-column
+                        prop=""
+                        label="操作"
+                        align="center"
+                >
+                    <template slot-scope="scope">
+                        <div class="opt">
+                            <span>编辑</span>
+                            <span>分享</span>
+                            <span @click="changeStatus(scope.row.id)" v-if="scope.row.status==0"> 停用</span>
+                            <span @click="changeStatus(scope.row.id)" v-if="scope.row.status==1"> 启用</span>
+                            <span>领取明细</span>
+                            <span @click="deleteGroupBuy(scope.row.id)">删除</span>
+                        </div>
+                    </template>
+                </el-table-column>
+            </el-table>
+        </div>
     </div>
-    <div class="table_list">
-      <el-table
-        class="version-manage-table"
-        header-row-class-name="tableClss"
-        border
-        style="width: 100%"
-      >
-        <el-table-column
-          prop="name"
-          label="活动名称"
-          align="center"
-        >
-        </el-table-column>
-
-        <el-table-column
-          prop="activityType"
-          label="活动类型"
-          align="center"
-        >
-        </el-table-column>
-
-        <el-table-column
-          prop="goodsName"
-          label="商品名称"
-          align="center"
-        >
-        </el-table-column>
-
-        <el-table-column
-          prop=""
-          label="有效期"
-          align="center"
-        >
-        </el-table-column>
-
-        <el-table-column
-          prop="status"
-          label="活动状态"
-          align="center"
-        >
-        </el-table-column>
-
-        <el-table-column
-          prop=""
-          label="成团人数"
-          align="center"
-        >
-        </el-table-column>
-
-        <el-table-column
-          prop=""
-          label="成团订单数"
-          align="center"
-        >
-        </el-table-column>
-
-        <el-table-column
-          prop=""
-          label="操作"
-          align="center"
-        >
-          <template>
-            <div class="opt">
-              <span>编辑</span>
-              <span>分享</span>
-              <span>停用</span>
-              <span>领取明细</span>
-              <span>删除</span>
-            </div>
-          </template>
-        </el-table-column>
-
-      </el-table>
-    </div>
-  </div>
 
 </template>
 <script>
-import { groupBuyList } from '@/api/admin/marketManage/spellGroup.js'
+    import {
+        groupBuyList,
+        addGroupBuyActivity,
+        updateGroupBuy,
+        changeStatusActivity,
+        deleteGroupBuyActivity
+    } from '@/api/admin/marketManage/spellGroup.js'
 
-export default {
-  data () {
-    return {
-      tabSwitch: '2',
-      tabInfo: [{
-        title: '全部砍价活动',
-        name: '1'
-      }, {
-        title: '进行中',
-        name: '2'
-      }, {
-        title: '未开始',
-        name: '3'
-      }, {
-        title: '已过期',
-        name: '4'
-      }, {
-        title: '已停用',
-        name: '5'
-      }],
-      tabIndex: 3,
-      currentPage: 1
+    export default {
+        data() {
+            return {
+                tableData: undefined,
+                tabSwitch: '2',
+                tabInfo: [{
+                    title: '全部砍价活动',
+                    name: '1'
+                }, {
+                    title: '进行中',
+                    name: '2'
+                }, {
+                    title: '未开始',
+                    name: '3'
+                }, {
+                    title: '已过期',
+                    name: '4'
+                }, {
+                    title: '已停用',
+                    name: '5'
+                }],
+                tabIndex: 3,
+                currentPage: 1,
+                pageRows: 20,
+                tabRequestData: {
+                    type: this.tabSwitch,
+                    currentPage: this.currentPage,
+                    pageRows: 20.
+                }
+            }
+        },
+        mounted() {
+        },
+        methods: {
+            handleClick(tab) {
+                console.log(this.tabRequestData)
+                this.tabRequestData.type =parseInt(tab.index) + 1;
+                this.tabRequestData.currentPage =this.currentPage;
+                this.tabRequestData.pageRows =this.pageRows;
+                groupBuyList(this.tabRequestData).then(res => {
+                    console.log(res.content.dataList)
+                    this.handleData(res.content.dataList)
+
+                }).catch(() => {
+                    this.$message.error('保存失败')
+                })
+            },
+            //表格数据处理
+            handleData(data) {
+                data.map((item, index) => {
+                    item.vaildDate = `${item.startTime}至${item.endTime}`
+                })
+                this.tableData = data;
+            },
+            changeStatus(id) {
+                let obj = {
+                    "id": id
+                }
+                changeStatusActivity(obj).then(res => {
+                    console.log("change=>res = " + res);
+                   this.refreshTable();
+                })
+            },
+            deleteGroupBuy(data) {
+                let obj = {
+                    "id": data
+                }
+                deleteGroupBuyActivity(obj).then(res => {
+                    console.log(res);
+                })
+            },
+            refreshTable(){
+                groupBuyList(this.tabRequestData).then(res => {
+                    this.handleData(res.content.dataList)
+                }).catch(() => {
+                    this.$message.error('刷新失败')
+                })
+            }
+            ,
+            addActivity() {
+                console.log(111)
+            }
+        }
     }
-  },
-  mounted () {
-  },
-  methods: {
-    handleClick (tab) {
-      console.log(tab)
-      let obj = {
-        'type': parseInt(tab.index) + 1,
-        'currentPage': 1,
-        'pageRows': 1
-      }
-      groupBuyList(obj).then(res => {
-        console.log(res)
-      }).catch(() => {
-        this.$message.error('保存失败')
-      })
-    },
-    addActivity () {
-      console.log(111)
-    }
-  }
-}
 </script>
 <style lang="scss" scoped>
-.content {
-  padding: 10px;
-  min-width: 100%;
-  font-size: 14px;
-  height: 100%;
-  .main {
-    position: relative;
-    background-color: #fff;
-    padding: 10px 20px 10px 20px;
-  }
-}
-.p_top_right {
-  display: flex;
-  /deep/ .el-button {
-    padding: none;
-    height: 32px;
-  }
-  span {
-    white-space: nowrap;
-    height: 32px;
-    line-height: 32px;
-    margin-right: 10px;
-  }
-  .topRightDiv {
-    &:nth-of-type(2) {
-      margin: 0 10px 0 30px;
+    .content {
+        padding: 10px;
+        min-width: 100%;
+        font-size: 14px;
+        height: 100%;
+
+        .main {
+            position: relative;
+            background-color: #fff;
+            padding: 10px 20px 10px 20px;
+        }
     }
-  }
-}
-/deep/ .tableClss th {
-  background-color: #f5f5f5;
-  border: none;
-  height: 36px;
-  font-weight: bold;
-  color: #000;
-  padding: 8px 10px;
-}
-.table_list {
-  position: relative;
-  margin-top: 10px;
-  background-color: #fff;
-  padding: 10px 20px 10px 20px;
-}
-.opt {
-  text-align: left;
-  color: #5a8bff;
-  span {
-    cursor: pointer;
-  }
-}
-.balanceDialo .el-dialog__body {
-  padding-bottom: 0 !important;
-}
-.balanceDialo .el-dialog__footer {
-  border-top: 1px solid #eee;
-}
-.setUpDialog .el-dialog__body {
-  padding-top: 10px !important;
-}
-.add_coupon {
-  float: left;
-  margin-left: 65%;
-}
+
+    .p_top_right {
+        display: flex;
+
+        /deep/ .el-button {
+            padding: none;
+            height: 32px;
+        }
+
+        span {
+            white-space: nowrap;
+            height: 32px;
+            line-height: 32px;
+            margin-right: 10px;
+        }
+
+        .topRightDiv {
+            &:nth-of-type(2) {
+                margin: 0 10px 0 30px;
+            }
+        }
+    }
+
+    /deep/ .tableClss th {
+        background-color: #f5f5f5;
+        border: none;
+        height: 36px;
+        font-weight: bold;
+        color: #000;
+        padding: 8px 10px;
+    }
+
+    .table_list {
+        position: relative;
+        margin-top: 10px;
+        background-color: #fff;
+        padding: 10px 20px 10px 20px;
+    }
+
+    .opt {
+        text-align: left;
+        color: #5a8bff;
+
+        span {
+            cursor: pointer;
+        }
+    }
+
+    .balanceDialo .el-dialog__body {
+        padding-bottom: 0 !important;
+    }
+
+    .balanceDialo .el-dialog__footer {
+        border-top: 1px solid #eee;
+    }
+
+    .setUpDialog .el-dialog__body {
+        padding-top: 10px !important;
+    }
+
+    .add_coupon {
+        float: left;
+        margin-left: 65%;
+    }
 </style>
