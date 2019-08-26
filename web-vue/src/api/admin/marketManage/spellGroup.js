59aebdcc033a56d6bdc90e62720851c61c95a8de
kongdecheng <kongdecheng@wangdian.cn>
2019-08-26 06:28:41 +0000

    多人拼团

--- web-vue/src/api/admin/marketManage/spellGroup.js
+++ web-vue/src/api/admin/marketManage/spellGroup.js
@@ -1,10 +1,44 @@
 import service from '@/util/request.js'
 
 // 查询团购列表
-export function groupBuyList (data) {
-  return service({
-    url: '/api/admin/market/groupbuy/list',
-    method: 'post',
-    data: data
-  })
+export function groupBuyList(data) {
+    return service({
+        url: '/api/admin/market/groupbuy/list',
+        method: 'post',
+        data: data
+    })
+}
+
+//添加团购活动
+export function addGroupBuyActivity(data) {
+    return service({
+        url: '/api/admin/market/groupbuy/add',
+        method: 'post',
+        data: data
+    })
+}
+
+//跟新团购活动
+export function updateGroupBuy(data) {
+    return service({
+        url: '/api/admin/market/groupbuy/update',
+        method: 'post',
+        data: data
+    })
+}
+// 改变活动状态
+export function changeStatusActivity(data) {
+    return service({
+        url: '/api/admin/market/groupbuy/change/status',
+        method: 'post',
+        data: data
+    })
+}
+//删除活动
+export function deleteGroupBuyActivity(data) {
+    return service({
+        url:'/api/admin/market/groupbuy/delete',
+        method:'post',
+        data:data
+    })
 }
