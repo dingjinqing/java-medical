c137addddf5cfd961f388c24f6ad7c8eae615fd5
suntengfei <suntengfei@wangdian.cn>
2019-08-28 03:30:51 +0000

    线上调试

--- web-vue/src/api/system/programManage.js
+++ web-vue/src/api/system/programManage.js
@@ -1,8 +1,8 @@
-import request from '@/util/request'
+import service from '@/util/request'
 
 //  同步微信开放平台小程序模板
 export function synchronizationRequest () {
-  return request({
+  return service({
     url: '/api/system/mp/version/syn',
     method: 'post'
   })
@@ -10,7 +10,7 @@ export function synchronizationRequest () {
 
 //  小程序版本管理 分页
 export function smallProManRequest (data) {
-  return request({
+  return service({
     url: '/api/system/mp/version/list',
     method: 'post',
     data: data
@@ -18,7 +18,7 @@ export function smallProManRequest (data) {
 }
 //  设置为当前使用版本接口
 export function setNowVersionRequest (data) {
-  return request({
+  return service({
     url: `/api/system/mp/version/set/${data}`,
     method: 'get'
   })
@@ -26,7 +26,7 @@ export function setNowVersionRequest (data) {
 
 // 当前包版本设置
 export function setCurrentVersionRequest (data) {
-  return request({
+  return service({
     url: '/api/system/mp/package/version',
     method: 'post',
     data: data
@@ -35,7 +35,7 @@ export function setCurrentVersionRequest (data) {
 
 // 小程序授权列表
 export function authListRequest (data) {
-  return request({
+  return service({
     url: '/api/system/mp/auth/list',
     method: 'post',
     data: data
@@ -44,7 +44,7 @@ export function authListRequest (data) {
 
 // 获取小程序版本下拉列表
 export function SpinnerListRequest () {
-  return request({
+  return service({
     url: '/api/system/mp/version/user/version/list',
     method: 'get'
   })
@@ -52,7 +52,7 @@ export function SpinnerListRequest () {
 
 // 小程序版本统计
 export function versionStatRequest (data) {
-  return request({
+  return service({
     url: '/api/system/mp/version/stat',
     method: 'post',
     data: data
@@ -61,7 +61,7 @@ export function versionStatRequest (data) {
 
 // 小程序操作日志
 export function operateLogRequest (data) {
-  return request({
+  return service({
     url: '/api/system/mp/operate/log/list',
     method: 'post',
     data: data
@@ -70,7 +70,7 @@ export function operateLogRequest (data) {
 
 // 小程序授权信息
 export function authInformationRequest (data) {
-  return request({
+  return service({
     url: `/api/system/mp/get/${data}`,
     method: 'get'
   })
@@ -78,7 +78,7 @@ export function authInformationRequest (data) {
 
 // 小程序授权信息页面设置弹窗统一设置接口
 export function publishRequest (data) {
-  return request({
+  return service({
     url: '/api/system/mp/publish',
     method: 'post',
     data: data
@@ -87,7 +87,7 @@ export function publishRequest (data) {
 
 // 当前模板id查询
 export function nowIdRequest (data) {
-  return request({
+  return service({
     url: `/api/system/mp/info/useTemplateId/${data}`,
     method: 'get'
   })
@@ -95,7 +95,7 @@ export function nowIdRequest (data) {
 
 // 小程序授权信息页面按钮统一处理接口
 export function publishSetRequest (data) {
-  return request({
+  return service({
     url: '/api/system/mp/publish',
     method: 'post',
     data: data
