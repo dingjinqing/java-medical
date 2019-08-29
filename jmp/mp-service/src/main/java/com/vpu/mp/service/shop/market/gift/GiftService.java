5f09ed5b3022d679365a8ea63aeada89863472aa
zhengbaole <zhengbaole@wangdian.cn>
2019-08-29 03:58:58 +0000

    修改赠品活动列表查询赠送次数方法

--- jmp/mp-service/src/main/java/com/vpu/mp/service/shop/market/gift/GiftService.java
+++ jmp/mp-service/src/main/java/com/vpu/mp/service/shop/market/gift/GiftService.java
@@ -338,10 +338,10 @@ public class GiftService extends ShopBaseService {
         return db().select(TABLE.ID, TABLE.NAME, TABLE.START_TIME, TABLE.END_TIME,
             TABLE.LEVEL, TABLE.STATUS, DSL.count(ORDER_GOODS.REC_ID).as("giftTimes"))
             .from(TABLE)
-            .leftJoin(ORDER_GOODS)
-            .on(ORDER_GOODS.ACTIVITY_ID.eq(TABLE.ID))
-            .where(TABLE.DEL_FLAG.eq((byte) 0).and(ORDER_GOODS.IS_GIFT.eq(1)
-                .and(ORDER_GOODS.ACTIVITY_TYPE.eq(OrderConstant.GOODS_TYPE_GIFT))));
+            .leftJoin(ORDER_GOODS).on(ORDER_GOODS.IS_GIFT.eq(1)
+                .and(ORDER_GOODS.ACTIVITY_ID.eq(TABLE.ID)
+                    .and(ORDER_GOODS.ACTIVITY_TYPE.eq(OrderConstant.GOODS_TYPE_GIFT))))
+            .where(TABLE.DEL_FLAG.eq((byte) 0));
     }
 
     /**
