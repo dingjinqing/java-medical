1cb3afde62e02f595dcef1c67b9977c969d36c78
huangronggang <huangronggang@wangdian.cn>
2019-08-29 06:03:51 +0000

    打包一口价入口

---
+++ web-vue/src/view/admin/index/leftNavComponents/first_market_manage/packagePrice/packagePriceList.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+<script>
+import wrapper from '@/components/admin/wrapper/wrapper'
+import statusTab from '@/components/admin/status/statusTab'
+export default {
+  components: {
+    wrapper,
+    statusTab
+  }
+}
+</script>
