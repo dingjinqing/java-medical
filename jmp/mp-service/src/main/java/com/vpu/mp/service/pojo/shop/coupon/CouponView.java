2f05948b26811de7dd90d2be9dcbaf849737ccea
wangbingbing <wangbingbing@wangdian.cn>
2019-08-30 10:19:36 +0000

    优惠券弹窗接口字段补充

--- jmp/mp-service/src/main/java/com/vpu/mp/service/pojo/shop/coupon/CouponView.java
+++ jmp/mp-service/src/main/java/com/vpu/mp/service/pojo/shop/coupon/CouponView.java
@@ -11,39 +11,36 @@ import java.sql.Timestamp;
  **/
 @Data
 public class CouponView {
-
+    /** 优惠券id */
     private Integer id;
-
+    /** 优惠券类型，voucher指定金额券，discount折扣券 */
+    private String actCode;
     /** 优惠券名称 */
     private String actName;
-
     /** 面额 */
     private BigDecimal denomination;
-
-    /** 是否有最低使用限制 0没有，1有 */
-    private Byte useConsumeRestrict;
-
-    /** 最低使用限制，满多少可用 */
+    /** 使用限制 0：无限制 1：有限制 */
+    private Integer useConsumeRestrict;
+    /** 满多少可用 */
     private BigDecimal leastConsume;
-
     /** 剩余数量 */
     private Integer surplus;
-
     /** 优惠券有效期类型标记，1领取后开始指定时间段内有效，0固定时间段有效 */
     private Byte validityType;
-
-    /** 0类型有效期开始时间 */
-    private Timestamp startTime;
-
-    /** 0类型有效期结束时间 */
-    private Timestamp endTime;
-
-    /** 1类型有效天数 */
+    /** 优惠券优惠券有效天数 */
     private Integer validity;
-
-    /** 1类型有效小时数 */
+    /** 优惠券有效小时数 */
     private Integer validityHour;
-
-    /** 1类型有效分钟数 */
+    /** 优惠券有效分钟数 */
     private Integer validityMinute;
+    /** 优惠券有效期开始时间 */
+    private Timestamp startTime;
+    /** 优惠券有效期结束时间 */
+    private Timestamp endTime;
+    /** 指定商品可用的商品ID串，为空时为全部商品可用 */
+    private String recommendGoodsId;
+    /** 指定平台分类可用的平台分类ID串，为空时为全部平台分类可用 */
+    private String recommendCatId;
+    /** 指定商家分类可用的商家分类ID串，为空时为全部商品商家分类可用 */
+    private String recommendSortId;
 }
