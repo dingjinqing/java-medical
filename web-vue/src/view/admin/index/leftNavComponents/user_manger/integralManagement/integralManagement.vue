c0cb0e069e3fe050b978416ae32d5fea642e5b80
suntengfei <suntengfei@wangdian.cn>
2019-08-30 10:49:01 +0000

    积分管理页面

--- web-vue/src/view/admin/index/leftNavComponents/user_manger/integralManagement/integralManagement.vue
+++ web-vue/src/view/admin/index/leftNavComponents/user_manger/integralManagement/integralManagement.vue
@@ -17,6 +17,162 @@
         >前端展示设置</el-tab-pane>
       </el-tabs>
     </div>
+    <!--积分使用规则模块-->
+    <div class="integralManagementMain integralUsage">
+      <div class="title">
+        <span></span>
+        积分使用规则
+      </div>
+      <div class="intLimit">
+        <div class="intLimitTop">
+          <span>积分支付限制</span>
+          <div>
+            <el-radio
+              v-model="limitRadio"
+              label="1"
+            >不限制</el-radio>
+            <el-radio
+              v-model="limitRadio"
+              label="2"
+            >自定义</el-radio>
+            <div
+              v-if="limitRadio==='2'"
+              style="margin-top:10px"
+            >
+              <span>每单支付的积分数量少于
+                <el-input-number
+                  size="small"
+                  v-model="limitIntegralNum"
+                  controls-position="right"
+                  :min="1"
+                  :max="100000"
+                ></el-input-number>积分，不可使用积分支付
+                <span style="margit-left:20px;color:#999">请填写大于100积分且为100积分的整数倍的数值</span>
+              </span>
+            </div>
+          </div>
+        </div>
+        <div class="intLimitTop intLimitFooter">
+          <span>积分抵扣比例</span>
+          <div>
+            <span>用户可使用积分抵扣订单折后金额的
+              <el-input-number
+                size="small"
+                v-model="limitIntegralNum"
+                controls-position="right"
+                :min="1"
+                :max="100000"
+              ></el-input-number>%，不可使用积分支付
+              <span style="margit-left:20px;color:#999">不填默认积分折扣订单折后金额的50%</span>
+            </span>
+          </div>
+        </div>
+      </div>
+      <div class="title">
+        <span></span>
+        积分获取规则
+        <i style="color:#999">和会员卡获取积分相互排斥，以会员卡优先</i>
+      </div>
+      <!--积分获取规则下方代码块-->
+      <div class="intContent">
+        <span class="intTitle">购物送积分</span>
+        <el-switch
+          v-model="shopValue"
+          active-color="#13ce66"
+          inactive-color="#ff4949"
+        >
+        </el-switch>
+        <span style="display:inline-block;margin:0 20px">{{shopValue?'已开启':'已关闭'}}</span>
+        <span style="color:#999">开关开启，则订单完成后会给用户按照积分获取规则赠送积分，关闭则不赠送</span>
+      </div>
+      <div class="intContent">
+        <span class="intTitle">门店买单送积分</span>
+        <el-switch
+          v-model="doorValue"
+          active-color="#13ce66"
+          inactive-color="#ff4949"
+        >
+        </el-switch>
+        <span style="display:inline-block;margin:0 20px">{{doorValue?'已开启':'已关闭'}}</span>
+        <span style="color:#999">开关开启，则会在用户门店买单后按照购物送积分规则给用户赠送积分，关闭则不赠送</span>
+      </div>
+      <div class="intContent">
+        <div class="loginDiv">
+          <span
+            class="intTitle"
+            style="margin-right:15px"
+          >登陆送积分</span>
+          <el-switch
+            v-model="loginValue"
+            active-color="#13ce66"
+            inactive-color="#ff4949"
+          >
+          </el-switch>
+          <div>
+            <span style="display:inline-block;margin:0 20px">{{loginValue?'已开启':'已关闭'}}</span>
+            <span style="color:#999">开关开启，则登陆后会给用户按照积分获取规则赠送积分，关闭则不赠送</span>
+            <div
+              v-if="loginValue"
+              class="hiddenLoginDiv"
+            >
+              <span>登陆送</span>
+              <div>
+                <el-input-number
+                  size="small"
+                  v-model="loginIntegralNum"
+                  controls-position="right"
+                  :min="1"
+                  :max="100000"
+                ></el-input-number>积分
+                <span style="color:#f66">每日仅首次登陆赠送积分</span>
+              </div>
+
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="intContent signDiv">
+        <span
+          class="intTitle"
+          style="margin-right:15px"
+        >签到送积分</span>
+        <el-switch
+          v-model="signInValue"
+          active-color="#13ce66"
+          inactive-color="#ff4949"
+        >
+        </el-switch>
+        <div class="signHiddenDiv">
+          <span style="display:inline-block;margin:0 20px">{{signInValue?'已开启':'已关闭'}}</span>
+          <span style="color:#999;margin-right:10px;display:inline-block">开关开启，则系统开启签到14天</span><i style="cursor:pointer;color:#5a8bff">查看签到会员</i>
+          <div
+            v-if="true"
+            class="hiddenLoginDiv"
+          >
+            <div
+              v-for="(item,index) in signData"
+              :key="index"
+              style="margin-bottom:5px"
+            >
+              <span>连续签到{{item}}天，送</span>
+              <el-input
+                v-model="signInput"
+                size="small"
+              ></el-input>
+              <span>积分</span>
+              <span
+                @click="handleToAdd()"
+                v-if="index===0"
+                style="cursor:pointer"
+              >
+                <img :src="$imageHost+'/image/admin/sign_jia.png'">
+              </span>
+            </div>
+
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 <script>
@@ -26,12 +182,23 @@ export default {
   },
   data () {
     return {
-      activeName: 'first'
+      activeName: 'first',
+      limitRadio: '1',
+      limitIntegralNum: '',
+      shopValue: true,
+      doorValue: true,
+      loginValue: true,
+      signInValue: true,
+      loginIntegralNum: '',
+      signData: 1
     }
   },
   methods: {
     handleClick (tab, event) {
       console.log(tab, event)
+    },
+    handleToAdd () {
+      this.signData++
     }
   }
 }
@@ -51,6 +218,91 @@ export default {
     overflow: hidden;
     overflow-y: auto;
     padding: 15px 25px;
+  }
+  .integralUsage {
+    margin-top: 10px;
+    .title {
+      span {
+        display: inline-block;
+        margin-left: 20px;
+        border-left: 2px solid #5a8bff;
+        height: 14px;
+        width: 8px;
+        margin-bottom: -1px;
+      }
+      height: 40px;
+      line-height: 40px;
+      background: #eef1f6;
+      font-size: 14px;
+      i {
+        display: inline-block;
+        margin-left: 30px;
+      }
+    }
+    .intLimit {
+      .intLimitTop {
+        margin: 20px 0;
+        display: flex;
+        /deep/ .el-input-number {
+          margin: 0 10px;
+        }
+        span {
+          margin-right: 10px;
+        }
+      }
+      .intLimitFooter {
+        span {
+          height: 32px;
+          line-height: 32px;
+        }
+      }
+    }
+    .intContent {
+      margin-top: 20px;
+      .intTitle {
+        display: inline-block;
+        width: 100px;
+        text-align: right;
+        margin-right: 10px;
+      }
+      .loginDiv {
+        display: flex;
+        .hiddenLoginDiv {
+          padding-left: 20px;
+          margin: 20px 0;
+          display: flex;
+          span {
+            height: 32px;
+            line-height: 32px;
+          }
+          div {
+            margin: 0 10px;
+          }
+        }
+      }
+    }
+    .signDiv {
+      display: flex;
+      .hiddenLoginDiv {
+        padding-left: 20px;
+        margin: 20px 0;
+        div {
+          /deep/ .el-input {
+            width: 70px;
+            margin: 0 20px;
+          }
+          display: flex;
+          span {
+            white-space: nowrap;
+            display: flex;
+            align-items: center;
+          }
+          span:nth-of-type(2) {
+            margin-right: 20px;
+          }
+        }
+      }
+    }
   }
 }
 </style>
--- web-vue/src/view/admin/index/leftNavComponents/user_manger/integralManagement/integralRule.vue
+++ web-vue/src/view/admin/index/leftNavComponents/user_manger/integralManagement/integralRule.vue
@@ -7,17 +7,351 @@
       <span></span>
       积分通用规则
     </div>
+    <div class="content">
+      <span>积分有效期</span>
+      <div class="radioDiv">
+        <div>
+          <el-radio
+            v-model="radio"
+            label="1"
+          >永久有效</el-radio>
+        </div>
+        <div>
+          <el-radio
+            v-model="radio"
+            label="2"
+          >从获得开始至</el-radio>
+          <div>
+            <el-select
+              v-model="yearValue"
+              placeholder="请选择"
+              size="small"
+            >
+              <el-option
+                v-for="item in yearOptions"
+                :key="item.value"
+                :label="item.label"
+                :value="item.value"
+              >
+              </el-option>
+            </el-select>
+          </div>
+          <div>
+            <el-select
+              v-model="mounthValue"
+              placeholder="请选择"
+              size="small"
+            >
+              <el-option
+                v-for="item in mounthOptions"
+                :key="item.value"
+                :label="item.label"
+                :value="item.value"
+              >
+              </el-option>
+            </el-select>
+            月
+          </div>
+          <div>
+            <el-select
+              v-model="dayValue"
+              placeholder="请选择"
+              size="small"
+              :disabled="mounthValue==='0'"
+            >
+              <el-option
+                v-for="item in dayOptions"
+                :key="item.value"
+                :label="item.label"
+                :value="item.value"
+              >
+              </el-option>
+            </el-select>
+            日
+          </div>
+        </div>
+        <div style="color:#FF0000">
+          例如，设置未"明年的01月01日",即是指用户今年获得的积分将在明年1月1日24:00:00失效
+        </div>
+        <div class="integralNumDiv">
+          <el-radio
+            v-model="radio"
+            label="3"
+          >从获得积分当天起</el-radio>
+          <el-input-number
+            size="small"
+            v-model="integralNum"
+            controls-position="right"
+            :min="1"
+            :max="100"
+          ></el-input-number>
+          <div>
+            <el-select
+              v-model="integralDateValue"
+              placeholder="请选择"
+              size="small"
+            >
+              <el-option
+                v-for="item in integralDateOptions"
+                :key="item.value"
+                :label="item.label"
+                :value="item.value"
+              >
+              </el-option>
+
+            </el-select>
+            内有效
+          </div>
+        </div>
+      </div>
+    </div>
+    <div
+      class="content"
+      style="margin-top:0"
+    >
+      <span>积分润换比</span>
+      <div class="radioDiv">
+        100积分=1元RMB
+      </div>
+    </div>
   </div>
 </template>
 <script>
 export default {
+  data () {
+    return {
+      radio: '2',
+      yearValue: '4',
+      yearOptions: [{
+        value: '0',
+        label: '一年后'
+      }, {
+        value: '1',
+        label: '两年后'
+      }, {
+        value: '2',
+        label: '三年后'
+      }, {
+        value: '3',
+        label: '四年后'
+      }, {
+        value: '4',
+        label: '五年后'
+      }],
+      mounthValue: '4',
+      mounthOptions: [
+        {
+          value: '0',
+          label: '请选择：'
+        }, {
+          value: '1',
+          label: '1'
+        }, {
+          value: '2',
+          label: '2'
+        }, {
+          value: '3',
+          label: '3'
+        }, {
+          value: '4',
+          label: '4'
+        }, {
+          value: '5',
+          label: '5'
+        }, {
+          value: '6',
+          label: '6'
+        }, {
+          value: '7',
+          label: '7'
+        }, {
+          value: '8',
+          label: '8'
+        }, {
+          value: '9',
+          label: '9'
+        }, {
+          value: '10',
+          label: '10'
+        }, {
+          value: '11',
+          label: '11'
+        }, {
+          value: '12',
+          label: '12'
+        }],
+      dayValue: '4',
+      dayOptions: [
+        {
+          value: '0',
+          label: '请选择：'
+        }, {
+          value: '1',
+          label: '1'
+        }, {
+          value: '2',
+          label: '2'
+        }, {
+          value: '3',
+          label: '3'
+        }, {
+          value: '4',
+          label: '4'
+        }, {
+          value: '5',
+          label: '5'
+        }, {
+          value: '6',
+          label: '6'
+        }, {
+          value: '7',
+          label: '7'
+        }, {
+          value: '8',
+          label: '8'
+        }, {
+          value: '9',
+          label: '9'
+        }, {
+          value: '10',
+          label: '10'
+        }, {
+          value: '11',
+          label: '11'
+        }, {
+          value: '12',
+          label: '12'
+        }, {
+          value: '13',
+          label: '13'
+        }, {
+          value: '14',
+          label: '14'
+        }, {
+          value: '15',
+          label: '15'
+        }, {
+          value: '16',
+          label: '16'
+        }, {
+          value: '17',
+          label: '17'
+        }, {
+          value: '18',
+          label: '18'
+        }, {
+          value: '19',
+          label: '19'
+        }, {
+          value: '20',
+          label: '20'
+        }, {
+          value: '21',
+          label: '21'
+        }, {
+          value: '22',
+          label: '22'
+        }, {
+          value: '23',
+          label: '23'
+        }, {
+          value: '24',
+          label: '24'
+        }, {
+          value: '25',
+          label: '25'
+        }, {
+          value: '26',
+          label: '26'
+        }, {
+          value: '27',
+          label: '27'
+        }, {
+          value: '28',
+          label: '28'
+        }, {
+          value: '29',
+          label: '29'
+        }, {
+          value: '30',
+          label: '30'
+        }, {
+          value: '31',
+          label: '31'
+        }],
+      integralNum: '',
+      integralDateValue: '2',
+      integralDateOptions: [{
+        value: '0',
+        label: '日'
+      }, {
+        value: '1',
+        label: '周'
+      },
+      {
+        value: '2',
+        label: '月'
+      }]
+    }
+  },
+  watch: {
+    mounthValue (newData) {
+      if (newData === '0') {
+        this.dayValue = ''
+      }
+    }
+  },
+  methods: {
 
+  }
 }
 </script>
 <style lang="scss" scoped>
 .integralRule {
   .top {
     padding-bottom: 10px;
+  }
+  .title {
+    span {
+      display: inline-block;
+      margin-left: 20px;
+      border-left: 2px solid #5a8bff;
+      height: 14px;
+      width: 8px;
+      margin-bottom: -1px;
+    }
+    height: 40px;
+    line-height: 40px;
+    background: #eef1f6;
+    font-size: 14px;
+  }
+  .content {
+    margin-top: 30px;
+    display: flex;
+    .radioDiv {
+      display: flex;
+      flex-direction: column;
+      margin-left: 30px;
+    }
+    .radioDiv > div {
+      display: flex;
+      margin-bottom: 30px;
+      align-items: center;
+      /deep/ .el-radio__label {
+        color: #333;
+      }
+      /deep/ .el-input {
+        width: 110px;
+      }
+    }
+    .radioDiv > div > div {
+      margin-right: 20px;
+    }
+    .integralNumDiv {
+      /deep/ .el-input {
+        width: 130px !important;
+      }
+    }
   }
 }
 </style>
