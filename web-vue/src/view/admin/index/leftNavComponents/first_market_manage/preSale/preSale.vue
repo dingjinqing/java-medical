d367fae440fd605370a283036bedd738a4e7c285
zhengbaole <zhengbaole@wangdian.cn>
2019-09-02 01:29:36 +0000

    定金膨胀活动列表页

--- web-vue/src/view/admin/index/leftNavComponents/first_market_manage/preSale/preSale.vue
+++ web-vue/src/view/admin/index/leftNavComponents/first_market_manage/preSale/preSale.vue
@@ -48,7 +48,11 @@
             label="活动名称"
             align="center"
           > </el-table-column>
-
+          <el-table-column
+            prop="presaleName"
+            label="活动名称"
+            align="center"
+          > </el-table-column>
           <el-table-column
             label="操作"
             align="center"
