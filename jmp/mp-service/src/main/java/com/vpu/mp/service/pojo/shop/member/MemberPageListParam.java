982912df60b5b8ea5b659cf646979e0f82986a22
huangzhuangzhuang <huangzhuangzhuang@wangdian.cn>
2019-09-03 09:06:14 +0000

    会员卡查询

--- jmp/mp-service/src/main/java/com/vpu/mp/service/pojo/shop/member/MemberPageListParam.java
+++ jmp/mp-service/src/main/java/com/vpu/mp/service/pojo/shop/member/MemberPageListParam.java
@@ -12,23 +12,20 @@ import lombok.Setter;
 @Setter
 public class MemberPageListParam extends BaseMemberPojo {
 
+	/** -手机号 */
 	private String mobile;
+	/** - 微信昵称 */
 	private String username;
-	/**
-	 * 来源 门店来源-1未录入0后台>0为门店
-	 */
+	/** -来源 ： -1 未录入 0后台>0为门店  {@link com.vpu.mp.service.pojo.shop.member.SourceNameEnum.SCAN_QRCODE }*/
 	private Integer source;
-	/**
-	 * 邀请人
-	 */
+	/** - 会员卡id  */
+	private Integer cardId;
+	/** -邀请人*/
 	private String inviteUserName;
-	/**
-	 * 创建时间
-	 */
+	/** - 创建时间 */
 	private String createTime;
-	/**
-	 * 结束时间
-	 */
+	/** -结束时间*/
 	private String endTime;
+	
 
 }
