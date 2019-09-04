fff00ffd9790fd43169cf35ab3ba16902b92d4a6
liangchen <liangchen@wangdian.cn>
2019-09-04 10:19:22 +0000

    新增-查询运费模板配置

--- jmp/mp-web/src/main/java/com/vpu/mp/controller/admin/AdminGoodsDeliverController.java
+++ jmp/mp-web/src/main/java/com/vpu/mp/controller/admin/AdminGoodsDeliverController.java
@@ -1,5 +1,6 @@
 package com.vpu.mp.controller.admin;
 
+import java.io.IOException;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,6 +9,17 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonPointer;
+import com.fasterxml.jackson.core.JsonToken;
+import com.fasterxml.jackson.core.ObjectCodec;
+import com.fasterxml.jackson.core.JsonParser.NumberType;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.jsontype.TypeSerializer;
+import com.fasterxml.jackson.databind.node.JsonNodeType;
 import com.vpu.mp.service.foundation.data.JsonResult;
 import com.vpu.mp.service.foundation.util.PageResult;
 import com.vpu.mp.service.pojo.shop.area.AreaProvinceVo;
@@ -186,5 +198,16 @@ public class AdminGoodsDeliverController extends AdminBaseController {
 	public JsonResult getBox() {
 		List<GoodsDeliverBoxVo> boxVos = shop().goods.goodsDeliver.getBox();
 		return success(boxVos);
+	}
+	/**
+	 * 默认运费模板配置
+	 *
+	 * @param
+	 * @return
+	 */
+	@GetMapping("/getconfig")
+	public JsonResult getDefaultDeliverTemplate() {
+		String config = shop().config.deliverTemplateConfigService.getDefaultDeliverTemplate();
+		return success(config);
 	}
 }
