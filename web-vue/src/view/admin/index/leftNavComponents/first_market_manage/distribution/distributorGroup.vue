a3737954fb23efdd1ff52a5b66745e4d9e95a977
changle <changle@wangdian.cn>
2019-09-05 08:48:02 +0000

    分组列表添加分销员

--- web-vue/src/view/admin/index/leftNavComponents/first_market_manage/distribution/distributorGroup.vue
+++ web-vue/src/view/admin/index/leftNavComponents/first_market_manage/distribution/distributorGroup.vue
@@ -55,7 +55,7 @@
         <el-button @click="dialogVisible = false">取 消</el-button>
         <el-button
           type="primary"
-          @click="confirm"
+          @click="addConfirm"
         >确 定</el-button>
       </span>
     </el-dialog>
@@ -104,11 +104,156 @@
             <div class="opt">
               <span @click="edit(scope.row.id)">编辑</span>
               <span @click="del(scope.row.id)">删除</span>
-              <span>添加分销员</span>
+              <span @click="addDistributor">添加分销员</span>
             </div>
           </template>
         </el-table-column>
       </el-table>
+      <!-- 添加分销员弹窗 -->
+      <el-dialog
+        title="添加分销员"
+        :visible.sync="centerDialogVisible"
+        width="70%"
+        center
+      >
+        <div class="options">
+          <span>分销员手机号:</span>
+          <el-input
+            class="optionInput"
+            prop="title"
+            size="small"
+          ></el-input>
+          <span>分销员昵称:</span>
+          <el-input
+            class="optionInput"
+            prop="title"
+            size="small"
+          ></el-input>
+          <span>真是姓名:</span>
+          <el-input
+            class="optionInput"
+            prop="title"
+            size="small"
+          ></el-input>
+        </div>
+        <div class="options">
+          <span>分销员ID:</span>
+          <el-input
+            class="optionInput"
+            prop="title"
+            size="small"
+          ></el-input>
+          <span class="labelClass">分销员等级</span>
+          <el-select
+            size="small"
+            v-model="valueLevel"
+            placeholder="请选择等级"
+          >
+            <el-option
+              v-for="level in groupLevelList"
+              :key="level.levelId"
+              :label="level.label"
+              :value="level.levelName"
+            >
+            </el-option>
+          </el-select>
+          <span class="labelClass">分销员分组</span>
+          <el-select
+            size="small"
+            v-model="valueGroup"
+            placeholder="请选择分组"
+          >
+            <el-option
+              v-for="group in groupNameList"
+              :key="group.id"
+              :label="group.groupName"
+              :value="group.groupName"
+            >
+            </el-option>
+          </el-select>
+        </div>
+        <div class="options">
+          <el-button
+            type="primary"
+            size="small"
+          >筛选</el-button>
+          <el-button
+            type="primary"
+            size="small"
+            plain
+          >重置</el-button>
+        </div>
+        <div class="languageContent">
+          <table width='100%'>
+            <thead>
+              <tr>
+                <td>
+                  <el-checkbox></el-checkbox><i class="tdTopText">全选本页</i>
+                </td>
+                <td>分销员ID</td>
+                <td>分销员手机号</td>
+                <td>分销员昵称</td>
+                <td>真实姓名</td>
+                <td>下级用户数</td>
+                <td>累计获得佣金金额</td>
+                <td>分销员组</td>
+                <td>当前等级</td>
+              </tr>
+            </thead>
+            <tbody>
+              <tr
+                v-for="(item,index) in distributorList"
+                :key="index"
+                @click.prevent="handleClick(index,item)"
+              >
+                <td>
+                  <div class="tdCenter">
+                    <el-checkbox v-model="item.ischecked"></el-checkbox>
+                  </div>
+
+                </td>
+                <td class="isLeft">
+                  {{item.userId}}
+
+                </td>
+                <td class="tb_decorate_a">
+                  {{item.mobile}}
+                </td>
+                <td class="tb_decorate_a">
+                  {{item.username}}
+                </td>
+                <td class="tb_decorate_a">
+                  {{item.realName}}
+                </td>
+                <td class="tb_decorate_a">
+                  {{item.nextNumber}}
+                </td>
+                <td class="tb_decorate_a">
+                  {{item.totalFanliMoney}}
+                </td>
+                <td class="tb_decorate_a">
+                  {{item.groupName}}
+                </td>
+                <td class="tb_decorate_a">
+                  {{item.levelName}}
+                </td>
+
+              </tr>
+            </tbody>
+          </table>
+        </div>
+        <span
+          slot="footer"
+          class="dialog-footer"
+        >
+          <el-button @click="cancel">取 消</el-button>
+          <el-button
+            v-model="opt"
+            type="primary"
+            @click="addConfirm()"
+          >确 定</el-button>
+        </span>
+      </el-dialog>
     </div>
     <pagination
       :page-params.sync="pageParams"
@@ -118,13 +263,18 @@
 </template>
 
 <script>
-import { distributionGroup, distributionGroupDel, distributionGroupAdd, distributionGroupEdit, distributionGroupSave } from '@/api/admin/marketManage/distribution.js'
+import { distributionGroup, distributionGroupDel, distributionGroupAdd, distributionGroupEdit, distributionGroupSave, distributorList, distributorLevelList, distributorGroupList } from '@/api/admin/marketManage/distribution.js'
 // 引入分页
 import pagination from '@/components/admin/pagination/pagination'
 export default {
   components: { pagination },
   data () {
     return {
+      groupNameList: [],
+      groupLevelList: [],
+      distributorList: [],
+      valueGroup: '',
+      valueLevel: '',
       tableData: [],
       radio: '1',
       currentPage: null,
@@ -135,6 +285,7 @@ export default {
       groupName: null,
       opt: 0,
       dialogVisible: false,
+      centerDialogVisible: false,
       id: ''
     }
   },
@@ -143,6 +294,19 @@ export default {
   },
   mounted () {
     this.groupList()
+  },
+  watch: {
+    checkedAll (newData, oldData) {
+      if (newData === true) {
+        this.distributorList.map((item, index) => {
+          item.ischecked = true
+        })
+      } else {
+        this.distributorList.map((item, index) => {
+          item.ischecked = false
+        })
+      }
+    }
   },
   methods: {
     handleClose (done) {
@@ -236,6 +400,55 @@ export default {
       })
       this.dialogVisible = true
       this.opt = 1
+    },
+    addDistributor () {
+      this.centerDialogVisible = true
+      distributorGroupList().then(res => {
+        this.groupNameList = res.content
+      })
+
+      distributorLevelList().then(res => {
+        this.groupLevelList = res.content
+        this.distributorLevel = res.content.dataList
+      })
+      distributorList(this.pageParams).then(res => {
+        if (res.error === 0) {
+          this.distributorList = res.content.dataList
+          let flag = res.content.dataList.filter((item, index) => {
+            return item.ischecked === false
+          })
+          if (flag.length === 0) {
+            this.checkedAll = true
+          }
+        }
+
+        console.log(res)
+      })
+    },
+    // 行选中高亮
+    handleClick (index, item) {
+      this.clickIindex = index
+      // console.log(this.trList[index].ischecked)
+      this.trLidistributorListst[index].ischecked = !this.distributorList[index].ischecked
+      let flag = this.distributorList.filter((item, index) => {
+        return item.ischecked === false
+      })
+      console.log(flag)
+      if (flag.length === 0) {
+        this.checkedAll = true
+      } else {
+        this.checkedAll = false
+      }
+      // let arr = []
+      this.goodsIdsArr.push(item.goodsId)
+
+      console.log('选中', index, item)
+    },
+    cancel () {
+      this.centerDialogVisible = false
+    },
+    addConfirm () {
+      this.centerDialogVisible = false
     }
   }
 }
@@ -297,5 +510,27 @@ span {
   span {
     cursor: pointer;
   }
+}
+.optionInput {
+  width: 15%;
+  margin-left: 10px;
+  margin-right: 30px;
+}
+tbody td {
+  text-align: center;
+  border: 1px solid #eff1f5;
+  color: #666;
+}
+td {
+  padding: 8px 7px;
+  vertical-align: middle !important;
+  text-align: center;
+}
+thead td {
+  background: #faf9f8;
+  text-align: center;
+  color: #333;
+  padding: 8px 10px;
+  vertical-align: middle !important;
 }
 </style>
