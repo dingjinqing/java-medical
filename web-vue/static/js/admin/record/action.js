0607138d8206e2d1dcb06911936f2c0eb52a5c21
liuyang <liuyang01@wangdian.cn>
2019-09-10 06:28:00 +0000

    文件修改

--- web-vue/static/js/admin/record/action.js
+++
@@ -1,9 +0,0 @@
-function picker() {
-    return WdatePicker({dateFmt: 'yyyy-MM-dd HH:mm:ss'});
-}
-
-$(function () {
-    $('.btn-choose').click(function () {
-        $('#form1').submit();
-    })
-});
