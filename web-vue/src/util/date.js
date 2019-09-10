d00e6d6e2ebf7a4017f0867c122e40dfc77dc479
lixiaobing <lixiaobing@wangdian.cn>
2019-09-10 03:09:06 +0000

    日期转换工具类js添加字符串转换Date对象函数

--- web-vue/src/util/date.js
+++ web-vue/src/util/date.js
@@ -7,6 +7,15 @@ export const format = s => {
   return moment(s).format('YYYY-MM-DD HH:mm:ss')
 }
 
+/* 解析日期字符串为js Date对象  */
+export const parseDate = dateStr => {
+  if (!dateStr) {
+    return null
+  }
+  let m = moment(dateStr, 'YYYY-MM-DD HH:mm:ss')
+  return m.toDate()
+}
+
 export const range = range => ({
   v1: range[0],
   v2: range[1]
