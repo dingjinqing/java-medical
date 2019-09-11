8d9bb6f5b205ce1ab2b85753f0ff2572cd575df9
changle <changle@wangdian.cn>
2019-09-11 08:24:56 +0000

    优惠券列表添加优惠类型

--- jmp/mp-service/src/main/java/com/vpu/mp/service/pojo/shop/coupon/CouponListVo.java
+++ jmp/mp-service/src/main/java/com/vpu/mp/service/pojo/shop/coupon/CouponListVo.java
@@ -14,6 +14,7 @@ import lombok.Data;
 public class CouponListVo {
 	 	private Integer    id;
 	    private String     actName;
+	    private String     actCode;
 	    private Timestamp  startTime;
 	    private Timestamp  endTime;
 	    private BigDecimal denomination;
