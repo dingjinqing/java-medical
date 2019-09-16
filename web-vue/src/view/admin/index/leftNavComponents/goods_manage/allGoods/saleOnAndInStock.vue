cf8a7af7e65a434c6eb7cfc0d8614430a6a1c195
lixiaobing <lixiaobing@wangdian.cn>
2019-09-16 05:57:46 +0000

    全部商品组件

---
+++ web-vue/src/view/admin/index/leftNavComponents/goods_manage/allGoods/saleOnAndInStock.vue
@@ -0,0 +1,32 @@
+<template>
+  <!-- 出售中仓库中商品组件 -->
+  <div class="saleOnAndInStock">
+    <el-input v-model="a"/>
+  </div>
+</template>
+<script>
+export default {
+  name: 'saleOnAndInStock',
+  beforeRouteEnter  (to, from, next) {
+    if (to.name === 'goodsForSale') {
+
+    } else {
+
+    }
+    next()
+  },
+  data () {
+    return {
+      a: 0
+    }
+  },
+  methods: {
+
+  },
+  mounted () {
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
---
+++ web-vue/src/view/admin/index/leftNavComponents/goods_manage/allGoods/saleOut.vue
@@ -0,0 +1,27 @@
+<template>
+  <!--已售罄中商品组件-->
+  <div class="saleOut">
+    <el-input v-model="a"/>
+  </div>
+</template>
+<script>
+
+export default {
+  name: 'saleOut',
+  // 数据data
+  data () {
+    return {
+      a: 0
+    }
+  },
+  methods: {
+  },
+  mounted () {
+
+  }
+}
+</script>
+
+<style lang="css" scoped>
+
+</style>
