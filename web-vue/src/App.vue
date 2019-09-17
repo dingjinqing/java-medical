806b35d2e6fab8bf9289039b64d41fd7817f78e5
suntengfei <suntengfei@wangdian.cn>
2019-09-17 10:05:44 +0000

    解决全部订单滚动bug

--- web-vue/src/App.vue
+++ web-vue/src/App.vue
@@ -32,9 +32,4 @@ export default {
 </script>
 <style lang="scss" src="./style/system/common.scss"></style> <!--引入common全局样式-->
 <style src="./style/reset.css">
-#app {
-  /* height: 100%;
-  position: absolute; */
-  width: 100%;
-}
 </style>
