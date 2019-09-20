bb0b9e26b5b21fc17be6d978106998d935c978f0
wangbingbing <wangbingbing@wangdian.cn>
2019-09-20 08:07:48 +0000

    限时降价接口修改

--- jmp/mp-service/src/main/java/com/vpu/mp/service/pojo/shop/market/reduceprice/ReducePriceVo.java
+++ jmp/mp-service/src/main/java/com/vpu/mp/service/pojo/shop/market/reduceprice/ReducePriceVo.java
@@ -46,5 +46,5 @@ public class ReducePriceVo {
     private List<ReducePriceGoodsVo> reducePriceGoods;
 
     /** 分享设置 */
-    private ShopShareConfig shopShareConfig;
+    private ShopShareConfig shareConfig;
 }
