2d25bd88c36e49120b23d6b21090af95414780d8
zhaojianqiang <zhaojianqiang@wangdian.cn>
2019-09-23 08:28:35 +0000

    更改隐藏底部和isEnable

---
+++ jmp/mp-service/src/main/java/com/vpu/mp/service/pojo/shop/auth/ShopEnableReq.java
@@ -0,0 +1,23 @@
+package com.vpu.mp.service.pojo.shop.auth;
+
+
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * 
+ * @author zhaojianqiang
+ *
+ * 2019年9月23日 下午4:06:12
+ */
+@Data
+@NoArgsConstructor
+public class ShopEnableReq {
+
+	public Byte isEnable;
+	public Integer shopId;
+	public Byte hidBottom;
+	
+
+}
