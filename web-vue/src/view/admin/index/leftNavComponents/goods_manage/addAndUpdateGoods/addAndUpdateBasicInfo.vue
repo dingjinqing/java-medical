e3c7a82eb0a8ce70a2f2d5f19bcc57ca2d02e231
lixiaobing <lixiaobing@wangdian.cn>
2019-09-24 06:38:44 +0000

    商家分类下拉框修改

--- web-vue/src/view/admin/index/leftNavComponents/goods_manage/addAndUpdateGoods/addAndUpdateBasicInfo.vue
+++ web-vue/src/view/admin/index/leftNavComponents/goods_manage/addAndUpdateGoods/addAndUpdateBasicInfo.vue
@@ -200,13 +200,8 @@
                 size="small"
                 style="width:170px;"
               >
-                <el-option
-                  v-for="item in sortSelectOptions"
-                  :key="item.sortId"
-                  :label="item.sortName"
-                  :value="item.sortId"
-                  :style="{'marginLeft': item.level===1? '10px':'0px'}"
-                />
+                <el-option v-for="(item,index) in sortSelectOptions" :label="item.sortName" :value="item.sortId" :key="index"
+                           :style="{paddingLeft: (item.level+1)*20+'px'}"/>
               </el-select>
             </el-form-item>
             <el-form-item
@@ -613,9 +608,53 @@ export default {
     sortAndLabelAndBrandSelectInit () {
       return goodsSortAndGoodsBrandInitApi().then(res => {
         const { content: { goodsLabels, goodsSorts } } = res
-        this.sortSelectOptions = goodsSorts
+        this.sortSelectOptions = this._disposeGoodsSortAndCatData(goodsSorts, 'sortId')
         this.labelSelectOptions = goodsLabels
       })
+    },
+    /* 处理后台传入的商家分类数据 */
+    _disposeGoodsSortAndCatData (data, idName) {
+      let retObj = {}
+
+      for (let i = 0; i < data.length; i++) {
+        let item = data[i]
+
+        // 是否自身节点被创建过（子节点先遍历到了）
+        let selfItem = retObj[item[idName]]
+        if (selfItem === undefined) {
+          // 未遍历到则初始化自己
+          retObj[item[idName]] = {'item': item, children: []}
+          selfItem = retObj[item[idName]]
+        } else {
+          // 已创建过，（因提前遍历了子节点而创建）
+          selfItem.item = item
+        }
+
+        let parentItem = retObj[item.parentId]
+        // 有父亲直接插入
+        if (parentItem !== undefined) {
+          parentItem.children.push(selfItem)
+        } else {
+          // 没有则创建临时父亲
+          retObj[item.parentId] = {'item': null, children: [selfItem]}
+        }
+      }
+
+      let retArr = []
+
+      if (data.length === 0) {
+        return retArr
+      }
+      let rootArr = retObj['0'].children
+      // 处理结果将对象变为数组
+      for (let i = 0; i < rootArr.length; i++) {
+        let retItem = rootArr[i]
+        retArr.push(retItem.item)
+        if (retItem.children.length > 0) {
+          rootArr.splice(i + 1, 0, ...(retItem.children))
+        }
+      }
+      return retArr
     },
     /* 标签下拉框选择事件 */
     labelSelectChange () {
