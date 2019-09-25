0753c34e8d73b94f2d5afb5027c921c2437c258d
zhaoxin <zhaoxin@wangdian.cn>
2019-09-25 06:38:01 +0000

    交易流程相关配置页面提交

--- web-vue/src/view/admin/index/leftNavComponents/base_manger/tradeConfigure/tradeProcessConfigure.vue
+++ web-vue/src/view/admin/index/leftNavComponents/base_manger/tradeConfigure/tradeProcessConfigure.vue
@@ -202,39 +202,85 @@
           <span>选择商品品牌</span>
         </div>
       </div>
-      <!-- <div class="goodsWrapper">
-        <div class="addGoods">
-          <img :src="src">
-          <span>选择平台分类</span>
-        </div>
-      </div>
-      <div class="goodsWrapper">
-        <div class="addGoods">
-          <img :src="src">
-          <span>选择商家分类</span>
-        </div>
-      </div>
-      <div class="goodsWrapper">
-        <div class="addGoods">
-          <img :src="src">
-          <span>选择商品标签</span>
-        </div>
-      </div>
-      <div class="goodsWrapper">
-        <div class="addGoods">
-          <img :src="src">
-          <span>选择商品品牌</span>
-        </div>
-      </div> -->
-
-      <!-- </div> -->
     </section>
+
+    <!-- 微信物流助手对接配置 -->
+    <section class="settingWrapper">
+      <div class="title">
+        <span></span>
+        微信物流助手对接配置
+      </div>
+      <div
+        class="WeChatExpress"
+        style="display:flex;"
+      >
+        <el-switch
+          v-model="value1"
+          active-color="#13ce66"
+          inactive-color="#ff4949"
+          style="margin: 17px 10px 0;"
+        ></el-switch>
+        <div class="switchText">{{value1?'已开启':'已关闭'}}</div>
+        <!-- 右侧第三部分 - 已开启、已关闭后边的内容 -->
+        <div class="expressInfo">
+          <div class="grayText">开关打开，已发货商品物流信息将展示在小程序前端订单页面，用户可直接查看物流信息。</div>
+          <div class="grayText">开关关闭，用户在小程序端查看物流信息时将自动跳转至“快递100”小程序。</div>
+          <div style="display:flex;line-height:25px">
+            <span style="color:red;">注：开关开启时，若选用微信物流助手不支持的物流公司发货，将无法在小程序端查看物流信息 </span>
+            <span style="color: #5A8BFF;margin-left: 20px;">查看支持的物流公司</span>
+          </div>
+          <!-- 发货地址部分 -->
+          <div class="addressContent">
+            <span class="address">请选择发货地址</span>
+            <areaLinkage @areaData="handleAreaData" />
+            <el-input
+              size="small"
+              style="width:180px"
+            ></el-input>
+          </div>
+          <!-- 快递表格数据部分 -->
+          <div class="expressTable">
+            <table>
+              <thead>
+                <tr>
+                  <td>物流公司</td>
+                  <td>电子面单账号</td>
+                  <td>状态</td>
+                  <td>操作</td>
+                </tr>
+              </thead>
+              <tbody>
+                <tr
+                  v-for="item in expressCompany"
+                  :key="item.name"
+                >
+                  <td style="width:190px">{{item.name}}</td>
+                  <td style="width:150px">{{item.account}}</td>
+                  <td style="width:90px">{{item.state}}</td>
+                  <td style="color:5A8BFF;cursor:pointer;width:70px">{{item.operate}}</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <div class="btn">
+      <el-button
+        type="primary"
+        size="small"
+      >保存</el-button>
+    </div>
+
   </div>
 </template>
 
 <script>
+import areaLinkage from '@/components/admin/areaLinkage/areaLinkage.vue'
 
 export default {
+  components: { areaLinkage },
   data () {
     return {
       deliverMethods: [
@@ -254,12 +300,27 @@ export default {
         { info: '自定义信息', value: '' }
       ],
       value: '',
-      src: `${this.$imageHost}/image/admin/icon_jia.png`
+      value1: '',
+      src: `${this.$imageHost}/image/admin/icon_jia.png`,
+      province: ``,
+      district: ``,
+      city: ``,
+      expressCompany: [
+        { name: '百世快递', account: '', state: '未签约', operate: '签约' },
+        { name: '顺丰', account: '', state: '未签约', operate: '签约' },
+        { name: '德邦', account: '', state: '未签约', operate: '签约' },
+        { name: '圆通', account: '', state: '未签约', operate: '签约' },
+        { name: '中通', account: '', state: '未签约', operate: '签约' },
+        { name: '中国邮政', account: '', state: '未签约', operate: '签约' }
+      ]
     }
   },
   methods: {
     handleTake () {
       console.log('自提')
+    },
+    handleAreaData (val) {
+      this.province = val['province']
     }
   }
 }
@@ -324,6 +385,47 @@ export default {
     .bottom {
       margin: 0 0 15px 10px;
     }
+    .WeChatExpress {
+      font-size: 14px;
+      div {
+        margin-bottom: 10px;
+      }
+      .switchText {
+        line-height: 25px;
+        color: #333;
+        width: 55px;
+        margin: 15px 20px 0 10px;
+      }
+      .expressInfo {
+        margin-top: 15px;
+        .grayText {
+          color: #999;
+          line-height: 25px;
+        }
+        .addressContent {
+          color: #333;
+          display: flex;
+          .address {
+            line-height: 25px;
+            margin-right: 20px;
+          }
+        }
+        .expressTable {
+          width: 500px;
+          line-height: 35px;
+          table {
+            width: 100%;
+            text-align: center;
+            thead {
+              background: #f5f5f5;
+            }
+            tbody td {
+              border: 1px solid #eee;
+            }
+          }
+        }
+      }
+    }
   }
   .settingWrapper:nth-of-type(1) {
     margin-top: 20px;
@@ -348,6 +450,12 @@ export default {
         margin: 10px 0;
       }
     }
+  }
+  .btn {
+    display: flex;
+    justify-content: center;
+    width: 100%;
+    margin-top: 30px;
   }
 }
 </style>
