24b076166b9eade5f861fc586ece4ef93c4dc606
kongdecheng <kongdecheng@wangdian.cn>
2019-09-26 10:26:50 +0000

    我要送礼--商品信息vo

--- jmp/mp-service/src/main/java/com/vpu/mp/service/pojo/shop/market/givegift/record/GiftRecordGoodsVo.java
+++ jmp/mp-service/src/main/java/com/vpu/mp/service/pojo/shop/market/givegift/record/GiftRecordGoodsVo.java
@@ -2,6 +2,8 @@ package com.vpu.mp.service.pojo.shop.market.givegift.record;
 
 import lombok.Data;
 
+import java.math.BigDecimal;
+
 /**
  * @author 孔德成
  * @date 2019/9/26 17:43
@@ -9,7 +11,12 @@ import lombok.Data;
 @Data
 public class GiftRecordGoodsVo {
 
-
-
-
+    /**
+     * 商品id
+     */
+    private Integer goodsId;
+    private String goodsSn;
+    private String goodsName;
+    private BigDecimal goodsPrice;
+    private Integer goodsNumber;
 }
