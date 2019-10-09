af58bcdbb98455bdfe62d92ae258d49ef61cd85a
zhaojianqiang <zhaojianqiang@wangdian.cn>
2019-10-09 03:34:36 +0000

    加log

--- jmp/mp-web/src/main/java/com/vpu/mp/controller/wxapp/WxAppLoginController.java
+++ jmp/mp-web/src/main/java/com/vpu/mp/controller/wxapp/WxAppLoginController.java
@@ -27,6 +27,7 @@ public class WxAppLoginController extends WxAppBaseController {
 	 */
 	@PostMapping("/api/wxapp/login")
 	public JsonResult login(@RequestBody WxAppLoginParam param) throws WxErrorException {
+		logger().info("小程序登录");
 		WxAppSessionUser user = wxAppAuth.login(param);
 		if(user==null) {
 			//登录失败
