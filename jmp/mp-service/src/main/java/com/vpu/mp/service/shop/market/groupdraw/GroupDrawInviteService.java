d7160cbf8831224a9b6fbf10afcbbce065869eed
zhaojianqiang <zhaojianqiang@wangdian.cn>
2019-10-09 03:01:08 +0000

    createInviteRecord

--- jmp/mp-service/src/main/java/com/vpu/mp/service/shop/market/groupdraw/GroupDrawInviteService.java
+++ jmp/mp-service/src/main/java/com/vpu/mp/service/shop/market/groupdraw/GroupDrawInviteService.java
@@ -6,6 +6,8 @@ import org.springframework.stereotype.Service;
 
 import static com.vpu.mp.db.shop.tables.GroupDrawInvite.GROUP_DRAW_INVITE;
 
+import java.util.Map;
+
 /**
  * 拼团抽奖邀请用户
  *
@@ -26,5 +28,19 @@ public class GroupDrawInviteService extends ShopBaseService {
             .and(GROUP_DRAW_INVITE.GOODS_ID.eq(goodsId))
             .and(GROUP_DRAW_INVITE.USER_ID.eq(userId))
             .and(GROUP_DRAW_INVITE.IS_USED.eq((byte) 0))).fetchOneInto(GROUP_DRAW_INVITE);
+    }
+    
+    
+    
+    public void createInviteRecord(String path,Map<String, String> query,Byte isNew) {
+    	GroupDrawInviteRecord newRecord = db().newRecord(GROUP_DRAW_INVITE);
+    	newRecord.setIdentityId(Integer.parseInt(query.get("group_draw_id")));
+    	newRecord.setPath(path);
+    	newRecord.setGoodsId(Integer.parseInt(query.get("goods_id")));
+    	newRecord.setGroupId(Integer.parseInt(query.get("group_id")));
+    	newRecord.setInviteUserId(Integer.parseInt(query.get("invite_id")));
+    	newRecord.setUserId(Integer.parseInt(query.get("user_id")));
+    	newRecord.setIsNew(isNew);
+    	newRecord.update();
     }
 }
