9f71ba54909f07e9a6cab2e743863c19bc18f8eb
zhaojianqiang <zhaojianqiang@wangdian.cn>
2019-10-10 08:13:34 +0000

    加log

--- jmp/mp-service/src/main/java/com/vpu/mp/service/shop/user/user/UserLoginRecordService.java
+++ jmp/mp-service/src/main/java/com/vpu/mp/service/shop/user/user/UserLoginRecordService.java
@@ -18,19 +18,22 @@ import com.vpu.mp.db.shop.tables.records.UserLoginRecordRecord;
 public class UserLoginRecordService extends ShopBaseService {
 
 	public void userLoginRecord(Integer userId, UserLoginRecordRecord record) {
+		logger().info("记录小程序登录");
 		UserLoginRecordRecord res = db().selectFrom(USER_LOGIN_RECORD).where(USER_LOGIN_RECORD.USER_ID.eq(userId))
 				.and(USER_LOGIN_RECORD.CREATE_TIME.gt(DSL.currentTimestamp())).fetchAny();
 		// 有记录更新登陆次数，没有记录加记录 一小时一条数据
 		if (res == null) {
 			record.setCount(1);
-			record.insert();
+			int insert = record.insert();
+			logger().info("插入小程序登录"+insert);
 		} else {
 			res.setCount(res.getCount() + 1);
 			if (record.getLat() != null && (!record.getLat().equals(res.getLat()))) {
 				res.setLat(record.getLat());
 				res.setLng(record.getLng());
 			}
-			res.update();
+			int update = res.update();
+			logger().info("更新小程序登录"+update);
 		}
 	}
 
