4b6321e996ffba1df3823992894603b73917a299
suntengfei <suntengfei@wangdian.cn>
2019-10-11 08:13:36 +0000

    图片导航模块

--- wxapp/jwxapp/components/decorate/card/index.js
+++ wxapp/jwxapp/components/decorate/card/index.js
@@ -5,11 +5,13 @@ global.wxComponent({
   mixins: [base],
   methods: {
     onPropChange(newVal, oldVal, changedPath) {
+      console.log(newVal)
+      // 根据背景类型来判断是采用背景颜色还是背景图片
       if (newVal.bg_type == 0) {
         newVal.bg = newVal.bg_color = newVal.bg_color || '#e6cb96';
       }
       if (newVal.bg_type == 1) {
-        newVal.bg = 'url(' + newVal.imageUrl + newVal.bg_img + '!big)';
+        newVal.bg = 'url(' + newVal.bg_img + ')';
       }
       console.log(newVal)
     },
