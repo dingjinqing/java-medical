ff28d3f10f7396d79be049ffb9d2fe773671e875
luguangyao <luguangyao@wangdian.cn>
2019-10-11 09:27:29 +0000

    feat:EsGoods所需常量类

---
+++ jmp/mp-service/src/main/java/com/vpu/mp/service/shop/goods/es/EsGoodsConstant.java
@@ -0,0 +1,21 @@
+package com.vpu.mp.service.shop.goods.es;
+
+/**
+ * EsGoods常量类
+ * @author 卢光耀
+ * @date 2019/10/10 10:11 上午
+ *
+*/
+public class EsGoodsConstant {
+
+    public static class EsGoodsShowPriceReducePeriodAction{
+        public static final Byte EVERY_DAY = 1;
+
+        public static final Byte EVERY_MONTH = 2;
+
+        public static final Byte EVERY_WEEK = 3;
+
+
+
+    }
+}
