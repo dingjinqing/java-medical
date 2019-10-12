16696680ef33da3f0fd3b35b9733406d23277f9a
zhaojianqiang <zhaojianqiang@wangdian.cn>
2019-10-12 08:28:15 +0000

    新版个人中心

--- jmp/mp-service/src/main/java/com/vpu/mp/service/shop/store/verify/StoreVerifierService.java
+++ jmp/mp-service/src/main/java/com/vpu/mp/service/shop/store/verify/StoreVerifierService.java
@@ -3,23 +3,20 @@ package com.vpu.mp.service.shop.store.verify;
 import static com.vpu.mp.db.shop.tables.OrderVerifier.ORDER_VERIFIER;
 import static com.vpu.mp.db.shop.tables.User.USER;
 
-import com.vpu.mp.db.shop.tables.records.OrderVerifierRecord;
-import com.vpu.mp.service.foundation.excel.ExcelFactory;
-import com.vpu.mp.service.foundation.excel.ExcelTypeEnum;
-import com.vpu.mp.service.foundation.excel.ExcelWriter;
-import com.vpu.mp.service.pojo.shop.store.verifier.*;
-import org.apache.poi.ss.usermodel.Workbook;
 import org.jooq.InsertValuesStep3;
 import org.jooq.Record;
+import org.jooq.Result;
 import org.jooq.SelectWhereStep;
 import org.jooq.tools.StringUtils;
 import org.springframework.stereotype.Service;
 
+import com.vpu.mp.db.shop.tables.records.OrderVerifierRecord;
 import com.vpu.mp.service.foundation.data.DelFlag;
 import com.vpu.mp.service.foundation.service.ShopBaseService;
 import com.vpu.mp.service.foundation.util.PageResult;
-
-import java.util.List;
+import com.vpu.mp.service.pojo.shop.store.verifier.VerifierAddParam;
+import com.vpu.mp.service.pojo.shop.store.verifier.VerifierListQueryParam;
+import com.vpu.mp.service.pojo.shop.store.verifier.VerifierListVo;
 
 /**
  * @author 王兵兵
@@ -27,12 +24,10 @@ import java.util.List;
  * 2019年7月11日
  */
 @Service
-
-
 public class StoreVerifierService extends ShopBaseService{
 	/**
 	 *	 门店核销员列表分页查询
-	 * @param param
+	 * @param VerifierListQueryParam
 	 * @return VerifierListVo
 	 */
 	public PageResult<VerifierListVo> getPageList(VerifierListQueryParam param) {
@@ -63,47 +58,22 @@ public class StoreVerifierService extends ShopBaseService{
 	
 	/**
 	 * 添加核销员
-	 * @param param
+	 * @param VerifierAddParam
 	 * @return
 	 */
-	public void addVerifiers(VerifierAddParam param) {
-        this.transaction(()->{
-            for(int userId : param.getUserIds()){
-                OrderVerifierRecord record = db().newRecord(ORDER_VERIFIER);
-                record.setStoreId(param.getStoreId());
-                record.setUserId(userId);
-                record.setDelFlag(DelFlag.NORMAL_VALUE);
-                record.store();
-            }
-        });
+	public Boolean addVerifiers(VerifierAddParam param) {
+		InsertValuesStep3<? extends Record,Integer,Integer,Integer>  step = db().insertInto(ORDER_VERIFIER,ORDER_VERIFIER.STORE_ID,ORDER_VERIFIER.USER_ID,ORDER_VERIFIER.VERIFY_ORDERS);
+		Integer[] userIds = param.getUserIds();
+		Integer storeId = param.getStoreId();
+		for(int i = 0;i<userIds.length;i++) {
+			step.values(storeId, userIds[i], 0);
+		}
+		return step.execute() > 0 ? true : false;
 	}
-
-    /**
-     * 删除核销员
-     * @param verifier
-     * @return
-     */
-    public void delStoreVerifier(VerifierSimpleParam verifier) {
-        db().update(ORDER_VERIFIER).set(ORDER_VERIFIER.DEL_FLAG,DelFlag.DISABLE_VALUE).where(ORDER_VERIFIER.STORE_ID.eq(verifier.getStoreId()).and(ORDER_VERIFIER.USER_ID.eq(verifier.getUserId()))).execute();
-    }
-
-    /**
-     *	 门店核销员列表导出
-     * @param param
-     * @return VerifierListVo
-     */
-    public Workbook exportStoreVerifierList(VerifierListQueryParam param, String lang) {
-        SelectWhereStep<? extends Record> select = db().select(ORDER_VERIFIER.USER_ID,USER.USERNAME,USER.MOBILE,ORDER_VERIFIER.VERIFY_ORDERS)
-            .from(ORDER_VERIFIER)
-            .leftJoin(USER).on(ORDER_VERIFIER.USER_ID.eq(USER.USER_ID));
-        select = this.buildOptions(select, param);
-        select.where(ORDER_VERIFIER.DEL_FLAG.eq(DelFlag.NORMAL.getCode())).orderBy(ORDER_VERIFIER.CREATE_TIME);
-        List<VerifierExportVo> storeVerifierList =  select.fetchInto(VerifierExportVo.class);
-
-        Workbook workbook= ExcelFactory.createWorkbook(ExcelTypeEnum.XLSX);
-        ExcelWriter excelWriter = new ExcelWriter(lang,workbook);
-        excelWriter.writeModelList(storeVerifierList, VerifierExportVo.class);
-        return workbook;
-    }
 	
+	
+	public Result<OrderVerifierRecord> getUserVerifyStores(Integer userId) {
+		return db().selectFrom(ORDER_VERIFIER)
+				.where(ORDER_VERIFIER.USER_ID.eq(userId).and(ORDER_VERIFIER.DEL_FLAG.eq((byte) 0))).fetch();
+	}
 }
