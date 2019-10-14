5b457060aecdfb0f37796d6b6aa2a42d6ea0d3d3
lixiaobing <lixiaobing@wangdian.cn>
2019-10-14 10:47:28 +0000

    测试-分享码

--- jmp/mp-service/src/main/java/com/vpu/mp/service/shop/goods/GoodsService.java
+++ jmp/mp-service/src/main/java/com/vpu/mp/service/shop/goods/GoodsService.java
@@ -1327,7 +1327,8 @@ public class GoodsService extends ShopBaseService {
         String paramName = "goods_id=";
         String urlParam = paramName + goodsId;
         logger().debug("获取商品小程序展示页面 service进入");
-        String mpQrCode = qrCodeService.getMpQrCode(QrCodeTypeEnum.GOODS_ITEM, urlParam);
+//        String mpQrCode = qrCodeService.getMpQrCode(QrCodeTypeEnum.GOODS_ITEM, urlParam);
+        String mpQrCode = qrCodeService.getMpQrCode(QrCodeTypeEnum.INDEX, urlParam);
         GoodsQrCodeVo goodsQrCodeVo = new GoodsQrCodeVo();
         goodsQrCodeVo.setImgFullUrl(mpQrCode);
         goodsQrCodeVo.setPageUrl(QrCodeTypeEnum.GOODS_ITEM.getPathUrl(urlParam));
