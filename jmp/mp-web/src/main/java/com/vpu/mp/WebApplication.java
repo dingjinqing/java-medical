f9918da9abde076882afbff321536053fc7e96eb
luguangyao <luguangyao@wangdian.cn>
2019-10-16 03:51:15 +0000

    fix:close spring ES config

--- jmp/mp-web/src/main/java/com/vpu/mp/WebApplication.java
+++ jmp/mp-web/src/main/java/com/vpu/mp/WebApplication.java
@@ -4,18 +4,24 @@ import java.util.TimeZone;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.elasticsearch.ElasticsearchAutoConfiguration;
+import org.springframework.boot.autoconfigure.elasticsearch.rest.RestClientAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.cache.annotation.EnableCaching;
 
 /**
  * 
  * @author lixinguo
  *
  */
-@SpringBootApplication(exclude={DataSourceAutoConfiguration.class,HibernateJpaAutoConfiguration.class})
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class,HibernateJpaAutoConfiguration.class,
+    ElasticsearchAutoConfiguration.class,RestClientAutoConfiguration.class})
 public class WebApplication {
 	public static void main(String[] args) {
 		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Shanghai"));
+		//close JOOQ banner
+        System.getProperties().setProperty("org.jooq.no-logo", "true");
 		SpringApplication app = new SpringApplication(WebApplication.class);
 		app.run(args);
 	}
