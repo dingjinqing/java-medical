673691ffd20f6ee25791b24425d7b305f14c66a3
lixiaobing <lixiaobing@wangdian.cn>
2019-10-17 07:58:00 +0000

    代码合并问题提交

--- jmp/mp-service/src/main/java/com/vpu/mp/service/shop/goods/GoodsBrandService.java
+++ jmp/mp-service/src/main/java/com/vpu/mp/service/shop/goods/GoodsBrandService.java
@@ -1,31 +1,27 @@
 package com.vpu.mp.service.shop.goods;
 
-import static com.vpu.mp.db.shop.Tables.BRAND_CLASSIFY;
-import static com.vpu.mp.db.shop.Tables.GOODS;
-import static com.vpu.mp.db.shop.Tables.GOODS_BRAND;
-
-import java.sql.Timestamp;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
-
-import org.jooq.*;
-import org.jooq.impl.DSL;
-import org.jooq.tools.StringUtils;
-import org.springframework.stereotype.Service;
-
 import com.vpu.mp.db.shop.tables.records.BrandClassifyRecord;
 import com.vpu.mp.db.shop.tables.records.GoodsBrandRecord;
 import com.vpu.mp.service.foundation.data.DelFlag;
 import com.vpu.mp.service.foundation.service.ShopBaseService;
+import com.vpu.mp.service.foundation.util.ChineseToPinYinUtil;
 import com.vpu.mp.service.foundation.util.PageResult;
-import com.vpu.mp.service.pojo.shop.goods.brand.GoodsBrand;
-import com.vpu.mp.service.pojo.shop.goods.brand.GoodsBrandBatchParam;
-import com.vpu.mp.service.pojo.shop.goods.brand.GoodsBrandClassifyParam;
-import com.vpu.mp.service.pojo.shop.goods.brand.GoodsBrandClassifyVo;
-import com.vpu.mp.service.pojo.shop.goods.brand.GoodsBrandPageListParam;
-import com.vpu.mp.service.pojo.shop.goods.brand.GoodsBrandVo;
+import com.vpu.mp.service.pojo.shop.goods.GoodsConstant;
+import com.vpu.mp.service.pojo.shop.goods.brand.*;
+import com.vpu.mp.service.pojo.wxapp.goods.brand.GoodsBrandMpVo;
+import com.vpu.mp.service.pojo.wxapp.goods.brand.GoodsBrandMpWithClassifyVo;
+import com.vpu.mp.service.pojo.wxapp.goods.brand.GoodsBrandT;
+import com.vpu.mp.service.shop.image.ImageService;
+import org.jooq.*;
+import org.jooq.impl.DSL;
+import org.jooq.tools.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.sql.Timestamp;
+import java.util.*;
+
+import static com.vpu.mp.db.shop.Tables.*;
 
 /**
  * 商品品牌
@@ -39,6 +35,9 @@ public class GoodsBrandService extends ShopBaseService {
     private final static String BRAND_NUM = "brand_num";
 
     private final static int DEFAULT_GOODS_BRAND_ID = 0;
+
+    @Autowired
+    ImageService imageService;
 
     /**
      * 分页获取品牌信息
@@ -65,6 +64,10 @@ public class GoodsBrandService extends ShopBaseService {
         select.orderBy(GOODS_BRAND.FIRST.desc(), GOODS_BRAND.CREATE_TIME.desc());
 
         PageResult<GoodsBrand> pageResult = this.getPageResult(select, param.getCurrentPage(), param.getPageRows(), GoodsBrand.class);
+
+        pageResult.dataList.forEach(goodsBrand -> {
+            goodsBrand.setLogoUrl(getImgFullUrlUtil(goodsBrand.getLogo()));
+        });
 
         return pageResult;
     }
@@ -187,6 +190,7 @@ public class GoodsBrandService extends ShopBaseService {
 
         gb.setGoodsIds(goodsIds);
 
+        gb.setLogoUrl(getImgFullUrlUtil(gb.getLogo()));
         return gb;
     }
 
@@ -228,19 +232,6 @@ public class GoodsBrandService extends ShopBaseService {
         } else {
             return false;
         }
-    }
-    /**
-     * 根据brandId列出所有品牌
-     *
-     * @return
-     */
-    public Map<Integer,GoodsBrandVo>  listGoodsBrandNameByIds(List<Integer> ids) {
-        return db().select(GOODS_BRAND.ID, GOODS_BRAND.BRAND_NAME)
-            .from(GOODS_BRAND)
-            .where(GOODS_BRAND.DEL_FLAG.eq(DelFlag.NORMAL.getCode()))
-            .and(GOODS_BRAND.ID.in(ids))
-            .fetch().into(GoodsBrandVo.class)
-            .stream().collect(Collectors.toMap(GoodsBrandVo::getId,x->x));
     }
 
     /**
@@ -325,7 +316,6 @@ public class GoodsBrandService extends ShopBaseService {
 
     /**
      * 根据classifyId查询品牌分类
-     *
      * @param param
      * @return
      */
@@ -371,8 +361,8 @@ public class GoodsBrandService extends ShopBaseService {
                 .where(GOODS_BRAND.CLASSIFY_ID.eq(param.getClassifyId()))
                 .fetch().into(Integer.class);
 
-
-            deleteGoodsBrand(brandIds);
+            //解除品牌关联关系
+           db().update(GOODS_BRAND).set(GOODS_BRAND.CLASSIFY_ID,0).where(GOODS_BRAND.ID.in(brandIds)).execute();
         });
     }
 
@@ -401,8 +391,85 @@ public class GoodsBrandService extends ShopBaseService {
 
     /**
      * 查询所有有效的品牌，按照名称拼音进行组织
+     * @return 相同开头名称的处于同一个list集合中。
      */
-    public void getAllBrandGroupByPinYinName(){
+    public Map<String,List<GoodsBrandMpVo>> getAllBrandGroupByPinYinName(){
+        List<GoodsBrandMpVo> pinYinVos = db().select(GOODS_BRAND.ID, GOODS_BRAND.BRAND_NAME, GOODS_BRAND.E_NAME, GOODS_BRAND.LOGO)
+            .from(GOODS_BRAND)
+            .where(GOODS_BRAND.DEL_FLAG.eq(DelFlag.NORMAL.getCode())).orderBy(GOODS_BRAND.FIRST.desc(), GOODS_BRAND.CREATE_TIME)
+            .fetchInto(GoodsBrandMpVo.class);
 
+        TreeMap<String,List<GoodsBrandMpVo>> resultMap = new TreeMap<>();
+        for (GoodsBrandMpVo pinYinVo : pinYinVos) {
+            String c = ChineseToPinYinUtil.getStartAlphabet(pinYinVo.getBrandName());
+            pinYinVo.setPinYinStart(c);
+            pinYinVo.setLogo(getImgFullUrlUtil(pinYinVo.getLogo()));
+
+            List<GoodsBrandMpVo> list = resultMap.get(c);
+            if (list == null) {
+                list = new ArrayList<>();
+                resultMap.put(c,list);
+            }
+            list.add(pinYinVo);
+        }
+
+        return resultMap;
+    }
+
+    /**
+     * 获取所有推荐品牌（前端按照品牌列表显示）
+     * @return 推荐品牌集合
+     */
+    public List<GoodsBrandMpVo> getAllRecommendBrand(){
+        List<GoodsBrandMpVo> goodsBrandMpVos = db().selectFrom(GOODS_BRAND).where(GOODS_BRAND.DEL_FLAG.eq(DelFlag.NORMAL.getCode()))
+            .and(GOODS_BRAND.IS_RECOMMEND.eq(GoodsConstant.RECOMMEND_BRAND)).orderBy(GOODS_BRAND.FIRST.desc(), GOODS_BRAND.CREATE_TIME.desc())
+            .fetchInto(GoodsBrandMpVo.class);
+
+        goodsBrandMpVos.forEach(goodsBrandMpVo -> goodsBrandMpVo.setLogo(getImgFullUrlUtil(goodsBrandMpVo.getLogo())));
+        return goodsBrandMpVos;
+    }
+
+    /**
+     * 获取所有推荐品牌（前端按照品牌分类显示）
+     */
+    public List<List<GoodsBrandMpWithClassifyVo>> getAllRecommendBrandGroupByClassify(){
+        // 获取品牌和其所关联的品牌分类id的对应map
+        Map<Integer, List<GoodsBrandMpWithClassifyVo>> goodsBrandMap =
+            db().select(GOODS_BRAND.ID, GOODS_BRAND.BRAND_NAME, GOODS_BRAND.E_NAME, GOODS_BRAND.LOGO,GOODS_BRAND.CLASSIFY_ID,BRAND_CLASSIFY.CLASSIFY_NAME)
+            .from(GOODS_BRAND).leftJoin(BRAND_CLASSIFY).on(GOODS_BRAND.CLASSIFY_ID.eq(BRAND_CLASSIFY.CLASSIFY_ID))
+            .where(GOODS_BRAND.DEL_FLAG.eq(DelFlag.NORMAL.getCode())).and(GOODS_BRAND.IS_RECOMMEND.eq(GoodsConstant.RECOMMEND_BRAND))
+            .orderBy(GOODS_BRAND.FIRST.desc(), GOODS_BRAND.CREATE_TIME.desc())
+            .fetchGroups(GOODS_BRAND.CLASSIFY_ID, GoodsBrandMpWithClassifyVo.class);
+
+        // 获取品牌分类
+        List<GoodsBrandT> brandClassify = db().selectFrom(BRAND_CLASSIFY).where(BRAND_CLASSIFY.DEL_FLAG.eq(DelFlag.NORMAL.getCode()))
+            .orderBy(BRAND_CLASSIFY.FIRST.desc(), BRAND_CLASSIFY.CREATE_TIME.desc()).fetchInto(GoodsBrandT.class);
+
+        List<List<GoodsBrandMpWithClassifyVo>> returnVo = new ArrayList<>();
+
+        for (GoodsBrandT goodsBrandT : brandClassify) {
+            Integer classifyId = goodsBrandT.getClassifyId();
+            if (goodsBrandMap.get(classifyId) != null) {
+                returnVo.add(goodsBrandMap.get(classifyId));
+            }
+        }
+        if (goodsBrandMap.get(GoodsConstant.NO_CLASSIFY_ID) != null) {
+            returnVo.add(goodsBrandMap.get(GoodsConstant.NO_CLASSIFY_ID));
+        }
+        return returnVo;
+    }
+
+    /**
+     * 将相对路劲修改为全路径
+     *
+     * @param relativePath 相对路径
+     * @return null或全路径
+     */
+    private String getImgFullUrlUtil(String relativePath) {
+        if (org.apache.commons.lang3.StringUtils.isBlank(relativePath)) {
+            return null;
+        } else {
+            return imageService.imageUrl(relativePath);
+        }
     }
 }
