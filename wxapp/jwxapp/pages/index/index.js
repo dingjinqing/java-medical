8d01bc3cf594660b04f9a360c2295223fbc325f1
liuyang <liuyang01@wangdian.cn>
2019-10-17 01:45:51 +0000

    还原index

--- wxapp/jwxapp/pages/index/index.js
+++ wxapp/jwxapp/pages/index/index.js
@@ -1,20 +1,30 @@
 // pages/index/index.js
-Page({
-
+var util = require("../../utils/util.js")
+var decorate = require("../common/decorate.js")
+global.wxPage({
+  mixins: [decorate],
   /**
    * 页面的初始数据
    */
   data: {
-
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    console.log(options)
+    // this._options = options;
+    this.requestDecoratePageData(this._options.page, this._options.scene, this.renderData.bind(this), true);
   },
+  //  渲染装修模块
+  renderData(pageContent) {
+    console.log(pageContent)
 
+    this.setData({
+      pageContent: pageContent
+    })
+  },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
