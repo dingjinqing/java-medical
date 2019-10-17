aebf59d702aaed4001e984eab775d4ab4f6ffd11
zhaojianqiang <zhaojianqiang@wangdian.cn>
2019-10-17 06:34:51 +0000

    加log

--- jmp/mp-web/src/main/java/com/vpu/mp/controller/wxapp/HelpController.java
+++ jmp/mp-web/src/main/java/com/vpu/mp/controller/wxapp/HelpController.java
@@ -1,5 +1,7 @@
 package com.vpu.mp.controller.wxapp;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,16 +18,26 @@ import com.vpu.mp.service.foundation.data.JsonResult;
  */
 @RestController
 public class HelpController extends BaseController {
+	
+	
+	private Logger log=LoggerFactory.getLogger(this.getClass());
+	
+	
+	
 	@GetMapping(value = "/wxapp/score/scoreDocument")
 	public JsonResult scoreDocument(@RequestParam Integer shop_id, @RequestParam Integer user_id) {
+		log.info("查询积分说明");
 		ShopRecord sRecord = saas.shop.getShopById(shop_id);
 		if(sRecord==null) {
+			log.info("店铺不存在");
 			return fail();
 		}
 		ShopCfgRecord scoreNum = saas.getShopApp(shop_id).userCard.scoreService.score.getScoreNum("score_document");
 		if(scoreNum!=null) {
+			log.info("设置查询积分说明");
 			return success(scoreNum.getV());
 		}
+		log.info("未设置查询积分说明");
 		return fail();
 	}
 
