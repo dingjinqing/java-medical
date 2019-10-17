1387fcf32f6668f2d9e22c11e1e4b007914e2bba
kongdecheng <kongdecheng@wangdian.cn>
2019-10-17 06:04:26 +0000

    购物车--查询,修改,删除接口

--- jmp/mp-service/src/main/java/com/vpu/mp/service/shop/user/cart/UserCartService.java
+++ jmp/mp-service/src/main/java/com/vpu/mp/service/shop/user/cart/UserCartService.java
@@ -1,11 +1,13 @@
 package com.vpu.mp.service.shop.user.cart;
 
 import com.vpu.mp.service.foundation.service.ShopBaseService;
+import org.springframework.stereotype.Service;
 
 /**
  * @author 孔德成
  * @date 2019/10/15 10:35
  */
+@Service
 public class UserCartService extends ShopBaseService {
 
 
