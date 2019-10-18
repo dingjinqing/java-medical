8f7abaddb39102ac4c0b1371be6e1d65119bd99e
lixiaobing <lixiaobing@wangdian.cn>
2019-10-18 09:05:13 +0000

    测试商品分类

--- jmp/mp-web/src/main/java/com/vpu/mp/controller/wxapp/WxAppGoodsBrandSortController.java
+++ jmp/mp-web/src/main/java/com/vpu/mp/controller/wxapp/WxAppGoodsBrandSortController.java
@@ -1,8 +1,12 @@
 package com.vpu.mp.controller.wxapp;
 
 import com.vpu.mp.service.foundation.data.JsonResult;
+import com.vpu.mp.service.foundation.util.Util;
+import com.vpu.mp.service.pojo.wxapp.goods.goodssort.GoodsSortMenuVo;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * @author 李晓冰
@@ -17,6 +21,17 @@ public class WxAppGoodsBrandSortController extends WxAppBaseController{
      */
     @PostMapping("/api/wxapp/sort/init")
     public JsonResult goodsSortPageInit(){
-       return success(shop().goodsMp.goodsBrandSortMp.goodsSortPageInit());
+        logger().debug("进入/api/wxapp/sort/init");
+        logger().debug("进入/api/wxapp/sort/init");
+        logger().debug("进入/api/wxapp/sort/init");
+        logger().debug("进入/api/wxapp/sort/init");
+        logger().debug("进入/api/wxapp/sort/init");
+        List<GoodsSortMenuVo> goodsSortMenuVos = shop().goodsMp.goodsBrandSortMp.goodsSortPageInit();
+        logger().debug(Util.toJson(goodsSortMenuVos));
+        logger().debug("返回/api/wxapp/sort/init");
+        logger().debug("返回/api/wxapp/sort/init");
+        logger().debug("返回/api/wxapp/sort/init");
+        logger().debug("返回/api/wxapp/sort/init");
+        return success(goodsSortMenuVos);
     }
 }
