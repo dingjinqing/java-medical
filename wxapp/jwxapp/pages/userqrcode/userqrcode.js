0deb6ad5411c32719c80033e8824fee2aad9f315
zhaojianqiang <zhaojianqiang@wangdian.cn>
2019-10-18 09:22:00 +0000

    二维码

---
+++ wxapp/jwxapp/pages/userqrcode/userqrcode.js
@@ -0,0 +1,105 @@
+// pages/userqrcode/userqrcode.js
+var app = getApp()
+var util = require('../../utils/util.js');
+var Url = app.globalData.baseUrl;
+var imageUrl = app.globalData.imageUrl;
+var posterBase64 = '';
+global.wxPage({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    myQrCode: '',
+    userName: '',
+    userAvatar: '',
+    imageUrl: app.globalData.imageUrl,
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    var _this = this;
+    util.api('/api/wxapp/user/qrcode', function (res) {
+      if (res.error == 0) {
+        let imgStr = res.content.substr(res.content.indexOf('/upload'))
+        util.api('/api/wxapp/upayyun/image', function (d) {
+          if (d.error == 0) {
+            posterBase64 = d.content;
+            _this.setData({
+              userQrCode: res.content
+            })
+          }
+        }, { image_path: imgStr });
+      }
+    })
+    wx.hideShareMenu();
+  },
+
+  bindSaveIamge(e) {
+    if (posterBase64) {
+      util.base64ImageHandle(posterBase64, function (res) {
+        util.toast_success('保存成功');
+      });
+    } else {
+      util.toast_fail('正在生成中...')
+    }
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+    var that = this;
+    var Name = util.getCache('nickName');
+    var Avatar = util.getCache('avatarUrl');
+    var QrCode = util.getCache('myQrCode');
+    return {
+      path: 'pages/userqrcode/userqrcode?userName=' + Name + '&userAvatar=' + Avatar + '&myQrCode=' + QrCode + '&invite_id=' + util.getCache('user_id'),
+    }
+  }
+
+})
