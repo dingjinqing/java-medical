0760c65b3da9ad32eecb69ca872e85a780b08cb7
wangshuai <wangshuai@wangdian.cn>
2019-10-24 02:02:22 +0000

    移动位置

---
+++ jmp/mp-service/src/main/java/com/vpu/mp/service/pojo/wxapp/order/base/BaseParam.java
@@ -0,0 +1,17 @@
+package com.vpu.mp.service.pojo.wxapp.order.base;
+
+import com.vpu.mp.service.pojo.wxapp.login.WxAppSessionUser;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * 基础param
+ * @author 王帅
+ *
+ */
+@Getter
+@Setter
+public class BaseParam {
+	private WxAppSessionUser wxUserInfo;
+}
