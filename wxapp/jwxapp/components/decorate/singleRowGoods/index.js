07ba0eed9ed045a6d893ae42f02b77977c172181
suntengfei <suntengfei@wangdian.cn>
2019-10-25 10:16:42 +0000

    页面装修商品模块部分

---
+++ wxapp/jwxapp/components/decorate/singleRowGoods/index.js
@@ -0,0 +1,15 @@
+var base = require("../mixins/base.js");
+var util = require("../../../utils/util.js")
+
+global.wxComponent({
+  mixins: [base],
+  methods: {
+    onPropChange(newVal, oldVal, changedPath) {
+      console.log(newVal, 'single')
+
+    },
+    bindMenuClick(e) {
+
+    },
+  }
+});
