f91b99c6260d7206e576434903a91336c126fa7d
liuyang <liuyang01@wangdian.cn>
2019-10-29 11:09:10 +0000

    会员卡列表调试

--- wxapp/jwxapp/pages/cardlist/cardlist.js
+++ wxapp/jwxapp/pages/cardlist/cardlist.js
@@ -1,137 +1,194 @@
 let util = require('../../utils/util.js');
 global.wxPage({
-
   /**
    * 页面的初始数据
    */
   data: {
-    list:[
-      {title:'猜猜这是谁建的会员卡'},
-      {title:'猜猜这是谁建的会员卡'},
-      {title:'猜猜这是谁建的会员卡'},
-      {title:'猜猜这是谁建的会员卡'},
-      {title:'猜猜这是谁建的会员卡'},
-      {title:'猜猜这是谁建的会员卡'},
-      {title:'猜猜这是谁建的会员卡'},
-      {title:'猜猜这是谁建的会员卡'},
-      {title:'猜猜这是谁建的会员卡'},
-      {title:'猜猜这是谁建的会员卡'},
-      {title:'猜猜这是谁建的会员卡'},
-      {title:'猜猜这是谁建的会员卡'},
-      {title:'猜猜这是谁建的会员卡'},
-      {title:'猜猜这是谁建的会员卡'},
-      {title:'猜猜这是谁建的会员卡'},
-      {title:'猜猜这是谁建的会员卡'}
-    ]
+    dataList: null,
+    pageParams: null
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-    
+  onLoad: function(options) {
+    this.requestList();
+  },
+  requestList() {
+    let currentPage = this.data.pageParams
+      ? this.data.pageParams.currentPage
+      : 1;
+    util.api(
+      '/api/card/list',
+      res => {
+        if (res.error === 0) {
+          let dataList = this.formatData(res.content.dataList);
+          this.setData({
+            pageParams: res.content.page,
+            ['dataList[' + (parseInt(currentPage) - 1) + ']']: res.content
+              .dataList
+          });
+        }
+        console.log(res);
+      },
+      {
+        currentPage: currentPage,
+        pageRows: 2
+      }
+    );
   },
   // 触摸开始
-  handleTouchStart (e) {
+  handleTouchStart(e) {
     this.startX = e.touches[0].clientX;
-    let idx = e.currentTarget.dataset.index;
-    let target = `list[${idx}].startRight`
+    let dataIdx = e.currentTarget.dataset.data_idx;
+    let itemIdx = e.currentTarget.dataset.item_idx;
+    let target = `dataList[${dataIdx}][${itemIdx}].startRight`;
     this.setData({
-      [target]: this.data.list[idx].x ? this.data.list[idx].x : 0
-    })
+      [target]: this.data.dataList[dataIdx][itemIdx].x
+        ? this.data.dataList[dataIdx][itemIdx].x
+        : 0
+    });
   },
   // 触摸移动
-  handleTouchMove (e) {
+  handleTouchMove(e) {
     var touch = e.touches[0];
-    let idx = e.currentTarget.dataset.index;
-    let target = `list[${idx}].x`
+    let dataIdx = e.currentTarget.dataset.data_idx;
+    let itemIdx = e.currentTarget.dataset.item_idx;
+    let target = `dataList[${dataIdx}][${itemIdx}].x`;
     let endX = touch.clientX;
     let right = 0;
-    if ((endX - this.startX) < 0) {
-      let startRight = this.data.list[idx].startRight;
+    if (!this.data.dataList[dataIdx][itemIdx].cardStopImg) return
+    if (endX - this.startX < 0) {
+      let startRight = this.data.dataList[dataIdx][itemIdx].startRight;
       var change = this.startX - endX;
-          startRight += change;
-          if (startRight > 60) startRight = 60;
-          right = startRight
+      startRight += change;
+      if (startRight > 60) startRight = 60;
+      right = startRight;
     } else {
-      var startRight = this.data.list[idx].startRight;
+      var startRight = this.data.dataList[dataIdx][itemIdx].startRight;
       var change = this.startX - endX;
-          startRight += change;
-          if (startRight < 0) startRight = 0;
-          right  = startRight;
+      startRight += change;
+      if (startRight < 0) startRight = 0;
+      right = startRight;
     }
     this.setData({
       [target]: right
-    })
+    });
   },
   // 触摸结束
-  handleTouchEnd(e){
-    let idx = e.currentTarget.dataset.index;
-    let target = `list[${idx}].x`
+  handleTouchEnd(e) {
+    let dataIdx = e.currentTarget.dataset.data_idx;
+    let itemIdx = e.currentTarget.dataset.item_idx;
+    let target = `dataList[${dataIdx}][${itemIdx}].x`;
     let right = 0;
-    this.data.list[idx].x  > 60 / 2 ? right = 60 : right = 0
+    this.data.dataList[dataIdx][itemIdx].x > 60 / 2
+      ? (right = 60)
+      : (right = 0);
     this.setData({
       [target]: right
-    })
+    });
+  },
+  formatData(dataList) {
+    let newData = dataList.map(item => {
+      item.cardTypeName = this.getTypeName(item.cardType);
+      item.cardBgStyle = this.getCardBg(item);
+      item.cardStopImg = this.getCardStopImage(item);
+      return item;
+    });
+    return newData;
+  },
+  getTypeName(cardType) {
+    switch (cardType) {
+      case 0:
+        return '普通卡';
+      case 1:
+        return '限次卡';
+      case 2:
+        return '等级卡';
+    }
+  },
+  getCardBg(cardItem) {
+    console.log(cardItem);
+    switch (cardItem.bgType) {
+      case 0:
+        return `background-color:${cardItem.bgColor};`;
+      case 1:
+        return `background:url('${this.data.imageUrl}${cardItem.bgImg}') no-repeat top left / 100% 100%;`;
+    }
+  },
+  getCardStopImage(cardItem){
+    if(cardItem.cardType === 2 && cardItem.flag === 2){
+      return `${this.data.imageUrl}image/wxapp/card_stop.png`
+    }
+    if (cardItem.expire === 1){
+      return `${this.data.imageUrl}image/wxapp/card_out_time.png`
+    }
+    return ``
   },
   // 删除会员卡
-  delCard(e){
+  delCard(e) {
     let card_no = e.currentTarget.dataset.card_no;
-    util.showModal('', '您确定要删除该会员卡？', function () {
-      var animate = '';
-      util.api('/api/wxapp/card/del', function (res) {
-        if (res.error === 0) {
-          
-        }
-      }, { card_no: card_no })
-    }, true, '取消', '确定')
+    util.showModal(
+      '',
+      '您确定要删除该会员卡？',
+      function() {
+        var animate = '';
+        util.api(
+          '/api/wxapp/card/del',
+          function(res) {
+            if (res.error === 0) {
+            }
+          },
+          { card_no: card_no }
+        );
+      },
+      true,
+      '取消',
+      '确定'
+    );
   },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
-
-  },
+  onReady: function() {},
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
-
-  },
+  onShow: function() {},
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
-
-  },
+  onHide: function() {},
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
-
-  },
+  onUnload: function() {},
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
-
-  },
+  onPullDownRefresh: function() {},
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
-
+  onReachBottom: function() {
+    if (
+      this.data.pageParams &&
+      this.data.pageParams.currentPage === this.data.pageParams.lastPage
+    )
+      return;
+    this.setData({
+      'pageParams.currentPage': this.data.pageParams.currentPage + 1
+    });
+    this.requestList();
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
-
-  }
-})
+  onShareAppMessage: function() {}
+});
