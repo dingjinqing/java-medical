11c67c511639a6756d2225c9c2f9e80c98e4cad9
wangshuai <wangshuai@wangdian.cn>
2019-10-29 03:27:43 +0000

    user address

---
+++ jmp/mp-service/src/main/java/com/vpu/mp/service/pojo/shop/member/address/UserAddressVo.java
@@ -0,0 +1,25 @@
+package com.vpu.mp.service.pojo.shop.member.address;
+
+import lombok.Data;
+
+/**
+ * 地址
+ * @author 王帅
+ *
+ */
+@Data
+public class UserAddressVo {
+
+	private Integer addressId;
+	private String address;
+	private String mobile;
+	private String consignee;
+	private String provinceName;
+	private Integer provinceCode;
+	private Integer cityCode;
+	private String cityName;
+	private Integer districtCode;
+	private String districtName;
+	private String lat;
+	private String lng;
+}
