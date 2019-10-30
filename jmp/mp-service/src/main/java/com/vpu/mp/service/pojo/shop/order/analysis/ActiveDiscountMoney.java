f25a0fdfb0f770dc2268f64186c6edcb76945f9d
kongdecheng <kongdecheng@wangdian.cn>
2019-10-30 06:24:09 +0000

    拼团报表

--- jmp/mp-service/src/main/java/com/vpu/mp/service/pojo/shop/order/analysis/ActiveDiscountMoney.java
+++ jmp/mp-service/src/main/java/com/vpu/mp/service/pojo/shop/order/analysis/ActiveDiscountMoney.java
@@ -6,6 +6,7 @@ import java.sql.Timestamp;
 import lombok.Data;
 
 /**
+ * 拼团报表 计算销售额bo类
  * @author 孔德成
  * @date 2019/8/5 10:07
  */
@@ -16,14 +17,25 @@ public class ActiveDiscountMoney {
     public static final String GOODS_NUMBER ="goodsNumber";
     public static final String DISCOUNTEDTOTALPRICE ="discountedTotalPrice";
 
+    /**
+     * 时间 %Y-%m-%d
+      */
     private Timestamp createTime;
-
+    /**
+     *  活动优惠金额
+      */
     private BigDecimal marketPrice;
-
+    /**
+     *  活动总金额
+     */
     private BigDecimal goodsPrice;
-
+    /**
+     *  商品数量
+     */
     private int goodsNumber;
-
+    /**
+     *  活动实付总金额
+     */
     private BigDecimal discountedTotalPrice;
 
     private String old;
