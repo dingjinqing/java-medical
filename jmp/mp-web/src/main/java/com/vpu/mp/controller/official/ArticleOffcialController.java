c3e001c25c9679ce7fa7d35ada2b026c92fffd33
liufei <liufei@wangdian.cn>
2019-10-31 01:26:18 +0000

    新闻接口修改

--- jmp/mp-web/src/main/java/com/vpu/mp/controller/official/ArticleOffcialController.java
+++ jmp/mp-web/src/main/java/com/vpu/mp/controller/official/ArticleOffcialController.java
@@ -1,44 +1,43 @@
 package com.vpu.mp.controller.official;
 
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.vpu.mp.service.foundation.data.JsonResult;
 import com.vpu.mp.service.foundation.data.JsonResultCode;
 import com.vpu.mp.service.foundation.util.PageResult;
 import com.vpu.mp.service.pojo.saas.article.ArticleListQueryParam;
 import com.vpu.mp.service.pojo.saas.article.ArticleParam;
 import com.vpu.mp.service.pojo.saas.article.ArticleVo;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
- * 
+ *
  * @author wangshuai
  *
  */
 @RestController
 @RequestMapping("/api/official/article")
 public class ArticleOffcialController extends OfficialBaseController{
-	
+
 	/**
 	 * 官网新闻资讯查询
 	 * @param ArticleListQueryParam
 	 * @returnJsonResult
 	 */
 	@PostMapping("/list")
-	public JsonResult get(@RequestBody ArticleListQueryParam param) {
+    public JsonResult getList(@RequestBody ArticleListQueryParam param) {
 		PageResult<ArticleVo> pageList = saas.article.getPageList(param);
 		return success(pageList);
 	}
-	
-	/**
+
+    /**
 	 * 官网新闻资讯文章具体查询
 	 * @param ArticleParam
 	 * @return JsonResult
 	 */
 	@PostMapping("/get")
-	private JsonResult get(@RequestBody ArticleParam article ) {
+    public JsonResult getDetail(@RequestBody ArticleParam article) {
 		if(null == article.getArticleId()) {
 			return fail(JsonResultCode.CODE_ARTICLE_ARTICLEID_ISNULL);
 		}
