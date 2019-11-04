355256bf95ff9241dd8ac7ae009318eae38d524c
lipengfei <lipengfei@huife.com>
2019-11-04 07:58:44 +0000

    小程序装修-砍价

---
+++ web-vue/src/view/admin/index/leftNavComponents/first_web_decoration/picture_setting/decorationModules/marketingComponents/Bargain.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="modules">
+    <div
+      class="showModule"
+      :class="activeBorder?'activeBorder':''"
+    >
+      <div>我要砍价</div>
+    </div>
+    <!--放这里-->
+    <div
+      class="setHere activeSetHere"
+      :class="activeSetHere?'middleModulesActive':''"
+    >
+      {{$t('commoditySearch.putItHere')}}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {}
+  }
+}
+</script>
