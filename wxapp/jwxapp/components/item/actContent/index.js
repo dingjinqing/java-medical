1d147187290eaf4857c3294b2ec0cb1941f20275
liuyang <liuyang01@wangdian.cn>
2019-11-06 15:34:35 +0000

    活动信息

---
+++ wxapp/jwxapp/components/item/actContent/index.js
@@ -0,0 +1,23 @@
+// components/item/actContent/index.js
+global.wxComponent({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
