d1dcaaa288e113b105b17a7788103f82d346e6d1
zhaojianqiang <zhaojianqiang@wangdian.cn>
2019-11-11 09:16:19 +0000

    好物圈

---
+++ jmp/mp-service/src/main/java/com/vpu/mp/service/pojo/shop/goods/goods/shopMoment/ListProduct.java
@@ -0,0 +1,15 @@
+package com.vpu.mp.service.pojo.shop.goods.goods.shopMoment;
+
+import lombok.Data;
+
+/**
+ * 
+ * @author zhaojianqiang
+ *
+ * 2019年11月11日 下午4:09:02
+ */
+@Data
+public class ListProduct {
+	private Integer goodsId;
+	private Product product;
+}
