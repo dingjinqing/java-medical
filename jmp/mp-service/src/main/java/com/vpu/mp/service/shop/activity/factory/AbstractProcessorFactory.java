4adb46dcae3f411f5955a13487c2538339a885b3
lixiaobing <lixiaobing@wangdian.cn>
2019-11-12 11:20:29 +0000

    修改AbstractProcessorFactory，动态获取泛型类型

--- jmp/mp-service/src/main/java/com/vpu/mp/service/shop/activity/factory/AbstractProcessorFactory.java
+++ jmp/mp-service/src/main/java/com/vpu/mp/service/shop/activity/factory/AbstractProcessorFactory.java
@@ -1,11 +1,14 @@
 package com.vpu.mp.service.shop.activity.factory;
 
 import com.vpu.mp.service.shop.activity.processor.ProcessorPriority;
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
@@ -15,6 +18,7 @@ import java.util.List;
  * @date 2019年10月30日
  */
 @Service
+@Slf4j
 public abstract class AbstractProcessorFactory<P,T> {
 
     protected List<P> processors = new ArrayList<>();
@@ -29,6 +33,7 @@ public abstract class AbstractProcessorFactory<P,T> {
     @PostConstruct
     @SuppressWarnings("unchecked")
     protected void init(){
+        getProcessorImplClass();
         if (sortProcessors == null || sortProcessors.size() == 0) {
             LoggerFactory.getLogger(this.getClass()).error("{}处理器工厂初始化失败", this.getClass());
             processors = new ArrayList<>();
@@ -36,8 +41,10 @@ public abstract class AbstractProcessorFactory<P,T> {
             sort();
             sortProcessors.forEach(processorPriority -> {
                 try {
-                    P p = (P) processorPriority;
-                    processors.add(p);
+                    Class<?> clazz =  getProcessorImplClass();
+                    if (clazz.isAssignableFrom(processorPriority.getClass())) {
+                        processors.add((P) processorPriority);
+                    }
                 }catch (Exception e){
 
                 }
@@ -45,5 +52,17 @@ public abstract class AbstractProcessorFactory<P,T> {
             LoggerFactory.getLogger(this.getClass()).debug(processors.toString());
         }
     }
+
+    private Class<?> getProcessorImplClass(){
+        Type genericSuperclass = this.getClass().getGenericSuperclass();
+        if (!(genericSuperclass instanceof ParameterizedType)) {
+            throw new IllegalArgumentException();
+        }
+        ParameterizedType parameterizedType = (ParameterizedType) genericSuperclass;
+
+        Type[] actualTypeArguments = parameterizedType.getActualTypeArguments();
+        return (Class<?>) actualTypeArguments[0];
+    }
+
     public abstract void doProcess(List<T> capsules,Integer userId);
 }
