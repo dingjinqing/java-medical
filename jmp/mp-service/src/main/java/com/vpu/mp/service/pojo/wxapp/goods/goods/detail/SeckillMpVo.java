38906cdfacbb6319da15a9c2b2edece0a030acdf
wangbingbing <wangbingbing@wangdian.cn>
2019-11-12 11:17:57 +0000

    详情页秒杀

---
+++ jmp/mp-service/src/main/java/com/vpu/mp/service/pojo/wxapp/goods/goods/detail/SeckillMpVo.java
@@ -0,0 +1,57 @@
+package com.vpu.mp.service.pojo.wxapp.goods.goods.detail;
+
+import com.vpu.mp.service.pojo.wxapp.goods.goods.GoodsActivityBaseMp;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.sql.Timestamp;
+import java.util.List;
+
+/**
+ * @author: 王兵兵
+ * @create: 2019-11-12 11:35
+ **/
+@Data
+public class SeckillMpVo extends GoodsActivityBaseMp {
+    /** 0正常;1该活动不存在;2该活动已停用;3该活动未开始;4该活动已结束;5商品已抢光;6该用户已达到限购数量上限;7该秒杀为会员专属，该用户没有对应会员卡; */
+    private Byte actState;
+
+    /** 当前前秒杀剩余库存(各规格库存之和) */
+    private Integer stock;
+
+    /** 每人限购数量 */
+    private Short limitAmount;
+
+    /** 规定的有效支付时间(分钟数) */
+    private Short limitPaytime;
+
+    /** 活动开始时间 */
+    private Timestamp startTime;
+
+    /** 活动结束时间 */
+    private Timestamp endTime;
+
+    /** 专属会员卡ID串，逗号隔开 */
+    private String cardId;
+
+    /** 秒杀规格 */
+    private List<SecKillPrd> secKillPrds;
+
+    @Getter
+    @Setter
+    private class SecKillPrd{
+        /** 规格ID */
+        private Integer productId;
+
+        /** 规格秒杀价 */
+        private BigDecimal seckillPrice;
+
+        /** 规格秒杀库存 */
+        private Integer stock;
+
+        /** 规格秒杀总库存 */
+        private Integer totalStock;
+    }
+}
