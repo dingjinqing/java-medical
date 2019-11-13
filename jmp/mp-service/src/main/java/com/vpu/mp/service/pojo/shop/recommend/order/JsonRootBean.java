f33b7260bd6d1f086c62eee293a2bed337570bd9
zhaojianqiang <zhaojianqiang@wangdian.cn>
2019-11-13 02:17:00 +0000

    添加订单好物圈

---
+++ jmp/mp-service/src/main/java/com/vpu/mp/service/pojo/shop/recommend/order/JsonRootBean.java
@@ -0,0 +1,23 @@
+/**
+  * Copyright 2019 bejson.com 
+  */
+package com.vpu.mp.service.pojo.shop.recommend.order;
+import java.util.List;
+
+/**
+ * Auto-generated: 2019-11-12 10:52:53
+ *
+ * @author bejson.com (i@bejson.com)
+ * @website http://www.bejson.com/java2pojo/
+ */
+public class JsonRootBean {
+
+    private List<OrderList> order_list;
+    public void setOrder_list(List<OrderList> order_list) {
+         this.order_list = order_list;
+     }
+     public List<OrderList> getOrder_list() {
+         return order_list;
+     }
+
+}
