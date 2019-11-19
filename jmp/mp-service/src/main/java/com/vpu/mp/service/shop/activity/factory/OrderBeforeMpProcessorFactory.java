53ad74f632bc4b441be433e498e3a7edecd69d87
lixiaobing <lixiaobing@wangdian.cn>
2019-11-19 03:12:44 +0000

    OrderBefore工厂，重写init方法

--- jmp/mp-service/src/main/java/com/vpu/mp/service/shop/activity/factory/OrderBeforeMpProcessorFactory.java
+++ jmp/mp-service/src/main/java/com/vpu/mp/service/shop/activity/factory/OrderBeforeMpProcessorFactory.java
@@ -4,9 +4,13 @@ import com.vpu.mp.service.foundation.data.BaseConstant;
 import com.vpu.mp.service.pojo.wxapp.order.OrderBeforeParam;
 import com.vpu.mp.service.pojo.wxapp.order.OrderBeforeVo;
 import com.vpu.mp.service.shop.activity.processor.OrderBeforeProcessor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.PostConstruct;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * 跳转结算页所需要的营销数据处理
@@ -14,12 +18,25 @@ import java.util.List;
  * @create: 2019-11-14 17:24
  **/
 @Service
+@Slf4j
 public class OrderBeforeMpProcessorFactory extends AbstractProcessorFactory<OrderBeforeProcessor, OrderBeforeParam> {
     @Override
     public void doProcess(List<OrderBeforeParam> capsules, Integer userId) {
+
     }
 
+    protected Map<Class<? extends OrderBeforeProcessor>,OrderBeforeProcessor> processorMap;
 
+    @Override
+    @PostConstruct
+    protected void init(){
+        super.init();
+        processorMap = new HashMap<>(processors.size());
+        for (OrderBeforeProcessor processor : processors) {
+            processorMap.put(processor.getClass(), processor);
+        }
+        log.debug(processorMap.toString());
+    }
 
     public void doProcess(OrderBeforeParam orderBeforeParam, OrderBeforeVo vo) {
         if(orderBeforeParam.getActivityType().equals(BaseConstant.ACTIVITY_TYPE_SEC_KILL)){
