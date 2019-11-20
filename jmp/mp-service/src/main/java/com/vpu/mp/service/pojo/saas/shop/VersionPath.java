5d920943b6b55b6948588eced8b541c8ecfe380a
zhaojianqiang <zhaojianqiang@wangdian.cn>
2019-11-20 03:30:23 +0000

    版本权限校验

---
+++ jmp/mp-service/src/main/java/com/vpu/mp/service/pojo/saas/shop/VersionPath.java
@@ -0,0 +1,15 @@
+package com.vpu.mp.service.pojo.saas.shop;
+
+import lombok.Data;
+
+/**
+ * 传送的url请求
+ * @author zhaojianqiang
+ *
+ * 2019年11月20日 上午11:17:50
+ */
+@Data
+public class VersionPath {
+	private String path;
+
+}
