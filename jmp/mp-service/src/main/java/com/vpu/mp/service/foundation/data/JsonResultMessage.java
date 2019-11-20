80e491d7962a04297b29ab1758b6fb5a66bcac26
zhaojianqiang <zhaojianqiang@wangdian.cn>
2019-11-20 03:48:44 +0000

    版本权限校验

--- jmp/mp-service/src/main/java/com/vpu/mp/service/foundation/data/JsonResultMessage.java
+++ jmp/mp-service/src/main/java/com/vpu/mp/service/foundation/data/JsonResultMessage.java
@@ -44,6 +44,7 @@ public class JsonResultMessage {
 	public static final String MSG_CODE_ACCOUNT_VERSIN_NO_POWER = "account.version.no.power";
 	public static final String MSG_ACCOUNT_SHOPTYPE_REGISTERED = "account.shoptype.notNull";
 	public static final String MSG_ACCOUNT_SYTEM_LOGIN_EXPIRED = "system.account.login.expired";
+	public static final String MSG_CODE_ACCOUNT_ENNAME_ISNULL = "enanme.isnull";
 
 	// 图片
 
