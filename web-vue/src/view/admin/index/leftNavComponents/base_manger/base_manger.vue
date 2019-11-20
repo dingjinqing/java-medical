aede5cfe598748bcc05fb8eedd5c5188f1bdb079
lipengfei <lipengfei@huife.com>
2019-11-20 08:59:22 +0000

    基础配置一进入打开第一个

--- web-vue/src/view/admin/index/leftNavComponents/base_manger/base_manger.vue
+++ web-vue/src/view/admin/index/leftNavComponents/base_manger/base_manger.vue
@@ -53,7 +53,7 @@ export default {
     defaultData () {
       // console.log(this.$route.params.otherTurn, 111)
       if (this.$route.params.isAuth) {
-        this.activeName = 'second'
+        this.activeName = 'first'
       }
       queryAuthdritionRequest().then(res => {
         if (res.error === 170016) {
