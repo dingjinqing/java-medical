029fa87316560f0e99405e96cae141a188fd2f9a
kongdecheng <kongdecheng@wangdian.cn>
2019-11-27 02:27:43 +0000

    订单号序列

---
+++ jmp/mp-service/src/main/java/com/vpu/mp/service/foundation/util/IncrSequenceUtil.java
@@ -0,0 +1,48 @@
+package com.vpu.mp.service.foundation.util;
+
+import com.vpu.mp.service.foundation.jedis.JedisManager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+/**
+ * 订单号序列工具
+ * @author 孔德成
+ * @date 2019/11/27 10:21
+ */
+@Component
+public class IncrSequenceUtil {
+    private static Util util ;
+
+
+    private static JedisManager jedisManager;
+
+    public static final String DATE_FORMAT_FULL_NO_UNDERLINE = "yyyyMMddHHmmss";
+
+    @Autowired
+    public void setJedisManager(JedisManager jedisManager){
+        IncrSequenceUtil.jedisManager =jedisManager;
+    }
+
+
+    /**
+     * 订单号序列
+     * @param prefix
+     * @param dateFormat
+     * @param key
+     * @return
+     */
+    public static String generateOrderSn(String prefix,String dateFormat,String key){
+        return new StringBuilder(prefix)
+                .append(DateUtil.getLocalTimeDateBySelf(dateFormat))
+                .append(jedisManager.getIncrSequence(key)).toString();
+    }
+
+    /**
+     * 订单号序列
+     * @param prefix
+     * @return
+     */
+    public static String generateOrderSn(String prefix){
+        return generateOrderSn(prefix,DATE_FORMAT_FULL_NO_UNDERLINE,prefix);
+    }
+}
