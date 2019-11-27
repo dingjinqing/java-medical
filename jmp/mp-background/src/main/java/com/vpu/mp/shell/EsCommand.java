421b2c65dc0600cfc13b4b21fdd53ccc69545f4c
luguangyao <luguangyao@wangdian.cn>
2019-11-27 09:49:10 +0000

    feat:add

--- jmp/mp-background/src/main/java/com/vpu/mp/shell/EsCommand.java
+++ jmp/mp-background/src/main/java/com/vpu/mp/shell/EsCommand.java
@@ -1,11 +1,11 @@
 package com.vpu.mp.shell;
 
 
-import java.util.Collections;
-import java.util.List;
-import java.util.Objects;
+import java.io.IOException;
+import java.util.*;
 import java.util.stream.Collectors;
 
+import com.vpu.mp.service.shop.goods.es.goods.EsGoodsConstant;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.shell.Availability;
 import org.springframework.shell.standard.ShellCommandGroup;
@@ -36,7 +36,9 @@ public class EsCommand {
     @Autowired
     private SshShellHelper helper;
 
-    @ShellMethod("ElasticSearch Index . --all[false|true]default true --shop-id<shopId>")
+
+
+    @ShellMethod("ElasticSearch goods Index . --all[false|true]default true --shop-id<shopId>")
     @ShellMethodAvailability("argsAvailability")
     public void es(@ShellOption(arity = 1, defaultValue = "true") boolean all,@ShellOption( defaultValue = "0")int shopId) {
 
@@ -53,23 +55,33 @@ public class EsCommand {
             esThreadConfig.doIndexByShopId(x);
         });
     }
-    @ShellMethod("ElasticSearch Index . --all[false|true]default true --shopId<shopId>")
-    public void ess() {
-        for (int i = 0; i < 1000; i++) {
-            log.info("\nsend--->【{}】",i);
-            saas.getShopApp(674625).esGoodsService.addEsGoodsIndex(Collections.singletonList(2),i);
+    @ShellMethod("ElasticSearch lanbel Index . --all[false|true]default true --shopId<shopId>")
+    public void esl(@ShellOption(arity = 1, defaultValue = "true") boolean all,@ShellOption( defaultValue = "0")int shopId) {
+        List<Integer> shopIdList = null;
+        if( all ){
+            shopIdList = saas.shop.getAll().stream().map(ShopRecord::getShopId).collect(Collectors.toList());
+        }else if( 0 != shopId ){
+            shopIdList = Collections.singletonList(shopId);
+        }
+        if( Objects.requireNonNull(shopIdList).isEmpty() ){
+            helper.printError("Please shopId is not null");
+        }
+        shopIdList.forEach(x->{
+            esThreadConfig.doLabelIndexByShopId(x);
+        });
+    }
+    @ShellMethod("ElasticSearch goods label test query . --id")
+    public void eslt(@ShellOption( defaultValue = "0")int shopId,@ShellOption(arity = 1, defaultValue = "true") String id) {
+        try {
+            saas.getShopApp(shopId).esGoodsLabelSearchService.getGoodsLabelByGoodsId(
+                Arrays.stream(id.split(",")).map(Integer::valueOf).collect(Collectors.toList()),
+                EsGoodsConstant.ADMIN_GOODS_LIST_PAGE
+            );
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
-    public static void main(String[] args) {
-        EsTaskParam param = new EsTaskParam();
-        param.setGoodsIdList(Collections.singletonList(2));
-        param.setShopId(1);
-        String jonStr = Util.toJson(param);
-
-        System.out.println(jonStr);
-        EsTaskParam p = Util.parseJson(jonStr,EsTaskParam.class);
-    }
     private Availability argsAvailability(){
         if( helper.checkAuthorities(Collections.singletonList("admin")) ){
             return Availability.unavailable("admin command is only for an admin users !");
