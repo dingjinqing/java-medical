d42ad6d08ab070d19e454b0329ddc7140c4f7a98
huangzhuangzhuang <huangzhuangzhuang@wangdian.cn>
2019-11-28 08:16:06 +0000

    更新

--- jmp/mp-web/src/main/java/com/vpu/mp/controller/admin/AdminMemberCardController.java
+++ jmp/mp-web/src/main/java/com/vpu/mp/controller/admin/AdminMemberCardController.java
@@ -5,9 +5,6 @@ import java.math.BigDecimal;
 import java.util.List;
 
 import javax.validation.Valid;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -36,6 +33,7 @@ import com.vpu.mp.service.pojo.shop.member.card.CodeReceiveVo;
 import com.vpu.mp.service.pojo.shop.member.card.PowerCardParam;
 import com.vpu.mp.service.pojo.shop.member.card.SearchCardParam;
 import com.vpu.mp.service.pojo.shop.member.data.AccountData;
+import com.vpu.mp.service.pojo.shop.member.data.UserCardData;
 import com.vpu.mp.service.pojo.shop.operation.RecordTradeEnum;
 
 /**
@@ -47,14 +45,13 @@ import com.vpu.mp.service.pojo.shop.operation.RecordTradeEnum;
 @RestController
 @RequestMapping(value = "/api/admin/member")
 public class AdminMemberCardController extends AdminBaseController {
-	private Logger logger = LoggerFactory.getLogger(getClass());
 
 	/**
 	 * 会员卡 - 创建
 	 */
 	@PostMapping("/card/add")
 	public JsonResult createMemberCard(@RequestBody CardParam card) {
-		logger.info("创建会员卡");
+		logger().info("创建会员卡");
 		shop().member.card.createMemberCard(card);
 		return this.success();
 	}
@@ -64,7 +61,7 @@ public class AdminMemberCardController extends AdminBaseController {
      */
 	@PostMapping("/card/update")
 	public JsonResult updateCard(@RequestBody CardParam param) {
-		logger.info("更新会员卡");
+		logger().info("更新会员卡");
 		shop().member.card.updateMemberCard(param);
 		return success();
 	}
@@ -73,7 +70,7 @@ public class AdminMemberCardController extends AdminBaseController {
      */
 	@PostMapping("/card/test/update")
 	public JsonResult updateCardTest(@RequestBody CardParam param) {
-		logger.info("更新会员卡测试");
+		logger().info("更新会员卡测试");
 		return success();
 	}
 	
@@ -86,7 +83,7 @@ public class AdminMemberCardController extends AdminBaseController {
 	@PostMapping("/card/list")
 	public JsonResult getCardList(@RequestBody SearchCardParam param) {
 
-		logger.info(param.toString());
+		logger().info(param.toString());
 		PageResult<? extends BaseCardVo> result = shop().member.card.getCardList(param);
 		
 		return success(result);
@@ -100,7 +97,7 @@ public class AdminMemberCardController extends AdminBaseController {
 	 */
 	@PostMapping("/card/power")
 	public JsonResult powerCard(@RequestBody PowerCardParam param) {
-		logger.info("开始处理禁用或启动会员卡");
+		logger().info("开始处理禁用或启动会员卡");
 		shop().member.card.powerCard(param);
 		return success();
 	}
@@ -113,7 +110,7 @@ public class AdminMemberCardController extends AdminBaseController {
 	 */
 	@PostMapping("/card/delete")
 	public JsonResult deleteCard(@RequestBody @Valid CardIdParam param) {
-		logger.info("开始删除会员卡");
+		logger().info("开始删除会员卡");
 		shop().member.card.deleteCard(param);
 		return success();
 	}
@@ -126,7 +123,7 @@ public class AdminMemberCardController extends AdminBaseController {
 	 */
 	@PostMapping("/card/get")
 	public JsonResult getCardById(@RequestBody @Valid CardIdParam param) {
-		logger.info("获取会员卡的详细信息");
+		logger().info("获取会员卡的详细信息");
 		BaseCardVo card = shop().member.card.getCardDetailById(param);
 		return success(card);
 	}	
@@ -136,7 +133,7 @@ public class AdminMemberCardController extends AdminBaseController {
 	 */
 	@PostMapping("/card/all/get")
 	public JsonResult getAllUserCard() {
-		logger.info("获取所有会员卡弹窗");
+		logger().info("获取所有会员卡弹窗");
 		List<CardBasicVo> allUserCard = shop().member.card.getAllUserCard();
 		return success(allUserCard);
 	}
@@ -147,7 +144,7 @@ public class AdminMemberCardController extends AdminBaseController {
 	 */
 	@PostMapping("/card/exclusive/get")
 	public JsonResult getCardExclusive() {
-		logger.info("获取所有专享会员卡弹窗");
+		logger().info("获取所有专享会员卡弹窗");
 		List<CardBasicVo> allUserCard = shop().member.card.getCardExclusive();
 		return success(allUserCard);
 	}
@@ -157,7 +154,7 @@ public class AdminMemberCardController extends AdminBaseController {
 	 */
 	@PostMapping("/cardholder")
 	public JsonResult getAllCardHolders(@RequestBody CardHolderParam param) {
-		logger.info("获取所有持卡会员");
+		logger().info("获取所有持卡会员");
 		PageResult<CardHolderVo> result = shop().member.card.getAllCardHolder(param);
 		return success(result);
 	}
@@ -169,7 +166,7 @@ public class AdminMemberCardController extends AdminBaseController {
 	 */
 	@PostMapping("/code/receivelist")
 	public JsonResult getReceiveList(@RequestBody CodeReceiveParam param) {
-		logger.info("获取会员卡领取详情");
+		logger().info("获取会员卡领取详情");
 		PageResult<CodeReceiveVo> result = shop().member.card.getReceiveList(param);
 		return success(result);
 	}
@@ -181,7 +178,7 @@ public class AdminMemberCardController extends AdminBaseController {
 	 */
 	@PostMapping("/card/batch/get/{cardId}")
 	public JsonResult getCardBatchList(@PathVariable Integer cardId) {
-		logger.info("获取会员卡所有批次");
+		logger().info("获取会员卡所有批次");
 		List<CardBatchVo> cardBatchList = shop().member.card.getCardBatchList(cardId);
 		return success(cardBatchList);
 	}
@@ -193,7 +190,7 @@ public class AdminMemberCardController extends AdminBaseController {
 	 */
 	@PostMapping("/card/batch/delete/{id}")
 	public JsonResult deleteCardBatch(@PathVariable Integer id) {
-		logger.info("删除会员卡批次");
+		logger().info("删除会员卡批次");
 		shop().member.card.deleteCardBatch(id);
 		return success();
 	}
@@ -288,6 +285,7 @@ public class AdminMemberCardController extends AdminBaseController {
 	@PostMapping(value="/api/card/getcard")
 	public JsonResult getCard() {
 		logger().info("领取会员卡");
+		
 		AccountData accountData = AccountData.newBuilder().
 	            userId(134).
 	            orderSn("123456").
@@ -305,8 +303,10 @@ public class AdminMemberCardController extends AdminBaseController {
 	            //资金流量-支出
 	                tradeFlow(RecordTradeEnum.TRADE_FLOW_OUT.val()).build();
 		
+		
+
 		try {
-			shop().recordTradeService.updateUserEconomicData(accountData);
+			shop().recordTradeService.updateUserEconomicData(UserCardData.newBuilder().userId(6).cardNo("4448016508219965").build());
 		} catch (MpException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
