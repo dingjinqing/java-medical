77ecd9f59291ae082320eeb5dfd1f3e331fe0465
lipengfei <lipengfei@huife.com>
2019-11-28 06:23:10 +0000

    买单-会员卡

--- wxapp/jwxapp/pages1/shopcheckout/shopcheckout.js
+++ wxapp/jwxapp/pages1/shopcheckout/shopcheckout.js
@@ -1,46 +1,163 @@
 // pages1/shopcheckout/shopcheckout.js
 // 门店买单
 var util = require('../../utils/util.js');
-var score_money = 0;
-var account_money = 0;
-var charge_money = 0;
-var card_money = 0;
-var discount_block = 0;
-var cardMode = true;
-var invoice_info = {};
-var create_order = {};
-var card_arr = [];
-var card_choose = {};
-var ipt_money = 0;
-var score_enable;
-var score_num;
-var score_ratio;
-var text_area;
-var score_money_input;
-var user_account_input;
-var card_discount_input;
-var score_block = 0;
-var is_submit = false;
 
 global.wxPage({
-
   /**
    * 页面的初始数据
    */
   data: {
-    storeId: '',
-    PointPaymentVisible: false, // 积分支付是否显示
-    discountVisible: false, // 折扣是否显示
-    score_money: 0,
-    account_money: 0,
-    charge_money: 0,
-    card_money: 0,
-    discount_block: 0,
-    cardMode: true,
+    storeId: '', // 门店id
+    userId: '', // 用户id
+    storePayInfo: {
+      score: '', // 用户积分
+      account: '', // 用户余额
+      invoiceSwitch: false, // 是否默认选中发票开关
+      memberCardList: [], // 可用会员卡列表
+      shopLogo: '', // 店铺logo
+      shopBusinessState: 1, // 店铺营业状态
+      storeBusinessState: 1, // 门店营业状态
+      storeBuy: 1, // 门店是否支持买单开关
+      delFlag: 0, // 门店是否已删除 
+    },
+    orderInfo: {
+      totalPrice: '', // 总金额
+      cardChargeDis: 0, // 会员卡折扣抵扣金额
+      card_money: 0, // 会员卡余额抵扣金额
+      scoreDis: 0, // 积分抵扣金额
+      accountDis: 0, // 余额抵扣金额
+      orderAmount: '', // 订单金额
+      invoiceId: '', // 发票id
+      formId: '', // 微信formId
+      remark: '' // 留言
+    },
 
     showCardDialog: false, // 会员卡弹窗是否显示
-    cardList: [], // 会员卡列表
     useCard: {} // 选中的会员卡
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    if (!util.check_setting(options)) return;
+    let that = this
+    let storeId = options.storeId
+    this.setData({
+      storeId: storeId
+    })
+
+    // 请求用户信息
+    let params = {
+      storeId: storeId,
+      userId: util.getCache('user_id')
+    }
+    util.api('/api/wxapp/store/info', function (res) {
+      console.log(res)
+      var info = res.content;
+      if (info.storeBuy === 0) {
+        util.showModal('提示', '商家已禁止使用门店买单功能', function () {
+          util.reLaunch({
+            url: '/pages/index/index'
+          })
+        });
+        return;
+      }
+      if (info.delFlag == 1) {
+        util.showModal('提示', '该门店已删除', function () {
+          util.reLaunch({
+            url: '/pages/index/index'
+          })
+        });
+        return;
+      }
+      if (info.storeBusinessState == 0) {
+        util.showModal('提示', '该店铺未营业，随便逛逛', function () {
+          util.reLaunch({
+            url: '/pages/index/index'
+          })
+        });
+      }
+      if (info.storeBusinessState == 0) {
+        util.showModal('提示', '该门店未营业，随便逛逛', function () {
+          util.reLaunch({
+            url: '/pages/index/index'
+          })
+        });
+      }
+      //会员卡
+      var src_yes = that.data.imageUrl + 'image/wxapp/selected.png';
+      var src_no = that.data.imageUrl + 'image/wxapp/icon_rectangle.png';
+      var bg;
+      if (info.memberCardList && info.memberCardList.length > 0) {
+        info.memberCardList.forEach((item, i) => {
+          if (item.discount == null) {
+            item.discount = 10
+          }
+          item.src_yes = src_yes
+          item.src_no = src_no
+        })
+      }
+      for (var i in card_arr) {
+        if (card_arr[i].discount == null) {
+          card_arr[i].discount = 10;
+        }
+        card_arr[i].src_yes = src_yes;
+        card_arr[i].src_no = src_no;
+        card_arr[i].card_src = src_no;
+        card_arr[i].bg = bg;
+        card_arr[i].bg_img = that.data.imageUrl + card_arr[i].bg_img;
+        card_arr[i].shop_logo = info.shop_logo;
+        if (info.shop_logo == null) {//如果店铺头像为空
+          card_arr[i].shop_logo = that.data.imageUrl + 'image/wxapp/shop_logo_default.png';
+        }
+        if (card_arr[i].bg_type == 1) {//会员卡的背景显示
+          card_arr[i].bg = "url('" + card_arr[i].bg_img + "') no-repeat";
+        } else {
+          card_arr[i].bg = card_arr[i].bg_color;
+        }
+        if (card_arr[i].is_default == 1) {
+          useCard.card_name = card_arr[i].card_name;
+          useCard.discount = (1 - card_arr[i].discount / 10).toFixed(3);
+          card_arr[i].card_src = src_yes;
+          if (card_arr[i].money && card_arr[i].money > 0) {
+            useCard.money = card_arr[i].money;
+          }
+          create_order.card_no = card_arr[i].card_no;
+        } else {
+          useCard.card_name = card_arr[0].card_name;
+          useCard.discount = (1 - card_arr[0].discount / 10).toFixed(3);
+          card_arr[0].card_src = src_yes;
+          if (card_arr[0].money && card_arr[0].money > 0) {
+            useCard.money = card_arr[0].money;
+          }
+          create_order.card_no = card_arr[0].card_no;
+        }
+        if (card_arr[i].card_type == 0 && card_arr[i].buy_score) {
+          card_arr[i].buy_score = JSON.parse(card_arr[i].buy_score);
+          if (card_arr[i].buy_score[0].value == 0) {
+            var arrlen = card_arr[i].buy_score.length;
+            if (card_arr[i].buy_score[arrlen - 1].each_money) {
+              card_arr[i].buy_score1 = card_arr[i].buy_score.slice(1, arrlen - 1);
+            } else {
+              card_arr[i].buy_score1 = card_arr[i].buy_score.slice(1);
+            }
+          }
+          if (card_arr[i].buy_score[0].value == 1) {
+            card_arr[i].buy_score1 = card_arr[i].buy_score[card_arr[i].buy_score.length - 1];
+          }
+        }
+        if (card_arr[i].expire_time != null) {
+          card_arr[i].expire_time = card_arr[i].expire_time.substring(0, 10);
+        }
+      }
+      //发票
+      invoice_info.invoice_title = '';
+      that.setData({
+        storePayInfo: info,
+        useCard: info.memberCardList && info.memberCardList.length > 0 ? info.memberCardList[0] : {}
+      })
+    }, params)
   },
 
   /**
@@ -90,24 +207,24 @@ global.wxPage({
       if (i == id) {
         card_arr[i].card_src = card_arr[i].src_yes;
         create_order.card_no = card_arr[i].card_no;
-        card_choose.card_name = card_arr[i].card_name;
-        card_choose.discount = (1 - card_arr[i].discount / 10).toFixed(3);
+        useCard.card_name = card_arr[i].card_name;
+        useCard.discount = (1 - card_arr[i].discount / 10).toFixed(3);
         if (card_arr[i].money && card_arr[i].money > 0) {
-          card_choose.money = card_arr[i].money;
+          useCard.money = card_arr[i].money;
         } else {
-          card_choose.money = 0;
+          useCard.money = 0;
         }
       }
     }
     this.setData({
       card_arr: card_arr,
-      card_choose: card_choose
+      useCard: useCard
     })
   },
   cardConfirm: function (e) {
     if (ipt_money > 0) {
       var start_nomey = parseFloat(ipt_money);
-      card_money = (start_nomey * card_choose.discount).toFixed(2);
+      card_money = (start_nomey * useCard.discount).toFixed(2);
       start_nomey = start_nomey - card_money;
       this.data.info.total_price = start_nomey.toFixed(2);
     }
@@ -198,7 +315,7 @@ global.wxPage({
     this.data.info.discount_money = (Number(ipt_money) - Number(this.data.info.total_price)).toFixed(2);//总的折扣数
     this.setData({
       info: this.data.info,
-      card_choose: card_choose,
+      useCard: useCard,
       cardMode: true,
       card_money: card_money,
       score_enable: score_enable
@@ -259,7 +376,7 @@ global.wxPage({
     if (card_arr.length > 0) {
       if (ipt_money >= 0) {
         var start_nomey = ipt_money;
-        card_money = (start_nomey * card_choose.discount).toFixed(2);
+        card_money = (start_nomey * useCard.discount).toFixed(2);
         start_nomey = start_nomey - card_money;
       }
       this.data.info.ipt_money = ipt_money;
@@ -514,8 +631,8 @@ global.wxPage({
       return false;
     }
     else {
-      if (card_discount_input > parseInt(card_choose.money)) {
-        var tishi3 = "最多可以使用：" + card_choose.money + "会员卡余额";
+      if (card_discount_input > parseInt(useCard.money)) {
+        var tishi3 = "最多可以使用：" + useCard.money + "会员卡余额";
         util.showModal('提示', tishi3);
         this.setData({
           card_discount_input: '',
@@ -697,154 +814,7 @@ global.wxPage({
     console.log(e)
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-    if (!util.check_setting(options)) return;
-    let storeId = options.storeId
-    this.setData({
-      storeId: storeId
-    })
 
-    let that = this
-    cardMode = true;
-    invoice_info = {};
-    create_order = {};
-    card_arr = [];
-    card_choose = {};
-    ipt_money = '';
-    score_money = 0;
-    account_money = 0;
-    charge_money = 0;
-    card_money = 0;
-    score_enable = 0;
-    score_num = 0;//积分
-    text_area = '';
-    score_money_input = 0;
-    user_account_input = 0;
-    card_discount_input = 0;
-    score_ratio = 100;
-    score_block = 0;
-    create_order.store_id = options.store_id;
-
-    // 请求用户信息
-    let params = {
-      storeId: storeId
-    }
-    util.api('/api/wxapp/store/info', function (res) {
-      console.log(res)
-      var info = res.content;
-      if (info.storeBuy === 0) {
-        util.showModal('提示', '商家已禁止使用门店买单功能', function () {
-          util.reLaunch({
-            url: '/pages/index/index'
-          })
-        });
-        return;
-      }
-      if (info.delFlag == 1) {
-        util.showModal('提示', '该门店已删除', function () {
-          util.reLaunch({
-            url: '/pages/index/index'
-          })
-        });
-        return;
-      }
-      if (info.storeBusinessState == 0) {
-        util.showModal('提示', '该店铺未营业，随便逛逛', function () {
-          util.reLaunch({
-            url: '/pages/index/index'
-          })
-        });
-      }
-      if (info.storeBusinessState == 0) {
-        util.showModal('提示', '该门店未营业，随便逛逛', function () {
-          util.reLaunch({
-            url: '/pages/index/index'
-          })
-        });
-      }
-      score_num = info.score;//积分
-      info.total_price = 0;//消费总额
-      info.discount_money = 0;//总折扣
-      //会员卡
-      card_arr = info.memberCardList;
-      var src_yes = that.data.imageUrl + 'image/wxapp/selected.png';
-      var src_no = that.data.imageUrl + 'image/wxapp/icon_rectangle.png';
-      var bg;
-      for (var i in card_arr) {
-        if (card_arr[i].discount == null) {
-          card_arr[i].discount = 10;
-        }
-        card_arr[i].src_yes = src_yes;
-        card_arr[i].src_no = src_no;
-        card_arr[i].card_src = src_no;
-        card_arr[i].bg = bg;
-        card_arr[i].bg_img = that.data.imageUrl + card_arr[i].bg_img;
-        card_arr[i].shop_logo = info.shop_logo;
-        if (info.shop_logo == null) {//如果店铺头像为空
-          card_arr[i].shop_logo = that.data.imageUrl + 'image/wxapp/shop_logo_default.png';
-        }
-        if (card_arr[i].bg_type == 1) {//会员卡的背景显示
-          card_arr[i].bg = "url('" + card_arr[i].bg_img + "') no-repeat";
-        } else {
-          card_arr[i].bg = card_arr[i].bg_color;
-        }
-        if (card_arr[i].is_default == 1) {
-          card_choose.card_name = card_arr[i].card_name;
-          card_choose.discount = (1 - card_arr[i].discount / 10).toFixed(3);
-          card_arr[i].card_src = src_yes;
-          if (card_arr[i].money && card_arr[i].money > 0) {
-            card_choose.money = card_arr[i].money;
-          }
-          create_order.card_no = card_arr[i].card_no;
-        } else {
-          card_choose.card_name = card_arr[0].card_name;
-          card_choose.discount = (1 - card_arr[0].discount / 10).toFixed(3);
-          card_arr[0].card_src = src_yes;
-          if (card_arr[0].money && card_arr[0].money > 0) {
-            card_choose.money = card_arr[0].money;
-          }
-          create_order.card_no = card_arr[0].card_no;
-        }
-        if (card_arr[i].card_type == 0 && card_arr[i].buy_score) {
-          card_arr[i].buy_score = JSON.parse(card_arr[i].buy_score);
-          if (card_arr[i].buy_score[0].value == 0) {
-            var arrlen = card_arr[i].buy_score.length;
-            if (card_arr[i].buy_score[arrlen - 1].each_money) {
-              card_arr[i].buy_score1 = card_arr[i].buy_score.slice(1, arrlen - 1);
-            } else {
-              card_arr[i].buy_score1 = card_arr[i].buy_score.slice(1);
-            }
-          }
-          if (card_arr[i].buy_score[0].value == 1) {
-            card_arr[i].buy_score1 = card_arr[i].buy_score[card_arr[i].buy_score.length - 1];
-          }
-        }
-        if (card_arr[i].expire_time != null) {
-          card_arr[i].expire_time = card_arr[i].expire_time.substring(0, 10);
-        }
-      }
-      //发票
-      invoice_info.invoice_title = '';
-      that.setData({
-        info: info,
-        invoice_info: invoice_info,
-        card_choose: card_choose,
-        score_block: score_block,
-        ipt_money: ipt_money,
-        score_money: score_money,
-        account_money: account_money,
-        charge_money: charge_money,
-        card_money: card_money,
-        text_area: text_area,
-        score_money_input: '',
-        user_account_input: '',
-        card_discount_input: ''
-      })
-    }, params)
-  },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
