531bccaf0ea5bf44e2a5ba652ebfb189ed17bad9
suntengfei <suntengfei@wangdian.cn>
2019-12-03 01:22:55 +0000

    system样式修正

--- web-vue/src/view/system_new/layout/index.vue
+++ web-vue/src/view/system_new/layout/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="systemMain">
     <CustomHeader />
     <div class="system_contant">
       <LeftNavigation />
@@ -26,9 +26,16 @@ export default {
 </script>
 
 <style scoped lang="scss">
-.system_contant {
-  padding: 100px 10px 0 160px;
-  background: #e0e3ec;
-  min-height: 100vh;
+.systemMain {
+  position: absolute;
+  right: 0;
+  left: 0;
+  bottom: 0;
+  top: 0;
+  .system_contant {
+    padding: 100px 10px 0 160px;
+    background: #e0e3ec;
+    min-height: 100vh;
+  }
 }
 </style>
