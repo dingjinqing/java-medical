517f9973f8368ab47264f324568d18f17d6cb501
zhaojianqiang <zhaojianqiang@wangdian.cn>
2019-12-05 09:05:35 +0000

    测试

---
+++ jmp/mp-service/src/main/java/com/vpu/mp/service/pojo/wxapp/subscribe/TemplateVo.java
@@ -0,0 +1,19 @@
+package com.vpu.mp.service.pojo.wxapp.subscribe;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * 
+ * @author zhaojianqiang
+ *
+ * 2019年12月5日 下午4:44:33
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class TemplateVo {
+	private String templateId;
+	private Integer id;
+}
