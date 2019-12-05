643f0f6e716906e18c516c7f9e5112f24cbf05f5
liufei <liufei@wangdian.cn>
2019-12-05 01:50:07 +0000

    添加出参

--- jmp/mp-service/src/main/java/com/vpu/mp/service/pojo/shop/store/service/order/ServiceOrderListQueryVo.java
+++ jmp/mp-service/src/main/java/com/vpu/mp/service/pojo/shop/store/service/order/ServiceOrderListQueryVo.java
@@ -9,47 +9,49 @@ import lombok.Data;
  */
 @Data
 public class ServiceOrderListQueryVo {
-	
+
 	private Integer storeId;
-	
+
 	private String orderSn;
-	
-	/**
-	 *预约人ID 
+    private Integer orderId;
+
+
+    /**
+     *预约人ID
 	 */
 	private String userId;
-	
+
 	/**
-	 *预约人姓名 
+     *预约人姓名
 	 */
 	private String subscriber;
-	
+
 	private String serviceName;
-	
-	private String mobile;
-	
-	/**
-	 *预约日期 
+
+    private String mobile;
+
+    /**
+     *预约日期
 	 */
 	private String serviceDate;
-	
-	/**
-	 *预约时段 
+
+    /**
+     *预约时段
 	 */
 	private String servicePeriod;
-	
-	/**
-	 *技师名称 
+
+    /**
+     *技师名称
 	 */
 	private String technicianName;
-	
-	/**
+
+    /**
 	 * 预约支付金额
 	 */
 	private String serviceSubsist;
-	
-	/**
-	 *客户留言 
+
+    /**
+     *客户留言
 	 */
 	private String addMessage;
 }
