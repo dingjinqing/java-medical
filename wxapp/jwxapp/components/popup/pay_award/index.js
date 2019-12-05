a56397f795cce23cf255df410daf52c03056b149
liuyang <liuyang01@wangdian.cn>
2019-12-05 11:03:22 +0000

    支付有礼弹窗。

---
+++ wxapp/jwxapp/components/popup/pay_award/index.js
@@ -0,0 +1,22 @@
+var base = require("../base/base.js");
+global.wxComponent({
+  mixins: [base],
+
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+  }
+});
