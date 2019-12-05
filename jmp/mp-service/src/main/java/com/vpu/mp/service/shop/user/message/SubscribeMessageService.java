53cfe2664714adbea5058eafaf0450db2352e5a7
zhaojianqiang <zhaojianqiang@wangdian.cn>
2019-12-05 09:30:07 +0000

    测试

--- jmp/mp-service/src/main/java/com/vpu/mp/service/shop/user/message/SubscribeMessageService.java
+++ jmp/mp-service/src/main/java/com/vpu/mp/service/shop/user/message/SubscribeMessageService.java
@@ -258,7 +258,7 @@ public class SubscribeMessageService extends ShopBaseService {
 				logger().info("成功的templateId："+success.getTemplateId()+"插入结果"+insert);		
 			}else {
 				record.setStatus((byte)1);
-				record.setCanUseNum(record.getCanUseNum()+1);
+				record.setCanUseNum(record.getCanUseNum()==null?0:record.getCanUseNum()+1);
 				int update = record.update();
 				logger().info("成功的templateId："+success.getTemplateId()+"更新结果"+update);				
 			}
