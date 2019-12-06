ccecca2a58476651f2350e7ad96034c74fcda8c0
kangyaxin <kangyaxin@wangdian.cn>
2019-12-06 17:10:37 +0800

    满包邮国际化

--- web-vue/src/i18n/langs/views/admin_new/en/index/marketManagement/shipping/shippingEn.js
+++ web-vue/src/i18n/langs/views/admin_new/en/index/marketManagement/shipping/shippingEn.js
@@ -25,6 +25,24 @@ export const en = {
     }
   ],
 
+  // 表格
+  name: 'ActivityName',
+  startTime: 'CreateTime',
+  typeText: 'Active Commodities',
+  typeAll: 'All Merchandise',
+  typeNum: 'Some Commodities',
+  ruleText: 'Postal rules',
+  ruleTip1: 'Full',
+  ruleTip2: 'element',
+  ruleTip3: 'piece',
+  ruleTip4: 'free shipping',
+  expireTypeText: 'Validity',
+  to: 'to',
+  expireTypeAll: 'Permanent validity',
+  level: 'Priority Level',
+  statusText: 'Activity State',
+  option: 'Option',
+
   // 添加 / 编辑
   storeArr: [{
     name: 'Add merchandise',
