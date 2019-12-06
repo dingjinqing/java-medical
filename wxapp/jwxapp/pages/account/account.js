d35d3dc4a3c02798e6df3f5f87bc92b057fc8816
kangyaxin <kangyaxin@wangdian.cn>
2019-12-06 04:00:29 +0000

    个人中心余额

---
+++ wxapp/jwxapp/pages/account/account.js
@@ -0,0 +1,75 @@
+// pages/account/account.js
+var util = require('../../utils/util.js');
+var app = getApp()
+var Url = app.globalData.baseUrl;
+var account_list = [];
+global.wxPage({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    imageUrl: app.globalData.imageUrl,
+    page: 1,
+    bottom: false,
+    is_load: 0,
+    page: 1,
+    last_page: 1,
+    account_list: []
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    var that = this;
+    wx.hideShareMenu();
+    if (!util.check_setting(options)) return;
+    // util.api('/api/wxapp/user/number', function (res) {
+    //   that.setData({
+    //     num: res.content.account
+    //   })
+    // }, {})
+
+    get_accout(that);
+  },
+  onPullDownRefresh: function () {
+    wx.stopPullDownRefresh();
+  },
+  onReachBottom: function () {
+    var that = this;
+    that.setData({
+      is_load: 1
+    })
+    if (that.data.page == that.data.last_page) {
+      that.setData({
+        is_load: 0
+      })
+      return;
+    }
+    that.data.page = that.data.page + 1;
+    // util.api('/api/wxapp/account/list', function (res) {
+    //   var account_listL = res.content;
+    //   var ar_r = account_listL.data;
+    //   if (ar_r.length > 0) {
+    //     account_list = ar_r
+    //   }
+    //   that.setData({
+    //     account_list: that.data.account_list.concat(account_list)
+    //   })
+    // }, { pageNo: that.data.page })
+  },
+})
+function get_accout(that) {
+  // util.api('/api/wxapp/account/list', function (res) {
+  //   var account_listL = res.content;
+  //   that.data.last_page = account_listL.last_page;
+  //   var ar_r = account_listL.data;
+  //   if (ar_r.length > 0) {
+  //     account_list = ar_r
+  //   }
+  //   that.setData({
+  //     account_list: account_list
+  //   })
+  // }, { pageNo: that.data.page })
+}
---
+++ wxapp/jwxapp/pages1/balance/balance.js
@@ -0,0 +1,79 @@
+// pages/balance/balance.js
+var util = require('../../utils/util.js')
+var app = getApp()
+var imageUrl = app.globalData.imageUrl;
+var baseUrl = app.globalData.baseUrl;
+var is_block;
+var mobile = util.getCache('mobile');
+var account_info = [];
+var is_bind_mobile;
+global.wxPage({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    imageUrl: app.globalData.imageUrl,
+    baseUrl: app.globalData.baseUrl,
+    is_block: 0,
+    account_info: [],
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    if (!util.check_setting(options)) return;
+    var that = this;
+    wx.hideShareMenu();
+    // util.api('/api/wxapp/user/account/withdraw', function (res) {
+    //   if (res.error == 0) {
+    //     account_info = res.content;
+    //     is_bind_mobile = account_info.is_bind_mobile;
+    //     is_block = 0;
+    //     that.setData({
+    //       account_info: account_info,
+    //       is_block: is_block
+    //     })
+    //   }
+    // }, {})
+  },
+  give_tips: function () {
+    util.showModal("提示", "账户余额中已返利佣金可提现");
+  },
+  // 余额明细
+  yue_record: function () {
+    util.navigateTo({
+      url: '/pages/account/account',
+    })
+  },
+  // 提现记录
+  withdraw_record: function () {
+    util.navigateTo({
+      url: '/pages/widthdrawrecord/widthdrawrecord',
+    })
+  },
+  // 提现
+  to_money: function () {
+    var that = this;
+    if (account_info.withdraw_status == 0 || account_info.withdraw_status == null) {
+      util.showModal("提示", "系统暂时不支持提现");
+      return false
+    }
+    if (account_info.withdraw == 0) {
+      util.showModal("提示", "暂无可提现余额");
+      return false
+    }
+    if (is_bind_mobile == 1 && util.getCache('mobile') == '') {
+      util.checkSession(function () {
+        that.setData({
+          is_block: is_block = 1
+        })
+      })
+      return false;
+    }
+    util.navigateTo({
+      url: '/pages/widthdraw/widthdraw',
+    })
+  },
+})
