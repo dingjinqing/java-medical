24a7165d179aa38f4e7a96920a4325dcc2f638b4
changle <changle@wangdian.cn>
2019-12-12 11:43:46 +0800

    分销审核通过 | 未通过功能

---
+++ jmp/mp-service/src/main/java/com/vpu/mp/service/pojo/shop/distribution/DistributionApplyOptParam.java
@@ -0,0 +1,26 @@
+package com.vpu.mp.service.pojo.shop.distribution;
+
+import lombok.Data;
+
+/**
+ * 分销员审核操作入参类
+ * @Author 常乐
+ * @Date 2019-12-10
+ */
+@Data
+public class DistributionApplyOptParam {
+    /**
+     * 申请记录ID
+     */
+    private Integer id;
+
+    /**
+     * 分组id
+     */
+    private Integer groupId;
+
+    /**
+     * 审核内容
+     */
+    private String msg;
+}
