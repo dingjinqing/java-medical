971705017d1160f5f390f17af15920116da23a09
kangyaxin <kangyaxin@wangdian.cn>
2019-12-13 18:03:50 +0800

    优惠券领取

--- wxapp/jwxapp/components/decorate/coupon/index.js
+++ wxapp/jwxapp/components/decorate/coupon/index.js
@@ -5,19 +5,11 @@ global.wxComponent({
   mixins: [base],
   methods: {
     onPropChange(newVal, oldVal, changedPath) {
-      console.log(newVal, 'coupon-------------')
       for (var j in newVal.coupon_arr) {
         newVal.coupon_arr[j].denomination = parseFloat(newVal.coupon_arr[j].denomination);
       }
-      newVal.coupon_arr.forEach((item, index) => {
-        Object.assign(newVal.coupon_arr[index], newVal[index])
-      })
-      console.log(newVal.coupon_arr)
       newVal.isCoupon = 0;
-      console.log(this.data.comColor, this.data.borColor)
-
-
-      // validation_code、is_exclusive、alias_code、use_score
+      console.log(newVal)
     },
 
     bindFetchCoupon(e) {
@@ -45,21 +37,22 @@ global.wxComponent({
             util.toast_fail('领取失败');
           }
         }, {
-          code: d.code,
-          form_id: d.form_id,
-          open_id: d.open_id
-        })
+            code: d.code,
+            form_id: d.form_id,
+            open_id: d.open_id
+          })
       }
     },
     bindGetCoupon(e) {
       var d = this.eventData(e);
-      console.log(d)
       this._form_data = {
-        code: d.code,
+        // code: d.code,
+        code: e.target.dataset.score_number,
         form_id: e.detail.formId,
         open_id: util.getCache("openid"),
         coupon_key: d.key,
-        vali: d.vali
+        vali: d.vali,
+        coupon_id: e.target.dataset.coupon_id
       };
 
       var _this = this;
@@ -70,39 +63,68 @@ global.wxComponent({
       console.log(d.exclusive)
       if ((d.vali != '' && d.vali != undefined) || d.use_score == 1 ||
         (d.exclusive != '' && d.exclusive == 1)) {
-        util.jumpLink('/pages/getcoupon/getcoupon?code=' + d.code);
+        console.log(_this._form_data.code)
+          // 积分兑换页面
+        // util.jumpLink('/pages/getCoupon/getCoupon?code=' + d.code);
+        util.jumpLink('/pages/getCoupon/getCoupon?code=' + _this._form_data.code);
       } else {
+        // 直接领取
         wx.showLoading({
           title: '领取中···',
         })
         if (m['coupon_arr'][d.key].disableds == true) {
-          // util.api('/api/wxapp/coupon/get?code=' + d.code, function (res) {
-          //   wx.hideLoading();
-          //   if (res.error == 0) {
-          //     if (res.content == 0) {
-          //       util.showModal('提示', '您已领取的个数已达到可领取限制');
-          //       m.isCoupon = 0;
-          //       m['coupon_arr'][d.key].status = -1;
-          //       m['coupon_arr'][d.key].disableds = false;
-          //       _this.$set();
-          //     } else {
-          //       util.toast_success('领取成功', function () {
-          //         m['coupon_arr'][d.key].status = -1;
-          //         m['coupon_arr'][d.key].disableds = false;
-          //         _this.$set();
-          //       });
-          //     }
-          //   } else {
-          //     util.toast_fail(res.message.msg);
-          //   }
-          // }, {
-          //   form_id: _this._form_data.form_id,
-          //   open_id: _this._form_data.open_id
-          // });
+          util.api('/api/wxapp/coupon/get', function (res) {
+            wx.hideLoading();
+            if (res.error == 0) {
+              if (res.content == null) {
+                util.toast_success('领取成功', function () {
+                  m['coupon_arr'][d.key].status = -1;
+                  m['coupon_arr'][d.key].disableds = false;
+                  _this.$set();
+                });
+              } else {
+                util.showModal('提示', '您已领取的个数已达到可领取限制');
+                m.isCoupon = 0;
+                m['coupon_arr'][d.key].status = -1;
+                m['coupon_arr'][d.key].disableds = false;
+                _this.$set();
+              }
+            } else {
+              util.toast_fail(res.message.msg);
+            }
+          }, {
+              couponId: _this._form_data.coupon_id
+            });
         }
       }
-      console.log(e, d)
 
+
+      //   if (m['coupon_arr'][d.key].disableds == true) {
+      //     util.api('/api/wxapp/coupon/get?code=' + d.code, function (res) {
+      //       wx.hideLoading();
+      //       if (res.error == 0) {
+      //         if (res.content == 0) {
+      //           util.showModal('提示', '您已领取的个数已达到可领取限制');
+      //           m.isCoupon = 0;
+      //           m['coupon_arr'][d.key].status = -1;
+      //           m['coupon_arr'][d.key].disableds = false;
+      //           _this.$set();
+      //         } else {
+      //           util.toast_success('领取成功', function () {
+      //             m['coupon_arr'][d.key].status = -1;
+      //             m['coupon_arr'][d.key].disableds = false;
+      //             _this.$set();
+      //           });
+      //         }
+      //       } else {
+      //         util.toast_fail(res.message.msg);
+      //       }
+      //     }, {
+      //         form_id: _this._form_data.form_id,
+      //         open_id: _this._form_data.open_id
+      //       });
+      //   }
+      // }
     },
     bindCodeBlur(e) {
       this._input_code = e.detail.value;
