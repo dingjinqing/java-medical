40f57f75fc65bc14a30ee0820d8ed3b906d1b597
liangchen <liangchen@wangdian.cn>
2019-12-13 16:01:48 +0800

    页面访问统计bug测试

--- jmp/mp-service/src/main/java/com/vpu/mp/service/foundation/util/PropertiesUtil.java
+++ jmp/mp-service/src/main/java/com/vpu/mp/service/foundation/util/PropertiesUtil.java
@@ -6,7 +6,6 @@ import org.springframework.core.io.ClassPathResource;
 import java.io.InputStream;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Objects;
 import java.util.Properties;
 
 /**
@@ -24,28 +23,13 @@ public class PropertiesUtil {
      */
     public static Map<String, String> toMap(String filePath) {
         Properties properties = new Properties();
-//        URL url = ClassLoader.getSystemResource(filePath);
-//        URL url = ClassLoader.getSystemResourceAsStream(filePath);
-//        URL url = PropertiesUtil.class.getClassLoader().getResource(filePath);
-//        log.info("文件地址为："+url);
-        InputStream inputStream = ClassLoader.getSystemResourceAsStream(filePath);
         ClassPathResource resource = new ClassPathResource(filePath);
-
         try {
-            if (Objects.isNull(inputStream)) {
-                log.debug("以方式【ClassLoader.getSystemResourceAsStream(filePath)】获取资源文件失败！");
-
-                inputStream = resource.getInputStream();
-                if (Objects.isNull(inputStream)) {
-                    log.debug("以方式【new ClassPathResource(filePath)】获取资源文件失败！");
-                }
-            }
-//            properties.load(new FileInputStream(new File(url.getFile())));
+            InputStream inputStream = resource.getInputStream();
             properties.load(inputStream);
         } catch (Exception e) {
             e.printStackTrace();
         }
-        log.info("转换完成的properties："+properties);
         HashMap<String, String> map = new HashMap<>(100);
         properties.forEach((k, v) -> map.put((String) k, (String) v));
         return map;
