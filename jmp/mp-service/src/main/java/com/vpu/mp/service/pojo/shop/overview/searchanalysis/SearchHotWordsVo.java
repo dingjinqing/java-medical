44ab482abeb40c0063e3824f69b31cf3ed484646
liangchen <liangchen@wangdian.cn>
2019-12-16 16:40:15 +0800

    搜索统计接口调试

--- jmp/mp-service/src/main/java/com/vpu/mp/service/pojo/shop/overview/searchanalysis/SearchHotWordsVo.java
+++ jmp/mp-service/src/main/java/com/vpu/mp/service/pojo/shop/overview/searchanalysis/SearchHotWordsVo.java
@@ -2,16 +2,18 @@ package com.vpu.mp.service.pojo.shop.overview.searchanalysis;
 
 import lombok.Data;
 
+import java.util.List;
+
 /**
  * @author liangchen
  * @date 2019年7月26日
  */
 @Data
 public class SearchHotWordsVo {
-
-	private String hotWords;
-	private Integer searchCount;
-
+    /** 是否开启搜索热词统计 */
+    private Integer isOpenHotWords;
+    /** 数据 */
+    private List<SearchHotWordsData> data;
     /** 开始日期 */
     private String startTime;
     /** 结束日期 */
--- jmp/mp-service/src/main/java/com/vpu/mp/service/shop/overview/SearchAnalysisService.java
+++ jmp/mp-service/src/main/java/com/vpu/mp/service/shop/overview/SearchAnalysisService.java
@@ -1,68 +1,119 @@
 package com.vpu.mp.service.shop.overview;
 
 import com.vpu.mp.service.foundation.service.ShopBaseService;
-import com.vpu.mp.service.pojo.shop.overview.searchanalysis.SearchHistoryParam;
-import com.vpu.mp.service.pojo.shop.overview.searchanalysis.SearchHistoryVo;
-import com.vpu.mp.service.pojo.shop.overview.searchanalysis.SearchHotWordsVo;
+import com.vpu.mp.service.pojo.shop.config.SearchConfig;
+import com.vpu.mp.service.pojo.shop.overview.searchanalysis.*;
+import com.vpu.mp.service.shop.config.SearchConfigService;
+import org.apache.commons.lang3.math.NumberUtils;
 import org.jooq.impl.DSL;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 import static com.vpu.mp.db.shop.Tables.SEARCH_HISTORY;
 /**
  * 搜索统计
- * 
+ *
  * @author liangchen
  * @date 2019年7月23日
  */
 @Service
 public class SearchAnalysisService extends ShopBaseService {
-	
+    @Autowired SearchConfigService searchConfigService;
+    /** 日期标识符 */
+    private static final Integer CUSTOM_DAYS = 0;
+    /** 返回条数限制 */
+    private static final Integer LIMIT_NUM = 10;
+    /**
+     *得到之前的某一天(字符串类型)
+     *@param days N天前
+     *@return preDay(String)
+     */
+    public String getDate(Integer days) {
+        //格式化日期
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        //获取当前时间
+        Calendar c = Calendar.getInstance();
+        //计算指定日期
+        c.add(Calendar.DATE, - days);
+        Date time = c.getTime();
+        //返回格式化后的String日期
+        return sdf.format(time);
+    }
 	/**
 	 * 搜索历史统计
-	 * 
-	 * @param param
-	 * @return List<OverviewSearchAnalysisHistoryVo>
+	 *
+	 * @param param 查询时间段
+	 * @return 10条搜索历史统计信息
 	 */
-	public List<SearchHistoryVo> getSearchHistory(SearchHistoryParam param) {
-		
-		String startTime = param.getStartTime();
-		String endTime = param.getEndTime();
-		List<SearchHistoryVo> searchHistoryVos =
-            db().select(SEARCH_HISTORY.HOT_WORDS,
-						DSL.sum(SEARCH_HISTORY.SEARCH_COUNT).as("searchCount"))
+	public SearchHistoryVo getSearchHistory(SearchHistoryParam param) {
+        //得到时间
+        if (!param.getType().equals(CUSTOM_DAYS)){
+            param.setStartTime(getDate(param.getType()));
+            param.setEndTime(getDate(NumberUtils.INTEGER_ZERO));
+        }
+        //设置返回时间
+        SearchHistoryVo result = new SearchHistoryVo();
+        result.setStartTime(param.getStartTime());
+        result.setEndTime(param.getEndTime());
+		//查询配置信息-是否开启搜索历史统计
+        SearchConfig searchConfig = searchConfigService.getSearchConfig();
+        if(null==searchConfig) {
+            searchConfig=new SearchConfig(1, 1, 0);
+        }
+        result.setIsOpenHistory(searchConfig.isOpenHistory);
+        //查找搜索历史数据
+		List<SearchHistoryData> searchHistoryData =
+            db().select(SEARCH_HISTORY.HOT_WORDS,DSL.sum(SEARCH_HISTORY.SEARCH_COUNT).as("count"))
 				.from(SEARCH_HISTORY)
-				.where(SEARCH_HISTORY.CREATE_TIME.between(Timestamp.valueOf(startTime), Timestamp.valueOf(endTime)))
-				.and(SEARCH_HISTORY.IS_HOT_WORDS.eq((byte)0))
+                .where(SEARCH_HISTORY.DEL_FLAG.eq(NumberUtils.BYTE_ZERO))
+                .and(SEARCH_HISTORY.UPDATE_TIME.between(Timestamp.valueOf(param.getStartTime()), Timestamp.valueOf(param.getEndTime())))
 				.groupBy(SEARCH_HISTORY.HOT_WORDS)
-				.orderBy(DSL.sum(SEARCH_HISTORY.SEARCH_COUNT).as("searchCount").desc())
-				.fetchInto(SearchHistoryVo.class);
-		
-		return searchHistoryVos;
-				
+				.orderBy(DSL.sum(SEARCH_HISTORY.SEARCH_COUNT).as("count").desc())
+                .limit(LIMIT_NUM)
+				.fetchInto(SearchHistoryData.class);
+		result.setData(searchHistoryData);
+		return result;
 	}
 	/**
 	 * 搜索热词统计
-	 * 
-	 * @param param
-	 * @return List<OverviewSearchAnalysisHotVo>
+	 *
+	 * @param param 查询时间段
+     * @return 10条搜索热词统计信息
 	 */
-	public List<SearchHotWordsVo> getHotSearchHistory(SearchHistoryParam param) {
-		
-		String startTime = param.getStartTime();
-		String endTime = param.getEndTime();
-		List<SearchHotWordsVo> searchHotWordsVos = db().select(SEARCH_HISTORY.HOT_WORDS,
-						DSL.sum(SEARCH_HISTORY.SEARCH_COUNT).as("searchCount"))
+	public SearchHotWordsVo getHotSearchHistory(SearchHistoryParam param) {
+        //得到时间
+        if (!param.getType().equals(CUSTOM_DAYS)){
+            param.setStartTime(getDate(param.getType()));
+            param.setEndTime(getDate(NumberUtils.INTEGER_ZERO));
+        }
+        //设置返回时间
+        SearchHotWordsVo result = new SearchHotWordsVo();
+        result.setStartTime(param.getStartTime());
+        result.setEndTime(param.getEndTime());
+        //查询配置信息-是否开启搜索热词统计
+        SearchConfig searchConfig = searchConfigService.getSearchConfig();
+        if(null==searchConfig) {
+            searchConfig=new SearchConfig(1, 1, 0);
+        }
+        result.setIsOpenHotWords(searchConfig.isOpenHotWords);
+        //查找搜索历史数据
+		List<SearchHotWordsData> searchHotWordsData =
+            db().select(SEARCH_HISTORY.HOT_WORDS, DSL.sum(SEARCH_HISTORY.SEARCH_COUNT).as("count"))
 				.from(SEARCH_HISTORY)
-				.where(SEARCH_HISTORY.CREATE_TIME.between(Timestamp.valueOf(startTime), Timestamp.valueOf(endTime)))
-				.and(SEARCH_HISTORY.IS_HOT_WORDS.eq((byte)1))
+                .where(SEARCH_HISTORY.DEL_FLAG.eq(NumberUtils.BYTE_ZERO))
+                .and(SEARCH_HISTORY.IS_HOT_WORDS.eq(NumberUtils.BYTE_ONE))
+				.and(SEARCH_HISTORY.UPDATE_TIME.between(Timestamp.valueOf(param.getStartTime()), Timestamp.valueOf(param.getEndTime())))
 				.groupBy(SEARCH_HISTORY.HOT_WORDS)
-				.orderBy(DSL.sum(SEARCH_HISTORY.SEARCH_COUNT).as("searchCount").desc())
-				.fetchInto(SearchHotWordsVo.class);
-		
-		return searchHotWordsVos;
-				
+				.orderBy(DSL.sum(SEARCH_HISTORY.SEARCH_COUNT).as("count").desc())
+                .limit(LIMIT_NUM)
+				.fetchInto(SearchHotWordsData.class);
+        result.setData(searchHotWordsData);
+		return result;
 	}
 }
