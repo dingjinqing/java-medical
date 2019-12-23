ed81c1700c034b90684ee9f77923809f4c5cb328
wangshuai <wangshuai01@wangdian.cn>
2019-12-23 16:15:20 +0800

    下单锁注解bug
    Signed-off-by: wangshuai <wangshuai01@wangdian.cn>

--- jmp/mp-service/src/main/java/com/vpu/mp/service/foundation/util/lock/annotation/operation/ReleaseRedisLocks.java
+++ jmp/mp-service/src/main/java/com/vpu/mp/service/foundation/util/lock/annotation/operation/ReleaseRedisLocks.java
@@ -2,8 +2,15 @@ package com.vpu.mp.service.foundation.util.lock.annotation.operation;
 
 import com.vpu.mp.service.foundation.util.lock.annotation.RedisLock;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
 /**
  * @author 王帅
  */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
 public @interface ReleaseRedisLocks {
 }
