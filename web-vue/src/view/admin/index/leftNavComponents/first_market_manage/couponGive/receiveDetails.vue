28f6f0db4e351da83c8ce2c1e18b1260301673ce
kangyaxin <kangyaxin@wangdian.cn>
2019-12-24 11:42:13 +0800

    定向发券优化

--- web-vue/src/view/admin/index/leftNavComponents/first_market_manage/couponGive/receiveDetails.vue
+++ web-vue/src/view/admin/index/leftNavComponents/first_market_manage/couponGive/receiveDetails.vue
@@ -1,146 +1,147 @@
 <template>
   <div class="content">
     <!-- 筛选条件 -->
-    <wrapper>
-      <div class="main">
-        <el-form :inline="true">
-          <el-form-item
-            size="small"
-            :label="$t('couponGive.username')"
-          >
-            <el-input
-              v-model="username"
-              :placeholder="$t('couponGive.usernamePlaceholder')"
-            />
-          </el-form-item>
-          <el-form-item
-            size="small"
-            :label="$t('couponGive.mobile')"
-          >
-            <el-input
-              v-model="mobile"
-              :placeholder="$t('couponGive.mobilePlaceholder')"
-            />
-          </el-form-item>
-          <el-form-item
-            size="small"
-            :label="$t('couponGive.useStatus')"
-          >
-            <el-select v-model="isUsed">
-              <el-option
-                :label="$t('couponGive.all')"
-                value="-1"
-              ></el-option>
-              <el-option
-                :label="$t('couponGive.notUsed')"
-                value="0"
-              ></el-option>
-              <el-option
-                :label="$t('couponGive.alreadyUsed')"
-                value="1"
-              ></el-option>
-              <el-option
-                :label="$t('couponGive.expired')"
-                value="2"
-              ></el-option>
-              <el-option
-                :label="$t('couponGive.repealed')"
-                value="3"
-              ></el-option>
-            </el-select>
-          </el-form-item>
-          <el-button
-            size="small"
-            type="primary"
-            @click="onSubmit"
-          >{{$t('couponGive.filter')}}</el-button>
-          <el-button size="small">{{$t('couponGive.export')}}</el-button>
-        </el-form>
-      </div>
-    </wrapper>
-    <!-- 表格数据 -->
-    <wrapper>
-      <div class="table_list">
-        <el-table
-          class="version-manage-table"
-          header-row-class-name="tableClss"
-          :data="tableData"
-          border
-          style="width: 100%"
+    <div class="main">
+      <el-form :inline="true">
+        <el-form-item
+          size="small"
+          :label="$t('couponGive.username') + '：'"
         >
-          <el-table-column
-            prop="username"
-            :label="$t('couponGive.username')"
-            align="center"
-          ></el-table-column>
-          <el-table-column
-            prop="mobile"
-            :label="$t('couponGive.mobile')"
-            align="center"
-          ></el-table-column>
-          <el-table-column
-            prop="couponName"
-            :label="$t('couponGive.couponName')"
-            align="center"
-          ></el-table-column>
-          <el-table-column
-            prop="accessMode"
-            :label="$t('couponGive.accessMode')"
-            align="center"
-            :formatter="accessModeName"
-          ></el-table-column>
-          <el-table-column
-            prop="isUsed"
-            :label="$t('couponGive.isUsed')"
-            align="center"
-            :formatter="isUsedName"
-          ></el-table-column>
-          <el-table-column
-            prop="orderSn"
-            :label="$t('couponGive.orderSn')"
-            align="center"
-          ></el-table-column>
-          <el-table-column
-            :label="$t('couponGive.validityPeriod')"
-            align="center"
-          >
-            <template slot-scope="scope">
-              <span v-html="scope.row.validDate"></span>
-            </template>
-          </el-table-column>
-          <el-table-column
-            prop="createTime"
-            :label="$t('couponGive.recTime')"
-            align="center"
-          ></el-table-column>
-          <el-table-column
-            prop="usedTime"
-            :label="$t('couponGive.useTime')"
-            align="center"
-          ></el-table-column>
-          <el-table-column
-            prop=""
-            :label="$t('couponGive.operate')"
-            align="center"
-          >
-            <template slot-scope="scope">
+          <el-input
+            v-model="username"
+            :placeholder="$t('couponGive.usernamePlaceholder')"
+          />
+        </el-form-item>
+        <el-form-item
+          size="small"
+          :label="$t('couponGive.mobile') + '：'"
+        >
+          <el-input
+            v-model="mobile"
+            :placeholder="$t('couponGive.mobilePlaceholder')"
+          />
+        </el-form-item>
+        <el-form-item
+          size="small"
+          :label="$t('couponGive.useStatus') + '：'"
+        >
+          <el-select v-model="isUsed">
+            <el-option
+              :label="$t('couponGive.all')"
+              value="-1"
+            ></el-option>
+            <el-option
+              :label="$t('couponGive.notUsed')"
+              value="0"
+            ></el-option>
+            <el-option
+              :label="$t('couponGive.alreadyUsed')"
+              value="1"
+            ></el-option>
+            <el-option
+              :label="$t('couponGive.expired')"
+              value="2"
+            ></el-option>
+            <el-option
+              :label="$t('couponGive.repealed')"
+              value="3"
+            ></el-option>
+          </el-select>
+        </el-form-item>
+        <el-button
+          size="small"
+          type="primary"
+          @click="onSubmit"
+          style="margin-left: 10px;"
+        >{{$t('couponGive.filter')}}</el-button>
+        <el-button size="small">{{$t('couponGive.export')}}</el-button>
+      </el-form>
+    </div>
+    <!-- 表格数据 -->
+    <div class="table_list">
+      <el-table
+        class="version-manage-table"
+        header-row-class-name="tableClss"
+        :data="tableData"
+        border
+        style="width: 100%"
+      >
+        <el-table-column
+          prop="username"
+          :label="$t('couponGive.username')"
+          align="center"
+        ></el-table-column>
+        <el-table-column
+          prop="mobile"
+          :label="$t('couponGive.mobile')"
+          align="center"
+        ></el-table-column>
+        <el-table-column
+          prop="couponName"
+          :label="$t('couponGive.couponName')"
+          align="center"
+        ></el-table-column>
+        <el-table-column
+          prop="accessMode"
+          :label="$t('couponGive.accessMode')"
+          align="center"
+          :formatter="accessModeName"
+        ></el-table-column>
+        <el-table-column
+          prop="isUsed"
+          :label="$t('couponGive.isUsed')"
+          align="center"
+          :formatter="isUsedName"
+        ></el-table-column>
+        <el-table-column
+          prop="orderSn"
+          :label="$t('couponGive.orderSn')"
+          align="center"
+        ></el-table-column>
+        <el-table-column
+          :label="$t('couponGive.validityPeriod')"
+          align="center"
+          width="160px"
+        >
+          <template slot-scope="scope">
+            <span>{{ scope.row.startTime }}<br />{{ $t('promoteList.to') }}<br />{{ scope.row.endTime }}</span>
+            <!-- <span v-html="scope.row.validDate"></span> -->
+          </template>
+        </el-table-column>
+        <el-table-column
+          prop="createTime"
+          :label="$t('couponGive.recTime')"
+          align="center"
+          width="160px"
+        ></el-table-column>
+        <el-table-column
+          prop="usedTime"
+          :label="$t('couponGive.useTime')"
+          align="center"
+          width="160px"
+        ></el-table-column>
+        <el-table-column
+          prop=""
+          :label="$t('couponGive.operate')"
+          align="center"
+        >
+          <template slot-scope="scope">
 
-              <el-button
-                v-if="scope.row.isUsed === `未使用`"
-                @click="stopCoupon(scope.row.id)"
-                type="text"
-              >{{$t('couponGive.abolish')}}
-              </el-button>
-            </template>
-          </el-table-column>
-        </el-table>
-        <pagination
-          :page-params.sync="pageParams"
-          @pagination="loadData"
-        />
+            <el-button
+              v-if="scope.row.isUsed === `未使用`"
+              @click="stopCoupon(scope.row.id)"
+              type="text"
+            >{{$t('couponGive.abolish')}}
+            </el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+      <pagination
+        :page-params.sync="pageParams"
+        @pagination="loadData"
+      />
 
-      </div>
-    </wrapper>
+    </div>
   </div>
 </template>
 <script>
@@ -174,10 +175,7 @@ export default {
       this.pageParams.mobile = this.mobile
       this.pageParams.username = this.username
       this.pageParams.isUsed = this.isUsed
-      console.log('pageParams:', this.pageParams)
       receiveDetails(this.pageParams).then(res => {
-        console.log('pageInfo:', res.content.dataList)
-        this.handData(res.content.dataList)
         this.tableData = res.content.dataList
         this.pageParams = res.content.page
       })
@@ -186,13 +184,6 @@ export default {
     onSubmit () {
       this.pageParams.currentPage = 1
       this.loadData()
-    },
-    // 表格数据处理
-    handData (data) {
-      data.map((item, index) => {
-        item.validDate = `${item.startTime}<br/>${this.$t('promoteList.to')}<br/>${item.endTime}`
-      })
-      this.tableData = data
     },
     // 获取方式 标识转文字
     accessModeName (row, column) {
@@ -243,11 +234,21 @@ export default {
   padding: 10px;
   min-width: 100%;
   font-size: 14px;
-  height: 100%;
   .main {
-    position: relative;
     background-color: #fff;
-    padding: 10px 20px 10px 20px;
+    min-height: 50px;
+    line-height: 50px;
+    .el-input {
+      line-height: 50px;
+    }
+    .el-select {
+      line-height: 50px;
+    }
+    .el-form-item {
+      margin-bottom: 0;
+      margin-right: 0;
+      margin-left: 10px;
+    }
   }
 }
 /deep/ .tableClss th {
@@ -261,6 +262,6 @@ export default {
   position: relative;
   margin-top: 10px;
   background-color: #fff;
-  padding: 10px 20px 0 20px;
+  padding: 10px;
 }
 </style>
