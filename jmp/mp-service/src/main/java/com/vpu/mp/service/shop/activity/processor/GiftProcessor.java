81170216adab37c4270237a16e15887bca590554
wangshuai <wangshuai01@wangdian.cn>
2019-12-25 16:36:26 +0800

    null
    Signed-off-by: wangshuai <wangshuai01@wangdian.cn>

--- jmp/mp-service/src/main/java/com/vpu/mp/service/shop/activity/processor/GiftProcessor.java
+++ jmp/mp-service/src/main/java/com/vpu/mp/service/shop/activity/processor/GiftProcessor.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Service;
 public class GiftProcessor implements Processor{
     @Override
     public Byte getPriority() {
-        return null;
+        return 0;
     }
 
     @Override
