0c246f33acee1ee04cd3468bdf03007a2bc48ada
kongdecheng <kongdecheng@wangdian.cn>
2019-12-26 16:26:49 +0800

    拼团活动数据比

--- jmp/mp-service/src/main/java/com/vpu/mp/service/shop/order/OrderReadService.java
+++ jmp/mp-service/src/main/java/com/vpu/mp/service/shop/order/OrderReadService.java
@@ -582,7 +582,7 @@ public class OrderReadService extends ShopBaseService {
 		}
 		//好物圈
 
-		//TODO 拼团
+		// 拼团
 		if(orderType.indexOf(Byte.valueOf(OrderConstant.GOODS_TYPE_PIN_GROUP).toString()) != -1){
 			GroupOrderVo groupOrder = groupBuyList.getByOrder(order.getOrderSn());
 			//未退款
