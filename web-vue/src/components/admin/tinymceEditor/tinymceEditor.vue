2481561d9db2e6fbad5ba40da442dbb30c019833
suntengfei <suntengfei@wangdian.cn>
2019-12-27 16:39:33 +0800

    工具栏抽屉取消

--- web-vue/src/components/admin/tinymceEditor/tinymceEditor.vue
+++ web-vue/src/components/admin/tinymceEditor/tinymceEditor.vue
@@ -72,6 +72,7 @@ export default {
         toolbar: this.toolbar,
         branding: false,
         menubar: false,
+        toolbar_drawer: false,
         // 此处为图片上传处理函数，这个直接用了base64的图片形式上传图片，
         // 如需ajax上传可参考https://www.tiny.cloud/docs/configure/file-image-upload/#images_upload_handler
         images_upload_handler: (blobInfo, success, failure) => {
