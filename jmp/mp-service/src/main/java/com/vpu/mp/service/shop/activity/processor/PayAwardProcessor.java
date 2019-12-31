19de79d6e5b2e8949a83875fe2850e26ac4c71af
kongdecheng <kongdecheng@wangdian.cn>
2019-12-31 15:20:24 +0800

    支付有礼库存限制

--- jmp/mp-service/src/main/java/com/vpu/mp/service/shop/activity/processor/PayAwardProcessor.java
+++ jmp/mp-service/src/main/java/com/vpu/mp/service/shop/activity/processor/PayAwardProcessor.java
@@ -156,7 +156,7 @@ public class PayAwardProcessor extends ShopBaseService implements Processor, Cre
                 logger().info("支付有礼没有配置奖品");
                 return;
             }
-            Integer joinAwardCount = jedisManager.getIncrValueAndSave(REDIS_PAY_AWARD + order.getUserId(), 60000,
+            Integer joinAwardCount = jedisManager.getIncrValueAndSave(REDIS_PAY_AWARD + order.getUserId()+payAward.getId(), 60000,
                     () -> payAwardRecordService.getJoinAwardCount(order.getUserId(), payAward.getId()).toString()).intValue();
             logger().info("用户:{},参与次数:{}", order.getUserId(), joinAwardCount);
             int circleTimes = joinAwardCount / payAwardSize;
