88529db4509c68dcdbf55059238b36929d2ebcfb
wangshuai <wangshuai01@wangdian.cn>
2020-01-03 14:02:28 +0800

    订单模块：快递公司表增加快递100code bug:ExecuteResult构造方法使用修改
    Signed-off-by: wangshuai <wangshuai01@wangdian.cn>

--- jmp/mp-service/src/main/java/com/vpu/mp/service/shop/order/action/DeleteService.java
+++ jmp/mp-service/src/main/java/com/vpu/mp/service/shop/order/action/DeleteService.java
@@ -49,11 +49,11 @@ public class DeleteService  extends ShopBaseService implements IorderOperate <Or
         logger().info("订单删除start");
 		OrderListMpVo order = orderInfo.getByOrderId(param.getOrderId(), OrderListMpVo.class);
 		if(order == null) {
-			return ExecuteResult.create(JsonResultCode.CODE_ORDER_NOT_EXIST);
+			return ExecuteResult.create(JsonResultCode.CODE_ORDER_NOT_EXIST, null);
 		}
 		if(!OrderOperationJudgment.isDelete(order)) {
             logger().error("该订单不可删除");
-			return ExecuteResult.create(JsonResultCode.CODE_ORDER_DELETE_OPERATION_NOT_SUPPORTED);
+			return ExecuteResult.create(JsonResultCode.CODE_ORDER_DELETE_OPERATION_NOT_SUPPORTED, null);
 		}		
 		orderInfo.delete(order);
 		//操作记录
