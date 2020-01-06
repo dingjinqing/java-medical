2339a36393b6a5aee5b7cb534cec01b4e9e62edb
zhaojianqiang <zhaojianqiang@wangdian.cn>
2020-01-06 16:10:38 +0800

    优惠券判断

--- jmp/mp-service/src/main/java/com/vpu/mp/service/pojo/shop/coupon/mpGetCouponParam.java
+++ jmp/mp-service/src/main/java/com/vpu/mp/service/pojo/shop/coupon/mpGetCouponParam.java
@@ -1,6 +1,8 @@
 package com.vpu.mp.service.pojo.shop.coupon;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 /**
  * 小程序端用户领取优惠券入参
@@ -8,6 +10,8 @@ import lombok.Data;
  * date:2019-11-21
  */
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class mpGetCouponParam {
     private Integer couponId;
     private Integer userId;
