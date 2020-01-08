d153e82cc3db7c2415ca1002e0677c179654abb2
wangbingbing <wangbingbing@huice.com>
2020-01-08 13:34:22 +0800

    装修异步接口 拼团抽奖

--- jmp/mp-service/src/main/java/com/vpu/mp/service/shop/market/groupdraw/GroupDrawService.java
+++ jmp/mp-service/src/main/java/com/vpu/mp/service/shop/market/groupdraw/GroupDrawService.java
@@ -1,5 +1,6 @@
 package com.vpu.mp.service.shop.market.groupdraw;
 
+import com.vpu.mp.config.DomainConfig;
 import com.vpu.mp.db.shop.tables.records.GroupDrawRecord;
 import com.vpu.mp.service.foundation.data.BaseConstant;
 import com.vpu.mp.service.foundation.service.ShopBaseService;
@@ -14,6 +15,7 @@ import com.vpu.mp.service.pojo.shop.order.OrderConstant;
 import com.vpu.mp.service.pojo.shop.qrcode.QrCodeTypeEnum;
 import com.vpu.mp.service.shop.image.QrCodeService;
 import com.vpu.mp.service.shop.order.info.OrderInfoService;
+import jodd.util.StringUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.math.NumberUtils;
 import org.jooq.*;
@@ -59,10 +61,8 @@ public class GroupDrawService extends ShopBaseService {
     @Autowired
     public GroupDrawUserService groupDrawUser;
 
-    /** 启用 **/
-    private static final byte GROUP_DRAW_ENABLED = 1;
-    /** 禁用 **/
-    private static final byte GROUP_DRAW_DISABLED = 0;
+    @Autowired
+    public DomainConfig domainConfig;
 
     private final QrCodeService qrCode;
 
@@ -87,8 +87,8 @@ public class GroupDrawService extends ShopBaseService {
      * 停用活动
      */
     public void disableGroupDraw(Integer id) {
-        int result = db().update(GROUP_DRAW).set(GROUP_DRAW.STATUS, GROUP_DRAW_DISABLED)
-            .where(GROUP_DRAW.ID.eq(id).and(GROUP_DRAW.STATUS.ne(GROUP_DRAW_DISABLED))).execute();
+        int result = db().update(GROUP_DRAW).set(GROUP_DRAW.STATUS, ACTIVITY_STATUS_DISABLE)
+            .where(GROUP_DRAW.ID.eq(id).and(GROUP_DRAW.STATUS.ne(ACTIVITY_STATUS_DISABLE))).execute();
         if (0 == result) {
             throw new IllegalStateException("Invalid group draw id or it has already been disabled.");
         }
@@ -190,12 +190,12 @@ public class GroupDrawService extends ShopBaseService {
                     select.and(GROUP_DRAW.END_TIME.lessThan(currentTimeStamp()));
                     break;
                 case NAVBAR_TYPE_DISABLED:
-                    select.and(GROUP_DRAW.STATUS.eq(GROUP_DRAW_DISABLED));
+                    select.and(GROUP_DRAW.STATUS.eq(ACTIVITY_STATUS_DISABLE));
                     break;
                 default:
             }
             if (NAVBAR_TYPE_DISABLED != status) {
-                select.and(GROUP_DRAW.STATUS.eq(GROUP_DRAW_ENABLED));
+                select.and(GROUP_DRAW.STATUS.eq(ACTIVITY_STATUS_NORMAL));
             }
         }
         select.and(GROUP_DRAW.DEL_FLAG.eq((byte) 0));
@@ -219,19 +219,16 @@ public class GroupDrawService extends ShopBaseService {
         String goodsId = vo.getGoodsId();
         String couponId = vo.getRewardCouponId();
         // 活动状态判断
-        switch (status) {
-            case GROUP_DRAW_ENABLED:
-                if (startTime.after(currentTimeStamp())) {
-                    vo.setStatus(NAVBAR_TYPE_NOT_STARTED);
-                } else if (startTime.before(currentTimeStamp()) && endTime.after(currentTimeStamp())) {
-                    vo.setStatus(NAVBAR_TYPE_ONGOING);
-                } else {
-                    vo.setStatus(NAVBAR_TYPE_FINISHED);
-                }
-                break;
-            case GROUP_DRAW_DISABLED:
-                vo.setStatus(NAVBAR_TYPE_DISABLED);
-                break;
+        if(status.equals(ACTIVITY_STATUS_NORMAL)){
+            if (startTime.after(currentTimeStamp())) {
+                vo.setStatus(NAVBAR_TYPE_NOT_STARTED);
+            } else if (startTime.before(currentTimeStamp()) && endTime.after(currentTimeStamp())) {
+                vo.setStatus(NAVBAR_TYPE_ONGOING);
+            } else {
+                vo.setStatus(NAVBAR_TYPE_FINISHED);
+            }
+        }else if(status.equals(ACTIVITY_STATUS_DISABLE)){
+            vo.setStatus(NAVBAR_TYPE_DISABLED);
         }
         // 商品数量
         int goodsCount = goodsId.split(",").length;
@@ -264,7 +261,7 @@ public class GroupDrawService extends ShopBaseService {
     private GroupDrawRecord createGroupDrawRecord(GroupDrawAddParam param) {
         return new GroupDrawRecord(null, param.getName(), param.getStartTime(),
             param.getEndTime(), param.getGoodsId(), param.getMinJoinNum(), param.getPayMoney(), param.getJoinLimit(),
-            param.getOpenLimit(), param.getLimitAmount(), param.getToNumShow(), GROUP_DRAW_ENABLED, (byte) 1, null,
+            param.getOpenLimit(), param.getLimitAmount(), param.getToNumShow(), ACTIVITY_STATUS_NORMAL, (byte) 1, null,
             null, (byte) 0, null, param.getRewardCouponId());
     }
 
@@ -455,6 +452,10 @@ public class GroupDrawService extends ShopBaseService {
         int joinUserNumber = groupDrawUsers.getJoinGroupNumByGroupDraw(moduleGroupDraw.getGroupDrawId());
         if(groupDraw.getToNumShow() <= joinUserNumber){
             moduleGroupDraw.setJoinUserNum(joinUserNumber);
+        }
+
+        if(StringUtil.isNotEmpty(moduleGroupDraw.getModuleImg())){
+            moduleGroupDraw.setModuleImg(domainConfig.imageUrl(moduleGroupDraw.getModuleImg()));
         }
 
         return moduleGroupDraw;
