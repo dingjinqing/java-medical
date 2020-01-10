a15057218852c070cb4926a3af8a87f29e2aae67
changle <changle@wangdian.cn>
2020-01-10 14:11:27 +0800

    小程序优惠券详情添加使用说明；用户优惠券删除接口开发

--- jmp/mp-web/src/main/java/com/vpu/mp/controller/admin/AdminCouponController.java
+++ jmp/mp-web/src/main/java/com/vpu/mp/controller/admin/AdminCouponController.java
@@ -4,6 +4,7 @@ import com.vpu.mp.service.foundation.data.JsonResult;
 import com.vpu.mp.service.foundation.util.PageResult;
 import com.vpu.mp.service.pojo.shop.coupon.*;
 import com.vpu.mp.service.pojo.shop.coupon.hold.CouponHoldListVo;
+import com.vpu.mp.service.shop.ShopApplication;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.constraints.NotNull;
@@ -136,4 +137,15 @@ public class AdminCouponController extends AdminBaseController{
 		PageResult<CouponHoldListVo> detail = shop().coupon.getDetail(param);
 		return this.success(detail);
 	}
+
+    /**
+     * 删除用户优惠券
+     * @param id
+     * @return
+     */
+	@GetMapping("/admin/avail/coupon/del")
+	public JsonResult availCouponDel(Integer id){
+        boolean res = shop().coupon.availCouponDel(id);
+        return this.success(res);
+    }
 }
