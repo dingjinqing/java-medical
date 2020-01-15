6eb6b59b719fbc4a266d7fbbdf663bb1ded2b5e9
changle <changle@wangdian.cn>
2020-01-15 18:26:38 +0800

    分销员申请出参类完善

--- jmp/mp-service/src/main/java/com/vpu/mp/service/pojo/wxapp/distribution/DistributorApplyDetailParam.java
+++ jmp/mp-service/src/main/java/com/vpu/mp/service/pojo/wxapp/distribution/DistributorApplyDetailParam.java
@@ -1,5 +1,6 @@
 package com.vpu.mp.service.pojo.wxapp.distribution;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 /**
@@ -17,4 +18,38 @@ public class DistributorApplyDetailParam {
      * 审核状态 0：审核中；1：通过；2：拒绝
      */
     public Integer status;
+    /**
+     * 个人信息
+     */
+    public String activationFields;
+
+    @Data
+    public static class InfoField{
+        /**
+         * 真实姓名
+         */
+        @JsonProperty(value = "real_name")
+        public String realName;
+        /**
+         * 手机号
+         */
+        @JsonProperty(value = "mobile")
+        public String mobile;
+        /**
+         * 身份证号
+         */
+        @JsonProperty(value = "cid")
+        public String cid;
+        /**
+         * 地址
+         */
+        @JsonProperty(value = "address")
+        public String address;
+        /**
+         *性别
+         */
+        @JsonProperty(value = "sex")
+        public String sex;
+
+    }
 }
