048b2e82f4060fbc1864167b2a4d6e237c577780
wangshuai <wangshuai01@wangdian.cn>
2020-01-16 10:22:46 +0800

    git 格式化
    Signed-off-by: wangshuai <wangshuai01@wangdian.cn>

--- jmp/mp-service/src/main/java/com/vpu/mp/service/foundation/data/DelFlag.java
+++ jmp/mp-service/src/main/java/com/vpu/mp/service/foundation/data/DelFlag.java
@@ -4,8 +4,8 @@ import lombok.Getter;
 
 /**
  * 删除标识枚举类
- *
- * @author wangshuai
+ * 
+ * @author wangshuai1
  *
  */
 @Getter
@@ -23,15 +23,15 @@ public enum DelFlag {
 
 	/**
 	 * 数据库记录假删除时，在记录唯一字段添加的前缀值
-	 *
+	 * 
 	 * @author 李晓冰
 	 */
 	public static final String DEL_ITEM_SPLITER = "_";
 	public static final String DEL_ITEM_PREFIX = "del" + DEL_ITEM_SPLITER;
 	/**
 	 * 数据库假删除标志，jooq中该字段设置值的时候使用枚举会报错。
-	 *
-	 * @author 李晓冰
+	 * 
+	 * @author 李晓冰r
 	 */
 	public static final Byte NORMAL_VALUE = 0;
 	public static final Byte DISABLE_VALUE = 1;
