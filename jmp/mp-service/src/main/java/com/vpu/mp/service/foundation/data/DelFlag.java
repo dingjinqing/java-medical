90085d3cb9f08aeddd84f351686a6de9836ca6b6
huangzhuangzhuang <huangzhuangzhuang@wangdian.cn>
2020-01-16 10:10:37 +0800

    格式化

--- jmp/mp-service/src/main/java/com/vpu/mp/service/foundation/data/DelFlag.java
+++ jmp/mp-service/src/main/java/com/vpu/mp/service/foundation/data/DelFlag.java
@@ -9,29 +9,29 @@ import lombok.Getter;
  */
 @Getter
 public enum DelFlag {
-    // 正常状态
-    NORMAL((byte) 0),
-    // 删除状态
-    DISABLE((byte) 1);
+	// 正常状态
+	NORMAL((byte) 0),
+	// 删除状态
+	DISABLE((byte) 1);
 
-    private byte code;
+	private byte code;
 
-    private DelFlag(byte code) {
-        this.code = code;
-    }
+	private DelFlag(byte code) {
+		this.code = code;
+	}
 
-    /**
-     * 数据库记录假删除时，在记录唯一字段添加的前缀值
-     *
-     * @author 李晓冰
-     */
-    public static final String DEL_ITEM_SPLITER = "_";
-    public static final String DEL_ITEM_PREFIX = "del" + DEL_ITEM_SPLITER;
-    /**
-     * 数据库假删除标志，jooq中该字段设置值的时候使用枚举会报错。
-     *
-     * @author 李晓冰
-     */
-    public static final Byte NORMAL_VALUE = 0;
-    public static final Byte DISABLE_VALUE = 1;
+	/**
+	 * 数据库记录假删除时，在记录唯一字段添加的前缀值
+	 *
+	 * @author 李晓冰
+	 */
+	public static final String DEL_ITEM_SPLITER = "_";
+	public static final String DEL_ITEM_PREFIX = "del" + DEL_ITEM_SPLITER;
+	/**
+	 * 数据库假删除标志，jooq中该字段设置值的时候使用枚举会报错。
+	 *
+	 * @author 李晓冰
+	 */
+	public static final Byte NORMAL_VALUE = 0;
+	public static final Byte DISABLE_VALUE = 1;
 }
