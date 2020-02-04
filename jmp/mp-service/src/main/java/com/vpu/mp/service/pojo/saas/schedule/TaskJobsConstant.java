fb350f73e08bba7c2e46e38e079786beec1a371b
wangbingbing <wangbingbing@huice.com>
2020-02-04 18:38:21 +0800

    队列取消微信订单调试

--- jmp/mp-service/src/main/java/com/vpu/mp/service/pojo/saas/schedule/TaskJobsConstant.java
+++ jmp/mp-service/src/main/java/com/vpu/mp/service/pojo/saas/schedule/TaskJobsConstant.java
@@ -50,13 +50,14 @@ public class TaskJobsConstant {
         GIVE_COUPON(1004, RabbitConfig.EXCHANGE_MARKETING, RabbitConfig.BINDING_EXCHANGE_COUPON_KEY),
     	/**订单 */
     	WX_IMPORTORDER(1006, RabbitConfig.EXCHANGE_WX, RabbitConfig.BINDING_EXCHANGE_IMPORTORDER_KEY),
-        WX_CLOSEORDER(1010, RabbitConfig.EXCHANGE_WX, RabbitConfig.BINDING_EXCHANGE_CLOSE_ORDER_KEY),
     	/**收藏 */
     	WX_ADDSHOPPINGLIST(1007, RabbitConfig.EXCHANGE_WX, RabbitConfig.BINDING_EXCHANGE_ADDSHOPPINGLIST_KEY),
     	/**物品信息 */
     	WX_IMPORTPRODUCT(1008, RabbitConfig.EXCHANGE_WX, RabbitConfig.BINDING_EXCHANGE_IMPORTPRODUCT_KEY),
     	/** 读取Excel用，也可以其他未分类异步任务使用*/
-    	OTHER_MQ(1009, RabbitConfig.EXCHANGE_MARKETING, RabbitConfig.BINDING_EXCHANGE_OTHER_KEY);
+    	OTHER_MQ(1009, RabbitConfig.EXCHANGE_MARKETING, RabbitConfig.BINDING_EXCHANGE_OTHER_KEY),
+        /**关闭订单 */
+        WX_CLOSEORDER(1010, RabbitConfig.EXCHANGE_WX, RabbitConfig.BINDING_EXCHANGE_CLOSE_ORDER_KEY);
     	
         private Integer executionType;
         private String exchangeName;
