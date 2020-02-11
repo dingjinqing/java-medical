6e289339de554f2072e6e6c05b46010550a53ab6
kongdecheng <kongdecheng@wangdian.cn>
2020-02-11 16:26:01 +0800

    拼团列表修改

--- jmp/mp-service/src/main/java/com/vpu/mp/service/shop/market/goupbuy/GroupBuyListService.java
+++ jmp/mp-service/src/main/java/com/vpu/mp/service/shop/market/goupbuy/GroupBuyListService.java
@@ -81,7 +81,8 @@ public class GroupBuyListService extends ShopBaseService {
                 .from(GROUP_BUY_LIST)
                 .groupBy(GROUP_BUY_LIST.ACTIVITY_ID);
 
-        SelectConditionStep<? extends Record> records = db().select(GROUP_BUY_DEFINE.ID, GROUP_BUY_DEFINE.NAME, GOODS.GOODS_NAME, GROUP_BUY_DEFINE.ACTIVITY_TYPE,
+        SelectConditionStep<? extends Record> records = db().select(
+                GROUP_BUY_DEFINE.ID, GROUP_BUY_DEFINE.NAME, GOODS.GOODS_NAME,GROUP_BUY_DEFINE.LEVEL, GROUP_BUY_DEFINE.ACTIVITY_TYPE,
                 GROUP_BUY_DEFINE.START_TIME, GROUP_BUY_DEFINE.END_TIME, GROUP_BUY_DEFINE.STATUS, GROUP_BUY_DEFINE.LIMIT_AMOUNT,
                 DSL.ifnull(table.field(GROUP_ORDER_NUM), 0).as(GROUP_ORDER_NUM))
                 .from(GROUP_BUY_DEFINE)
