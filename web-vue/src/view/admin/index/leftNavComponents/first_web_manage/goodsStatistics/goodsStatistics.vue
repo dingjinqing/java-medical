6f11f7fe5d2f18956eb7aa9549e7068b7b796b17
liufei <liufei@wangdian.cn>
2020-02-13 16:02:29 +0800

    商品统计页面样式完善

--- web-vue/src/view/admin/index/leftNavComponents/first_web_manage/goodsStatistics/goodsStatistics.vue
+++ web-vue/src/view/admin/index/leftNavComponents/first_web_manage/goodsStatistics/goodsStatistics.vue
@@ -1,98 +1,45 @@
 <template>
-  <div class="goodsSytle">
-    商品统计页面
-    <div class="table">
-      <table class="table_content">
-        <thead>
-          <tr>
-            <th></th>
-            <th
-              v-for="(item, index) in refDate"
-              :key="index"
-            >{{item}}</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr
-            v-for="(item, index) in tableData"
-            :key="index"
-          >
-            <td>{{goodsName[index]}}</td>
-            <td
-              v-for="(item1, index1) in item"
-              :key="index1"
-            >{{item1}}</td>
-          </tr>
-        </tbody>
-      </table>
+  <div class="userStatistics">
+    <div class="userStatistics_content">
+      <el-tabs v-model="userStatistics">
+        <el-tab-pane
+          label="商品概览"
+          name="first"
+        >
+        </el-tab-pane>
+
+        <el-tab-pane
+          label="商品效果"
+          name="second"
+        >
+          <goodsEffect />
+        </el-tab-pane>
+      </el-tabs>
     </div>
+    <goodsOverview v-if="userStatistics === 'first'" />
+
   </div>
 </template>
 
 <script>
-
+import goodsOverview from './goodsOverview'
+import goodsEffect from './goodsEffect'
 export default {
+  components: { goodsOverview, goodsEffect },
   data () {
     return {
-      refDate: ['2020-10-10', '2020-10-11', '2020-10-12'],
-      goodsName: ['花露水', '毛衣', '手机'],
-      tableData: [
-        [12, 13, 14],
-        [13, 14, 12],
-        [13, 14, 12]
-      ],
-      search: ''
-    }
-  },
-  methods: {
-    handleEdit (index, row) {
-      console.log(index, row)
-    },
-    handleDelete (index, row) {
-      console.log(index, row)
+      userStatistics: 'first'
     }
   }
 }
 
 </script>
 <style lang="scss" scoped>
-.goodsSytle {
-  .table {
-    margin-top: 20px;
-    border: 1px solid #efedee;
-    .table_content {
-      width: 100%;
-      tr {
-        th {
-          height: 60px;
-          line-height: 60px;
-          background: #f5f5f5;
-          border: 1px solid #efedee;
-        }
-      }
-      td {
-        text-align: center;
-        height: 60px;
-        line-height: 60px;
-        border: 1px solid #efedee;
-      }
-      .out {
-        width: 200px;
-        height: 60px;
-        position: relative;
-      }
-      .out::before {
-        content: "";
-        position: absolute;
-        left: 25px;
-        top: 10px;
-        width: 74%;
-        box-sizing: border-box;
-        border-bottom: 1px solid #e5e5e5;
-        transform-origin: bottom center;
-        transform: rotateZ(16deg) scale(1.414);
-      }
-    }
+.userStatistics {
+  padding: 10px;
+  .userStatistics_content {
+    background: #fff;
+    padding: 15px 0 0 20px;
   }
 }
 </style>
