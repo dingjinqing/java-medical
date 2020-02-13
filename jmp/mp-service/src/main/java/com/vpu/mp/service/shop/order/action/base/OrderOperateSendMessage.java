facc9f6e59df07503d1ff74d9108cd344fe7e690
wangshuai <wangshuai01@wangdian.cn>
2020-02-13 16:54:51 +0800

    发送模板消息
    Signed-off-by: wangshuai <wangshuai01@wangdian.cn>

---
+++ jmp/mp-service/src/main/java/com/vpu/mp/service/shop/order/action/base/OrderOperateSendMessage.java
@@ -0,0 +1,12 @@
+package com.vpu.mp.service.shop.order.action.base;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * 订单操作发送模板消息
+ */
+@Service
+public class OrderOperateSendMessage {
+    public void send() {
+    }
+}
