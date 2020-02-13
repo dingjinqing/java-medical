b1a22157e7d94f9b81e61581878b5de0193a59d9
liuyang <liuyang01@wangdian.cn>
2020-02-13 12:11:39 +0800

    基础footer

--- wxapp/jwxapp/components/item/footer/index.js
+++ wxapp/jwxapp/components/item/footer/index.js
@@ -1,12 +1,126 @@
 const util = require("../../../utils/util.js");
+let actType = {
+  1:{
+    footerButtonName:{
+      left:{name:'单独购买',event:'checkBuy'},
+      right:{
+        'name-0':'一键开团',
+        'name-1':'团长优惠价',
+        event:'checkGroup'
+      }
+    },
+    dialogButtonName:{
+      left:{
+        right:{
+            name:'立即购买',
+            event:'toCheckOut'
+        }
+      },
+      right:{
+        right:{
+          name:'一键开团',
+          event:'actCheckOut'
+        }
+      },
+      default:{
+        right:{
+          name:'一键开团',
+          event:'actCheckOut'
+        }
+      }
+    }
+  },
+  3:{
+    footerButtonName:{
+      left:{name:'单独购买',event:'checkBuy'},
+      right:{name:'砍价拿',event:'ckeckBargain'}
+    },
+    dialogButtonName:{
+      left:{
+        left:{
+          name:'加入购物车',
+          event:'addCart'
+        },
+        right:{
+          name:'立即购买',
+          event:'toCheckOut'
+        }
+      },
+      right:{
+        right:{
+          name:'砍价拿',
+          event:'goBargain'
+        }
+      },
+      default:{
+        right:{
+          name:'砍价拿',
+          event:'goBargain'
+        }
+      }
+    }
+  },
+  5:{
+    footerButtonName:{
+      right:{
+        name:'立即购买',
+        event:'checkSkill'
+      }
+    },
+    dialogButtonName:{
+      right:{
+        right:{
+          name:'立即购买',
+          event:'actCheckOut'
+        }
+      }
+    }
+  },
+  default:{
+    footerButtonName:{
+      left:{
+        name:'加入购物车',
+        event:'checkAddCart'
+      },
+      right:{
+        name:'立即购买',
+        event:'checkCheckout'
+      }
+    },
+    dialogButtonName:{
+      left:{
+        left:{
+          name:'确定',
+          event:'addCart'
+        }
+      },
+      right:{
+        right:{
+          name:'确定',
+          event:'toCheckOut'
+        }
+      },
+      default:{
+        left:{
+          name:'加入购物车',
+          event:'addCart'
+        },
+        right:{
+          name:'立即购买',
+          event:'toCheckOut'
+        }
+      }
+    }
+  }
+}
 global.wxComponent({
   /**
    * 组件的属性列表
    */
   properties: {
-    origin: {
+    position: {
       type: String,
-      value: "item"
+      value: "footer" //dialog
     },
     isDefaultPrd: Boolean,
     productInfo: {
@@ -18,30 +132,7 @@ global.wxComponent({
     },
     triggerButton: {
       type: String,
-      value: '',
-      observer(val) {
-        console.log(val)
-        if (val === 'left') {
-          this.setData({
-            rightButtonShow: false,
-            leftButtonShow: true,
-            leftButtonName: '确定'
-          })
-        } else if (val === 'right') {
-          this.setData({
-            leftButtonShow: false,
-            rightButtonShow: true,
-            rightButtonName: '立即购买'
-          })
-        } else {
-          this.setData({
-            leftButtonName: '加入购物车',
-            rightButtonName: '立即购买',
-            rightButtonShow: true,
-            leftButtonShow: true
-          })
-        }
-      }
+      value: ''
     },
     activity: {
       type: Object,
@@ -57,30 +148,101 @@ global.wxComponent({
       }
     }
   },
-
+  lifetimes:{
+    ready(){
+      console.log(111)
+      this.initFooter()
+    }
+  },
   /**
    * 组件的初始数据
    */
   data: {
-    rightButtonShow: true,
-    leftButtonShow: true,
-    leftButtonName: '加入购物车',
-    rightButtonName: '立即购买',
-    cartNum: 0
+    cartNum: 0,
   },
 
   /**
    * 组件的方法列表
    */
   methods: {
-    leftClick() {
-      if (this.checkOrigin('left')) return //判断商品详情页点击 底部footer按钮
+    initFooter(){
+      this.setData({
+        buttonData : this.getButtonData(),
+        leftStyle:`color:${(this.data.main_setting.comColor != "#ff6666" || !this.data.main_setting.comColor)?"#fff":"#f66"};background:${this.data.main_setting.commonColor};`,
+        rightStyle:`color:#fff;background:${this.data.main_setting.comColor};`
+      })
+    },
+    checkPosition(position) {
+      if (this.data.position === 'footer' && !this.data.isDefaultPrd) {
+        this.triggerEvent('showSpecDialog',position)
+        return true
+      }
+      return false
+    },
+    checkAddCart(){
+      if(this.checkPosition('left')) return
       this.addCart()
     },
-    rightClick() {
-      if (this.checkOrigin('right')) return
-      // if (!this.data.dealtAct) util.showModal('提示','失败',()=>{},'','','确认')
+    checkCheckout(){
+      if(this.checkPosition('right')) return
       this.toCheckOut()
+    },
+    checkBuy(){
+      if(this.checkPosition('left')) return
+      this.toCheckOut()
+    },
+    ckeckBargain(){
+      if(this.checkPosition('right')) return
+      this.goBargain()
+    },
+    checkGroup(){
+      if(this.checkPosition('right')) return
+      this.actCheckOut()
+    },
+    checkSkill(){
+      if(this.checkPosition('right')) return
+      this.actCheckOut()
+    },
+    goBargain(){
+      util.api('/api/wxapp/bargain/apply',res=>{
+        if (res.error == 0) {
+          var data = res.content;
+          if (data.resultCode == 0) {
+            var url = "/pages/bargaininfo/bargaininfo?record_id=" + data.recordId;
+            util.jumpLink(url);
+          } else if (data.resultCode == 1) {
+            util.showModal(this.$t("components.decorate.tips"), this.$t("components.decorate.doesNotExist"));
+          } else if (data.resultCode == 2) {
+            util.showModal(this.$t("components.decorate.tips"), this.$t("components.decorate.activityDeactivated"));
+          } else if (data.resultCode == 3) {
+            util.showModal(this.$t("components.decorate.tips"), this.$t("components.decorate.notStart"));
+          } else if (data.resultCode == 4) {
+            util.showModal(this.$t("components.decorate.tips"), this.$t("components.decorate.activityEnded"));
+          } else if (data.resultCode == 5) {
+            util.showModal(this.$t("components.decorate.tips"), this.$t("components.decorate.insufficientInventory"));
+          } else if (data.resultCode == -1) {
+            util.showModal(this.$t("components.decorate.tips"), this.$t("components.decorate.operationFailed"));
+          }
+        } else {
+          
+        }
+      },{
+        bargainId:this.data.activity.activityId,
+        prdId:this.data.productInfo.prdId
+      })
+    },
+    getButtonData(){
+      let buttonData = {}
+      if(this.data.position === 'footer'){
+        buttonData = this.data.activity && [1,3,5].includes(this.data.activity.activityType) ? actType[this.data.activity.activityType]['footerButtonName'] : actType['default']['footerButtonName']
+        if(this.data.activity && this.data.activity.activityType === 1){
+          buttonData['right'].name = buttonData['right'][`name-${this.data.activity.isGrouperCheap}`]
+        }
+      } else if (this.data.position === 'spec'){
+        let position = this.data.activity && [1,3,5].includes(this.data.activity.activityType) ? actType[this.data.activity.activityType]['dialogButtonName'] : actType['default']['dialogButtonName']
+        buttonData = this.data.triggerButton ? position[this.data.triggerButton] : position['default']
+      }
+      return buttonData
     },
     getCartNum() {
       // let { goodsId } = this.data.productInfo
@@ -113,14 +275,12 @@ global.wxComponent({
         }
       );
     },
-    checkOrigin(trigger) {
-      if (this.data.origin === 'item' && !this.data.isDefaultPrd) {
-        this.triggerEvent('showSpecDialog', trigger)
-        return true
-      }
-      return false
-    },
+    
     toCheckOut() {
+      util.jumpLink(`pages/checkout/checkout${this.getUrlParams({ goodsList: JSON.stringify([this.data.productInfo]) })}`, "navigateTo")
+      this.triggerEvent('close')
+    },
+    actCheckOut(){
       util.jumpLink(`pages/checkout/checkout${this.getUrlParams({ goodsList: JSON.stringify([this.data.productInfo]), activityType: this.data.activity ? this.data.activity.activityType : null, activityId: this.data.activity ? this.data.activity.activityId : null })}`, "navigateTo")
       this.triggerEvent('close')
     },
