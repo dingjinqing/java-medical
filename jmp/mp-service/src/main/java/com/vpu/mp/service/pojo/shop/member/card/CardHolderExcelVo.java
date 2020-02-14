660895da856f5056d0f4eba3476a44071a83afec
zhaojianqiang <zhaojianqiang@wangdian.cn>
2020-02-14 10:17:56 +0800

    持卡会员导出

--- jmp/mp-service/src/main/java/com/vpu/mp/service/pojo/shop/member/card/CardHolderExcelVo.java
+++ jmp/mp-service/src/main/java/com/vpu/mp/service/pojo/shop/member/card/CardHolderExcelVo.java
@@ -3,6 +3,8 @@ package com.vpu.mp.service.pojo.shop.member.card;
 import java.sql.Timestamp;
 
 import com.vpu.mp.service.foundation.excel.annotation.ExcelColumn;
+import com.vpu.mp.service.foundation.excel.annotation.ExcelIgnore;
+import com.vpu.mp.service.foundation.excel.annotation.ExcelSheet;
 
 import lombok.Data;
 
@@ -13,34 +15,36 @@ import lombok.Data;
  * @time 下午2:46:43
  */
 @Data
+@ExcelSheet
 public class CardHolderExcelVo {
 	/** - 会员ID */
-	@ExcelColumn(columnIndex = 0, columnName = "user.import.name")
+	@ExcelColumn(columnIndex = 0, columnName = "usercard.import.userId")
 	private Integer userId;
 	
 	/** - 昵称 */
-	@ExcelColumn(columnIndex = 1, columnName = "user.import.name")
+	@ExcelColumn(columnIndex = 1, columnName = "usercard.import.username")
 	private String username;
 	
-	@ExcelColumn(columnIndex = 2, columnName = "user.import.name")
+	@ExcelColumn(columnIndex = 2, columnName = "usercard.import.mobile")
 	/** - 手机号 */
 	private String mobile;
 	
-	@ExcelColumn(columnIndex = 3, columnName = "user.import.name")
+	@ExcelColumn(columnIndex = 3, columnName = "usercard.import.invitedName")
 	/** - 邀请人 */
 	private String invitedName;
 	
-	@ExcelColumn(columnIndex = 4, columnName = "user.import.name")
+	@ExcelColumn(columnIndex = 4, columnName = "usercard.import.nacreateTimeme")
 	/** -领卡时间 */
 	private Timestamp createTime;
 	
-	@ExcelColumn(columnIndex = 5, columnName = "user.import.name")
+	@ExcelColumn(columnIndex = 5, columnName = "usercard.import.cardNo")
 	/** - 会员卡号 */
 	private String cardNo;
 	
-	@ExcelColumn(columnIndex = 6, columnName = "user.import.name")
+	@ExcelColumn(columnIndex = 6, columnName = "usercard.import.nflag")
 	private String nflag;
 	
 	/** -过期时间 */
+	@ExcelIgnore
 	private Timestamp expireTime;
 }
