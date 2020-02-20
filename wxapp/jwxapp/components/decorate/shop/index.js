e083bfd268422fbe64b16377492ddfa5f132b703
kangyaxin <kangyaxin@wangdian.cn>
2020-02-20 15:08:52 +0800

    店招设置优化

--- wxapp/jwxapp/components/decorate/shop/index.js
+++ wxapp/jwxapp/components/decorate/shop/index.js
@@ -6,15 +6,7 @@ global.wxComponent({
     onPropChange(newVal, oldVal, changedPath) {
       console.log(newVal, newVal.bg_url)
       newVal.bg_url = 'http://jmpdevimg.weipubao.cn' + newVal.bg_url;
-      // newVal.shop_bg_path = 'http://jmpdevimg.weipubao.cn' + newVal.shop_bg_path;
-      // var strRegex = "^((https|http|ftp|rtsp|mms)?://)";
-      // var re = new RegExp(strRegex);
-      // console.log(re.test(newVal.bg_url) + newVal.bg_url)
-      // if (re.test(newVal.bg_url)) {
-      //   newVal.bg_url = newVal.bg_url;
-      // } else {
-      // newVal.bg_url = newVal.imageUrl + newVal.bg_url;
-      // }
+      newVal.shop_bg_path = 'http://jmpdevimg.weipubao.cn' + newVal.shop_bg_path;
     },
 
   }
