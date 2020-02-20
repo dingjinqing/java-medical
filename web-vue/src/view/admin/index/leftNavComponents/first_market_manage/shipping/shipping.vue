05d5fef510333e00da9902c25430e7604f1fbcde
kangyaxin <kangyaxin@wangdian.cn>
2020-02-20 09:37:58 +0800

    满包邮创建时间优化

--- web-vue/src/view/admin/index/leftNavComponents/first_market_manage/shipping/shipping.vue
+++ web-vue/src/view/admin/index/leftNavComponents/first_market_manage/shipping/shipping.vue
@@ -51,7 +51,7 @@
         >
         </el-table-column>
         <el-table-column
-          prop=""
+          prop="createTime"
           :label="$t('shipping.startTime')"
           align="center"
         >
