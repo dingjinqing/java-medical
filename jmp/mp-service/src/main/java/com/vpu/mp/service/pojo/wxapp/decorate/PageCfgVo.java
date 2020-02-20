9971341b7b06498bc9a74f5c6dc7a0844ae4d4c6
wangbingbing <wangbingbing@huice.com>
2020-02-20 16:45:47 +0800

    装修Module类的默认值与前端保持一致

--- jmp/mp-service/src/main/java/com/vpu/mp/service/pojo/wxapp/decorate/PageCfgVo.java
+++ jmp/mp-service/src/main/java/com/vpu/mp/service/pojo/wxapp/decorate/PageCfgVo.java
@@ -13,23 +13,23 @@ import lombok.Setter;
 @Data
 public class PageCfgVo {
     @JsonProperty(value = "is_ok")
-    Byte isOk;
+    Byte isOk=1;
     @JsonProperty(value = "cat_id")
-    Integer catId;
+    Integer catId=0;
     @JsonProperty(value = "page_name")
-    String pageName;
+    String pageName="";
     @JsonProperty(value = "bg_types")
-    Byte bgTypes;
+    Byte bgTypes=0;
     @JsonProperty(value = "has_bottom")
-    Byte hasBottom;
+    Byte hasBottom=0;
     @JsonProperty(value = "show_margin")
-    Byte showMargin;
+    Byte showMargin=0;
     @JsonProperty(value = "margin_val")
-    Integer marginVal;
+    Integer marginVal=0;
     @JsonProperty(value = "page_bg_color")
-    String pageBgColor;
+    String pageBgColor="#ffffff";
     @JsonProperty(value = "page_bg_image")
-    String pageBgImage;
+    String pageBgImage="";
     @JsonProperty(value = "last_cur_idx")
     Integer lastCurIdx;
     @JsonProperty(value = "pictorial")
@@ -46,36 +46,36 @@ public class PageCfgVo {
          *是否添加分享海报
          */
         @JsonProperty(value = "is_add")
-        Byte isAdd;
+        Byte isAdd=0;
 
         /**
          *是所有用户可见还是仅分销员可见
          */
         @JsonProperty(value = "user_visibility")
-        Byte userVisibility;
+        Byte userVisibility=0;
 
         /**
          *按钮名称
          */
         @JsonProperty(value = "share_btn_name")
-        String shareBtnName;
+        String shareBtnName="";
 
         /**
          *分享语
          */
         @JsonProperty(value = "share_desc")
-        String shareDesc;
+        String shareDesc="";
 
         /**
          *分享图片路径
          */
         @JsonProperty(value = "share_img_path")
-        String shareImgPath;
+        String shareImgPath="";
 
         /**
          *按钮名称长度
          */
         @JsonProperty(value = "name_length")
-        Integer nameLength;
+        Integer nameLength=0;
     }
 }
