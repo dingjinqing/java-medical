9907efeeb37c4524ecce81b9a9cb1f18388e3f15
zhaojianqiang <zhaojianqiang@wangdian.cn>
2020-02-21 11:50:08 +0800

    解决冲突

--- jmp/mp-service/src/main/java/com/vpu/mp/service/shop/image/QrCodeService.java
+++ jmp/mp-service/src/main/java/com/vpu/mp/service/shop/image/QrCodeService.java
@@ -114,8 +114,16 @@ public class QrCodeService extends ShopBaseService {
 
         //获取小程序分享码
         Integer shopId = getShopId();
+
+        log.info("获取二维码，shopId:"+shopId);
+
         MpAuthShopRecord mp = saas.shop.mp.getAuthShopByShopId(shopId);
+        if(mp==null) {
+        	log.info("店铺还没有授权小程序，shopId:"+shopId);
+        	return null;
+        }
         String appId =  mp.getAppId();
+        log.info("获取二维码，appId:"+appId);
 
         //二维码图片大小
         int qrcodWidth = 430;
