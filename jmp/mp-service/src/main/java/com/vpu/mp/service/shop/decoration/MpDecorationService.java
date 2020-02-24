b06995a7ff7a0c1265ad6028dd3ba9ff7baf0ddd
wangbingbing <wangbingbing@huice.com>
2020-02-24 21:09:08 +0800

    页面预览 修复

--- jmp/mp-service/src/main/java/com/vpu/mp/service/shop/decoration/MpDecorationService.java
+++ jmp/mp-service/src/main/java/com/vpu/mp/service/shop/decoration/MpDecorationService.java
@@ -454,8 +454,8 @@ public class MpDecorationService extends ShopBaseService {
      * @return
      * @throws IOException
      */
-    private ModuleSecKill convertSeckillForIndex(ObjectMapper objectMapper, Entry<String, JsonNode> node, UserRecord user) throws IOException {
-        ModuleSecKill moduleSecKill = objectMapper.readValue(node.getValue().toString(), ModuleSecKill.class);
+    private ModuleSeckill convertSeckillForIndex(ObjectMapper objectMapper, Entry<String, JsonNode> node, UserRecord user) throws IOException {
+        ModuleSeckill moduleSecKill = objectMapper.readValue(node.getValue().toString(), ModuleSeckill.class);
         moduleSecKill.setNeedRequest(true);
         return moduleSecKill;
     }
@@ -863,8 +863,8 @@ public class MpDecorationService extends ShopBaseService {
      * @return
      * @throws IOException
      */
-    private ModuleSecKill convertSeckillForModule(ObjectMapper objectMapper, Entry<String, JsonNode> node, UserRecord user) throws IOException {
-        ModuleSecKill moduleSecKill = objectMapper.readValue(node.getValue().toString(), ModuleSecKill.class);
+    private ModuleSeckill convertSeckillForModule(ObjectMapper objectMapper, Entry<String, JsonNode> node, UserRecord user) throws IOException {
+        ModuleSeckill moduleSecKill = objectMapper.readValue(node.getValue().toString(), ModuleSeckill.class);
 
         // 转换实时信息
         return saas.getShopApp(getShopId()).seckill.getPageIndexSeckill(moduleSecKill);
