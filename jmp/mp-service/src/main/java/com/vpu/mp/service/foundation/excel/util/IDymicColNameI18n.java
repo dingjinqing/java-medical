a46b65f0e0735a49457763ef5b896088c04b6b59
huangzhuangzhuang <huangzhuangzhuang@wangdian.cn>
2020-02-24 11:33:40 +0800

    用户动态列导出

---
+++ jmp/mp-service/src/main/java/com/vpu/mp/service/foundation/excel/util/IDymicColNameI18n.java
@@ -0,0 +1,13 @@
+package com.vpu.mp.service.foundation.excel.util;
+/**
+ * excel动态列名国际化
+ * @author 黄壮壮
+ *
+ */
+public interface IDymicColNameI18n {
+	/**
+	 * 获取动态列的列名国际化
+	 * @param name
+	 */
+	public void i18nName(String name);
+}
