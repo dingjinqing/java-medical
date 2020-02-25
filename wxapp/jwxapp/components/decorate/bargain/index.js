6c1e88eb61e9c8b174b0d79b03bd3e386b4ab873
suntengfei <suntengfei@wangdian.cn>
2020-02-25 15:27:09 +0800

    小程序砍价跳转逻辑优化

--- wxapp/jwxapp/components/decorate/bargain/index.js
+++ wxapp/jwxapp/components/decorate/bargain/index.js
@@ -57,7 +57,7 @@ global.wxComponent({
         this.navigateToItem(d.goods_id);
       } else {
         console.log('触发', d)
-        if (d.is_prd == 1) {
+        if (d.is_prd == 0) {
           console.log(d.link)
           util.jumpLink(d.link);
         } else {
