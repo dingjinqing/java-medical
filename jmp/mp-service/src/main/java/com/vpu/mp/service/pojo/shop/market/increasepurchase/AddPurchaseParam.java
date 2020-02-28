787e1eaf9f724d0de1a2617b38e7a9367f49d551
liufei <liufei@wangdian.cn>
2020-02-28 14:52:19 +0800

    加价购活动添加换购商品运费策略字段

--- jmp/mp-service/src/main/java/com/vpu/mp/service/pojo/shop/market/increasepurchase/AddPurchaseParam.java
+++ jmp/mp-service/src/main/java/com/vpu/mp/service/pojo/shop/market/increasepurchase/AddPurchaseParam.java
@@ -3,6 +3,7 @@ package com.vpu.mp.service.pojo.shop.market.increasepurchase;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 
+import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import java.sql.Timestamp;
 import java.util.List;
@@ -10,31 +11,49 @@ import java.util.List;
 /**
  * @author liufei
  * @date 2019/8/14
- * @description
  */
 @Data
 public class AddPurchaseParam {
-    /** 活动名称 */
+    /**
+     * 活动名称
+     */
     @NotNull
     private String name;
-    /** 活动起始时间 */
+    /**
+     * 活动起始时间
+     */
     @NotNull
-    @JsonFormat(shape=JsonFormat.Shape.STRING,pattern="yyyy-MM-dd hh:mm:ss")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd hh:mm:ss")
     private Timestamp startTime;
-    /** 活动结束时间 */
+    /**
+     * 活动结束时间
+     */
     @NotNull
-    @JsonFormat(shape=JsonFormat.Shape.STRING,pattern="yyyy-MM-dd hh:mm:ss")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd hh:mm:ss")
     private Timestamp endTime;
-    /** 活动优先级 */
+    /**
+     * 活动优先级
+     */
     @NotNull
     private Short level;
-    /** 单笔最大换购数量 */
+    /**
+     * 单笔最大换购数量
+     */
     private Short maxChangePurchase;
-    /** 主商品列表字符串，逗号分隔 */
+    /**
+     * 主商品列表字符串，逗号分隔
+     */
     @NotNull
     private String goodsId;
-    /** 加价购规则定义 */
+    /**
+     * 加价购规则定义
+     */
     @NotNull
     private List<PurchaseRule> rules;
+    /**
+     * 换购商品运费策略，0免运费，1使用原商品运费模板
+     */
+    @NotEmpty
+    private Byte redemptionFreight;
 
 }
