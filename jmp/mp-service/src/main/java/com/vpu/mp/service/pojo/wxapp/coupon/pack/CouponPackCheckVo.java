1edfb7bd18b829cc74d6a3420a2d44ea5d87acb7
wangbingbing <wangbingbing@huice.com>
2020-03-04 10:26:44 +0800

    优惠券礼包下单接口完善

--- jmp/mp-service/src/main/java/com/vpu/mp/service/pojo/wxapp/coupon/pack/CouponPackCheckVo.java
+++ jmp/mp-service/src/main/java/com/vpu/mp/service/pojo/wxapp/coupon/pack/CouponPackCheckVo.java
@@ -11,7 +11,12 @@ import lombok.Setter;
 @Getter
 public class CouponPackCheckVo {
     /**
-     * 活动状态，0正常，1活动已结束，2活动未开始，3领取礼包达到上限，4单用户领取礼包达到上限，5积分不足
+     * 活动状态，0正常，1活动已结束，2活动未开始，3领取礼包达到上限，4单用户领取礼包达到上限，5积分不足,6直接领取下单成功
      */
-    private Byte state;
+    private Byte state = 0;
+
+    /**
+     * state==6时的订单数据
+     */
+    private String orderSn;
 }
