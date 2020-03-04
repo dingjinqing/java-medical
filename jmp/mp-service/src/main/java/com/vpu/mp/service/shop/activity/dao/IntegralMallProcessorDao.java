cb2337a3776d0ea8a4929500766a1441f169989c
wangshuai <wangshuai01@wangdian.cn>
2020-03-04 09:39:17 +0800

    与晓冰重复修改
    Signed-off-by: wangshuai <wangshuai01@wangdian.cn>

--- jmp/mp-service/src/main/java/com/vpu/mp/service/shop/activity/dao/IntegralMallProcessorDao.java
+++ jmp/mp-service/src/main/java/com/vpu/mp/service/shop/activity/dao/IntegralMallProcessorDao.java
@@ -4,11 +4,21 @@ import com.vpu.mp.db.shop.tables.records.IntegralMallDefineRecord;
 import com.vpu.mp.db.shop.tables.records.IntegralMallProductRecord;
 import com.vpu.mp.service.foundation.data.BaseConstant;
 import com.vpu.mp.service.foundation.data.DelFlag;
-import com.vpu.mp.service.foundation.service.ShopBaseService;
+import com.vpu.mp.service.foundation.data.JsonResultCode;
+import com.vpu.mp.service.foundation.exception.MpException;
+import com.vpu.mp.service.foundation.util.BigDecimalUtil;
 import com.vpu.mp.service.foundation.util.DateUtil;
+import com.vpu.mp.service.pojo.shop.market.integralconvert.IntegralConvertProductVo;
+import com.vpu.mp.service.pojo.shop.market.integralconvert.IntegralConvertSelectParam;
+import com.vpu.mp.service.pojo.shop.market.integralconvert.IntegralConvertSelectVo;
 import com.vpu.mp.service.pojo.wxapp.goods.goods.detail.integral.IntegralMallMpVo;
 import com.vpu.mp.service.pojo.wxapp.goods.goods.detail.integral.IntegralMallPrdMpVo;
+import com.vpu.mp.service.pojo.wxapp.order.OrderBeforeParam;
+import com.vpu.mp.service.shop.market.integralconvert.IntegralConvertService;
+import com.vpu.mp.service.shop.member.ScoreCfgService;
 import com.vpu.mp.service.shop.member.ScoreService;
+import org.apache.commons.collections4.CollectionUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.jooq.Condition;
 import org.jooq.impl.DSL;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,6 +27,8 @@ import org.springframework.stereotype.Service;
 import java.math.BigDecimal;
 import java.sql.Timestamp;
 import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 import static com.vpu.mp.db.shop.tables.IntegralMallDefine.INTEGRAL_MALL_DEFINE;
@@ -28,10 +40,13 @@ import static com.vpu.mp.db.shop.tables.IntegralMallRecord.INTEGRAL_MALL_RECORD;
  * @date 2020年03月03日
  */
 @Service
-public class IntegralMallProcessorDao extends ShopBaseService {
+public class IntegralMallProcessorDao extends IntegralConvertService {
 
     @Autowired
     private ScoreService scoreService;
+
+    @Autowired
+    private ScoreCfgService scoreCfgService;
 
     /**
      * 获取积分兑换商品活动信息
@@ -120,5 +135,64 @@ public class IntegralMallProcessorDao extends ShopBaseService {
     private List<IntegralMallProductRecord> getIntegralPrdInfo(Integer activityId){
       return  db().selectFrom(INTEGRAL_MALL_PRODUCT).where(INTEGRAL_MALL_PRODUCT.INTEGRAL_MALL_DEFINE_ID.eq(activityId))
             .fetchInto(IntegralMallProductRecord.class);
+    }
+
+    /**
+     * 活动详情
+     * @param activityId
+     * @return
+     */
+    public IntegralConvertSelectVo getDetail(Integer activityId) {
+        return selectOne(new IntegralConvertSelectParam(activityId));
+
+    }
+
+    public void orderCheck(OrderBeforeParam param, IntegralConvertSelectVo activityInfo) throws MpException {
+        if (activityInfo == null ||
+            param.getDate().after(activityInfo.getEndTime()) ||
+            DelFlag.DISABLE_VALUE.equals(activityInfo.getDelFlag()) ||
+            BaseConstant.ACTIVITY_STATUS_DISABLE.equals(activityInfo.getStatus()) ||
+            activityInfo.getGoodsId() == null ||
+            CollectionUtils.isEmpty(activityInfo.getProductVo())) {
+            logger().error("积分兑换活动停用");
+            throw new MpException(JsonResultCode.CODE_ORDER_ACTIVITY_DISABLE);
+        }
+        //此次购买数量
+        int currentNum = 0;
+        Map<Integer, IntegralConvertProductVo> productMap = activityInfo.getProductVo().stream().collect(Collectors.toMap(IntegralConvertProductVo::getPrdId, Function.identity()));
+        for (OrderBeforeParam.Goods goods : param.getGoods()) {
+            //累加
+            currentNum += goods.getGoodsNumber();
+
+            if(productMap.get(goods.getProductId()) == null) {
+                logger().error("积分兑换规格不存在");
+                throw new MpException(JsonResultCode.CODE_ORDER_GOODS_NOT_EXIST);
+            }
+            if(productMap.get(goods.getProductId()).getStock().intValue() < goods.getGoodsNumber()) {
+                logger().error("积分兑换活动库存不足");
+                throw new MpException(JsonResultCode.CODE_ORDER_ACTIVITY_GOODS_OUT_OF_STOCK);
+            }
+        }
+       /* if(activityInfo.getMaxExchangeNum() != 0) {
+            if(currentNum > activityInfo.getMaxExchangeNum() || currentNum + getUserExchangeCount(param.getWxUserInfo().getUserId(), activityInfo.getId()) > activityInfo.getMaxExchangeNum()) {
+                throw new MpException(JsonResultCode.CODE_ORDER_ACTIVITY_NUMBER_LIMIT);
+            }
+        }*/
+    }
+
+    public void orderInit(OrderBeforeParam param, IntegralConvertSelectVo activityInfo) throws MpException {
+        //不使用优惠券
+        param.setCouponSn(StringUtils.EMPTY);
+        //积分兑换比例
+        Map<Integer, IntegralConvertProductVo> productMap = activityInfo.getProductVo().stream().collect(Collectors.toMap(IntegralConvertProductVo::getScore, Function.identity()));
+        for (OrderBeforeParam.Goods goods : param.getGoods()) {
+            IntegralConvertProductVo prd = productMap.get(goods.getProductId());
+            //实际价格 = 兑换价格 + 积分(积分换算成对应金额)
+            goods.setProductPrice(
+                BigDecimalUtil.add(
+                    prd.getMoney(),
+                    BigDecimalUtil.multiply(new BigDecimal(prd.getScore()), new BigDecimal(scoreCfgService.getScoreProportion()))));
+
+        }
     }
 }
