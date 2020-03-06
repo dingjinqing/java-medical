43fefcac9a138e89d09f456f7da2452e96b7dded
wangbingbing <wangbingbing@huice.com>
2020-03-06 14:38:08 +0800

    砍价订单导出优化

--- jmp/mp-service/src/main/java/com/vpu/mp/service/pojo/wxapp/market/bargain/BargainRecordInfo.java
+++ jmp/mp-service/src/main/java/com/vpu/mp/service/pojo/wxapp/market/bargain/BargainRecordInfo.java
@@ -32,6 +32,8 @@ public class BargainRecordInfo {
     private BigDecimal prdPrice;
     private String prdDesc;
     private Integer prdNumber;
+    /** 1默认规格，0自定义规格（多规格）*/
+    private Byte is_default_product;
 
     private Byte bargainType;
     private Timestamp startTime;
