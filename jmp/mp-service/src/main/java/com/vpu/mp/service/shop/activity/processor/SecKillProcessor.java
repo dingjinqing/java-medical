3f524b83941f04db5250708fe16525e71bafc134
lixiaobing <lixiaobing@wangdian.cn>
2020-03-09 22:12:45 +0800

    小程序-商品列表-秒杀活动id设置

--- jmp/mp-service/src/main/java/com/vpu/mp/service/shop/activity/processor/SecKillProcessor.java
+++ jmp/mp-service/src/main/java/com/vpu/mp/service/shop/activity/processor/SecKillProcessor.java
@@ -69,7 +69,7 @@ public class SecKillProcessor implements Processor,ActivityGoodsListProcessor,Go
 
             capsule.setRealPrice(record3.get(SEC_KILL_PRODUCT_DEFINE.SEC_KILL_PRICE));
             GoodsActivityBaseMp activity = new GoodsActivityBaseMp();
-            activity.setActivityId(record3.get(SEC_KILL_DEFINE.SK_ID));
+            activity.setActivityId(record3.get(SEC_KILL_PRODUCT_DEFINE.SK_ID));
             activity.setActivityType(BaseConstant.ACTIVITY_TYPE_SEC_KILL);
             capsule.getGoodsActivities().add(activity);
             capsule.getProcessedTypes().add(BaseConstant.ACTIVITY_TYPE_SEC_KILL);
