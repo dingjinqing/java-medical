c525757675d01c4c804f602f76bc945d977e60ae
huangzhuangzhuang <huangzhuangzhuang@wangdian.cn>
2020-03-12 19:52:09 +0800

    update button css

--- web-vue/src/view/admin/index/leftNavComponents/user_manger/membershipCard/subcomponents/cardCustomRightsDialog.vue
+++ web-vue/src/view/admin/index/leftNavComponents/user_manger/membershipCard/subcomponents/cardCustomRightsDialog.vue
@@ -32,8 +32,8 @@
                 </el-form-item>
             </el-form>
             <div slot="footer" class="dialog-footer">
-                <el-button @click="dialogVisiable = false">取 消</el-button>
-                <el-button type="primary" @click="dialogVisiable = false">确 定</el-button>
+                <el-button @click="dialogVisiable = false" size="small">取 消</el-button>
+                <el-button type="primary" @click="dialogVisiable = false" size="small">确 定</el-button>
             </div>
         </el-dialog>
         <ImageDialog
