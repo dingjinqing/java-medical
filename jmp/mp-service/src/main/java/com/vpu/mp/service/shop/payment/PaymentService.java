3df1dc09ef679553b7572e7ded4e64d7c795ba6f
wangshuai <wangshuai01@wangdian.cn>
2020-03-12 10:25:44 +0800

    预售支付回调扣商品库存
    Signed-off-by: wangshuai <wangshuai01@wangdian.cn>
    
    # Conflicts:
    #	jmp/mp-service/src/main/java/com/vpu/mp/service/shop/payment/PaymentService.java

--- jmp/mp-service/src/main/java/com/vpu/mp/service/shop/payment/PaymentService.java
+++ jmp/mp-service/src/main/java/com/vpu/mp/service/shop/payment/PaymentService.java
@@ -18,9 +18,11 @@ import com.vpu.mp.service.pojo.shop.payment.PayCode;
 import com.vpu.mp.service.pojo.shop.payment.PaymentRecordParam;
 import com.vpu.mp.service.pojo.shop.payment.PaymentVo;
 import com.vpu.mp.service.pojo.wxapp.order.OrderBeforeParam;
+import com.vpu.mp.service.pojo.wxapp.order.goods.OrderGoodsBo;
 import com.vpu.mp.service.shop.activity.factory.OrderCreateMpProcessorFactory;
 import com.vpu.mp.service.shop.order.action.InsteadPayService;
 import com.vpu.mp.service.shop.order.action.PayService;
+import com.vpu.mp.service.shop.order.atomic.AtomicOperation;
 import com.vpu.mp.service.shop.order.goods.OrderGoodsService;
 import com.vpu.mp.service.shop.order.info.OrderInfoService;
 import com.vpu.mp.service.shop.order.store.StoreOrderService;
@@ -70,9 +72,11 @@ public class PaymentService extends ShopBaseService {
     @Autowired
     private SubOrderService subOrderService;
 
-
     @Autowired
     private InsteadPayService insteadPayService;
+
+    @Autowired
+    private AtomicOperation atomicOperation;
 
 	/**
 	 * 营销活动processorFactory
@@ -222,6 +226,7 @@ public class PaymentService extends ShopBaseService {
                     if(orderInfo.getIsLock().equals(OrderConstant.NO)) {
                         //修改相应预售商品数量销量库存
                         marketProcessorFactory.processUpdateStock(pay.createOrderBeforeParam(orderInfo), orderInfo);
+                        atomicOperation.updateStockAndSalesByLock(orderInfo, orderGoodsService.getByOrderId(orderInfo.getOrderId()).into(OrderGoodsBo.class), false);
                     }
 				} else {
 					// 全款支付方式时，则直接标记为尾款已支付
