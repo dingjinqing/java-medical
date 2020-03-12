13649997fa99693deb1f2221aa09fbaaaee19aa4
huangzhuangzhuang <huangzhuangzhuang@wangdian.cn>
2020-03-12 19:50:13 +0800

    finish custom right dialog develop

--- web-vue/src/view/admin/index/leftNavComponents/user_manger/membershipCard/subcomponents/cardCustomRightsDialog.vue
+++ web-vue/src/view/admin/index/leftNavComponents/user_manger/membershipCard/subcomponents/cardCustomRightsDialog.vue
@@ -6,11 +6,48 @@
             width="30%"
         >
 
+            <el-form :inline-message="true">
+                <el-form-item label="权益名称:" :rules="[{required: true}]">
+                    <el-input size="small" style="width: 165px;"></el-input>
+                    <span class="tips">最多可填20个字</span>
+                </el-form-item>
+                <el-form-item label="权益图标:" :rules="[{required: true}]">
+                    <span class="add-image" @click="addImg">
+                         <img :src="$imageHost +'/image/admin/shop_beautify/add_decorete.png'">
+                    </span>
+                    <span class="tips">建议尺寸：30*30像素</span>
+                </el-form-item>
+                <el-form-item label="权益说明:" :rules="[{required: true}]" class="right-content">
+                    <div class="content">
+                        <el-input
+                            type="textarea"
+                            :rows="5"
+                            maxlength="200"
+                            show-word-limit
+                            placeholder="请输入内容"
+                            v-model="textarea"
+                            style="width: 300px;">
+                        </el-input>
+                    </div>
+                </el-form-item>
+            </el-form>
+            <div slot="footer" class="dialog-footer">
+                <el-button @click="dialogVisiable = false">取 消</el-button>
+                <el-button type="primary" @click="dialogVisiable = false">确 定</el-button>
+            </div>
         </el-dialog>
+        <ImageDialog
+            pageIndex='userCardAdd'
+            :tuneUp="imgDisable"
+            :imageSize="[30, 30]"
+        />
     </div>
 </template>
 <script>
 export default {
+  components: {
+    ImageDialog: () => import('@/components/admin/imageDalog')
+  },
   props: {
     visible: {
       type: Boolean,
@@ -27,9 +64,45 @@ export default {
         this.$emit('update:visible', val)
       }
     }
+  },
+  data () {
+    return {
+      imgDisable: false
+    }
+  },
+  methods: {
+    addImg () {
+      this.imgDisable = !this.imgDisable
+    }
   }
 }
 </script>
 <style scoped lang="scss">
+.add-image{
+    display: inline-block;
+    background: #fff;
+    border: 1px solid #e4e4e4;
+    width: 70px;
+    height: 70px;
+    text-align: center;
+    line-height: 70px;
+    margin-top: 10px;
+    img{
+        vertical-align: middle;
+    }
+    cursor: pointer;
+}
+.add-image:hover{
+    background: #f5f3f3;
+}
+.tips{
+    margin-left: 10px;
+    color: #999;
+}
+.right-content{
+    .content{
+        margin-top: 10px;
+    }
+}
 
 </style>
