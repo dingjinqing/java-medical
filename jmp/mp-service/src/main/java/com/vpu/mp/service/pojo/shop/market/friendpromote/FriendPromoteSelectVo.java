25e877d3bb8943486e9e446ede8ebeb554bda306
liangchen <liangchen@wangdian.cn>
2020-03-12 16:07:43 +0800

    优化admin助力查询单个活动

--- jmp/mp-service/src/main/java/com/vpu/mp/service/pojo/shop/market/friendpromote/FriendPromoteSelectVo.java
+++ jmp/mp-service/src/main/java/com/vpu/mp/service/pojo/shop/market/friendpromote/FriendPromoteSelectVo.java
@@ -1,5 +1,6 @@
 package com.vpu.mp.service.pojo.shop.market.friendpromote;
 
+import java.math.BigDecimal;
 import java.sql.Timestamp;
 
 import lombok.Data;
@@ -27,7 +28,37 @@ public class FriendPromoteSelectVo {
 	private String actName;
 	private String rewardContent;
 	private Byte rewardType;
-	
+	private Byte promoteType;
+    private Timestamp startTime;
+    private Timestamp endTime;
+    /** 奖励有效期 */
+    private Integer rewardDuration;
+    /** 奖励有效期单位 */
+    private Byte rewardDurationUnit;
+    /** 所需助力总值 */
+    private BigDecimal promoteAmount;
+    /** 所需助力次数 */
+    private Integer promoteTimes;
+    /** 发起限制次数，0不限制 */
+    private Byte launchLimitTimes;
+    /** 发起次数限制时长，0不限制 */
+    private Integer launchLimitDuration;
+    /** 发起次数限制时长单位：0小时 1天，2周，3月，4年 */
+    private Byte launchLimitUnit;
+    /** 0可不授权个人信息，1必须授权 */
+    private Byte promoteCondition;
+    /** 分享可得助力次数*/
+    private Byte shareCreateTimes;
+    private Byte useDiscount;
+    private Byte useScore;
+    private Byte activityShareType;
+    /** 自定义分享样式文案 */
+    private String customShareWord;
+    /** 自定义分享图片类型：0首页截图，1自定义图片 */
+    private Byte shareImgType;
+    /** 自定义分享样式图片路径 */
+    private String customImgPath;
+    private Integer promoteTimesPerDay;
 	private Byte failedSendType;
 	private Integer failedSendContent;
 
