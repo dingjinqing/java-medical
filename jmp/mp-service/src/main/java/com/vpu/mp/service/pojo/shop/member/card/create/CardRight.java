1d7a6ef3d5cbd77e431ee0406cd40303d1fd4586
huangzhuangzhuang <huangzhuangzhuang@wangdian.cn>
2020-03-13 12:55:13 +0800

    update author

--- jmp/mp-service/src/main/java/com/vpu/mp/service/pojo/shop/member/card/create/CardRight.java
+++ jmp/mp-service/src/main/java/com/vpu/mp/service/pojo/shop/member/card/create/CardRight.java
@@ -1,13 +1,12 @@
 package com.vpu.mp.service.pojo.shop.member.card.create;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.vpu.mp.service.foundation.util.Util;
 
 import lombok.Data;
 /**
  * 	卡权益具体信息
- * @author Q10Viking
+ * @author 黄壮壮
  *
  */
 @Data
@@ -15,19 +14,16 @@ public class CardRight {
 	/**
 	 * 	权限名称
 	 */
-	@JsonProperty("cright_name")
 	private String crightName;
 	
 	/**
 	 * 	权益图标url
 	 */
-	@JsonProperty("cright_image")
 	private String crightImage;
 	
 	/**
 	 * 	权益说明
 	 */
-	@JsonProperty("cright_content")
 	private String crightContent;
 	
 	public static void main(String[] args) throws JsonProcessingException {
