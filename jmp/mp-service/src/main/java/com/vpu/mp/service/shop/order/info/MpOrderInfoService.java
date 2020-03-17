d1bebd4fe263709c1febc840f6106f2b43e4b2f4
wangshuai <wangshuai01@wangdian.cn>
2020-03-17 22:07:25 +0800

    bug:ONLY_FULL_GROUP_BY order by 报错
    Signed-off-by: wangshuai <wangshuai01@wangdian.cn>

--- jmp/mp-service/src/main/java/com/vpu/mp/service/shop/order/info/MpOrderInfoService.java
+++ jmp/mp-service/src/main/java/com/vpu/mp/service/shop/order/info/MpOrderInfoService.java
@@ -8,8 +8,6 @@ import com.vpu.mp.service.pojo.wxapp.order.OrderListMpVo;
 import com.vpu.mp.service.pojo.wxapp.order.OrderListParam;
 import org.jooq.Condition;
 import org.jooq.Record;
-import org.jooq.Record1;
-import org.jooq.Record2;
 import org.jooq.SelectJoinStep;
 import org.jooq.SelectWhereStep;
 import org.jooq.impl.DSL;
@@ -41,38 +39,38 @@ public class MpOrderInfoService extends OrderInfoService{
      * @return
 	 */
 	public Map<Byte,Integer> getOrderStatusNum(Integer userId, OrderListParam param, boolean isContainSubOrder) {
-		//基础状态数量
-        SelectJoinStep<Record2<Integer, Byte>> countMapSelect = db().select(DSL.count(), TABLE.ORDER_STATUS).from(TABLE);
-        buildOptions(countMapSelect, param, false);
-        Map<Byte, Integer> countMap = countMapSelect.
-		where(setIsContainSubOrder(
+	    //搜索条件
+        Condition condition = DSL.noCondition();
+        if(param != null && !StringUtils.isBlank(param.getSearch())) {
+            condition.and(TABLE.ORDER_SN.contains(param.getSearch()).or(ORDER_GOODS.GOODS_NAME.contains(param.getSearch())));
+        }
+        //基础状态数量
+        Map<Byte, Integer> countMap =
+            setIsContainSearch(db().select(DSL.count(), TABLE.ORDER_STATUS).from(TABLE), param).
+            where(setIsContainSubOrder(
 				TABLE.DEL_FLAG.eq(DelFlag.NORMAL.getCode()).
 				and(TABLE.REFUND_STATUS.eq(OrderConstant.REFUND_DEFAULT_STATUS)).
 				and(TABLE.USER_ID.eq(userId))
-				, isContainSubOrder)).
+				, isContainSubOrder).and(condition)).
 		groupBy(TABLE.ORDER_STATUS).
 		fetch().
 		intoMap(TABLE.ORDER_STATUS , DSL.count());
 		//退款退货中
-        SelectJoinStep<Record1<Integer>> returningSelect = db().select(DSL.count()).from(TABLE);
-        buildOptions(returningSelect, param, false);
-        Integer returning = returningSelect.
-		where(setIsContainSubOrder(
+        Integer returning = setIsContainSearch(db().select(DSL.count()).from(TABLE), param).
+            where(setIsContainSubOrder(
 				TABLE.DEL_FLAG.eq(DelFlag.NORMAL.getCode()).
 				and(TABLE.REFUND_STATUS.eq(OrderConstant.REFUND_STATUS_FINISH)).
 				and(TABLE.USER_ID.eq(userId))
-				,isContainSubOrder)).
+				,isContainSubOrder).and(condition)).
 		fetchOneInto(Integer.class);
 		//退款退货完成
-        SelectJoinStep<Record1<Integer>> returnFinishSelect = db().select(DSL.count()).from(TABLE);
-        buildOptions(returnFinishSelect, param, false);
-        Integer returnFinish = returnFinishSelect.
-		where(setIsContainSubOrder(
+        Integer returnFinish = setIsContainSearch(db().select(DSL.count()).from(TABLE), param).
+            where(setIsContainSubOrder(
 				TABLE.DEL_FLAG.eq(DelFlag.NORMAL.getCode()).
 				and(TABLE.REFUND_STATUS.in(OrderConstant.REFUND_STATUS_AUDITING , OrderConstant.REFUND_STATUS_AUDIT_PASS, OrderConstant.REFUND_STATUS_AUDIT_NOT_PASS , OrderConstant.REFUND_STATUS_APPLY_REFUND_OR_SHIPPING , OrderConstant.REFUND_STATUS_FINISH , OrderConstant.REFUND_STATUS_REFUSE)).
 				and(TABLE.USER_ID.eq(userId)).
 				and(TABLE.ORDER_STATUS.notIn(OrderConstant.ORDER_FINISHED , OrderConstant.ORDER_RETURN_FINISHED , OrderConstant.ORDER_REFUND_FINISHED))
-				,isContainSubOrder)).
+				,isContainSubOrder).and(condition)).
 		fetchOneInto(Integer.class);
 		//初始化不可变map
 		Map<Byte,Integer> result = ImmutableMap.<Byte,Integer>builder()
@@ -92,7 +90,14 @@ public class MpOrderInfoService extends OrderInfoService{
 		return result;
 	}
 
-	/**
+    private SelectWhereStep<?> setIsContainSearch(SelectJoinStep<?> from, OrderListParam param) {
+        if(param != null && !StringUtils.isBlank(param.getSearch())) {
+            from.leftJoin(ORDER_GOODS).on(TABLE.ORDER_ID.eq(ORDER_GOODS.ORDER_ID));
+        }
+        return from;
+    }
+
+    /**
 	 * Map value type is Integer,set default value 0
 	 * @param countMap
 	 * @param status
