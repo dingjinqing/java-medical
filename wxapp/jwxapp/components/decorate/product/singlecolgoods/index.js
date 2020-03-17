96135928c0d6b38e19257859515d986e6ac1a0c2
suntengfei <suntengfei@wangdian.cn>
2020-03-17 19:21:29 +0800

    积分模块接口数据修复

--- wxapp/jwxapp/components/decorate/product/singlecolgoods/index.js
+++ wxapp/jwxapp/components/decorate/product/singlecolgoods/index.js
@@ -4,15 +4,11 @@ var util = require("../../../../utils/util.js")
 global.wxComponent({
   mixins: [base],
   methods: {
-    onPropChange(newVal, oldVal, changedPath) {
+    onPropChange (newVal, oldVal, changedPath) {
       console.log(newVal, 'single')
       // 处理活动
       this.handleToGoodsActivities(newVal.goodsListData)
 
-    },
-    // 处理活动数据
-    bindMenuClick(e) {
-
-    },
+    }
   }
 });
