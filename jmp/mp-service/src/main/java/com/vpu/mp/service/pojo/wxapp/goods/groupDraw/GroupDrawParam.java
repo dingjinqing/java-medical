73dcc3537f33e49260ab60e9f227891a4ecce7df
wangbingbing <wangbingbing@huice.com>
2020-03-17 13:48:18 +0800

    拼团抽奖分享功能

--- jmp/mp-service/src/main/java/com/vpu/mp/service/pojo/wxapp/goods/groupDraw/GroupDrawParam.java
+++ jmp/mp-service/src/main/java/com/vpu/mp/service/pojo/wxapp/goods/groupDraw/GroupDrawParam.java
@@ -2,7 +2,11 @@ package com.vpu.mp.service.pojo.wxapp.goods.groupDraw;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import jodd.util.StringUtil;
 import lombok.Data;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
 
 /**
  * 小程序拼团抽奖列表入参
@@ -14,5 +18,24 @@ import lombok.Data;
 public class GroupDrawParam {
 	@JsonProperty(value = "group_draw_id")
 	private Integer groupDrawId;
+
+    /**
+     * 小程序扫码进详情页带的参数
+     */
+    private String scene;
+
+    public void initScene(){
+        if((this.groupDrawId == null || this.groupDrawId <= 0) && StringUtil.isNotBlank(this.scene)){
+            String scene = null;
+            try {
+                scene = URLDecoder.decode(this.scene,"UTF-8");
+            } catch (UnsupportedEncodingException e) {
+            }
+            if(StringUtil.isNotEmpty(scene)){
+                String[] sceneParam = scene.split("=",2);
+                this.groupDrawId =  Integer.valueOf(sceneParam[1]);
+            }
+        }
+    }
 
 }
