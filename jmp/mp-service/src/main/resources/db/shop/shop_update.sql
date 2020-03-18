c122e107481435a7de57946b2bbc6aabdd2ed78a
huangzhuangzhuang <huangzhuangzhuang@wangdian.cn>
2020-03-18 13:52:26 +0800

    删除after

--- jmp/mp-service/src/main/resources/db/shop/shop_update.sql
+++ jmp/mp-service/src/main/resources/db/shop/shop_update.sql
@@ -133,6 +133,7 @@ ALTER TABLE `b2c_member_card` ADD COLUMN `renew_time` int(11) DEFAULT NULL COMME
 ALTER TABLE `b2c_member_card` ADD COLUMN `renew_date_type` tinyint(1) DEFAULT NULL COMMENT '0:日，1:周 2: 月';
 
 
+
 -- 商品表和规格表设置市场价字段可以为null
 ALTER TABLE b2c_goods MODIFY market_price DECIMAL(10,2) COMMENT '市场价格，多规格时取最高';
 ALTER TABLE b2c_goods_spec_product MODIFY prd_market_price DECIMAL(10,2) COMMENT '市场价格';
@@ -151,8 +152,8 @@ ALTER TABLE `b2c_group_integration_define` ADD COLUMN `activity_copywriting` TEX
 
 
 -- 2020-03-17 用户卡表添加包邮快照信息字段
--- ALTER TABLE `b2c_user_card` ADD COLUMN `free_limit` tinyint(3) DEFAULT -1 COMMENT '-1：不包邮，0:不限制，1：持卡有效期内，2：年，3：季，4：月，5：周，6：日' AFTER `id`;
--- ALTER TABLE `b2c_user_card` ADD COLUMN `free_num` int(11) DEFAULT 0 COMMENT '周期内包邮次数' AFTER `free_limit`;
+ALTER TABLE `b2c_user_card` ADD COLUMN `free_limit` tinyint(3) DEFAULT -1 COMMENT '-1：不包邮，0:不限制，1：持卡有效期内，2：年，3：季，4：月，5：周，6：日';
+ALTER TABLE `b2c_user_card` ADD COLUMN `free_num` int(11) DEFAULT 0 COMMENT '周期内包邮次数';
 
 
 
