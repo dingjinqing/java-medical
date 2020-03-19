c5f1fbc57ceac48b3f07667ee5229be8ec486651
huangzhuangzhuang <huangzhuangzhuang@wangdian.cn>
2020-03-19 11:22:08 +0800

    有效会员卡添加包邮信息

--- jmp/mp-service/src/main/java/com/vpu/mp/service/pojo/shop/member/card/ValidUserCardBean.java
+++ jmp/mp-service/src/main/java/com/vpu/mp/service/pojo/shop/member/card/ValidUserCardBean.java
@@ -6,6 +6,9 @@ import java.math.BigDecimal;
 import java.sql.Timestamp;
 import java.time.LocalDate;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.vpu.mp.service.pojo.shop.member.card.create.CardFreeship;
+
 /**
 * @author 黄壮壮
 * @Date: 2019年10月12日
@@ -27,6 +30,15 @@ public class ValidUserCardBean {
 	private Integer  exchangSurplus;
 	private Timestamp  createTime;
 	private Timestamp  updateTime;
+	/**
+	 * 	用户卡的快照包邮信息
+	 */
+	// 包邮周期类型
+	@JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+	protected Byte freeLimit;
+	// 包邮次数
+	@JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+	protected Integer freeNum;
 	
 	// memberCard表字段映射
 	private String cardName;
@@ -44,9 +56,24 @@ public class ValidUserCardBean {
 	private Byte storeUseSwitch;
 	private String storeList;
 	private String grade;
+	/**
+	 * freeship_limit包邮周期类型 -1：不包邮，0:不限制，1：持卡有效期内，2：年，3：季，4：月，5：周，6：日
+	 */
+	@JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+	protected Byte freeshipLimit;
+	/**
+	 * freeship_num 周期内包邮次数
+	 */
+	@JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+	protected Integer freeshipNum;
 	
 	// 头像
 	private String avatar;
     private LocalDate startDate;
     private LocalDate endDate;
+    
+    /**
+     * 	包邮信息描述
+     */
+    private CardFreeship cardFreeShip;
 }
