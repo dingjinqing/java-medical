8942ef304f7a858924cedda05b26ac5773453586
zhaojianqiang <zhaojianqiang@wangdian.cn>
2020-03-20 19:13:57 +0800

    提示的修改

--- web-vue/src/view/admin/index/leftNavComponents/user_manger/integralManagement/integralRule.vue
+++ web-vue/src/view/admin/index/leftNavComponents/user_manger/integralManagement/integralRule.vue
@@ -820,7 +820,7 @@ export default {
         let scoreProportion = Number(this.form.scoreProportion)
         let scorePayNum = Number(this.form.scorePayNum)
         if (scorePayNum < scoreProportion || scorePayNum % scoreProportion !== 0) {
-          this.$message.error('积分支付限制应大于等于' + scoreProportion + ',且为' + scorePayNum + '的整数倍')
+          this.$message.error('积分支付限制应大于等于' + scoreProportion + ',且为' + scoreProportion + '的整数倍')
           return false
         } else {
           return true
