fa140e1eabfa3128c9789d23b5add0dfe7b3bb31
suntengfei <suntengfei@wangdian.cn>
2020-03-23 17:21:16 +0800

    部分显示修复

--- wxapp/jwxapp/components/decorate/imgnav/index.js
+++ wxapp/jwxapp/components/decorate/imgnav/index.js
@@ -1,10 +1,12 @@
 var base = require("../mixins/base.js");
-
+var util = require("../../../utils/util.js")
 global.wxComponent({
   mixins: [base],
   methods: {
     bindJumpLink (e) {
       console.log(e.currentTarget.dataset)
+      var d = this.eventData(e);
+      util.jumpLink(d.link);
     }
   }
 });
