b83f15925e300d9e1d007c71a96bb845dcf24f2e
luguangyao <luguangyao@wangdian.cn>
2020-03-24 23:23:05 +0800

    Fix:修改微信公众号发送消息判断逻辑

--- jmp/mp-background/src/main/java/com/vpu/mp/mq/listener/MessageTemplateListener.java
+++ jmp/mp-background/src/main/java/com/vpu/mp/mq/listener/MessageTemplateListener.java
@@ -55,7 +55,7 @@ public class MessageTemplateListener implements BaseRabbitHandler {
             if( saas.getShopApp(param.getShopId()).wechatMessageTemplateService.sendMessage(param,info)){
                 //自定义模版消息更新发送记录状态
                 if( param.getType().equals(RabbitParamConstant.Type.GENERAL_TYPE) ){
-                    saas.getShopApp(param.getShopId()).messageTemplateService.updateTemplateStatus(info.getUserId(),param.getMessageTemplateId());
+                    saas.getShopApp(param.getShopId()).messageTemplateService.updateTemplateSendStatus(info.getUserId(),param.getMessageTemplateId());
                 }
 
             }else{
@@ -63,7 +63,9 @@ public class MessageTemplateListener implements BaseRabbitHandler {
             }
 
         });
-
+        if( RabbitParamConstant.Type.GENERAL_TYPE.equals(param.getType()) ){
+            saas.getShopApp(param.getShopId()).messageTemplateService.updateTemplateStatus(param.getMessageTemplateId());
+        }
         param.setUserIdList(failList);
 
         //更新taskJob进度和状态
