9d37da15031a6edbd06364ea8072516950ef95e0
kangyaxin <kangyaxin@wangdian.cn>
2020-03-24 08:31:30 +0800

    订阅消息补充

--- wxapp/jwxapp/pages/bargaininfo/bargaininfo.js
+++ wxapp/jwxapp/pages/bargaininfo/bargaininfo.js
@@ -162,25 +162,26 @@ global.wxPage({
   toWhere: function (e) {
     var bargain_id = bargain_info.recordInfo.bargainId;
     var goods_id = bargain_info.recordInfo.goodsId;
-    console.log('pages/item/item?aid=' + bargain_id + '&&atp=3&&gid=' + goods_id)
-    // 判断是否为多规格
-    if (bargain_info.recordInfo.isDefaultProduct == 0) {
-      var url = 'pages/item/item?aid=' + bargain_id + '&&atp=3&&gid=' + goods_id;
-      util.jumpLink(url);
-    } else if (bargain_info.recordInfo.isDefaultProduct == 1) {
-      util.api("/api/wxapp/bargain/apply", function (res) {
-        if (res.error == 0) {
-          util.reLaunch({
-            url: "/pages/bargaininfo/bargaininfo?record_id=" + res.content.recordId
+    util.getNeedTemplateId('invite', () => {
+      // 判断是否为多规格
+      if (bargain_info.recordInfo.isDefaultProduct == 0) {
+        var url = 'pages/item/item?aid=' + bargain_id + '&&atp=3&&gid=' + goods_id;
+        util.jumpLink(url);
+      } else if (bargain_info.recordInfo.isDefaultProduct == 1) {
+        util.api("/api/wxapp/bargain/apply", function (res) {
+          if (res.error == 0) {
+            util.reLaunch({
+              url: "/pages/bargaininfo/bargaininfo?record_id=" + res.content.recordId
+            })
+          } else {
+            util.showModal('提示', res.content);
+          }
+        }, {
+            bargainId: bargain_info.recordInfo.bargainId,
+            prdId: bargain_info.recordInfo.prdId
           })
-        } else {
-          util.showModal('提示', res.content);
-        }
-      }, {
-          bargainId: bargain_info.recordInfo.bargainId,
-          prdId: bargain_info.recordInfo.prdId
-        })
-    }
+      }
+    })
   },
   /**
    * 页面相关事件处理函数--监听用户下拉动作
