844b020f7392317634615fe50225738e07077137
liuyang <liuyang01@wangdian.cn>
2020-03-26 19:11:02 +0800

    单商品评价页部分字段调试

--- wxapp/jwxapp/pages1/goodscomment/goodscomment.js
+++ wxapp/jwxapp/pages1/goodscomment/goodscomment.js
@@ -1,20 +1,85 @@
-// pages1/goodscomment/goodscomment.js
+var util = require('../../utils/util.js')
 global.wxPage({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    commentTypeList:{
+      0:"全部评价",
+      1:"好评",
+      2:"中评",
+      3:"差评",
+      4:"有图"
+    }
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    let {type,goodsId} = options
+    this.setData({
+      type,
+      goodsId,
+      chooseTarget:type
+    })
+    this.requestCommentList()
   },
-
+  requestCommentList(){
+    util.api('/api/wxapp/comment/goods',res=>{
+      if(res.error === 0){
+        this.setData({
+          dataList:this.resetCommentList(res.content.comment),
+          commentNum:this.getCommentNum(res.content.number)
+        })
+        console.log(this.data.dataList)
+        console.log(this.data.commentNum)
+      }
+    },{
+      type:this.data.type,
+      goodsId:this.data.goodsId
+    })
+  },
+  resetCommentList(dataList){
+    return dataList.map(item=>{
+      return {
+        ...item,
+        commentImageList:this.setCommentImage(item.commImg),
+        commentStarNum:this.getStarList(item.commstar)
+      }
+    })
+  },
+  setCommentImage(commentImage){
+    if(!commentImage) return false
+    if(Array.isArray(commentImage)) return commentImage
+    return JSON.parse(commentImage)
+  },
+  getStarList(commstar){
+    let starList = []
+    for(let i = 0; i < commstar;i++){
+      starList.push(i)
+    }
+    return starList
+  },
+  getCommentNum(num){
+    let AllNum = num.reduce((defaultNum,item)=>{
+      if(item.type !== 4) defaultNum+=item.num
+      return defaultNum
+    },0)
+    return [
+      {type:0,num:AllNum},
+      ...num
+    ]   
+  },
+  toggleType(e){
+    let {type} = e.currentTarget.dataset
+    this.setData({
+      type,
+      chooseTarget:type
+    })
+    this.requestCommentList()
+  },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
@@ -35,7 +100,6 @@ global.wxPage({
   onHide: function () {
 
   },
-
   /**
    * 生命周期函数--监听页面卸载
    */
