24855decfe0ec96b7caa8c55cf5b746d966bdcdc
luguangyao <luguangyao@wangdian.cn>
2020-03-26 09:53:03 +0800

    Fix:修复删除品牌/分类索引未更新bug

--- jmp/mp-service/src/main/java/com/vpu/mp/service/shop/goods/GoodsBrandService.java
+++ jmp/mp-service/src/main/java/com/vpu/mp/service/shop/goods/GoodsBrandService.java
@@ -160,7 +160,7 @@ public class GoodsBrandService extends ShopBaseService {
             () ->
         {
             deleteGoodsBrand(Collections.singletonList(brandId));
-            esDataUpdateMqService.updateEsGoodsIndexByBrandId(brandId,getShopId());
+
         });
     }
 
@@ -174,6 +174,7 @@ public class GoodsBrandService extends ShopBaseService {
             .where(GOODS_BRAND.ID.in(brandIds))
             .execute();
 
+        esDataUpdateMqService.updateEsGoodsIndexByBrandId(brandIds,getShopId());
         saas().getShopApp(getShopId()).goods.clearBrandId(brandIds);
         //cache update
         goodsBrandDataHelper.delete(brandIds);
