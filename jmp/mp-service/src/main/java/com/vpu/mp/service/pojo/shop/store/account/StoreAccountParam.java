a33ed47098bf33c4a0b6e2444fc362b0dfa9f9a9
zhaojianqiang <zhaojianqiang@wangdian.cn>
2020-03-27 09:38:08 +0800

    国际化翻译

--- jmp/mp-service/src/main/java/com/vpu/mp/service/pojo/shop/store/account/StoreAccountParam.java
+++ jmp/mp-service/src/main/java/com/vpu/mp/service/pojo/shop/store/account/StoreAccountParam.java
@@ -16,8 +16,10 @@ import lombok.Data;
 @Data
 public class StoreAccountParam {
 	@NotNull
+	@Pattern(regexp = "^1[3|7|8]\\d{9}$|^19[8-9]\\d{8}$|^166\\d{8}|^15[0-3|5-9]\\d{8}|^14[5|7]\\d{8}$",message = JsonResultMessage.MSG_ACCOUNT_MOBILE_LENGTH_LIMIT)
 	private String mobile;
 	@NotNull
+	@Pattern(regexp = "^[^\\u4e00-\\u9fa5]{1,20}$",message = JsonResultMessage.MSG_ACCOUNT_USERNAME_LENGTH_LIMIT)
 	private String accountName;
 	@NotNull
 	private Byte accountType;
