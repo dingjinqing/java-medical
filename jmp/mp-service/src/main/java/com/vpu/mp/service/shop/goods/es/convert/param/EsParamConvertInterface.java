7aec1e5a4c933a6226201e1aa4671a69e7caab03
luguangyao <luguangyao@wangdian.cn>
2020-03-27 16:10:28 +0800

    Fix:es小程序新加排序功能实现

--- jmp/mp-service/src/main/java/com/vpu/mp/service/shop/goods/es/convert/param/EsParamConvertInterface.java
+++ jmp/mp-service/src/main/java/com/vpu/mp/service/shop/goods/es/convert/param/EsParamConvertInterface.java
@@ -2,8 +2,12 @@ package com.vpu.mp.service.shop.goods.es.convert.param;
 
 import com.vpu.mp.service.pojo.shop.goods.es.EsSearchParam;
 import com.vpu.mp.service.pojo.shop.goods.es.Fact;
+import com.vpu.mp.service.pojo.shop.goods.es.Sort;
 import com.vpu.mp.service.pojo.shop.goods.goods.GoodsPageListParam;
 import com.vpu.mp.service.pojo.wxapp.goods.goods.list.GoodsListMpParam;
+import com.vpu.mp.service.pojo.wxapp.goods.search.SortDirectionEnum;
+import com.vpu.mp.service.pojo.wxapp.goods.search.SortItemEnum;
+import org.elasticsearch.search.sort.SortOrder;
 
 /**
  * param转换接口
@@ -31,5 +35,15 @@ public interface EsParamConvertInterface {
         return Fact.builder().name(name)
             .fieldName(name)
             .build();
+    }
+    default Sort getSort(SortItemEnum sortItemEnum, SortDirectionEnum sortDirectionEnum){
+        Sort sort = new Sort();
+        if( GoodsPageListParam.ASC.equals(sortDirectionEnum.toString()) ){
+            sort.setSortOrder(SortOrder.ASC);
+        }else{
+            sort.setSortOrder(SortOrder.DESC);
+        }
+        sort.setSortName(sortItemEnum.getEsName());
+        return sort;
     }
 }
