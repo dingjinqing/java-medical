185e1061a137a9bf883fae86da3d36ea95a98223
kangyaxin <kangyaxin@wangdian.cn>
2020-03-30 16:48:04 +0800

    优惠券优化

--- wxapp/jwxapp/components/item/popup/coupon/index.js
+++ wxapp/jwxapp/components/item/popup/coupon/index.js
@@ -21,36 +21,36 @@ global.wxComponent({
           util.jumpLink('/pages/getCoupon/getCoupon?id=' + d.coupon_id);
         } else {
           // 直接领取
-          // util.api('api/wxapp/coupon/get', function (res) {
-          //   wx.hideLoading();
-          //   if (res.error == 0) {
-          //     if (res.content == 0) {
-          //       util.toast_success('领取成功')
-          //       _this.data.coupon_list.forEach((item, index) => {
-          //         if (item.id === couponId) {
-          //           item.canFetch = false
-          //         }
-          //       })
-          //       _this.$set();
-          //     } else if (res.content == 1) {
-          //       util.toast_fail('优惠券不存在');
-          //     } else if (res.content == 2) {
-          //       util.toast_fail('优惠券已过期');
-          //     } else if (res.content == 3) {
-          //       util.toast_fail('优惠券已停用');
-          //     } else if (res.content == 4) {
-          //       util.toast_fail('优惠券库存为0');
-          //     } else if (res.content == 5) {
-          //       util.toast_fail('可用积分不足');
-          //     } else if (res.content == 6) {
-          //       util.toast_fail('积分更新失败');
-          //     } else if (res.content == 7) {
-          //       util.toast_fail('领取次数达上限');
-          //     }
-          //   } else {
-          //     util.toast_fail('领取失败');
-          //   }
-          // }, { couponId: d.coupon_id });
+          util.api('api/wxapp/coupon/get', function (res) {
+            wx.hideLoading();
+            if (res.error == 0) {
+              if (res.content == 0) {
+                util.toast_success('领取成功')
+                _this.data.coupon_list.forEach((item, index) => {
+                  if (item.id === couponId) {
+                    item.canFetch = false
+                  }
+                })
+                _this.$set();
+              } else if (res.content == 1) {
+                util.toast_fail('优惠券不存在');
+              } else if (res.content == 2) {
+                util.toast_fail('优惠券已过期');
+              } else if (res.content == 3) {
+                util.toast_fail('优惠券已停用');
+              } else if (res.content == 4) {
+                util.toast_fail('优惠券库存为0');
+              } else if (res.content == 5) {
+                util.toast_fail('可用积分不足');
+              } else if (res.content == 6) {
+                util.toast_fail('积分更新失败');
+              } else if (res.content == 7) {
+                util.toast_fail('领取次数达上限');
+              }
+            } else {
+              util.toast_fail('领取失败');
+            }
+          }, { couponId: d.coupon_id });
         }
       }
     }
