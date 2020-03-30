e94c8ee43bbaf6f913596fb167ee1688f4bbb666
kangyaxin <kangyaxin@wangdian.cn>
2020-03-30 10:57:35 +0800

    优惠券领取优化

--- wxapp/jwxapp/components/item/popup/coupon/index.js
+++ wxapp/jwxapp/components/item/popup/coupon/index.js
@@ -9,55 +9,50 @@ global.wxComponent({
   methods: {
     // ...coupon,
     bindGetCoupon(e) {
-      // var d = this.eventData(e);
-      // d.form_id = e.detail.form_id;
-
-      // var _this = this;
-      // var coupon = this.data.coupon_list[d.coupon];
-      // coupon.disableds = true;
-      // this.$set();
-
-      // this.getCoupon(couponId, function(content) {
-      //   coupon.canFetch = 0;
-      //   coupon.disableds = false;
-      //   _this.$set();
-      //   _this.$emit("get_coupon_ok", couponId);
-      // });
-
+      var d = this.eventData(e);
       var _this = this;
-      var couponId = e.target.dataset.coupon_id
-      util.api('api/wxapp/coupon/get', function (res) {
-        wx.hideLoading();
-        if (res.error == 0) {
-          if (res.content == 0) {
-            util.toast_success('领取成功')
-            _this.data.coupon_list.forEach((item, index) => {
-              if (item.id === couponId) {
-                item.canFetch = false
-              }
-            })
-            _this.$set();
-          } else if (res.content == 1) {
-            util.toast_fail('优惠券不存在');
-          } else if (res.content == 2) {
-            util.toast_fail('优惠券已过期');
-          } else if (res.content == 3) {
-            util.toast_fail('优惠券已停用');
-          } else if (res.content == 4) {
-            util.toast_fail('优惠券库存为0');
-          } else if (res.content == 5) {
-            util.toast_fail('可用积分不足');
-          } else if (res.content == 6) {
-            util.toast_fail('积分更新失败');
-          } else if (res.content == 7) {
-            util.toast_fail('领取次数达上限');
-          }
+
+      if (d.use_score === true) {
+        // 积分领取
+        util.jumpLink('/pages/receiveCoupon/receiveCoupon?couponId=' + d.coupon_id);
+      } else {
+        if (d.vali !== '') {
+          // 领取页面
+          util.jumpLink('/pages/getCoupon/getCoupon?id=' + d.coupon_id);
         } else {
-          util.toast_fail('领取失败');
+          // 直接领取
+          // util.api('api/wxapp/coupon/get', function (res) {
+          //   wx.hideLoading();
+          //   if (res.error == 0) {
+          //     if (res.content == 0) {
+          //       util.toast_success('领取成功')
+          //       _this.data.coupon_list.forEach((item, index) => {
+          //         if (item.id === couponId) {
+          //           item.canFetch = false
+          //         }
+          //       })
+          //       _this.$set();
+          //     } else if (res.content == 1) {
+          //       util.toast_fail('优惠券不存在');
+          //     } else if (res.content == 2) {
+          //       util.toast_fail('优惠券已过期');
+          //     } else if (res.content == 3) {
+          //       util.toast_fail('优惠券已停用');
+          //     } else if (res.content == 4) {
+          //       util.toast_fail('优惠券库存为0');
+          //     } else if (res.content == 5) {
+          //       util.toast_fail('可用积分不足');
+          //     } else if (res.content == 6) {
+          //       util.toast_fail('积分更新失败');
+          //     } else if (res.content == 7) {
+          //       util.toast_fail('领取次数达上限');
+          //     }
+          //   } else {
+          //     util.toast_fail('领取失败');
+          //   }
+          // }, { couponId: d.coupon_id });
         }
-      }, {
-          couponId: couponId
-        });
+      }
     }
   }
 });
