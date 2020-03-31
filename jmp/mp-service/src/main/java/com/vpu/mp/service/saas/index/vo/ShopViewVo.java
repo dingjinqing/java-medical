17a71fed31ea2308846d670eaf488bc81bc9bcf6
luguangyao <luguangyao@wangdian.cn>
2020-03-31 17:35:20 +0800

    Fix:补充提交

--- jmp/mp-service/src/main/java/com/vpu/mp/service/saas/index/vo/ShopViewVo.java
+++ jmp/mp-service/src/main/java/com/vpu/mp/service/saas/index/vo/ShopViewVo.java
@@ -7,4 +7,31 @@ public class ShopViewVo {
 
     private AccountStatisticsInfo accountStatisticsInfo;
 
+    private OrderStatisticsInfo orderStatisticsInfo;
+
+    private UserStatisticsInfo userStatisticsInfo;
+
+    public AccountStatisticsInfo getAccountStatisticsInfo() {
+        return accountStatisticsInfo;
+    }
+
+    public void setAccountStatisticsInfo(AccountStatisticsInfo accountStatisticsInfo) {
+        this.accountStatisticsInfo = accountStatisticsInfo;
+    }
+
+    public OrderStatisticsInfo getOrderStatisticsInfo() {
+        return orderStatisticsInfo;
+    }
+
+    public void setOrderStatisticsInfo(OrderStatisticsInfo orderStatisticsInfo) {
+        this.orderStatisticsInfo = orderStatisticsInfo;
+    }
+
+    public UserStatisticsInfo getUserStatisticsInfo() {
+        return userStatisticsInfo;
+    }
+
+    public void setUserStatisticsInfo(UserStatisticsInfo userStatisticsInfo) {
+        this.userStatisticsInfo = userStatisticsInfo;
+    }
 }
