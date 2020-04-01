b4602c69a71789207afcad20e932264ae141ac05
wangshuai <wangshuai01@wangdian.cn>
2020-04-01 13:49:07 +0800

    分销初始化类型和优先级
    Signed-off-by: wangshuai <wangshuai01@wangdian.cn>

--- jmp/mp-service/src/main/java/com/vpu/mp/service/shop/activity/processor/RebateProcess.java
+++ jmp/mp-service/src/main/java/com/vpu/mp/service/shop/activity/processor/RebateProcess.java
@@ -2,6 +2,7 @@ package com.vpu.mp.service.shop.activity.processor;
 
 import com.vpu.mp.db.shop.tables.records.OrderInfoRecord;
 import com.vpu.mp.db.shop.tables.records.ReturnOrderRecord;
+import com.vpu.mp.service.foundation.data.BaseConstant;
 import com.vpu.mp.service.foundation.exception.MpException;
 import com.vpu.mp.service.pojo.shop.order.refund.OrderReturnGoodsVo;
 import com.vpu.mp.service.pojo.wxapp.goods.goods.activity.GoodsDetailCapsuleParam;
@@ -16,6 +17,17 @@ import java.util.List;
  * @author 王帅
  */
 public class RebateProcess implements Processor,ActivityGoodsListProcessor,GoodsDetailProcessor,CreateOrderProcessor{
+
+    @Override
+    public Byte getPriority() {
+        return 0;
+    }
+
+    @Override
+    public Byte getActivityType() {
+        return BaseConstant.ACTIVITY_TYPE_REBATE;
+    }
+
     @Override
     public void processForList(List<GoodsListMpBo> capsules, Integer userId) {
 
@@ -49,15 +61,5 @@ public class RebateProcess implements Processor,ActivityGoodsListProcessor,Goods
     @Override
     public void processGoodsDetail(GoodsDetailMpBo capsule, GoodsDetailCapsuleParam param) {
 
-    }
-
-    @Override
-    public Byte getPriority() {
-        return null;
-    }
-
-    @Override
-    public Byte getActivityType() {
-        return null;
     }
 }
