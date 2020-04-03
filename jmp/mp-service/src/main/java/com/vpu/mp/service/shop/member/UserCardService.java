86e5fd24d202436990adb1b98c5115c8798db567
liangchen <liangchen@wangdian.cn>
2020-04-03 14:58:47 +0800

    会员卡续费接口（待开发）

--- jmp/mp-service/src/main/java/com/vpu/mp/service/shop/member/UserCardService.java
+++ jmp/mp-service/src/main/java/com/vpu/mp/service/shop/member/UserCardService.java
@@ -1931,14 +1931,14 @@ public class UserCardService extends ShopBaseService {
 		
 		return UserOrderBean.builder().orderNum(0).totalMoneyPaid(BigDecimal.ZERO).build();
 	}
-	
+
 	/**
 	 * 	删除用户卡
 	 */
 	public int delUserCard(DefaultCardParam param) {
 		return userCardDao.updateUserCardFlag(param);
 	}
-	
+
 	/**
 	 * 	获取已经领取该卡的用户数量
 	 * @param cardId
@@ -1948,7 +1948,7 @@ public class UserCardService extends ShopBaseService {
 		int num = userCardDao.getCardNum(cardId);
 		return num;
 	}
-	
+
 	/**
 	 * 	获取该卡被领取的数量
 	 */
@@ -1957,8 +1957,8 @@ public class UserCardService extends ShopBaseService {
 		int num = userCardDao.getCardUserList(cardId);
 		return num;
 	}
-	
-	
+
+
 	/**
 	 * 	获取可该卡可正常使用的数量
 	 */
@@ -1967,5 +1967,14 @@ public class UserCardService extends ShopBaseService {
 		int num =userCardDao.getCanUseCardNum(card.getId(),CardUtil.isNeedActive(card.getActivation()));
 		return num;
 	}
+    /**
+     * 会员卡续费接口
+     * @param userId
+     * @param cardNo
+     */
+    public void cardRenew(Integer userId,String cardNo){
+
+    }
+
 }
 
