4b8b0d58fbac6a0c938e56ef27b82b5bc11b0c0f
huangzhuangzhuang <huangzhuangzhuang@wangdian.cn>
2020-04-07 11:10:20 +0800

    处理null情况

--- jmp/mp-service/src/main/java/com/vpu/mp/service/pojo/shop/member/MemberIndustryEnum.java
+++ jmp/mp-service/src/main/java/com/vpu/mp/service/pojo/shop/member/MemberIndustryEnum.java
@@ -131,7 +131,10 @@ public enum MemberIndustryEnum {
 		this.name = name;
 	}
 	
-	public static String getNameByCode(int code) {
+	public static String getNameByCode(Byte code) {
+		if(code == null) {
+			return null;
+		}
 		MemberIndustryEnum memberIndustryEnum = map.get(code);
 		return memberIndustryEnum.getName();
 	}
