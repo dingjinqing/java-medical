a93edd57bd2c7c13c7475f02ed3b58d25ebf3a67
wangshuai <wangshuai01@wangdian.cn>
2020-04-08 10:05:05 +0800

    2.9增加商品限购校验
    Signed-off-by: wangshuai <wangshuai01@wangdian.cn>

--- jmp/mp-service/src/main/java/com/vpu/mp/service/shop/order/action/CreateService.java
+++ jmp/mp-service/src/main/java/com/vpu/mp/service/shop/order/action/CreateService.java
@@ -635,15 +635,7 @@ public class CreateService extends ShopBaseService implements IorderOperate<Orde
                     temp.setGoodsPriceAction(calculateResult.getActivityType());
                 }
             }
-            //非加价购 && 非限次卡
-            if(Boolean.TRUE) {
-                if(temp.getGoodsInfo().getLimitBuyNum() > 0 && temp.getGoodsNumber() < temp.getGoodsInfo().getLimitBuyNum()){
-                    throw new MpException(JsonResultCode.CODE_ORDER_GOODS_LIMIT_MIN, "最小限购", temp.getGoodsInfo().getGoodsName(), temp.getGoodsInfo().getLimitBuyNum().toString());
-                }
-                if(temp.getGoodsInfo().getLimitMaxNum() > 0 && temp.getGoodsNumber() > temp.getGoodsInfo().getLimitMaxNum()){
-                    throw new MpException(JsonResultCode.CODE_ORDER_GOODS_LIMIT_MAX, "最大限购", temp.getGoodsInfo().getGoodsName(), temp.getGoodsInfo().getLimitBuyNum().toString());
-                }
-            }
+            goodsNumLimit(temp);
             //非加价购 改价
             if(Boolean.TRUE) {
                 temp.setProductPrice(calculateResult.getPrice());
@@ -654,6 +646,22 @@ public class CreateService extends ShopBaseService implements IorderOperate<Orde
         }
         param.setBos(boList);
         return boList;
+    }
+
+    /**
+     * 商品限购
+     * @param temp
+     * @throws MpException
+     */
+    private void goodsNumLimit(Goods temp) throws MpException {
+        if (temp.getIsAlreadylimitNum() != null && !temp.getIsAlreadylimitNum()) {
+            if (temp.getGoodsInfo().getLimitBuyNum() > 0 && temp.getGoodsNumber() < temp.getGoodsInfo().getLimitBuyNum()) {
+                throw new MpException(JsonResultCode.CODE_ORDER_GOODS_LIMIT_MIN, "最小限购", temp.getGoodsInfo().getGoodsName(), temp.getGoodsInfo().getLimitBuyNum().toString());
+            }
+            if (temp.getGoodsInfo().getLimitMaxNum() > 0 && temp.getGoodsNumber() > temp.getGoodsInfo().getLimitMaxNum()) {
+                throw new MpException(JsonResultCode.CODE_ORDER_GOODS_LIMIT_MAX, "最大限购", temp.getGoodsInfo().getGoodsName(), temp.getGoodsInfo().getLimitBuyNum().toString());
+            }
+        }
     }
 
     /**
@@ -669,7 +677,7 @@ public class CreateService extends ShopBaseService implements IorderOperate<Orde
         List<OrderGoodsBo> boList = new ArrayList<>(goods.size());
         for (Goods temp : goods) {
             OrderGoodsBo bo = orderGoods.initOrderGoods(temp);
-
+            goodsNumLimit(temp);
             boList.add(bo);
         }
         param.setBos(boList);
