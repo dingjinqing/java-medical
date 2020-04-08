8ea504b4c292fc050c5aea2705d2e552462ae364
huangzhuangzhuang <huangzhuangzhuang@wangdian.cn>
2020-04-08 16:50:17 +0800

    小程序会员卡详情添加审核未通过原因

--- wxapp/jwxapp/pages/cardinfo/cardinfo.js
+++ wxapp/jwxapp/pages/cardinfo/cardinfo.js
@@ -69,7 +69,7 @@ global.wxPage({
         } else if (cardInfo.examine && cardInfo.cardVerifyStatus === 3) {
 
           that.setData({
-            carStatus: "审核失败"
+            carStatus: "未通过"
           })
         } else {
           that.setData({
@@ -151,7 +151,7 @@ global.wxPage({
         } else if (cardInfo.examine && cardInfo.cardVerifyStatus === 3) {
 
           that.setData({
-            carStatus: "审核失败"
+            carStatus: "未通过"
           })
         } else {
           that.setData({
