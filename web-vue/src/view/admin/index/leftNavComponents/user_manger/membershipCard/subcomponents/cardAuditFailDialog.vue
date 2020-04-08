8294b40ef538160d4514732816065755aa68d1de
huangzhuangzhuang <huangzhuangzhuang@wangdian.cn>
2020-04-08 14:37:19 +0800

    审核不同过原因只读

--- web-vue/src/view/admin/index/leftNavComponents/user_manger/membershipCard/subcomponents/cardAuditFailDialog.vue
+++ web-vue/src/view/admin/index/leftNavComponents/user_manger/membershipCard/subcomponents/cardAuditFailDialog.vue
@@ -25,7 +25,7 @@
         </div>
         <div slot="footer" class="dialog-footer">
             <el-button @click="dialogVisiable = false">取 消</el-button>
-            <el-button type="primary" @click="handleDesc">确 定</el-button>
+            <el-button v-if="status === 1" type="primary" @click="handleDesc">确 定</el-button>
         </div>
         </el-dialog>
     </div>
@@ -40,6 +40,10 @@ export default {
     refuseDesc: {
       type: String,
       default: null
+    },
+    status: {
+      type: Number,
+      default: 1
     }
   },
   computed: {
