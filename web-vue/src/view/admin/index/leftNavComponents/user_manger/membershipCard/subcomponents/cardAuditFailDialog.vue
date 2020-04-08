1020a5a81c9f1a4fa8afb83b1656029259006246
huangzhuangzhuang <huangzhuangzhuang@wangdian.cn>
2020-04-08 12:19:14 +0800

    添加弹窗

---
+++ web-vue/src/view/admin/index/leftNavComponents/user_manger/membershipCard/subcomponents/cardAuditFailDialog.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <el-dialog
+        :visible.sync="dialogVisiable"
+        width="30%"
+        title="内层 Dialog">
+            <div slot="footer" class="dialog-footer">
+                <el-button @click="dialogVisiable = false">取 消</el-button>
+                <el-button type="primary" @click="dialogVisiable = false">确 定</el-button>
+            </div>
+        </el-dialog>
+    </div>
+</template>
+<script>
+export default {
+  props: {
+    visiable: {
+      type: Boolean,
+      default: false
+    }
+  },
+  computed: {
+    dialogVisiable: {
+      get () {
+        return this.visiable
+      },
+      set (val) {
+        this.$emit('update: visiable', val)
+      }
+    }
+  }
+}
+</script>
