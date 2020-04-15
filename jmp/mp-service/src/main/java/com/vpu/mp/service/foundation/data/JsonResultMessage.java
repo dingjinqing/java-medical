3b297f04fdcc8b715da3924359fa38f2de1c0d51
zhaojianqiang <zhaojianqiang@wangdian.cn>
2020-04-15 14:25:07 +0800

    解决冲突

--- jmp/mp-service/src/main/java/com/vpu/mp/service/foundation/data/JsonResultMessage.java
+++ jmp/mp-service/src/main/java/com/vpu/mp/service/foundation/data/JsonResultMessage.java
@@ -45,6 +45,12 @@ public class JsonResultMessage {
     public static final String MSG_ACCOUNT_SHOPTYPE_REGISTERED = "account.shoptype.notNull";
     public static final String MSG_ACCOUNT_SYTEM_LOGIN_EXPIRED = "system.account.login.expired";
     public static final String MSG_CODE_ACCOUNT_ENNAME_ISNULL = "enanme.isnull";
+    public static final String MSG_ACCOUNT_MOBILE_LENGTH_LIMIT = "account.mobile.length.limit";
+    public static final String MSG_CODE_ACCOUNT_MOBILE_SAME = "account.or.mobile.same";
+    public static final String MSG_ACCOUNT_USERNAME_LENGTH_LIMIT = "account.username.length.limit";
+    public static final String MSG_CODE_ACCOUNT_ID_NOT = "account.id.not";
+	public static final String MSG_CODE_SHOP_EXPIRE = "code.shop.expire";
+
 
     // 图片
 
