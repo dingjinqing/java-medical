09ec6832815a2fd1f5151432173a03562774886f
huangzhuangzhuang <huangzhuangzhuang@wangdian.cn>
2020-04-15 19:31:25 +0800

    分离出用户卡状态常量类

---
+++ jmp/mp-service/src/main/java/com/vpu/mp/service/pojo/shop/member/card/base/UserCardConstant.java
@@ -0,0 +1,16 @@
+package com.vpu.mp.service.pojo.shop.member.card.base;
+/**
+ * 	用户卡状态常量
+ * @author 黄壮壮
+ *
+ */
+public class UserCardConstant {
+	/*********************************************
+	 *  flag
+	 **********************************************/
+	/**
+	 * 	不可用状态
+	 */
+	public static Byte FLAG_CANNOT_USE = 2;
+	 
+}
