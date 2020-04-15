54b63d17d14dc619cc08739f61e2fe7077e89352
huangzhuangzhuang <huangzhuangzhuang@wangdian.cn>
2020-04-15 11:34:51 +0800

    注释格式化

--- jmp/mp-service/src/main/java/com/vpu/mp/service/pojo/shop/member/card/RankCardToVo.java
+++ jmp/mp-service/src/main/java/com/vpu/mp/service/pojo/shop/member/card/RankCardToVo.java
@@ -20,48 +20,70 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class RankCardToVo extends RankCardVo {
 	
-	/** 使用须知 */
+	/** 
+	 * 	使用须知
+	 */
 	private String desc;
-	/** 联系方式 */
+	/**
+	 * 	联系方式
+	 */
 	private String mobile;
 	
 	/**
-	 * 会员卡是否启用 1:使用中，2:停止使用
+	 * 	会员卡是否启用 1:使用中，2:停止使用
 	 */
 	//private Byte flag;
 	
-	/** 会员折扣: 全部商品；1代表全部商品，0代表指定商品 */
+	/** 
+	 * 	会员折扣: 全部商品；1代表全部商品，0代表指定商品
+	 */
 	private Byte discountIsAll;
-	/** -折扣： 商品id */
+	
+	/**
+	 * 	折扣： 商品id
+	 */
 	@JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
 	private String discountGoodsId;
 	private String[] goodsId;
-	/** -折扣： 商家分类id */
+	
+	/** 
+	 * 	折扣： 商家分类id
+	 */
 	@JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
 	private String discountSortId;
 	private String[] shopCategoryIds;
-	/** -折扣: 平台分类id */
+	
+	
+	/**
+	 * 	折扣: 平台分类id
+	 */
 	@JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
 	private String discountCatId;
 	private String[] platformCategoryIds;
-	/** -折扣 : 品牌分类id */
+	
+	/** 
+	 * 	折扣 : 品牌分类id
+	 */
 	@JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
 	private String discountBrandId;
 	private String[] brandId;
+	
 	/**
-	 * 积分获取开关， 0表示关闭，1表示开启
+	 * 	积分获取开关， 0表示关闭，1表示开启
+	 * 	开卡送多少积分 
 	 */
-	/** 开卡送多少积分 */
 	@JsonProperty("score")
 	private Integer sorce;
-	/** 购物送积分策略json序列化对象 */
-	//@JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
-	//private String buyScore;
-	/** 购物送积分策略json序列化对象 */
+	
+	/** 
+	 * 	购物送积分策略json序列化对象、
+	 */
 	private ScoreJson scoreJson;
 		
 	
-	/** 激活需要的信息 */
+	/** 
+	 * 	激活需要的信息
+	 */
 	private String[] activationCfgBox;
 	
 	/**
@@ -76,7 +98,7 @@ public class RankCardToVo extends RankCardVo {
 	protected CardCustomRights cardCustomRights;
 	
 	/**
-	 * 处理策略
+	 * 	处理策略
 	 */
 	@Override
 	public void changeJsonCfg() {
@@ -84,7 +106,7 @@ public class RankCardToVo extends RankCardVo {
 		log.info("执行RankCardToVo的处理策略");
 		super.changeJsonCfg();
 		
-		/** 购物送积分策略json对象 */
+		//	购物送积分策略json对象 
 		String buyScore = this.getBuyScore();
 		if (buyScore != null && !buyScore.equals("")) {
 			try {
@@ -96,32 +118,32 @@ public class RankCardToVo extends RankCardVo {
 		}
 		
 		
-		/** 激活需要填写的信息 */
+		//	激活需要填写的信息 
 		String activationCfg = getActivationCfg();
 		if(null != activationCfg) {
 			activationCfgBox = activationCfg.replaceAll("\\s+","").split(",");
 		}
 		
-		/** 积分指定商品 处理 */
-		/** 商品id */
+		//	积分指定商品 处理
+		//	 商品id 
 		if(isNotBlank(discountGoodsId)) {
 			goodsId = discountGoodsId.replaceAll("\\s+","").split(",");
 		}else {
 			goodsId = new String[] {};
 		}
-		/** 商家分类id */
+		//	 商家分类id 
 		if(isNotBlank(discountSortId)) {
 			shopCategoryIds = discountSortId.replaceAll("\\s+","").split(",");
 		}else {
 			shopCategoryIds = new String[] {};
 		}
-		/**平台分类id */
+		//	平台分类id
 		if(isNotBlank(discountCatId)) {
 			platformCategoryIds = discountCatId.replaceAll("\\s+","").split(",");
 		}else {
 			platformCategoryIds = new String[] {};
 		}
-		/** 品牌分类id */
+		//	品牌分类id 
 		if(isNotBlank(discountBrandId)) {
 			brandId = discountBrandId.replaceAll("\\s+","").split(",");
 		}else {
