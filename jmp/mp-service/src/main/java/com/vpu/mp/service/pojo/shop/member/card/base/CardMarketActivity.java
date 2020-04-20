56d0987319d75cd2f7ccaa039ca3460ccf033174
huangzhuangzhuang <huangzhuangzhuang@wangdian.cn>
2020-04-20 15:49:05 +0800

    添加会员卡涉及的营销活动

---
+++ jmp/mp-service/src/main/java/com/vpu/mp/service/pojo/shop/member/card/base/CardMarketActivity.java
@@ -0,0 +1,27 @@
+package com.vpu.mp.service.pojo.shop.member.card.base;
+
+/**
+ * 会员卡营销活动
+ * @author 黄壮壮
+ *
+ */
+public enum CardMarketActivity {
+	/**
+	 * 优惠券 0
+	 */
+	COUPON_ACT,
+	/**
+	 * 会员价 1
+	 */
+	MEMBER_PRICE_ACT,
+	
+	/**
+	 * 限时降价 2
+	 */
+	REDUCE_PRICE_ACT,
+	
+	/**
+	 * 首单特惠 3
+	 */
+	FIRST_SPECIAL_ACT
+}
