27a5e87ebf107851b36a31a7063f731d2c28e784
liangchen <liangchen@wangdian.cn>
2020-04-21 09:41:45 +0800

    定向发券明细列表

--- jmp/mp-service/src/main/java/com/vpu/mp/service/pojo/shop/coupon/give/CouponGiveDetailParam.java
+++ jmp/mp-service/src/main/java/com/vpu/mp/service/pojo/shop/coupon/give/CouponGiveDetailParam.java
@@ -12,19 +12,17 @@ import lombok.Data;
 public class CouponGiveDetailParam {
   /** 活动Id */
   private Integer accessId;
-  /** 优惠券Id */
-  private Integer actId;
   /** 手机号 */
   private String mobile;
   /** 用户昵称 */
   private String username;
   /** 是否使用 0：未使用 1：已使用 2：已过期 3：已废除 */
-  public static final Integer ISUSED_DEFAULT_VALUE = -1;
+  public static final Integer IS_USED_DEFAULT_VALUE = -1;
 
-  private Integer isUsed = ISUSED_DEFAULT_VALUE;
+  private Integer isUsed;
 
   /** 分页信息 */
-  private Integer currentPage;
+  private Integer currentPage = 1 ;
 
-  private Integer pageRows;
+  private Integer pageRows =20 ;
 }
