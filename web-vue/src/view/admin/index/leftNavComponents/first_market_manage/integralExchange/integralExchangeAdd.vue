efc86fc2570a2b400eda38d817119fd2b62bd828
suntengfei <suntengfei@wangdian.cn>
2020-04-23 15:03:58 +0800

    补充修改

--- web-vue/src/view/admin/index/leftNavComponents/first_market_manage/integralExchange/integralExchangeAdd.vue
+++ web-vue/src/view/admin/index/leftNavComponents/first_market_manage/integralExchange/integralExchangeAdd.vue
@@ -381,27 +381,32 @@ export default {
   data () {
     var validateTableData1 = (rule, value, callback) => {
       console.log(value)
+      let flag = true
       value.forEach((item, index) => {
         if (index !== (value.length - 1)) {
           if (!Number(item.stock)) {
+            flag = true
             callback(new Error(this.$t('mintegralExchange.cannotBeEmpty')))
           } else if ((Number(item.stock) !== '') && (Number(item.stock) > item.goodsStock)) {
+            flag = true
             callback(new Error(this.$t('mintegralExchange.tipsTwo')))
           } else if (Number(item.exchange.money) > Number(item.originPrice)) {
+            flag = true
             callback(new Error(this.$t('mintegralExchange.tipsThree')))
           } else if (!Number(item.exchange.money) && !Number(item.exchange.score)) {
+            flag = true
             callback(new Error('兑换价格或积分不能同时为空'))
           } else if (this.id !== -1) {
             if (Number(item.remainStock) > Number(item.goodsStock)) {
+              flag = true
               callback(new Error('剩余兑换商品库存要小于商品库存'))
-            } else {
-              callback()
             }
-          } else {
-            callback()
           }
         }
       })
+      if (flag) {
+        callback()
+      }
     }
     var validate2 = (rule, value, callback) => {
       console.log(value)
