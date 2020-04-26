a07045ba4774f5c186fc00660a444ce7ba3afd4a
suntengfei <suntengfei@wangdian.cn>
2020-04-26 16:08:24 +0800

    表单全局禁用机制及二维码样式调整

--- web-vue/src/view/admin/index/leftNavComponents/first_market_manage/formStatistics/formDecoration/decorationModules/graphicModule/auxiliaryBlank.vue
+++ web-vue/src/view/admin/index/leftNavComponents/first_market_manage/formStatistics/formDecoration/decorationModules/graphicModule/auxiliaryBlank.vue
@@ -16,7 +16,7 @@
         <!--模块编辑区结束-->
         <div
           class="item_operation"
-          v-if="activeBorder"
+          v-if="activeBorder&&!isProhibit"
         >
           <img
             class="up_img"
@@ -57,6 +57,7 @@ export default {
   },
   data () {
     return {
+      isProhibit: false,
       activeBorder: false, // 模块公共
       activeSetHere: false, // 模块公共
       hoverTips: 'hoverTips', // 英文适配  模块公共
@@ -109,6 +110,10 @@ export default {
     }
   },
   mounted () {
+    this.$nextTick(() => {
+      console.log(localStorage.getItem('isProhibitForm'))
+      this.isProhibit = JSON.parse(localStorage.getItem('isProhibitForm'))
+    })
     // 初始化语言
     this.langDefault() // 模块公共
     // 初始化数据
