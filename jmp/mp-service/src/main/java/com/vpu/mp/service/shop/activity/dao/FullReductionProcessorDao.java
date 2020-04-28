3327363d8d97145b54d120d57c8d86ff413358f9
kongdecheng <kongdecheng@wangdian.cn>
2020-04-28 15:36:04 +0800

    购物车满折满减折扣计算

--- jmp/mp-service/src/main/java/com/vpu/mp/service/shop/activity/dao/FullReductionProcessorDao.java
+++ jmp/mp-service/src/main/java/com/vpu/mp/service/shop/activity/dao/FullReductionProcessorDao.java
@@ -600,7 +600,7 @@ public class FullReductionProcessorDao extends MrkingStrategyService {
                         }
                     }else {
                         if (fullReductionRule.getFullMoney().compareTo(totalMoney)<=0){
-                            reduceMoney =fullReductionRule.getDiscount().multiply(new BigDecimal("0.1")).multiply(totalMoney);
+                            reduceMoney =BigDecimal.ONE.subtract(fullReductionRule.getDiscount().multiply(new BigDecimal("0.1"))).multiply(totalMoney);
                             logger().info("符合满{}元打{}折,共{}元,减{}",fullReductionRule.getFullMoney(),fullReductionRule.getReduceMoney(),totalMoney,reduceMoney);
 
                         }
@@ -613,7 +613,7 @@ public class FullReductionProcessorDao extends MrkingStrategyService {
                                 .collect(Collectors.toList());
                         //符合的商品计算减价金额
                         for (FullReductionGoodsCartBo goods : goodsCartBoList) {
-                            BigDecimal multiply = fullReductionRule.getDiscount().multiply(new BigDecimal("0.1")).multiply(goods.getMoney());
+                            BigDecimal multiply = BigDecimal.ONE.subtract(fullReductionRule.getDiscount().multiply(new BigDecimal("0.1"))).multiply(goods.getMoney());
                             logger().info("符合第{}件打{}折,件数{},减{}元",fullReductionRule.getAmount(),fullReductionRule.getDiscount(),goods.getNum(),multiply);
                             reduceMoney = reduceMoney.add(multiply);
                         }
