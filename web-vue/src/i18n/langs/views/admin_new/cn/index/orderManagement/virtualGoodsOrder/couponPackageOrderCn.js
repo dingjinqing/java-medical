001af3f722efcf419061080e082838961fd40f18
wangbingbing <wangbingbing@huice.com>
2020-04-30 17:11:27 +0800

    虚拟订单-优惠券礼包订单导出

--- web-vue/src/i18n/langs/views/admin_new/cn/index/orderManagement/virtualGoodsOrder/couponPackageOrderCn.js
+++ web-vue/src/i18n/langs/views/admin_new/cn/index/orderManagement/virtualGoodsOrder/couponPackageOrderCn.js
@@ -1,5 +1,10 @@
 export const cn = {
   couponPackageOrder: '优惠券包订单',
   couponPackage: '优惠券包',
-  partialRefund: '部分退款'
+  partialRefund: '部分退款',
+  userInfo: '下单用户信息',
+  orderSn: '订单号',
+  packName: '优惠券礼包',
+  startTime: '下单起始时间',
+  endTime: '下单结束时间'
 }
