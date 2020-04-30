867a5b89b0d5ce1d33e998f97a2492b60808a18d
liangchen <liangchen@wangdian.cn>
2020-04-30 17:07:54 +0800

    好友助力折扣商品bug

--- jmp/mp-service/src/main/java/com/vpu/mp/service/shop/activity/processor/MyPrizeProcessor.java
+++ jmp/mp-service/src/main/java/com/vpu/mp/service/shop/activity/processor/MyPrizeProcessor.java
@@ -11,12 +11,14 @@ import com.vpu.mp.service.foundation.util.Util;
 import com.vpu.mp.service.pojo.shop.market.friendpromote.FpRewardContent;
 import com.vpu.mp.service.pojo.shop.market.friendpromote.FriendPromoteSelectParam;
 import com.vpu.mp.service.pojo.shop.market.friendpromote.FriendPromoteSelectVo;
+import com.vpu.mp.service.pojo.shop.order.OrderConstant;
 import com.vpu.mp.service.pojo.shop.order.refund.OrderReturnGoodsVo;
 import com.vpu.mp.service.pojo.wxapp.order.OrderBeforeParam;
 import com.vpu.mp.service.shop.market.friendpromote.FriendPromoteService;
 import com.vpu.mp.service.shop.market.prize.PrizeRecordService;
 import com.vpu.mp.service.shop.order.info.OrderInfoService;
 import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.math.NumberUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -59,6 +61,8 @@ public class MyPrizeProcessor extends ShopBaseService implements Processor, Crea
     @Override
     public void processInitCheckedOrderCreate(OrderBeforeParam param) throws MpException {
         logger().info("我的奖品>>>>{}", param.getGoods().get(0).getGoodsInfo().getGoodsName());
+        String memberCardNo = param.getMemberCardNo();
+        String couponSn = param.getCouponSn();
         //不使用优惠券和会员卡
         param.setMemberCardNo(StringUtils.EMPTY);
         param.setCouponSn(StringUtils.EMPTY);
@@ -80,6 +84,20 @@ public class MyPrizeProcessor extends ShopBaseService implements Processor, Crea
                 FriendPromoteSelectParam selectParam = new FriendPromoteSelectParam();
                 selectParam.setId(prizeRecord.getActivityId());
                 FriendPromoteSelectVo actRecord = friendPromoteService.selectOne(selectParam);
+                //如果是折扣商品
+                if (NumberUtils.BYTE_ONE.equals(actRecord.getRewardType())){
+                    //如果可叠加优惠券、会员卡
+                    if (NumberUtils.BYTE_ONE.equals(actRecord.getUseDiscount())){
+                        param.setMemberCardNo(memberCardNo);
+                        param.setCouponSn(couponSn);
+                    }
+                    //如果不可抵扣积分
+                    if (NumberUtils.BYTE_ZERO.equals(actRecord.getUseScore())){
+                        if(param.getPaymentList() != null) {
+                            param.getPaymentList().remove(OrderConstant.PAY_CODE_SCORE_PAY);
+                        }
+                    }
+                }
                 FpRewardContent rewardContent = Util.json2Object(actRecord.getRewardContent(),FpRewardContent.class,false);
                 if (rewardContent!=null){
                     goods.setProductPrice(rewardContent.getMarketPrice());
