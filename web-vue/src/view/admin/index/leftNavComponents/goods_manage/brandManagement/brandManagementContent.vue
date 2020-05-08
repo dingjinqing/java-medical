1a76cd769c488bc8f113a78acaeb7df67d437099
suntengfei <suntengfei@wangdian.cn>
2020-05-08 16:52:09 +0800

    部分品牌跳转处理

--- web-vue/src/view/admin/index/leftNavComponents/goods_manage/brandManagement/brandManagementContent.vue
+++ web-vue/src/view/admin/index/leftNavComponents/goods_manage/brandManagement/brandManagementContent.vue
@@ -1004,15 +1004,12 @@ export default {
     // 点击包含在售商品数量
     handleToTurnGoodsNum (row) {
       console.log(row)
-      // this.$router.push({
-      //   name: 'goodsForSale',
-      //   params: {
-      //     brandTurn: {
-      //       id: row.id,
-      //       brandName: row.brandName
-      //     }
-      //   }
-      // })
+      this.$router.push({
+        name: 'goodsForSale',
+        params: {
+          brandTurnId: row.id
+        }
+      })
     }
   }
 }
