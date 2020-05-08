84f92c8691a127f80064945ee28cf9715cd9828a
huangzhuangzhuang <huangzhuangzhuang@wangdian.cn>
2020-05-08 12:27:08 +0800

    接口调试成功

--- web-vue/src/view/admin/index/leftNavComponents/user_manger/membershipCard/subcomponents/dialog/GradeCardSettingDialog.vue
+++ web-vue/src/view/admin/index/leftNavComponents/user_manger/membershipCard/subcomponents/dialog/GradeCardSettingDialog.vue
@@ -6,20 +6,28 @@
         width="30%"
         >
             <div class="top">
-                共0位会员拥有该卡
+                共{{userNum}}位会员拥有该卡
             </div>
             <div class="middle">
-                <el-radio v-model="radio" :label="1">直接停用</el-radio>
-                <el-radio v-model="radio" :label="2">停用后置换为其他等级卡</el-radio>
+                <el-radio v-model="radio" :label="0">直接停用</el-radio>
+                <el-radio v-model="radio" :label="1">停用后置换为其他等级卡</el-radio>
             </div>
             <div class="bottom">
-                <p v-if="radio===1">
+                <p v-if="radio===0">
                     直接停用本卡，则已经领取的等级用户也会停用该等级卡，不再享受等级会员权限
                 </p>
-                <div v-else-if="radio === 2">
+                <div v-else-if="radio === 1">
                     <p>
                         <span class="stop-tip">停用后拥有该卡用户默认设置为:</span>
-                        <el-select size="small" style="width: 130px;"></el-select>
+                        <el-select v-model="choosedCardId" size="small" style="width: 130px;">
+                            <el-option
+                              v-for="item in cardList"
+                              :key="item.id"
+                              :label="item.card_name"
+                              :value="item.id"
+                              >
+                            </el-option>
+                        </el-select>
                     </p>
                     <p>
                         直接停用本卡，拥有本卡的用户批量更换为设置的新等级卡
@@ -27,7 +35,7 @@
                 </div>
             </div>
             <span slot="footer" class="dialog-footer">
-                <el-button type="primary" @click="visiable = false">确 定</el-button>
+                <el-button type="primary" @click="handleOk">确 定</el-button>
                 <el-button @click="visiable = false">取 消</el-button>
             </span>
         </el-dialog>
@@ -41,6 +49,10 @@ export default {
     dialogVisiable: {
       type: Boolean,
       default: false
+    },
+    currentCard: {
+      type: Object,
+      required: true
     }
   },
   computed: {
@@ -49,31 +61,54 @@ export default {
         return this.dialogVisiable
       },
       set (val) {
+        this.clearData()
         return this.$emit('update:dialogVisiable', val)
+      }
+    },
+    userNum () {
+      return this.currentCard.cardUseStats.haveNormalNum
+    }
+  },
+  watch: {
+    dialogVisiable (newValue, oldValue) {
+      if (newValue) {
+        this.initValidCard()
       }
     }
   },
   data () {
     return {
-      radio: 1,
-      cardList: []
+      radio: 0,
+      cardList: [],
+      choosedCardId: null
     }
   },
   mounted () {
-    this.initValidCard()
+    this.langDefault()
   },
   methods: {
     initValidCard () {
       getAllValidGradeCard().then(res => {
         if (res.error === 0) {
           this.cardList = res.content
+          //  等级排序
           if (res.content.length > 1) {
             this.cardList = res.content.sort((a, b) => {
               return a.grade > b.grade ? 1 : b.grade > a.grade ? -1 : 0
             })
           }
+          //  过滤掉本张卡
+          this.cardList = this.cardList.filter(item => item.grade !== this.currentCard.grade)
         }
       })
+    },
+    handleOk () {
+      this.$emit('getStopCardCfg', {currentCardId: this.currentCard.id, stopPlan: this.radio, anotherNewCardId: this.choosedCardId})
+      this.visiable = false
+    },
+    clearData () {
+      this.radio = 0
+      this.choosedCardId = null
     }
   }
 }
