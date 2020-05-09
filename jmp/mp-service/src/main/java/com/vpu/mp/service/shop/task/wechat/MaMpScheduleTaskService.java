28dd07fe8a7d682b03960d2da73e9f7b2116c388
zhaojianqiang <zhaojianqiang@wangdian.cn>
2020-05-09 10:46:51 +0800

    合并冲突

--- jmp/mp-service/src/main/java/com/vpu/mp/service/shop/task/wechat/MaMpScheduleTaskService.java
+++ jmp/mp-service/src/main/java/com/vpu/mp/service/shop/task/wechat/MaMpScheduleTaskService.java
@@ -202,19 +202,19 @@ public class MaMpScheduleTaskService extends ShopBaseService {
 			return null;
 		}
 		// 尾款未支付前24小时提醒
-		log.info(getShopId()+"开始：尾款未支付前24小时提醒");
+		logger().info(getShopId()+"开始：尾款未支付前24小时提醒");
 		toSendPreSaleMessage(24, officeAppId, ZERO);
-		log.info(getShopId()+"结束：尾款未支付前24小时提醒");
+		logger().info(getShopId()+"结束：尾款未支付前24小时提醒");
 
         // 尾款未支付前2小时提醒
-		log.info(getShopId()+"开始：尾款未支付前2小时提醒");
+		logger().info(getShopId()+"开始：尾款未支付前2小时提醒");
 		toSendPreSaleMessage(2, officeAppId, ZERO);
-		log.info(getShopId()+"结束：尾款未支付前2小时提醒");
+		logger().info(getShopId()+"结束：尾款未支付前2小时提醒");
 
         // 尾款支付时间开始发送通知
-		log.info(getShopId()+"开始：尾款未支付前0小时提醒");
+		logger().info(getShopId()+"开始：尾款未支付前0小时提醒");
 		toSendPreSaleMessage(0, officeAppId, ONE);
-		log.info(getShopId()+"结束：尾款未支付前0小时提醒");
+		logger().info(getShopId()+"结束：尾款未支付前0小时提醒");
 
         return officeAppId;
 
@@ -284,14 +284,14 @@ public class MaMpScheduleTaskService extends ShopBaseService {
 	//发送模板消息和助力失败改变状态
 	private void promoteSendMessage(String officeAppId) {
 		//活动结束前一小时
-		log.info("开始活动结束前一小时");
+		logger().info("开始活动结束前一小时");
 		List<FriendPromoteSelectVo> hoursList = friendPromoteService.getLaunchListByHour(1);
 		for (FriendPromoteSelectVo item : hoursList) {
 			sendMessage(TWO, item, officeAppId);
 		}
-		log.info("结束活动结束前一小时");
+		logger().info("结束活动结束前一小时");
 		//助力失败
-		log.info("开始助力失败");
+		logger().info("开始助力失败");
 		List<FriendPromoteSelectVo> promoteFailedList = friendPromoteService.getPromoteFailedList(-24);
 		for (FriendPromoteSelectVo item : promoteFailedList) {
 			int upPromoteInfo = friendPromoteService.upPromoteInfo(FOUR, item.getId());
@@ -301,12 +301,12 @@ public class MaMpScheduleTaskService extends ShopBaseService {
 
             //发放失败奖励
 			if (item.getFailedSendType().equals(ONE)) {
-				log.info("发放失败奖励");
+				logger().info("发放失败奖励");
 				MrkingVoucherRecord infoById = couponGiveService.getInfoById(item.getFailedSendContent());
 				if(infoById==null) {
-					log.info("会员卡"+item.getFailedSendContent()+"不存在，找管理员");
+					logger().info("优惠券"+item.getFailedSendContent()+"不存在，找管理员");
 				}else {
-					CouponGiveQueueParam param=new CouponGiveQueueParam(getShopId(), Arrays.asList(item.getUserId()), item.getId(), new String[] {infoById.getAliasCode()}, ZERO, (byte)17);
+					CouponGiveQueueParam param=new CouponGiveQueueParam(getShopId(), Arrays.asList(item.getUserId()), item.getId(), new String[] {item.getFailedSendContent().toString()}, ZERO, (byte)17);
                     couponGiveService.handlerCouponGive(param);
 				}
 			}
@@ -339,21 +339,21 @@ public class MaMpScheduleTaskService extends ShopBaseService {
 			}
 
         }
-		log.info("结束助力失败");
-		log.info("开始助力失效前一小时");
+		logger().info("结束助力失败");
+		logger().info("开始助力失效前一小时");
 		//助力失效前一小时
 		List<FriendPromoteSelectVo> promoteWaitReceiveList = friendPromoteService.getPromoteWaitReceiveList(1);
 		for (FriendPromoteSelectVo item : promoteWaitReceiveList) {
 			sendMessage(FIVE, item, officeAppId);
 		}
-		log.info("结束助力失效前一小时");
+		logger().info("结束助力失效前一小时");
 
-        log.info("开始助力失效修改状态");
+        logger().info("开始助力失效修改状态");
 		List<FriendPromoteSelectVo> promoteWaitReceiveList2 = friendPromoteService.getPromoteWaitReceiveList(0);
 		for (FriendPromoteSelectVo item : promoteWaitReceiveList2) {
 			friendPromoteService.upPromoteInfo(THREE, item.getId());
 		}
-		log.info("结束助力失效修改状态");
+		logger().info("结束助力失效修改状态");
 	}
 
     //发送好友助力中奖结果
@@ -384,7 +384,7 @@ public class MaMpScheduleTaskService extends ShopBaseService {
 		UserRecord user = userService.getUserByUserId(vo.getUserId());
 		UserRecord userRecord = checkMp(user.getWxUnionId(), officeAppId);
 		if (null == userRecord) {
-			log.info("店铺："+getShopId()+"用户userId："+vo.getUserId()+"活动Id："+vo.getId()+"用户没有关注公众号");
+			logger().info("店铺："+getShopId()+"用户userId："+vo.getUserId()+"活动Id："+vo.getId()+"用户没有关注公众号");
 			return null;
 		}
 		userIdList.add(user.getUserId());
@@ -402,94 +402,96 @@ public class MaMpScheduleTaskService extends ShopBaseService {
 	}
 
     public String sendMessage(Byte type,FriendPromoteSelectVo vo,String officeAppId) {
-		log.info("sendMessage");
+		logger().info("sendMessage");
 		String title = "";
 		String content = "";
 		if(type.equals(FOUR)) {
-			log.info("type为4");
+			logger().info("type为4");
 			sendPromoteDrawMessage(vo, officeAppId);
-		}
-		switch (type) {
-		case ONE:
-			title = "好友助力通知";
-			content = "好友帮您助力啦！快去看看吧！";
-			break;
-		case TWO:
-			title = "助力进度通知";
-			content = "活动即将结束，赶快邀请好友帮忙助力吧！";
-			break;
-		case THREE:
-			title = "助力失败通知";
-			content = "很遗憾，本次活动助力失败，来看看其他活动吧~";
-			break;
-		case FIVE:
-			title = "奖励未领取通知";
-			content = "您有奖励即将失效，快来领取吧！";
-			break;
-		default:
-			break;
-		}
-		if(type.equals(ONE)) {
-			log.info("type为1");
-			log.info("店铺："+getShopId()+"用户userId："+vo.getUserId()+"活动Id："+vo.getId()+"状态是1,发送小程序");
-			MpAuthShopRecord authShop = saas.shop.mp.getAuthShopByShopId(getShopId());
-			if (null == authShop) {
-				log.info("店铺："+getShopId()+"没有绑定小程序");
-				return null;
+		}else {
+			switch (type) {
+			case ONE:
+				title = "好友助力通知";
+				content = "好友帮您助力啦！快去看看吧！";
+				break;
+			case TWO:
+				title = "助力进度通知";
+				content = "活动即将结束，赶快邀请好友帮忙助力吧！";
+				break;
+			case THREE:
+				title = "助力失败通知";
+				content = "很遗憾，本次活动助力失败，来看看其他活动吧~";
+				break;
+			case FIVE:
+				title = "奖励未领取通知";
+				content = "您有奖励即将失效，快来领取吧！";
+				break;
+			default:
+				break;
 			}
-			Integer tid = SubscribeMessageConfig.getTid(SubcribeTemplateCategory.INVITE_SUCCESS);
-			if(tid==null) {
-				log.info("SubcribeTemplateCategory中没有定义相关的类型");
-				return null;
-			}
-			boolean canUse = subscribeMessageService.getCanUse(vo.getUserId(), tid);
-			String page = "pages1/promoteinfo/promoteinfo?actCode=" + vo.getActCode() + "&launch_id=" + vo.getId();
-			String name2 = "已有好友帮忙助力";
-			String date = DateUtil.dateFormat(DateUtil.DATE_FORMAT_FULL, vo.getSuccessTime());
-			if(!canUse) {
-				log.info("店铺："+getShopId()+"用户userId："+vo.getUserId()+"活动Id："+vo.getId()+"不能发送小程序，转发送公众号");
-				//ACTIVITY_CONFIG
-				List<Integer> userIdList = new ArrayList<Integer>();
-				UserRecord user = userService.getUserByUserId(vo.getUserId());
-				UserRecord wxUserInfo = checkMp(user.getWxUnionId(), officeAppId);
-				if (null == wxUserInfo) {
-					log.info("店铺："+getShopId()+"用户userId："+vo.getUserId()+"活动Id："+vo.getId()+"用户没有关注公众号");
+			if(type.equals(ONE)) {
+				logger().info("type为1");
+				logger().info("店铺："+getShopId()+"用户userId："+vo.getUserId()+"活动Id："+vo.getId()+"状态是1,发送小程序");
+				MpAuthShopRecord authShop = saas.shop.mp.getAuthShopByShopId(getShopId());
+				if (null == authShop) {
+					logger().info("店铺："+getShopId()+"没有绑定小程序");
 					return null;
 				}
-				userIdList.add(user.getUserId());
-				String[][] data = new String[][] { { title, "#173177" }, {name2, "#173177" },
-					{ vo.getActName(), "#173177" }, { content, "#173177" }, { date, "#173177" },{"","#173177"}};
-					//发的公众号
+				Integer tid = SubscribeMessageConfig.getTid(SubcribeTemplateCategory.INVITE_SUCCESS);
+				if(tid==null) {
+					logger().info("SubcribeTemplateCategory中没有定义相关的类型");
+					return null;
+				}
+				boolean canUse = subscribeMessageService.getCanUse(vo.getUserId(), tid);
+				String page = "pages1/promoteinfo/promoteinfo?actCode=" + vo.getActCode() + "&launch_id=" + vo.getId();
+				String name2 = "已有好友帮忙助力";
+				String date = DateUtil.dateFormat(DateUtil.DATE_FORMAT_FULL, vo.getSuccessTime());
+				if(!canUse) {
+					logger().info("店铺："+getShopId()+"用户userId："+vo.getUserId()+"活动Id："+vo.getId()+"不能发送小程序，转发送公众号");
+					//ACTIVITY_CONFIG
+					List<Integer> userIdList = new ArrayList<Integer>();
+					UserRecord user = userService.getUserByUserId(vo.getUserId());
+					UserRecord wxUserInfo = checkMp(user.getWxUnionId(), officeAppId);
+					if (null == wxUserInfo) {
+						logger().info("店铺："+getShopId()+"用户userId："+vo.getUserId()+"活动Id："+vo.getId()+"用户没有关注公众号");
+						return null;
+					}
+					userIdList.add(user.getUserId());
+					String[][] data = new String[][] { { title, "#173177" }, {name2, "#173177" },
+						{ vo.getActName(), "#173177" }, { content, "#173177" }, { date, "#173177" },{"","#173177"}};
+						//发的公众号
+						RabbitMessageParam param = RabbitMessageParam.builder()
+								.mpTemplateData(
+										MpTemplateData.builder().config(MpTemplateConfig.ACTIVITY_CONFIG).data(data).build())
+								.page(page).shopId(getShopId()).userIdList(userIdList)
+								.type(RabbitParamConstant.Type.LOTTERY_TEAM).build();
+						saas.taskJobMainService.dispatchImmediately(param, RabbitMessageParam.class.getName(), getShopId(), TaskJobEnum.SEND_MESSAGE.getExecutionType());
+						return null;
+				}
+				logger().info("店铺："+getShopId()+"用户userId："+vo.getUserId()+"活动Id："+vo.getId()+"发送小程序");
+				String[][] data = new String[][] { { vo.getActName() }, { name2 }, { date } };
+				List<Integer> userIdList = new ArrayList<Integer>();
+				userIdList.add(vo.getUserId());
+				MaSubscribeData buildData = MaSubscribeData.builder().data307(data).build();
+				//发的小程序
 				RabbitMessageParam param = RabbitMessageParam.builder()
-						.mpTemplateData(
-								MpTemplateData.builder().config(MpTemplateConfig.ACTIVITY_CONFIG).data(data).build())
-						.page(page).shopId(getShopId()).userIdList(userIdList)
-						.type(RabbitParamConstant.Type.LOTTERY_TEAM).build();
+						.maTemplateData(
+								MaTemplateData.builder().config(SubcribeTemplateCategory.INVITE_SUCCESS).data(buildData).build())
+						.page(page).shopId(getShopId())
+						.userIdList(userIdList)
+						.type(RabbitParamConstant.Type.INVITE_SUCCESS_FRIEND_PROMOTE).build();
 				saas.taskJobMainService.dispatchImmediately(param, RabbitMessageParam.class.getName(), getShopId(), TaskJobEnum.SEND_MESSAGE.getExecutionType());
-				return null;
+			}else {
+				logger().info("type不为1，为"+type);
+				sendPromoteResultMessage(type, vo, officeAppId, title, content);
 			}
-			log.info("店铺："+getShopId()+"用户userId："+vo.getUserId()+"活动Id："+vo.getId()+"发送小程序");
-			String[][] data = new String[][] { { vo.getActName() }, { name2 }, { date } };
-			List<Integer> userIdList = new ArrayList<Integer>();
-			userIdList.add(vo.getUserId());
-			MaSubscribeData buildData = MaSubscribeData.builder().data307(data).build();
-			//发的小程序
-			RabbitMessageParam param = RabbitMessageParam.builder()
-					.maTemplateData(
-							MaTemplateData.builder().config(SubcribeTemplateCategory.INVITE_SUCCESS).data(buildData).build())
-					.page(page).shopId(getShopId())
-					.userIdList(userIdList)
-					.type(RabbitParamConstant.Type.INVITE_SUCCESS_FRIEND_PROMOTE).build();
-			saas.taskJobMainService.dispatchImmediately(param, RabbitMessageParam.class.getName(), getShopId(), TaskJobEnum.SEND_MESSAGE.getExecutionType());
-		}else {
-			log.info("type不为1，为"+type);
-			sendPromoteResultMessage(type, vo, officeAppId, title, content);
+			
 		}
 		return content;
 	}
 
     public String sendPromoteResultMessage(Byte type,FriendPromoteSelectVo vo,String officeAppId,String title,String content) {
-    	log.info("传入参数：type：{}，title：{},content:{}",type,title,content);
+    	logger().info("传入参数：type：{}，title：{},content:{}",type,title,content);
 		FriendPromoteSelectVo userLaunchInfo = friendPromoteService.getUserLaunchInfo(vo.getId());
 		if(userLaunchInfo!=null) {
 			String page="pages1/promoteinfo/promoteinfo?actCode="+userLaunchInfo.getActCode()+"&launch_id="+String.valueOf(vo.getId());
@@ -497,7 +499,7 @@ public class MaMpScheduleTaskService extends ShopBaseService {
 			UserRecord user = userService.getUserByUserId(vo.getUserId());
 			UserRecord wxUserInfo = checkMp(user.getWxUnionId(), officeAppId);
 			if (null == wxUserInfo) {
-				log.info("店铺："+getShopId()+"用户userId："+vo.getUserId()+"活动Id："+vo.getId()+"用户没有关注公众号");
+				logger().info("店铺："+getShopId()+"用户userId："+vo.getUserId()+"活动Id："+vo.getId()+"用户没有关注公众号");
 				return null;
 			}
 			userIdList.add(vo.getUserId());
