e6d8a9cc9c7ceb40a226af8585afd725ff3d182e
kangyaxin <kangyaxin@wangdian.cn>
2020-05-09 17:08:41 +0800

    助力优化

--- wxapp/jwxapp/pages1/promoteinfo/promoteinfo.js
+++ wxapp/jwxapp/pages1/promoteinfo/promoteinfo.js
@@ -170,7 +170,7 @@ global.wxPage({
     // 授权
     var code = 10000 + util.getCache('user_id')
     var str = '用户' + code
-    if ((str == util.getCache('nickName')) && promote_info.promoteCondition == 1) {
+    if ( ((str == util.getCache('nickName')) ||  util.getCache('nickName') == '') && promote_info.promoteCondition == 1 ) {
       that.setData({
         has_user: 0
       })
@@ -511,8 +511,12 @@ function promote_request(that) {
       }
       // 助力次数提示
       if (promote_info.canPromote && promote_info.canPromote.code == 0) {
-        util.showModal('提示', '今天的助力次数已用完了');
-      }
+        if (promote_info.promoteStatus == 2) {
+          util.showModal('提示', '助力已完成');
+        } else {
+          util.showModal('提示', '今天的助力次数已用完了');
+        }
+      } 
       that.setData({
         promote_info: promote_info,
         is_promote_value: is_promote_value,
