0970e6d81abe2116f5d166904bf35e4f06cc46f2
wangshuai <wangshuai01@wangdian.cn>
2020-05-09 17:58:58 +0800

    无单号物流id与php一致
    Signed-off-by: wangshuai <wangshuai01@wangdian.cn>

--- jmp/mp-service/src/main/java/com/vpu/mp/service/shop/express/ExpressService.java
+++ jmp/mp-service/src/main/java/com/vpu/mp/service/shop/express/ExpressService.java
@@ -21,7 +21,7 @@ import static com.vpu.mp.db.shop.tables.Shipping.SHIPPING;
 public class ExpressService extends ShopBaseService {
 
     /**无单号物流发货时置单号为空*/
-    public static final Byte NO_2_EXPRESS = 100;
+    public static final Byte NO_2_EXPRESS = 42;
 
     public final Shipping TABLE = SHIPPING;
 
