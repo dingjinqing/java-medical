020454ea684252fe384855e3298306298c8d037b
huangzhuangzhuang <huangzhuangzhuang@wangdian.cn>
2020-05-14 18:22:45 +0800

    周期内已兑换商品提示

---
+++ jmp/mp-service/src/main/java/com/vpu/mp/service/pojo/wxapp/card/vo/CardExchangTipVo.java
@@ -0,0 +1,26 @@
+package com.vpu.mp.service.pojo.wxapp.card.vo;
+
+import lombok.Data;
+
+/**
+ * 会员卡兑换商品使用情况
+ * @author 黄壮壮
+ *
+ */
+@Data
+public class CardExchangTipVo {
+	/**
+	 * 周期类型
+	 */
+	private Byte periodLimit;
+	
+	/**
+	 * 周期内可兑换商品数量
+	 */
+	private Byte periodNumber;
+	
+	/**
+	 * 周期内已经兑换的数量
+	 */
+	private Byte periodExchangGoodsNum;
+}
