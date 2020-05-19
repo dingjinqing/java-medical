9ea69e64228c7dec6e28020ef42d6c5f2edcf40c
huangzhuangzhuang <huangzhuangzhuang@wangdian.cn>
2020-05-19 09:39:13 +0800

    会员卡审核添加图片解析并优化代码结构

--- jmp/mp-service/src/main/java/com/vpu/mp/service/shop/member/CardVerifyService.java
+++ jmp/mp-service/src/main/java/com/vpu/mp/service/shop/member/CardVerifyService.java
@@ -1,21 +1,29 @@
 package com.vpu.mp.service.shop.member;
 
 import static com.vpu.mp.db.shop.Tables.CARD_EXAMINE;
+import static com.vpu.mp.db.shop.Tables.USER;
+import static com.vpu.mp.service.pojo.shop.member.card.CardConstant.REFUSED;
+import static com.vpu.mp.service.pojo.shop.member.card.CardConstant.VERIFIED;
 import static com.vpu.mp.service.pojo.shop.member.card.CardVerifyConstant.VSTAT_REFUSED;
 
+import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.Set;
+import java.util.stream.Collectors;
 
+import org.apache.commons.beanutils.PropertyUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.jooq.Record;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.vpu.mp.db.shop.tables.records.CardExamineRecord;
 import com.vpu.mp.db.shop.tables.records.MemberCardRecord;
 import com.vpu.mp.db.shop.tables.records.UserDetailRecord;
@@ -24,6 +32,8 @@ import com.vpu.mp.service.foundation.util.CardUtil;
 import com.vpu.mp.service.foundation.util.DateUtil;
 import com.vpu.mp.service.foundation.util.PageResult;
 import com.vpu.mp.service.foundation.util.Util;
+import com.vpu.mp.service.pojo.shop.member.MemberEducationEnum;
+import com.vpu.mp.service.pojo.shop.member.MemberIndustryEnum;
 import com.vpu.mp.service.pojo.shop.member.builder.ActiveOverDueVoBuilder;
 import com.vpu.mp.service.pojo.shop.member.builder.UserCardRecordBuilder;
 import com.vpu.mp.service.pojo.shop.member.card.ActiveAuditParam;
@@ -32,9 +42,13 @@ import com.vpu.mp.service.pojo.shop.member.card.ActiveOverDueVo;
 import com.vpu.mp.service.pojo.shop.member.card.CardBasicVo;
 import com.vpu.mp.service.pojo.shop.member.card.CardVerifyConstant;
 import com.vpu.mp.service.pojo.shop.member.card.CardVerifyResultVo;
+import com.vpu.mp.service.pojo.wxapp.card.param.CardCustomActionParam;
+import com.vpu.mp.service.shop.card.msg.CardMsgNoticeService;
 import com.vpu.mp.service.shop.member.dao.CardDaoService;
 import com.vpu.mp.service.shop.member.dao.CardVerifyDaoService;
+import com.vpu.mp.service.shop.member.wxapp.WxAppCardActivationService;
 import com.vpu.mp.service.shop.user.user.UserDetailService;
+
 
 /**
 * @author 黄壮壮
@@ -49,6 +63,8 @@ public class CardVerifyService extends ShopBaseService {
 	@Autowired public UserCardService userCardService;
 	@Autowired public UserDetailService userDetailService;
 	@Autowired public CardDaoService cardDaoSvc;
+	@Autowired public CardMsgNoticeService cardMsgNoticeSvc;
+	@Autowired public WxAppCardActivationService wxCardActSvc;
 	/**
 	 *  分页查询
 	 */
@@ -92,12 +108,10 @@ public class CardVerifyService extends ShopBaseService {
 		cExMap.values().removeIf(Objects::isNull);
 
         if(cExMap.size()>0) {
-
             UserDetailRecord userDetailRecord = new UserDetailRecord();
 			userDetailRecord.fromMap(cExMap);
 			userDetailRecord.setUserId(cEx.getUserId());
 			return userDetailRecord;
-
 		}else {
 			return null;
 		}
@@ -273,6 +287,172 @@ public class CardVerifyService extends ShopBaseService {
     }
     
     
+    /**
+	 * 审核不通过
+	 * 
+	 * @param param
+	 */
+	public void rejectActivateAudit(ActiveAuditParam param) {
+		CardExamineRecord record = setRejectData(param);
+		int res = cardDaoSvc.updateCardExamine(record);
+		if(res>0) {
+			logger().info("发送订阅消息");
+			CardExamineRecord re = cardDaoSvc.getCardExamineRecordById(record.getId());
+			cardMsgNoticeSvc.sendAuditSuccessMsg(re);		
+		}
+	}
     
     
+	/**
+	 * 审核不通过数据
+	 * 
+	 * @param
+	 * @return
+	 */
+	private CardExamineRecord setRejectData(ActiveAuditParam param) {
+		CardExamineRecord record = new CardExamineRecord();
+		record.setId(param.getId());
+		record.setRefuseTime(DateUtil.getSqlTimestamp());
+		record.setRefuseDesc(param.getRefuseDesc());
+		record.setStatus(REFUSED);
+		return record;
+	}
+    
+    
+	/**
+	 * 审核通过数据
+	 * 
+	 * @param id
+	 * @return
+	 */
+	private CardExamineRecord setPassData(Integer id, Timestamp now) {
+		CardExamineRecord record = new CardExamineRecord();
+		record.setId(id);
+		record.setPassTime(now);
+		record.setStatus(VERIFIED);
+		return record;
+	}
+	
+	
+	/**
+	 * 审核通过
+	 * 
+	 * @param param
+	 * @return
+	 */
+	public void passActivateAudit(ActiveAuditParam param) {
+		this.transaction(() -> {
+			Timestamp now = DateUtil.getSqlTimestamp();
+			logger().info("申请激活会员卡通过: " + now);
+			// 更新card_examine 信息
+			CardExamineRecord record = setPassData(param.getId(), now);
+			cardDaoSvc.updateCardExamine(record);
+			// 更新激活
+			cardDaoSvc.updateUserCardByCardNo(param.getCardNo(), now);
+		});
+	}
+	
+	
+	/**
+	 * 分页查询激活审核信息
+	 * 
+	 * @param param
+	 * @return
+	 */
+	public PageResult<ActiveAuditVo> getActivateAuditList(ActiveAuditParam param) {
+		logger().info("分页查询激活信息");
+		PageResult<? extends Record> results = getPageList(param);
+		PageResult<ActiveAuditVo> res = new PageResult<>();
+		res.setPage(results.getPage());
+		List<ActiveAuditVo> myList = new ArrayList<>();
+		
+		if(results.dataList.size()>0) {
+			// 所有的卡
+			List<String> nos = results.dataList.stream().map(x->x.get(CARD_EXAMINE.CARD_NO)).distinct().collect(Collectors.toList());
+			Map<String, MemberCardRecord> cardMap = cardDaoSvc.getCardByNo(nos.toArray(new String[0]));
+			Map<String,List<String>> cardCfgMap = new HashMap<>();
+			// 提前获取激活的选项
+			for(Map.Entry<String, MemberCardRecord> entry: cardMap.entrySet()) {
+				List<String> cfg = CardUtil.parseActivationCfg(entry.getValue().getActivationCfg());
+				cardCfgMap.put(entry.getKey(), cfg);
+			}
+			for(Record record: results.dataList) {
+				ActiveAuditVo vo = new ActiveAuditVo();
+				vo.setCardNo(record.get(CARD_EXAMINE.CARD_NO));
+				vo.setMobile(record.get(USER.MOBILE));
+				vo.setUsername(record.get(USER.USERNAME));
+				vo.setCreateTime(record.get(CARD_EXAMINE.CREATE_TIME));
+				vo.setStatus(record.get(CARD_EXAMINE.STATUS));
+				vo.setId(record.get(CARD_EXAMINE.ID));
+				vo.setRefuseDesc(record.get(CARD_EXAMINE.REFUSE_DESC));
+				// 激活数据项
+				List<String> activationCfg = cardCfgMap.get(vo.getCardNo());
+				for(String name: activationCfg) {
+					if("birthday".equals(name)) {
+						vo.setBirthDayDay(record.get(CARD_EXAMINE.BIRTHDAY_DAY));
+						vo.setBirthDayMonth(record.get(CARD_EXAMINE.BIRTHDAY_MONTH));
+						vo.setBirthDayYear(record.get(CARD_EXAMINE.BIRTHDAY_YEAR));
+					}else if("address".equals(name)){
+						vo.setProvinceCode(record.get(CARD_EXAMINE.PROVINCE_CODE));
+						vo.setCityCode(record.get(CARD_EXAMINE.CITY_CODE));
+						vo.setDistrictCode(record.get(CARD_EXAMINE.DISTRICT_CODE));
+					}else {
+						try {
+							Object value = record.get(Util.humpToUnderline(name));
+							PropertyUtils.setProperty(vo, name, value);
+						} catch (Exception e) {
+							// 该属性为空 
+						}		
+					}
+				}
+				
+				// deal with custom option
+				String customOpts = record.get(CARD_EXAMINE.CUSTOM_OPTIONS);
+				if(!StringUtils.isBlank(customOpts)) {
+					 List<CardCustomActionParam> opts = Util.json2Object(customOpts,new TypeReference<List<CardCustomActionParam>>() {
+				        }, false);
+					 
+					 //	deal with picture links
+					 for(CardCustomActionParam item: opts) {
+						 String[] links = item.getPictureLinks();
+						 if(null != links && links.length>0) {
+							 for(int i=0;i<links.length;i++) {
+								 links[i] = imageUrl(links[i]);
+							 }
+						 }
+					 }
+					 vo.setCustomOptions(opts);
+				}
+				myList.add(vo);
+			}
+		}
+		// deal with industry and education
+		for (ActiveAuditVo activeAuditVo : myList) {
+			// education
+			if(activeAuditVo.getEducation()!= null) {
+				String educationStr = MemberEducationEnum.getNameByCode((int)activeAuditVo.getEducation());
+				activeAuditVo.setEducationStr(educationStr);
+			}
+			// industry
+			if(activeAuditVo.getIndustryInfo() != null) {
+				String industry = MemberIndustryEnum.getNameByCode((int)activeAuditVo.getIndustryInfo());
+				activeAuditVo.setIndustry(industry);
+			}
+			// deal address
+			if(activeAuditVo.getCityCode()!=null && activeAuditVo.getCityCode()!=null) {
+				Map<String,Object> adMap = new HashMap<>();
+				adMap.put(WxAppCardActivationService.PROVINCE_CODE, activeAuditVo.getProvinceCode());
+				adMap.put(WxAppCardActivationService.CITY_CODE, activeAuditVo.getCityCode());
+				adMap.put(WxAppCardActivationService.DISTRICT_CODE, activeAuditVo.getDistrictCode());
+				wxCardActSvc.dealWithAddressCode(adMap);
+				activeAuditVo.setCity((String)adMap.get(WxAppCardActivationService.CITY_CODE));
+				activeAuditVo.setProvince((String)adMap.get(WxAppCardActivationService.PROVINCE_CODE));
+				activeAuditVo.setDistrict((String)adMap.get(WxAppCardActivationService.DISTRICT_CODE));
+			}
+			
+		}
+		res.setDataList(myList);
+		return res;
+	}
+
 }
