b6c5ed84158d7c7ab695458c5703292a05b9e45f
suntengfei <suntengfei@wangdian.cn>
2020-05-20 15:23:14 +0800

    会员卡充值、子模块导出接口调接

--- web-vue/src/api/admin/memberManage/memberValueAdd/memberValueAdd.js
+++ web-vue/src/api/admin/memberManage/memberValueAdd/memberValueAdd.js
@@ -25,3 +25,58 @@ export function getRenewalDetails (data) {
     data: data
   })
 }
+
+// 会员卡充值明细列表
+export function memberpCarRecharge (data) {
+  return service({
+    url: '/api/admin/user/card/charge/order',
+    method: 'post',
+    data: data
+  })
+}
+
+// 会员卡续费交易数据
+export function getRenewPurchase (data) {
+  return service({
+    url: '/api/admin/user/card/renew/analysis',
+    method: 'post',
+    data: data
+  })
+}
+
+// 会员卡续费交易数据
+export function getRechargePurchase (data) {
+  return service({
+    url: '/api/admin/user/card/charge/analysis',
+    method: 'post',
+    data: data
+  })
+}
+
+// 会员卡购买列表导出条数查询
+export function queryPurchaseExportNum (data) {
+  return service({
+    url: '/api/admin/order/member_card/export/rows',
+    method: 'post',
+    data: data
+  })
+}
+// 会员卡购买列表导出
+export function purchaseExport (data) {
+  return service({
+    url: '/api/admin/order/member_card/export',
+    method: 'post',
+    data: data,
+    responseType: 'blob'
+  })
+}
+
+// 会员卡续费记录列表导出
+export function renewExport (data) {
+  return service({
+    url: '/api/admin/user/card/renew/export',
+    method: 'post',
+    data: data,
+    responseType: 'blob'
+  })
+}
