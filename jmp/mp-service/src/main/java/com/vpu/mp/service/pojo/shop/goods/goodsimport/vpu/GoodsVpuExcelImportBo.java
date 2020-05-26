901ed016a0b4574d305266b87a571ac22ab5d308
lixiaobing <lixiaobing@wangdian.cn>
2020-05-26 23:18:55 +0800

    商品导入-mq反序列化类添加默认构造器

--- jmp/mp-service/src/main/java/com/vpu/mp/service/pojo/shop/goods/goodsimport/vpu/GoodsVpuExcelImportBo.java
+++ jmp/mp-service/src/main/java/com/vpu/mp/service/pojo/shop/goods/goodsimport/vpu/GoodsVpuExcelImportBo.java
@@ -2,6 +2,7 @@ package com.vpu.mp.service.pojo.shop.goods.goodsimport.vpu;
 
 import com.vpu.mp.service.pojo.shop.goods.goodsimport.GoodsExcelImportBase;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.math.BigDecimal;
@@ -12,6 +13,7 @@ import java.math.BigDecimal;
  */
 @Getter
 @Setter
+@NoArgsConstructor
 public class GoodsVpuExcelImportBo extends GoodsExcelImportBase {
     private int operateBusId;
 
