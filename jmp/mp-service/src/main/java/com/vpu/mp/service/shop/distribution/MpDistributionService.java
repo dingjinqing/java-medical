2ec90db7976cc275cd1d71b74d3298abd4b3bc53
changle <changle@wangdian.cn>
2020-05-27 15:01:48 +0800

    分销推广语接口添加审核开关配置

--- jmp/mp-service/src/main/java/com/vpu/mp/service/shop/distribution/MpDistributionService.java
+++ jmp/mp-service/src/main/java/com/vpu/mp/service/shop/distribution/MpDistributionService.java
@@ -265,6 +265,9 @@ public class MpDistributionService extends ShopBaseService{
      */
     public DistributionDocumentParam getDistributorDoc(){
         DistributionDocumentParam distributionDocument = distributionCfg.getDistributionDocument();
+        //获取分销审核开关配置
+        Byte judgeStatus = this.distributionCfg.getDistributionCfg().getJudgeStatus();
+        distributionDocument.setJudgeStatus(judgeStatus);
         return distributionDocument;
     }
 
