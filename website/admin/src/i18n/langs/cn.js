7ea3596fddd683f7f71603ece440b6030605bf63
zhaojianqiang <zhaojianqiang@wangdian.cn>
2020-05-28 15:51:46 +0800

    一些丢的文件

--- website/admin/src/i18n/langs/cn.js
+++ website/admin/src/i18n/langs/cn.js
@@ -944,7 +944,45 @@ const cn = {
   formDecorationHome, //  表单装修主页
   formStatisticsHome, // 表单统计列表主页
   formDecorationModel, // 表单统计装修所有模块
-  mintegralExchange // 营销管理积分兑换
+  mintegralExchange, // 营销管理积分兑换
+  // 授权公众号
+  serviceAuth: {
+    addButton: '添加授权',
+    tipsOne: '注意：仅可以授权认证服务号.授权完成认证服务号，关注服务号的用户在接收小程序消息通知时优先通过小程序接收',
+    tipsTop1: '由于微信平台相关要求，公众号授权完成后，需要在店铺内小程序管理-小程序授权绑定和该店铺小程序同主体的公众号，绑定后，使用该绑定公众号登录微信公众平台，在公众平台小程序小程序管理页面添加关联小程序，关联该小程序后，关注公众号的用户在接收小程序消息通知时即可通过公众号接收。小程序接收消息限制较大，详情查看',
+    tipsTop2: '模板消息下发条件说明',
+    tipsTop3: '此功能方便商家针对用户进行小程序消息通知。',
+    nickName: '服务号名称',
+    principalName: '主体名称',
+    alias: '公众微信号',
+    lastAuthTime: '授权时间',
+    mpNickName: '已绑定小程序',
+    isAuthOk: '授权状态',
+    operation: '操作',
+    payLook: '查看',
+    payManage: '提现配置',
+    payStoreName: '商户号:',
+    payKey: '支付秘钥:',
+    payCertContent: '支付证书:',
+    payKeyContent: '支付私钥:',
+    sure: '确 定',
+    cancel: '取 消',
+    haveAuth: '已授权',
+    haveCancle: '已取消',
+    title1: '授权列表',
+    title2: '服务号详情',
+    qurl: '公众号二维码',
+    type: '公众号类型',
+    newButton: '重新授权',
+    tipsTop4: ' 如果你的公众号已成功升级（从未认证升为认证号，或从订阅号升为服务号），请点击重新授权',
+    tipsTop5: '如何升级：',
+    tipsTop6: '如需对公众号进行微信认证，请登录“微信公众平台-> 公众号设置”，在“认证情况”栏目，点击申请微信认证',
+    authStats: '授权状态',
+    Wechat0: '订阅号',
+    Wechat1: '微信认证订阅号',
+    Wechat2: '服务号',
+    Wechat3: '微信认证服务号'
+  }
 }
 
 export default cn
