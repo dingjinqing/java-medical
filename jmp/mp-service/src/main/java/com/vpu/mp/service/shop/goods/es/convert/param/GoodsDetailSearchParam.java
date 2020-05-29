b4873896a85fc5a9103d4be7203004cf2f60e590
luguangyao <luguangyao@wangdian.cn>
2020-05-29 11:30:08 +0800

    Feat:新加商品详情接口入参

---
+++ jmp/mp-service/src/main/java/com/vpu/mp/service/shop/goods/es/convert/param/GoodsDetailSearchParam.java
@@ -0,0 +1,24 @@
+package com.vpu.mp.service.shop.goods.es.convert.param;
+
+public class GoodsDetailSearchParam {
+
+    private Integer goodsId;
+
+    private String vipLevel;
+
+    public Integer getGoodsId() {
+        return goodsId;
+    }
+
+    public void setGoodsId(Integer goodsId) {
+        this.goodsId = goodsId;
+    }
+
+    public String getVipLevel() {
+        return vipLevel;
+    }
+
+    public void setVipLevel(String vipLevel) {
+        this.vipLevel = vipLevel;
+    }
+}
