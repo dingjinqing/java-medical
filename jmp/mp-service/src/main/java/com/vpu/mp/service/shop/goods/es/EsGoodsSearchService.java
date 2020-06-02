e5ae20832468877cc7d9dfee4b4d2c061731b1f9
luguangyao <luguangyao@wangdian.cn>
2020-06-02 11:13:38 +0800

    Fix:代码修复

--- jmp/mp-service/src/main/java/com/vpu/mp/service/shop/goods/es/EsGoodsSearchService.java
+++ jmp/mp-service/src/main/java/com/vpu/mp/service/shop/goods/es/EsGoodsSearchService.java
@@ -2,6 +2,7 @@ package com.vpu.mp.service.shop.goods.es;
 
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
+import com.google.common.collect.Sets;
 import com.vpu.mp.service.foundation.util.PageResult;
 import com.vpu.mp.service.pojo.shop.goods.es.EsSearchParam;
 import com.vpu.mp.service.pojo.shop.goods.goods.GoodsPageListParam;
@@ -21,6 +22,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 /**
@@ -93,7 +95,7 @@ public class EsGoodsSearchService extends EsBaseSearchService{
                         }
                     });
                     vo.setGoodsPointLabels(specifiedLabelVos);
-                    vo.setGoodsNormalLabels(ordinaryLabelVos);
+                    vo.setGoodsNormalLabels(Sets.newHashSet(ordinaryLabelVos.iterator()));
                 }
                 voList.add(vo);
             });
