6947b950c982adbf9b7f61bf3b486d0df241c951
suntengfei <suntengfei@wangdian.cn>
2020-06-08 18:35:17 +0800

    同步代码

--- website/system/src/view/system/layout/notFound.vue
+++ website/system/src/view/system/layout/notFound.vue
@@ -10,11 +10,11 @@
         SORRY!您访问的页面找不到了，请重新加载或者点击下方的常用链接
       </div>
       <div class="link">
-        <div @click="handleToClick(0)">system后台</div>
+        <!-- <div @click="handleToClick(0)">system后台</div>
         <div
           style="margin: 0 20px"
           @click="handleToClick(1)"
-        >admin后台</div>
+        >admin后台</div> -->
         <div @click="handleToClick(2)">官网</div>
       </div>
     </div>
