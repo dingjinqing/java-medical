07d1707c8b824b9ab6e32380af301c5ae52c3094
huangzhuangzhuang <huangzhuangzhuang@wangdian.cn>
2020-06-09 14:18:47 +0800

    添加参数

--- website/admin/src/view/admin/index/leftNavComponents/user_manger/membershipCard/membershipCard.vue
+++ website/admin/src/view/admin/index/leftNavComponents/user_manger/membershipCard/membershipCard.vue
@@ -515,7 +515,8 @@ export default {
       let obj = {
         'currentPage': 0,
         'pageRows': 100,
-        'cardType': this.currentCardType
+        'cardType': this.currentCardType,
+        'filterStop': false
       }
       console.log(obj)
       // 请求后台api
@@ -698,7 +699,8 @@ export default {
           this.getBackEndData({
             'currentPage': 0,
             'pageRows': 100,
-            'cardType': this.currentCardType
+            'cardType': this.currentCardType,
+            'filterStop': false
           })
         }
       })
