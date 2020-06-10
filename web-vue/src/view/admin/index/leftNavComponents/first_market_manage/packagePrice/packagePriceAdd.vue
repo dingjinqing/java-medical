b244311165e8ec352268a306c48b4417de91f151
zhaoxin <zhaoxin01@wangdian.cn>
2020-06-10 08:48:05 +0800

    打包一口价-添加商品优化

--- web-vue/src/view/admin/index/leftNavComponents/first_market_manage/packagePrice/packagePriceAdd.vue
+++ web-vue/src/view/admin/index/leftNavComponents/first_market_manage/packagePrice/packagePriceAdd.vue
@@ -1191,27 +1191,42 @@ export default {
             'startTime': this.param.validity[0],
             'endTime': this.param.validity[1],
             'totalMoney': Number(this.param.totalMoney),
-            'totalRatio': Number(this.param.totalRatio),
-            'group1': {
-              'groupName': this.param.groupName,
-              'goodsNumber': this.param.goodsNumber,
-              'goodsIdList': this.selectedGoodsIdList,
-              'catIdList': this.platformIdList,
-              'sortIdList': this.bussinessIdList
-            },
-            'group2': {
-              'groupName': this.param.groupName2,
-              'goodsNumber': this.param.goodsNumber2,
-              'goodsIdList': this.selectedGoodsIdList2,
-              'catIdList': this.platformIdList2,
-              'sortIdList': this.bussinessIdList2
-            },
-            'group3': {
-              'groupName': this.param.groupName3,
-              'goodsNumber': this.param.goodsNumber3,
-              'goodsIdList': this.selectedGoodsIdList3,
-              'catIdList': this.platformIdList3,
-              'sortIdList': this.bussinessIdList3
+            'totalRatio': Number(this.param.totalRatio)
+          }
+          if (this.group1Flag === true) {
+            obj = {
+              ...obj,
+              'group1': {
+                'groupName': this.param.groupName,
+                'goodsNumber': this.param.goodsNumber,
+                'goodsIdList': this.selectedGoodsIdList,
+                'catIdList': this.platformIdList,
+                'sortIdList': this.bussinessIdList
+              }
+            }
+          }
+          if (this.group2Flag === true) {
+            obj = {
+              ...obj,
+              'group2': {
+                'groupName': this.param.groupName2,
+                'goodsNumber': this.param.goodsNumber2,
+                'goodsIdList': this.selectedGoodsIdList2,
+                'catIdList': this.platformIdList2,
+                'sortIdList': this.bussinessIdList2
+              }
+            }
+          }
+          if (this.group3Flag === true) {
+            obj = {
+              ...obj,
+              'group3': {
+                'groupName': this.param.groupName3,
+                'goodsNumber': this.param.goodsNumber3,
+                'goodsIdList': this.selectedGoodsIdList3,
+                'catIdList': this.platformIdList3,
+                'sortIdList': this.bussinessIdList3
+              }
             }
           }
           if (this.isEdite === false) {
@@ -1254,10 +1269,10 @@ export default {
         if (res.error === 0) {
           var data = res.content
           this.param = data
-          if (data.group2.groupName || data.group2.goodsNumber) {
+          if (data.group2 && (data.group2.groupName || data.group2.goodsNumber)) {
             this.group2Flag = true
           }
-          if (data.group3.groupName || data.group3.goodsNumber) {
+          if (data.group3 && (data.group3.groupName || data.group3.goodsNumber)) {
             this.group3Flag = true
           }
           let date = [data.startTime, data.endTime]
@@ -1277,28 +1292,32 @@ export default {
           this.bussinessIdList = data.group1.sortIdList
 
           // 商品组2
-          this.$set(this.param, 'groupName2', data.group2.groupName)
-          this.$set(this.param, 'goodsNumber2', data.group2.goodsNumber)
-          this.goodsList2 = data.group2.goodsList
-          data.group2.goodsList.map(item => {
-            this.selectedGoodsIdList2.push(item.goodsId)
-          })
-          this.platformList2 = data.group2.cateVoList
-          this.platformIdList2 = data.group2.catIdList
-          this.bussinessList2 = data.group2.sortVoList
-          this.bussinessIdList2 = data.group2.sortIdList
+          if (data.group2 && data.group2.groupName) {
+            this.$set(this.param, 'groupName2', data.group2.groupName)
+            this.$set(this.param, 'goodsNumber2', data.group2.goodsNumber)
+            this.goodsList2 = data.group2.goodsList
+            data.group2.goodsList.map(item => {
+              this.selectedGoodsIdList2.push(item.goodsId)
+            })
+            this.platformList2 = data.group2.cateVoList
+            this.platformIdList2 = data.group2.catIdList
+            this.bussinessList2 = data.group2.sortVoList
+            this.bussinessIdList2 = data.group2.sortIdList
+          }
 
           // 商品组3
-          this.$set(this.param, 'groupName3', data.group3.groupName)
-          this.$set(this.param, 'goodsNumber3', data.group3.goodsNumber)
-          this.goodsList3 = data.group3.goodsList
-          data.group3.goodsList.map(item => {
-            this.selectedGoodsIdList3.push(item.goodsId)
-          })
-          this.platformList3 = data.group3.cateVoList
-          this.platformIdList3 = data.group3.catIdList
-          this.bussinessList3 = data.group3.sortVoList
-          this.bussinessIdList3 = data.group3.sortIdList
+          if (data.group3 && data.group3.groupName) {
+            this.$set(this.param, 'groupName3', data.group3.groupName)
+            this.$set(this.param, 'goodsNumber3', data.group3.goodsNumber)
+            this.goodsList3 = data.group3.goodsList
+            data.group3.goodsList.map(item => {
+              this.selectedGoodsIdList3.push(item.goodsId)
+            })
+            this.platformList3 = data.group3.cateVoList
+            this.platformIdList3 = data.group3.catIdList
+            this.bussinessList3 = data.group3.sortVoList
+            this.bussinessIdList3 = data.group3.sortIdList
+          }
           console.log(this.param, 'get-return-data')
         } else {
           this.$message.error(res.message)
