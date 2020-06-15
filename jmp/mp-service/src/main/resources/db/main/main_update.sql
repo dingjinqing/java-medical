0e4199cafd1c98382d4db52994f9bbd368f1e705
luguangyao <luguangyao@wangdian.cn>
2020-06-15 16:29:12 +0800

    Fix:main库sql修复

--- jmp/mp-service/src/main/resources/db/main/main_update.sql
+++ jmp/mp-service/src/main/resources/db/main/main_update.sql
@@ -6,7 +6,6 @@
 /***********************2.8*********************END*/
 
 /***********************2.10********************BEGIN*/
-ALTER TABLE `b2c_shop` ADD COLUMN `expire_time` timestamp NULL COMMENT '到期时间';
 -- 20200527 李晓冰 扩充b2c_task_job_content content字段长度
 ALTER TABLE b2c_task_job_content MODIFY COLUMN content MEDIUMTEXT COMMENT '消息内容';
 
