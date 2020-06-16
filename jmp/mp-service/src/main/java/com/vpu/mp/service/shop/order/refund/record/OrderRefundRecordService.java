282ae561e0138d8054f82e5b8477a9feb6af62b8
wangshuai <wangshuai01@wangdian.cn>
2020-06-16 17:26:40 +0800

    退款优化
    Signed-off-by: wangshuai <wangshuai01@wangdian.cn>

--- jmp/mp-service/src/main/java/com/vpu/mp/service/shop/order/refund/record/OrderRefundRecordService.java
+++ jmp/mp-service/src/main/java/com/vpu/mp/service/shop/order/refund/record/OrderRefundRecordService.java
@@ -5,11 +5,11 @@ import com.vpu.mp.db.shop.tables.OrderRefundRecord;
 import com.vpu.mp.db.shop.tables.records.OrderRefundRecordRecord;
 import com.vpu.mp.db.shop.tables.records.PaymentRecordRecord;
 import com.vpu.mp.service.foundation.service.ShopBaseService;
-import com.vpu.mp.service.foundation.util.BigDecimalUtil;
 import com.vpu.mp.service.foundation.util.DateUtil;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 import static com.vpu.mp.db.shop.tables.OrderRefundRecord.ORDER_REFUND_RECORD;
 
@@ -22,15 +22,15 @@ import static com.vpu.mp.db.shop.tables.OrderRefundRecord.ORDER_REFUND_RECORD;
 public class OrderRefundRecordService extends ShopBaseService{
 	
 	public final OrderRefundRecord TABLE = ORDER_REFUND_RECORD;
-    private static final byte success = 1;
-    private static final byte fail = 2;
+    public static final Byte success = 1;
+    public static final Byte fail = 2;
     /**
      * 非系统金额退款记录
      * @param refundSn 退款流水号
      * @param refundResult 退款结果（失败==null）
      * @param retId 退订单号
      */
-	public void addRecord(String refundSn, PaymentRecordRecord payRecord, WxPayRefundResult refundResult, Integer retId) {
+	public void addRecord(String refundSn, PaymentRecordRecord payRecord, WxPayRefundResult refundResult, Integer retId, BigDecimal money, String errorDesc) {
         OrderRefundRecordRecord record = db().newRecord(TABLE);
         record.setRefundSn(refundSn);
         record.setPaySn(payRecord.getPaySn());
@@ -39,36 +39,100 @@ public class OrderRefundRecordService extends ShopBaseService{
         record.setRefundTime(DateUtil.getSqlTimestamp());
         record.setRetId(retId);
         record.setTransSn(payRecord.getTradeNo());
-
+        record.setRefundAmount(money);
         if(refundResult != null) {
             //成功
             record.setDealStatus(success);
             record.setDealStatusName("非系统金额退款成功");
             record.setDealRemark(refundResult.toString());
-            record.setRefundAmount(
-                BigDecimalUtil.divide(new BigDecimal(refundResult.getRefundFee().toString()), new BigDecimal("100"))
-            );
         }else {
             //失败
             record.setDealStatus(fail);
             record.setDealStatusName("非系统金额退款失败");
-            record.setDealRemark("见日志");
-            record.setRefundAmount(BigDecimal.ZERO);
+            record.setRemark1(errorDesc);
         }
         record.insert();
     }
 
     /**
-     * 判断改退款订单是否存在非系统金额退款失败情况
+     * 判断该退款订单是否存在非系统金额退款失败情况
      * @param retId
      * @return
      */
-    public boolean isReturnSucess(Integer retId){
-        OrderRefundRecordRecord count = db().selectFrom(TABLE).where(TABLE.RET_ID.eq(retId).and(TABLE.DEAL_STATUS.eq(fail))).fetchAny();
-        if(count != null) {
+    public boolean isExistFail(Integer retId){
+        if(retId == null) {
             return false;
         }
-        return true;
+        OrderRefundRecordRecord count = db().selectFrom(TABLE).where(TABLE.RET_ID.eq(retId).and(TABLE.DEAL_STATUS.eq(fail))).fetchAny();
+        if(count != null) {
+            return true;
+        }
+        return false;
 	}
 
+    /**
+     * 判断该退款订单是否存退款成功情况
+     * @param retId
+     * @return
+     */
+    public boolean isExistSucess(Integer retId){
+        OrderRefundRecordRecord count = db().selectFrom(TABLE).where(TABLE.RET_ID.eq(retId).and(TABLE.DEAL_STATUS.eq(success))).fetchAny();
+        if(count != null) {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * 判断该次退款成功记录
+     * @param retId
+     * @param paySn
+     * @param money
+     */
+    public OrderRefundRecordRecord getReturnRecord(Integer retId, String paySn, BigDecimal money) {
+        return db().selectFrom(TABLE).where(TABLE.RET_ID.eq(retId).and(TABLE.PAY_SN.eq(paySn)).and(TABLE.REFUND_AMOUNT.eq(money))).fetchAny();
+    }
+
+    /**
+     * 更新退款订单状态
+     * @param returnRecord
+     * @param status
+     */
+    public void updateStatus(OrderRefundRecordRecord returnRecord, Byte status, String desc) {
+        if(returnRecord == null) {
+            return;
+        }
+        db().update(TABLE).set(TABLE.DEAL_STATUS, status).set(TABLE.REMARK1, desc).where(TABLE.ID.eq(returnRecord.getId())).execute();
+    }
+
+    /**
+     * 查询该订单是否存在正在处理中的退款订单（且微信退款失败）
+     * @param orderSn
+     * @return
+     */
+    public String getFailReturnOrder(String orderSn) {
+        OrderRefundRecordRecord record = db().selectFrom(TABLE).where(TABLE.ORDER_SN.eq(orderSn)).fetchAny();
+        if(record == null) {
+            return null;
+        }
+        return record.getOrderSn();
+    }
+
+    /**
+     * 查询该订单失败数据
+     * @param retId
+     * @return
+     */
+    public List<OrderRefundRecordRecord> getSuccessRecord(Integer retId) {
+        return db().selectFrom(TABLE).where(TABLE.RET_ID.eq(retId).and(TABLE.DEAL_STATUS.eq(success))).fetch();
+    }
+
+    /**
+     * 查询该订单失败数据
+     * @param retId
+     * @return
+     */
+    public OrderRefundRecordRecord getFailRecord(Integer retId) {
+        return db().selectFrom(TABLE).where(TABLE.RET_ID.eq(retId).and(TABLE.DEAL_STATUS.eq(fail))).fetchAny();
+    }
 }
