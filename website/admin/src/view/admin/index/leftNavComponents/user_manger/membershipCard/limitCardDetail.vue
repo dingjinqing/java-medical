fca2f55ea480b427046aff1d365471617e809e5c
huangzhuangzhuang <huangzhuangzhuang@wangdian.cn>
2020-06-16 09:39:10 +0800

    领取之日默认为零修复

--- website/admin/src/view/admin/index/leftNavComponents/user_manger/membershipCard/limitCardDetail.vue
+++ website/admin/src/view/admin/index/leftNavComponents/user_manger/membershipCard/limitCardDetail.vue
@@ -296,7 +296,11 @@ export default {
       this.cardEffectTime.fixedDate = [data.startTime, data.endTime]
       if (data.startTime === null) { this.cardEffectTime.fixedDate = '' }
       console.log(this.cardEffectTime.fixedDate)
-      this.cardEffectTime.receiveDay = data.receiveDay
+      if (data.receiveDay !== null) {
+        this.cardEffectTime.receiveDay = data.receiveDay
+      } else {
+        this.cardEffectTime.receiveDay = void 0
+      }
       this.cardEffectTime.dateType = data.dateType ? String(data.dateType) : '0'
       // 适用商品
       this.cardSuiteGoodsCfgData.isExchange = data.cardExchangGoods.isExchange
