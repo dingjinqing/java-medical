3a10309bd7f18697641abb014acf4ae059ef30ae
suntengfei <suntengfei@wangdian.cn>
2020-06-19 18:59:25 +0800

    校验自定义上传图片

--- wxapp/jwxapp/pages/memberinfo/memberinfo.js
+++ wxapp/jwxapp/pages/memberinfo/memberinfo.js
@@ -385,7 +385,7 @@ global.wxPage({
 
           if (custom_arr.length > 0) {
             for (var i in custom_arr) {
-              if(custom_arr[i].customType == 3){
+              if (custom_arr[i].customType == 3) {
                 that.setData({
                   uploadPictureTitle: custom_arr[i].customTitle
                 })
@@ -732,6 +732,9 @@ global.wxPage({
             } else if (custom_arr[i].customType == 2 && custom_arr[i].text == '') {
               util.showModal("提示", "请填写" + custom_arr[i].customTitle);
               return;
+            } else if (custom_arr[i].customType === 3 && custom_arr[i].comm_img.length == 0) {
+              util.showModal("提示", "请上传图片");
+              return;
             }
           }
           if (custom_arr[i].customType == 0 || custom_arr[i].customType == 1) {
@@ -748,9 +751,9 @@ global.wxPage({
 
         }
       }
-      console.log(custom_options)
+      console.log(custom_arr)
     }
-
+    return
     //激活
     if (user_info.real_name == "" && this.data.if_realname == 1) {
       util.showModal("提示", "请填写真实姓名");
