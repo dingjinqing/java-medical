4386dd484311968b59eb2905ec72e168ffff14fa
lixinguo <lixinguo@wangdian.cn>
2020-06-30 14:17:38 +0800

    config dao 改造

--- jmp/mp-service/src/main/java/com/vpu/mp/service/shop/config/message/MessageConfigService.java
+++ jmp/mp-service/src/main/java/com/vpu/mp/service/shop/config/message/MessageConfigService.java
@@ -1,5 +1,6 @@
 package com.vpu.mp.service.shop.config.message;
 
+import com.vpu.mp.dao.shop.config.MessageTemplateConfigDao;
 import com.vpu.mp.db.shop.tables.records.MessageTemplateConfigRecord;
 import com.vpu.mp.service.foundation.service.ShopBaseService;
 import com.vpu.mp.service.pojo.shop.config.message.MessageConfigParam;
@@ -7,6 +8,7 @@ import com.vpu.mp.service.pojo.shop.config.message.MessageConfigVo;
 import com.vpu.mp.service.pojo.shop.market.message.RabbitParamConstant;
 
 import org.jooq.Batch;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -18,22 +20,19 @@ import static com.vpu.mp.db.shop.tables.MessageTemplateConfig.MESSAGE_TEMPLATE_C
 @Service
 public class MessageConfigService extends ShopBaseService {
 
+	@Autowired
+	MessageTemplateConfigDao messageTemplateConfigDao;
+	
     public void updateMessageConfig(MessageConfigParam param){
-        List<MessageTemplateConfigRecord> recordList = new ArrayList<>();
-        Batch batch = db().batchUpdate(param.getConfigs().stream()
-            .map(x ->
-                db().newRecord(MESSAGE_TEMPLATE_CONFIG, x)
-            )
-            .collect(Collectors.toList()));
-        batch.execute();
+    	messageTemplateConfigDao.batchUpdate(param.getConfigs());
     }
 
     public List<MessageConfigVo> getAllMessageConfig(){
-       return  db().selectFrom(MESSAGE_TEMPLATE_CONFIG).fetch().into(MessageConfigVo.class);
+       return  messageTemplateConfigDao.getAllMessageConfig();
     }
 
     public MessageConfigVo getMessageConfig(Integer id){
-        return  db().selectFrom(MESSAGE_TEMPLATE_CONFIG).where(MESSAGE_TEMPLATE_CONFIG.ID.eq(id)).fetchOneInto(MessageConfigVo.class);
+        return  messageTemplateConfigDao.getMessageConfig(id);
     }
 
     /**
@@ -45,9 +44,6 @@ public class MessageConfigService extends ShopBaseService {
     	if(type.equals(RabbitParamConstant.Type.MP_TEMPLE_TYP_NO)||type.equals(RabbitParamConstant.Type.MP_TEMPLE_TYPE_NO_USER)) {
     		return true;
     	}
-        Byte openMp = db().select(MESSAGE_TEMPLATE_CONFIG.OPEN_MP).
-            from(MESSAGE_TEMPLATE_CONFIG).
-            where(MESSAGE_TEMPLATE_CONFIG.ID.eq(type)).fetchOne(MESSAGE_TEMPLATE_CONFIG.OPEN_MP);
-        return openMp != null && openMp == 1;
+        return messageTemplateConfigDao.hasConfig(type);
     }
 }
