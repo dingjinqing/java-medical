e21d73773ca34dab741cb28dd92951989fa300ab
lianghong <lianghong@wangdian.cn>
2020-07-01 17:46:44 +0800

    首页改回来

--- website/index/src/view/index/aboutUs.vue
+++ website/index/src/view/index/aboutUs.vue
@@ -8,11 +8,11 @@
 </template>
 <script>
 import AboutUs from '@/view/index/aboutUs/aboutUsIndex'
-// import Development from '@/view/index/aboutUs/development'
-// import Environmental from '@/view/index/aboutUs/environmental'
-// import ContactUs from '@/view/index/aboutUs/contactUs'
+import Development from '@/view/index/aboutUs/development'
+import Environmental from '@/view/index/aboutUs/environmental'
+import ContactUs from '@/view/index/aboutUs/contactUs'
 export default {
-  components: { AboutUs }
+  components: { AboutUs, Development, Environmental, ContactUs }
 }
 </script>
 <style scoped>
