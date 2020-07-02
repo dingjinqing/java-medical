970b6eb783756ec081f8008cd4e1f5f7add7f054
lixiaobing <lixiaobing@wangdian.cn>
2020-07-02 14:30:15 +0800

    商品代码临时存储

---
+++ jmp/mp-common/src/main/java/com/vpu/mp/common/pojo/shop/goods/dao/SpecValDo.java
@@ -0,0 +1,13 @@
+package com.vpu.mp.common.pojo.shop.goods.dao;
+
+/**
+ * 规格值
+ * @author 李晓冰
+ * @date 2020年07月02日
+ */
+public class SpecValDo {
+    private Integer specValId;
+    private Integer specId;
+    private Integer goodsId;
+    private Integer specValName;
+}
