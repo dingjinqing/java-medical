92951bbda2dc72a9be8412ff0ce06f4d7ad9db73
lixiaobing <1160721056@qq.com>
2020-07-05 21:52:38 +0800

    药品新增-根据sku prd_desc 解析对应prd_specs

--- jmp/mp-common/src/main/java/com/vpu/mp/service/pojo/shop/sku/entity/Spec.java
+++ jmp/mp-common/src/main/java/com/vpu/mp/service/pojo/shop/sku/entity/Spec.java
@@ -1,8 +1,12 @@
 package com.vpu.mp.service.pojo.shop.sku.entity;
 
+import cn.hutool.core.util.StrUtil;
 import lombok.Data;
 
 import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 /**
  * @author 李晓冰
@@ -18,4 +22,16 @@ public class Spec {
      * 规格值
      */
     List<SpecVal> specVals;
+
+    /**
+     * 规格名集合转换为对应的Map
+     * @param specs 代转换规格名集合
+     * @return Map,key是名字，value是实体类
+     */
+    public static Map<String,Spec> mapNameToSpec(List<Spec> specs){
+        Map<String, Spec> collect = specs.stream().filter(spec -> StrUtil.isNotBlank(spec.getSpecName()))
+            .collect(Collectors.toMap(Spec::getSpecName, Function.identity(), (x1, x2) -> x1));
+
+        return collect;
+    }
 }
