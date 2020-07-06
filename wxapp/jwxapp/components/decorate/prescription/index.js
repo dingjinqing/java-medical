d837b5fea5e7a3c4e8a2b4cacfdfbd73db9fecda
liuyang <liuyang01@wangdian.cn>
2020-07-06 14:25:19 +0800

    购物车-订单-首页处方弹窗

--- wxapp/jwxapp/components/decorate/prescription/index.js
+++ wxapp/jwxapp/components/decorate/prescription/index.js
@@ -4,5 +4,11 @@ var util = require("../../../utils/util.js")
 global.wxComponent({
   mixins: [base],
   methods: {
+    handleShowDialog(){
+      console.log(111)
+      this.setData({
+        showPrescription:true
+      })
+    }
   }
 });
