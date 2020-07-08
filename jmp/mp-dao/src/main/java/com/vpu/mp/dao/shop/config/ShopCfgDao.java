b18a46ba526cd9599cc1bb3a75bbce89ea2051fe
kongdecheng <kongdecheng@wangdian.cn>
2020-07-08 11:02:56 +0800

    配置表判断错误

--- jmp/mp-dao/src/main/java/com/vpu/mp/dao/shop/config/ShopCfgDao.java
+++ jmp/mp-dao/src/main/java/com/vpu/mp/dao/shop/config/ShopCfgDao.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Repository;
 import com.vpu.mp.dao.foundation.base.ShopBaseDao;
 
 /**
- * 
+ *
  * @author lixinguo
  *
  */
@@ -26,12 +26,12 @@ public class ShopCfgDao extends ShopBaseDao {
 
 	/**
 	 * 判断key是否存在 true:存在 false:不存在
-	 * 
+	 *
 	 * @param key
 	 * @return
 	 */
 	public Boolean exists(String key) {
-		return null == db().select().from(SHOP_CFG).where(SHOP_CFG.K.eq(key)).fetchAny();
+		return null != db().select().from(SHOP_CFG).where(SHOP_CFG.K.eq(key)).fetchAny();
 	}
 
 	/**
