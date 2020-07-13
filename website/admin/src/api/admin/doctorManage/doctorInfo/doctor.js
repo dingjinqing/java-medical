96b2e8fb9d4cf5ff8d7ae3011b74915f9bed3a5a
lianghong <lianghong@wangdian.cn>
2020-07-13 17:35:04 +0800

    列表渲染，医师编辑，

--- website/admin/src/api/admin/doctorManage/doctorInfo/doctor.js
+++ website/admin/src/api/admin/doctorManage/doctorInfo/doctor.js
@@ -35,3 +35,20 @@ export function doctorList (data) {
     data: data
   })
 }
+
+// 编辑医师
+export function getDoctor (id) {
+  return service({
+    url: `/api/admin/doctor/info/${id}`,
+    method: 'get'
+  })
+}
+
+// 更新医师，停用，启用
+export function updateDoctor (data) {
+  return service({
+    url: '/api/admin/doctor/update',
+    method: 'post',
+    data: data
+  })
+}
