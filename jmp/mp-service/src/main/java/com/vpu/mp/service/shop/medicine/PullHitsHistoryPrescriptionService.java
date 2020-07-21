449f452afbec9e95c7d04d3c83b5ac5f91491662
zhaoxiaodong <zhaoxiaodong@huice.com>
2020-07-21 10:19:51 +0800

    添加service层统一拉取本地处方信息、病历信息

--- jmp/mp-service/src/main/java/com/vpu/mp/service/shop/medicine/PullHitsHistoryPrescriptionService.java
+++ jmp/mp-service/src/main/java/com/vpu/mp/service/shop/medicine/PullHitsHistoryPrescriptionService.java
@@ -15,7 +15,7 @@ import org.springframework.stereotype.Service;
  */
 
 @Service
-public class PullHitsHistoryPrescription {
+public class PullHitsHistoryPrescriptionService {
 
     @Autowired
     private MedicalHistoryService medicalHistoryService;
