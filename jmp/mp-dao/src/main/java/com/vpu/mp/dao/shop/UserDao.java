bbde6deaf89547b4562b3be786899f8b590b066f
zhaoxiaodong <zhaoxiaodong@huice.com>
2020-07-22 16:56:53 +0800

    医师身份认证接口

---
+++ jmp/mp-dao/src/main/java/com/vpu/mp/dao/shop/UserDao.java
@@ -0,0 +1,33 @@
+package com.vpu.mp.dao.shop;
+
+import com.vpu.mp.common.pojo.shop.table.UserDo;
+import com.vpu.mp.dao.foundation.base.ShopBaseDao;
+import org.springframework.stereotype.Repository;
+
+/**
+ * @Description
+ * @Author 赵晓东
+ * @Create 2020-07-22 16:09
+ **/
+
+import static com.vpu.mp.db.shop.Tables.*;
+
+@Repository
+public class UserDao extends ShopBaseDao {
+
+    /**
+     * 根据用户名和手机查询用户id，再讲该用户权限设置为医师
+     * @param name 用户姓名
+     * @param mobail 手机号
+     * @return UserDo
+     */
+    public UserDo updateDoctorAuth(String name, String mobail) {
+        UserDo userDo = db().select().from(USER).where(USER.USERNAME.eq(name)
+            .and(USER.MOBILE.eq(mobail))).fetchAnyInto(UserDo.class);
+         db().update(USER).set(USER.USER_TYPE, (byte) 1)
+            .where(USER.USERNAME.eq(name).and(USER.MOBILE.eq(mobail)))
+            .execute();
+         return userDo;
+    }
+
+}
