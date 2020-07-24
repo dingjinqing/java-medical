22ec963bc3df1ddd739700462d3d8d61e987b2a0
liuyang <liuyang01@wangdian.cn>
2020-07-24 16:03:58 +0800

    咨询页面请求

--- wxapp/jwxapp/pages2/patientChat/patientChat.js
+++ wxapp/jwxapp/pages2/patientChat/patientChat.js
@@ -10,7 +10,7 @@ global.wxPage({
   data: {
     time: '2020-07-23 13:35:01',
     page_name: 'saoyang',
-    send_message:''
+    send_message: ''
   },
 
   /**
@@ -19,9 +19,11 @@ global.wxPage({
   onLoad: function (options) {
     this.requsetMessage()
   },
-  getInputMessage(e){
+  getInputMessage(e) {
     let that = this
-    let {detail:message} = e 
+    let {
+      detail: message
+    } = e
     console.log(message)
     that.sendMessage(message)
   },
@@ -37,14 +39,14 @@ global.wxPage({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-
+    this.requsetMessage()
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-
+    clearInterval(this.timer)
   },
 
   /**
@@ -77,9 +79,9 @@ global.wxPage({
   sendMessage: function (message) {
     let that = this;
     if (message == '') return;
-    let imSessionItem ={
-      "type":0,
-      "message":""
+    let imSessionItem = {
+      "type": 0,
+      "message": ""
     }
     imSessionItem.message = message;
     util.api('/api/wxapp/im/session/send', res => {
@@ -89,14 +91,17 @@ global.wxPage({
       }
     }, {
       departmentId: 12,
-      patientId: 3,//患者
-      fromId: 2,//小程序用户
-      toId: 1,//to医生
-      imSessionItem:imSessionItem
+      patientId: 3, //患者
+      fromId: 2, //小程序用户
+      toId: 1, //to医生
+      imSessionItem: imSessionItem
     }, '', true);
   },
-  requsetMessage: function () {
-
+  requsetMessage () {
+    this.messageApi()
+    this.timer = setInterval(this.messageApi,15000)
+  },
+  messageApi () {
     util.api('/api/wxapp/im/session/pull', res => {
       console.log(res)
       if (res.error === 0) {
@@ -107,6 +112,6 @@ global.wxPage({
       patientId: 3,
       pullFromId: 2,
       selfId: 1
-    }, '', true);
+    }, '', false);
   }
 })
