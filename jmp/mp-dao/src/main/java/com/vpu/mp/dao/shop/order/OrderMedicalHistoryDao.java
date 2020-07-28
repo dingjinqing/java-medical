c403ee1b4c4904c098f52d967eb6fcbdff28dd5d
kongdecheng <kongdecheng@wangdian.cn>
2020-07-28 14:44:54 +0800

    订单历史诊断

---
+++ jmp/mp-dao/src/main/java/com/vpu/mp/dao/shop/order/OrderMedicalHistoryDao.java
@@ -0,0 +1,15 @@
+package com.vpu.mp.dao.shop.order;
+
+import com.vpu.mp.dao.foundation.base.ShopBaseDao;
+import org.springframework.stereotype.Repository;
+
+/**
+ * 订单历史诊断
+ * @author 孔德成
+ * @date 2020/7/28 14:43
+ */
+@Repository
+public class OrderMedicalHistoryDao extends ShopBaseDao {
+
+
+}
