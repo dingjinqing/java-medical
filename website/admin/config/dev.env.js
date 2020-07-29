09306b8fdb4eb2c1466b2f5d9f5e8f6dbf5462a6
lixiaobing <lixiaobing@wangdian.cn>
2020-07-29 11:51:13 +0800

    生产和测试环境修改前端调用api地址

--- website/admin/config/dev.env.js
+++ website/admin/config/dev.env.js
@@ -4,7 +4,7 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  // API_DOMAIN: '"medicaldev.weipubao.cn"',
-  API_DOMAIN: '"locahost:8086"',
+  // API_DOMAIN: '"locahost:8086"',
+  API_DOMAIN: '"medicaldev.weipubao.cn"',
   IMAGE_DOMAIN: '"medicaldevimg.weipubao.cn"'
 })
