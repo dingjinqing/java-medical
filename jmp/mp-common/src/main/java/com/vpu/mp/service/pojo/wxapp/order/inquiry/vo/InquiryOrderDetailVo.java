9d34dcbc2a2ec3b5f516c061c0c5ad5b1c7a3171
yangpengcheng <yangpengcheng@huice.com>
2020-07-30 17:02:11 +0800

    问诊订单详情增加图片url数组返回

--- jmp/mp-common/src/main/java/com/vpu/mp/service/pojo/wxapp/order/inquiry/vo/InquiryOrderDetailVo.java
+++ jmp/mp-common/src/main/java/com/vpu/mp/service/pojo/wxapp/order/inquiry/vo/InquiryOrderDetailVo.java
@@ -3,6 +3,8 @@ package com.vpu.mp.service.pojo.wxapp.order.inquiry.vo;
 import com.vpu.mp.common.pojo.shop.table.InquiryOrderDo;
 import lombok.Data;
 
+import java.util.List;
+
 /**
  * @author yangpengcheng
  * @date 2020/7/27
@@ -11,4 +13,5 @@ import lombok.Data;
 public class InquiryOrderDetailVo extends InquiryOrderDo {
     private String userName;
     private String userMobile;
+    private List<String> imgUrlList;
 }
