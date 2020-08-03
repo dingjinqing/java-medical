7e52b6a894ca0a5d8d8ff1a6ed74dc1c2de4c302
yangpengcheng <yangpengcheng@huice.com>
2020-08-03 18:35:30 +0800

    admin端咨询订单统计报表

--- jmp/mp-web/src/main/java/com/vpu/mp/controller/admin/AdminInquiryOrderController.java
+++ jmp/mp-web/src/main/java/com/vpu/mp/controller/admin/AdminInquiryOrderController.java
@@ -2,14 +2,20 @@ package com.vpu.mp.controller.admin;
 
 import com.vpu.mp.common.foundation.data.JsonResult;
 import com.vpu.mp.common.foundation.data.JsonResultCode;
+import com.vpu.mp.common.foundation.util.DateUtils;
 import com.vpu.mp.common.foundation.util.PageResult;
 import com.vpu.mp.common.pojo.shop.table.InquiryOrderDo;
 import com.vpu.mp.service.pojo.wxapp.order.inquiry.InquiryOrderListParam;
 import com.vpu.mp.service.pojo.wxapp.order.inquiry.InquiryOrderOnParam;
+import com.vpu.mp.service.pojo.wxapp.order.inquiry.InquiryOrderStatisticsParam;
 import com.vpu.mp.service.pojo.wxapp.order.inquiry.vo.InquiryOrderDetailVo;
+import com.vpu.mp.service.pojo.wxapp.order.inquiry.vo.InquiryOrderStatisticsVo;
+import org.apache.poi.ss.usermodel.Workbook;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * 问诊订单
@@ -44,4 +50,27 @@ public class AdminInquiryOrderController extends AdminBaseController{
         JsonResult result= shop().inquiryOrderService.refund(inquiryOrderOnParam);
         return result;
     }
+
+    /**
+     * 咨询订单统计报表
+     * @param param
+     * @return
+     */
+    @PostMapping("/api/admin/inquiry/order/statistics")
+    public JsonResult orderStatistics(@RequestBody InquiryOrderStatisticsParam param){
+        return success(shop().inquiryOrderService.orderStatistics(param));
+    }
+
+    /**
+     * 问诊订单统计报表导出
+     * @param param
+     * @param response
+     */
+    @PostMapping("/api/admin/inquiry/order/statistics/export")
+    public void orderStatisticsExport(@RequestBody InquiryOrderStatisticsParam param, HttpServletResponse response){
+        Workbook workbook=shop().inquiryOrderService.orderStatisticsExport(param,getLang());
+        String fileName = InquiryOrderStatisticsVo.EXPORT_FILE_NAME+ DateUtils.dateFormat(DateUtils.DATE_FORMAT_SHORT);
+        export2Excel(workbook,fileName,response);
+    }
+
 }
