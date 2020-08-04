788c1f3c0a4139327d4c1324ae512633899c15f5
liuyang <liuyang01@wangdian.cn>
2020-08-04 09:31:57 +0800

    查看大图

--- wxapp/jwxapp/pages2/doctorChat/doctorChat.js
+++ wxapp/jwxapp/pages2/doctorChat/doctorChat.js
@@ -257,5 +257,11 @@ global.wxPage({
         sessionId:this.data.targetUserInfo.id
       })
     }) 
+  },
+  viewImage(e){
+    let urls = [e.currentTarget.dataset.urls]
+    wx.previewImage({
+      urls
+    })
   }
 })
