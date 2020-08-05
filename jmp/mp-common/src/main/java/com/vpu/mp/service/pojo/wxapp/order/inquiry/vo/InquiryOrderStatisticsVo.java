566ea1299168462969f95be9b6433bafa89afbde
yangpengcheng <yangpengcheng@huice.com>
2020-08-05 09:35:12 +0800

    咨询订单统计报表导出

--- jmp/mp-common/src/main/java/com/vpu/mp/service/pojo/wxapp/order/inquiry/vo/InquiryOrderStatisticsVo.java
+++ jmp/mp-common/src/main/java/com/vpu/mp/service/pojo/wxapp/order/inquiry/vo/InquiryOrderStatisticsVo.java
@@ -2,6 +2,7 @@ package com.vpu.mp.service.pojo.wxapp.order.inquiry.vo;
 
 import com.vpu.mp.common.foundation.excel.annotation.ExcelColumn;
 import com.vpu.mp.common.foundation.excel.annotation.ExcelIgnore;
+import com.vpu.mp.common.foundation.excel.annotation.ExcelSheet;
 import lombok.Data;
 
 import java.math.BigDecimal;
@@ -12,6 +13,7 @@ import java.sql.Timestamp;
  * @date 2020/8/3
  **/
 @Data
+@ExcelSheet
 public class InquiryOrderStatisticsVo {
     @ExcelIgnore
     public static final String EXPORT_FILE_NAME="咨询统计导出-";
@@ -22,24 +24,24 @@ public class InquiryOrderStatisticsVo {
     private Integer doctorId;
     @ExcelIgnore
     private Integer departmentId;
-    @ExcelColumn(columnName = "医生姓名", columnIndex = 0)
+    @ExcelColumn(columnName = "医生姓名", columnIndex = 1)
     private String doctorName;
-    @ExcelColumn(columnName = "科室", columnIndex = 0)
+    @ExcelColumn(columnName = "科室", columnIndex = 2)
     private String departmentName;
     /**
      * 咨询单数
      */
-    @ExcelColumn(columnName = "咨询单数", columnIndex = 0)
+    @ExcelColumn(columnName = "咨询单数", columnIndex = 3)
     private String amount;
     /**
      * 咨询单次价格
      */
-    @ExcelColumn(columnName = "咨询单次价格", columnIndex = 0)
+    @ExcelColumn(columnName = "咨询单次价格", columnIndex = 4)
     private BigDecimal oncePrice;
     /**
      * 咨询总金额
      */
-    @ExcelColumn(columnName = "咨询总金额", columnIndex = 0)
+    @ExcelColumn(columnName = "咨询总金额", columnIndex = 5)
     private BigDecimal amountPrice;
 
 }
