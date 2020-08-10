c2da55508bc0b6baf945623d0d550ef5e0ea01b6
kongdecheng <kongdecheng@wangdian.cn>
2020-08-10 17:26:10 +0800

    处方管理处方列表

--- jmp/mp-common/src/main/java/com/vpu/mp/service/pojo/shop/prescription/PrescriptionListParam.java
+++ jmp/mp-common/src/main/java/com/vpu/mp/service/pojo/shop/prescription/PrescriptionListParam.java
@@ -1,26 +1,48 @@
 package com.vpu.mp.service.pojo.shop.prescription;
 
 import com.vpu.mp.common.pojo.shop.base.BasePageParam;
-import com.vpu.mp.service.pojo.shop.patient.UserPatientParam;
+import lombok.Data;
 
-import javax.validation.constraints.NotNull;
-import java.util.List;
+import java.sql.Timestamp;
 
 /**
  * @author 孔德成
  * @date 2020/7/6 9:36
  */
+@Data
 public class PrescriptionListParam extends BasePageParam {
 
     /**
      * 患者id
      */
-    @NotNull
-    private Integer patientId;
+    private String patientName;
+    /**
+     * 电话
+     */
+    private String patientMobile;
+    /**
+     * 处方号
+     */
+    private String prescriptionCode;
+    /**
+     * 科室名称
+     */
+    private String departmentName;
+    /**
+     * 医师名称
+     */
+    private String doctorName;
+    /**
+     * 诊断名称
+     */
+    private String    diagnosisName;
+    /**
+     * 就诊时间-开始
+     */
+    private Timestamp diagnoseStartTime;
+    /**
+     * 就诊时间-结束
+     */
+    private Timestamp diagnoseEndTime;
 
-    private Integer userId;
-
-    private UserPatientParam userPatientParam;
-
-    private List<String> prescriptionNos;
 }
