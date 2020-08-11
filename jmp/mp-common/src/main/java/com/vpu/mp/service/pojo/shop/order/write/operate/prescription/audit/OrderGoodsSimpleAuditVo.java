085ce1a259ccd16d48eba07e410ed758fd1847bc
kongdecheng <kongdecheng@wangdian.cn>
2020-08-11 09:37:31 +0800

    审核处方增加商品规格id

--- jmp/mp-common/src/main/java/com/vpu/mp/service/pojo/shop/order/write/operate/prescription/audit/OrderGoodsSimpleAuditVo.java
+++ jmp/mp-common/src/main/java/com/vpu/mp/service/pojo/shop/order/write/operate/prescription/audit/OrderGoodsSimpleAuditVo.java
@@ -19,6 +19,10 @@ public class OrderGoodsSimpleAuditVo {
      */
     private Integer goodsId;
     /**
+     * 商品规格id
+     */
+    private Integer productId;
+    /**
      * 商品数量
      */
     private Integer goodsNumber;
