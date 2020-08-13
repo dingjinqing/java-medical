42a2f0be6f3d632f41d42e4e514078e118d03f67
lianghong <lianghong@wangdian.cn>
2020-08-13 17:42:45 +0800

    功能

--- wxapp/jwxapp/pages2/doctorIndex/doctorIndex.js
+++ wxapp/jwxapp/pages2/doctorIndex/doctorIndex.js
@@ -89,6 +89,7 @@ global.wxPage({
       doctorName: this.data.name,
       mobile: this.data.mobile,
       hospitalCode: this.data.hosCode,
+      mobileCheckCode: this.data.mobileCheckCode,
       userId: util.getCache('user_id')
     })
   },
@@ -131,7 +132,7 @@ global.wxPage({
       util.showModal("提示", "请输入正确的手机号！");
       return false;
     }
-    util.api('/api/wxapp/user/patient/send/sms', res => {
+    util.api('/api/wxapp/doctor/send/check/code', res => {
       if (res.error === 0) {
         const TIME_COUNT = 60;
         this.setData({
