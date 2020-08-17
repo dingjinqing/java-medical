6a321cc4a7f860181830cedfb51199bdb43b6cb0
yangpengcheng <yangpengcheng@huice.com>
2020-08-17 11:50:46 +0800

    问诊订单主库同步

--- jmp/mp-web/src/main/java/com/vpu/mp/controller/system/SystemInquiryOrderController.java
+++ jmp/mp-web/src/main/java/com/vpu/mp/controller/system/SystemInquiryOrderController.java
@@ -1,11 +1,13 @@
 package com.vpu.mp.controller.system;
 
 import com.vpu.mp.common.foundation.data.JsonResult;
+import com.vpu.mp.common.foundation.util.DateUtils;
 import com.vpu.mp.service.pojo.saas.order.MainInquiryOrderStatisticsParam;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.vpu.mp.service.pojo.wxapp.order.inquiry.vo.InquiryOrderStatisticsVo;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * @author yangpengcheng
@@ -22,5 +24,32 @@ public class SystemInquiryOrderController  extends SystemBaseController{
     @PostMapping("/api/system/inquiry/order/statistics")
     public JsonResult list(@RequestBody MainInquiryOrderStatisticsParam param){
         return success(saas.mainInquiryOrderService.orderStatistics(param));
+    }
+
+    /**
+     * 报表导出
+     * @param param
+     * @param response
+     */
+    @PostMapping("/api/system/inquiry/order/statistics/export")
+    public void orderStatisticsExport(@RequestBody MainInquiryOrderStatisticsParam param, HttpServletResponse response){
+        Workbook workbook=saas.mainInquiryOrderService.orderStatisticsExport(param,getLang());
+        String fileName = InquiryOrderStatisticsVo.EXPORT_FILE_NAME+ DateUtils.dateFormat(DateUtils.DATE_FORMAT_SHORT);
+        export2Excel(workbook,fileName,response);
+    }
+
+    /**
+     * 咨询报表总数total查询
+     * @param param
+     * @return
+     */
+    @PostMapping("/api/system/inquiry/order/statistics/total")
+    public JsonResult orderStatisticsTotal(@RequestBody MainInquiryOrderStatisticsParam param){
+        return success(saas.mainInquiryOrderService.orderStatisticsTotal(param));
+    }
+
+    @PostMapping("/api/system/inquiry/order/test/{shopId}")
+    public void testMapping(@PathVariable Integer shopId){
+        saas.getShopApp(shopId).shopTaskService.tableTaskService.inquiryOrderSynchronize(shopId);
     }
 }
