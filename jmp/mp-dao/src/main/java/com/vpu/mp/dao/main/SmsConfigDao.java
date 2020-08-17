355f6a68dbebe64ca11cb61de553e3948ab2be6c
zhaoxiaodong <zhaoxiaodong@huice.com>
2020-08-17 14:59:44 +0800

    admin端短信配置

---
+++ jmp/mp-dao/src/main/java/com/vpu/mp/dao/main/SmsConfigDao.java
@@ -0,0 +1,44 @@
+package com.vpu.mp.dao.main;
+
+import com.vpu.mp.dao.foundation.base.MainBaseDao;
+import com.vpu.mp.service.pojo.shop.sms.SmsConfigParam;
+import com.vpu.mp.service.pojo.shop.sms.SmsConfigVo;
+import lombok.Data;
+import org.springframework.stereotype.Repository;
+
+import static com.vpu.mp.db.main.Tables.SHOP_SMS_CONFIG;
+
+/**
+ * @author 赵晓东
+ * @description
+ * @create 2020-08-17 14:13
+ **/
+
+@Repository
+public class SmsConfigDao extends MainBaseDao {
+
+    /**
+     * 修改店铺短信发送配置
+     * @param smsConfigParam 店铺配置参数
+     */
+    public void updateSmsConfig(SmsConfigParam smsConfigParam) {
+        db().update(SHOP_SMS_CONFIG)
+            .set(SHOP_SMS_CONFIG.USER_CHECK_CODE_NUM, smsConfigParam.getUserCheckCodeNum())
+            .set(SHOP_SMS_CONFIG.PATIENT_CHECK_CODE_NUM, smsConfigParam.getPatientCheckCodeNum())
+            .set(SHOP_SMS_CONFIG.MARKETING_NUM, smsConfigParam.getMarketingNum())
+            .set(SHOP_SMS_CONFIG.INDUSTRY_NUM, smsConfigParam.getIndustryNum())
+            .where(SHOP_SMS_CONFIG.SHOP_ID.eq(smsConfigParam.getShopId())).execute();
+    }
+
+    /**
+     * 打开短信配置页面时显示之前配置内容
+     * @param shopId 店铺id
+     * @return SmsConfigVo
+     */
+    public SmsConfigVo selectSmsConfig(Integer shopId) {
+        return db().select().from(SHOP_SMS_CONFIG)
+            .where(SHOP_SMS_CONFIG.SHOP_ID.eq(shopId))
+            .fetchAnyInto(SmsConfigVo.class);
+    }
+
+}
--- jmp/mp-service/src/main/resources/db/main/db_main.sql
+++ jmp/mp-service/src/main/resources/db/main/db_main.sql
@@ -1955,4 +1955,18 @@ CREATE TABLE `b2c_user_summary_trend` (
   KEY `ref_type` (`ref_date`,`type`)
 );
 
+-- 店铺短信配置表
+create table `b2c_shop_sms_config` (
+    `id` int(11) not null auto_increment,
+    `shop_id` int(11) not null comment '店铺id',
+    `user_check_code_num` int(11) not null default 15 comment '每个用户可发送验证码数量',
+    `patient_check_code_num` int(11) not null default 15 comment '每个患者可发送验证码数量',
+    `marketing_num` int(11) not null default 2000 comment '每天可发送营销短信数量',
+    `industry_num` int(11) not null default 2000 comment '每天可发送行业短信数量',
+    `is_delete`     tinyint(1)   not null default '0',
+    `create_time`   timestamp    not null default current_timestamp,
+    `update_time`   timestamp    not null default current_timestamp on update current_timestamp comment '最后修改时间',
+    primary key (`id`)
+)comment = '店铺短信配置表';
+
 
