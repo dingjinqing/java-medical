371f8b660f74d8f9c58008b5c376330df31afacb
yangpengcheng <yangpengcheng@huice.com>
2020-08-21 17:15:41 +0800

    问诊接诊，超时退款 推送消息提醒

--- jmp/mp-service/src/main/java/com/vpu/mp/service/shop/maptemplatesend/MapTemplateSendService.java
+++ jmp/mp-service/src/main/java/com/vpu/mp/service/shop/maptemplatesend/MapTemplateSendService.java
@@ -129,7 +129,7 @@ public class MapTemplateSendService extends ShopBaseService {
             {param.getDiseaseDetail()},
             {param.getDoctorName()},
             {param.getRemark()},
-            {param.getDiseaseDetail()},
+            {param.getDepartmentName()},
         };
 
         MaSubscribeData data = MaSubscribeData.builder().data47(maData).build();
