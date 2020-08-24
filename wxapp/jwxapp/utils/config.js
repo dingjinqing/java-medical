c1835392fc5f526f1c9ff49e16c084e38ca74fd6
yangpengcheng <yangpengcheng@huice.com>
2020-08-24 15:45:40 +0800

    医师返利配置

--- wxapp/jwxapp/utils/config.js
+++ wxapp/jwxapp/utils/config.js
@@ -1,2 +1,6 @@
 var config = wx.getExtConfigSync();
+// config.main_host = 'localhost:8086'
+// config.image_host= 'medicaldevimg.weipubao.cn'
+// config.shop_id = '965554'
+// config.schema="http"
 module.exports = config;
