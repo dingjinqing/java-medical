031159c40c2f51a0e7aec38562746ec19bba8155
lixiaobing <lixiaobing@wangdian.cn>
2020-08-25 10:38:37 +0800

    sql项目根据p3c修改

--- sql/src/main/java/com/vpu/sql/config/source/ShopDataSource.java
+++ sql/src/main/java/com/vpu/sql/config/source/ShopDataSource.java
@@ -7,12 +7,23 @@ import java.nio.file.Path;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * @author luguangyao
+ */
 public interface ShopDataSource {
+    /**
+     * @param sql
+     */
     void execute(String sql);
 
-
+    /**
+     * @param path
+     */
     void execute(Path path);
 
-
+    /**
+     * @param configs
+     * @return
+     */
     Map<String,DataSource> getShopDataSource(List<DbConfig> configs);
 }
