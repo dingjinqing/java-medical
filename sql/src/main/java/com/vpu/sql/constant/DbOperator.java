cdaf298421edfa08b4185aacf7c4885451809a5c
lixiaobing <lixiaobing@wangdian.cn>
2020-08-25 14:11:15 +0800

    sql项目添加注释和关键日志

---
+++ sql/src/main/java/com/vpu/sql/constant/DbOperator.java
@@ -0,0 +1,33 @@
+package com.vpu.sql.constant;
+
+
+/**
+ * sql操作
+ * @author 卢光耀
+ * @date 2020-01-06 14:03
+ */
+public enum DbOperator {
+    /**
+     * 删除，创建，修改结构，修改数据，新增数据，截断
+     */
+    DROP("drop"), CREATE("create"), ALTER("alter"), UPDATE("update"), INSERT("insert"), TRUNCATE("truncate");
+
+    private String operator;
+
+    DbOperator(String operator) {
+        this.operator = operator;
+    }
+
+    public String getOperator() {
+        return this.operator;
+    }
+
+    public static DbOperator getDBOperator(String operator) {
+        for (DbOperator dbOperator : values()) {
+            if (dbOperator.getOperator().equals(operator)) {
+                return dbOperator;
+            }
+        }
+        return null;
+    }
+}
