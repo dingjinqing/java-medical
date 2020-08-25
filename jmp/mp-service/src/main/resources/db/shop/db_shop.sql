ecbcc3d3cc52b4d4f9d2a0992392ad17954923ed
lixiaobing <lixiaobing@wangdian.cn>
2020-08-25 16:36:08 +0800

    sql语句错误解决

--- jmp/mp-service/src/main/resources/db/shop/db_shop.sql
+++ jmp/mp-service/src/main/resources/db/shop/db_shop.sql
@@ -5514,24 +5514,24 @@ create table `b2c_user_doctor_attention`(
 )comment ='用户关注医师';
 
 -- 医师评价和打分
-create table `b2c_doctor_comment`(
-    `id`   int(11)      not null auto_increment,
-    `user_id` int(11) not null comment '用户id',
-    `user_name` varchar(60) not null default "" comment '用户昵称',
-    `patient_id` int(11) not null comment '患者id',
-    `doctor_id` int(11) not null comment '医师id',
-    `order_sn` varchar(20) not null default "" comment '订单编号',
-    `im_session_id` int(11)  not null comment '会话id',
-    `stars` tinyint(1) not null default 5 comment '评价星级1~5',
-    `is_anonymou` tinyint(1) not null default 0 comment '匿名状态 0.未匿名；1.匿名',
-    `tag` varchar(100) default '' comment '评价标签',
-    `comm_note` varchar(1000) not null default "" comment '评论内容',
-    `audit_status` tinyint(1) not null default 0 comment '0:未审批,1:审批通过,2:审批未通过',
-    `is_delete`     tinyint(1)   not null default '0',
-    `create_time`   timestamp    not null default current_timestamp,
-    `update_time`   timestamp    not null default current_timestamp on update current_timestamp comment '最后修改时间',
-    primary key(`id`)
-)comment ='医师评价和打分';
+create table `b2c_doctor_comment` (
+    `id` int(11) NOT NULL AUTO_INCREMENT,
+    `user_id` int(11) NOT NULL COMMENT '用户id',
+    `user_name` varchar(60)  NOT NULL DEFAULT '' COMMENT '用户昵称',
+    `patient_id` int(11) NOT NULL COMMENT '患者id',
+    `doctor_id` int(11) NOT NULL COMMENT '医师id',
+    `order_sn` varchar(20)  NOT NULL DEFAULT '' COMMENT '订单编号',
+    `im_session_id` int(11) NOT NULL COMMENT '会话id',
+    `stars` tinyint(1) NOT NULL DEFAULT '5' COMMENT '评价星级1~5',
+    `is_anonymou` tinyint(1) NOT NULL DEFAULT '0' COMMENT '匿名状态 0.未匿名；1.匿名',
+    `tag` varchar(100)  DEFAULT '' COMMENT '评价标签',
+    `comm_note` varchar(1000)  NOT NULL DEFAULT '' COMMENT '评论内容',
+    `audit_status` tinyint(1) NOT NULL DEFAULT '0' COMMENT '0:未审批,1:审批通过,2:审批未通过',
+    `is_delete` tinyint(1) NOT NULL DEFAULT '0',
+    `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '最后修改时间',
+    PRIMARY KEY (`id`)
+) COMMENT='医师评价和打分';
 
 -- 医师评价回复
 create table `b2c_doctor_comment_reply`(
