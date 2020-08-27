a5a3470b5d868bf85714319af4be8d0894ae04c2
kongdecheng <kongdecheng@wangdian.cn>
2020-08-27 17:49:36 +0800

    p3c

--- jmp/mp-background/src/main/java/com/vpu/mp/schedule/OrderScheduleTask.java
+++ jmp/mp-background/src/main/java/com/vpu/mp/schedule/OrderScheduleTask.java
@@ -1,8 +1,6 @@
 package com.vpu.mp.schedule;
 
-import com.vpu.mp.common.foundation.util.Util;
 import com.vpu.mp.db.main.tables.records.ShopRecord;
-import com.vpu.mp.service.foundation.jedis.JedisKeyConstant;
 import com.vpu.mp.service.foundation.jedis.JedisManager;
 import com.vpu.mp.service.saas.SaasApplication;
 import org.jooq.Result;
@@ -33,22 +31,25 @@ public class OrderScheduleTask {
      * 拼团订单处理（自动成团、退款等）
      * 每一分钟执行一次
      */
-    @Scheduled(cron = "0 */1 * * * ?")
+    /**      *
+
     public void monitorGroupBuyOrders() {
-        Result<ShopRecord> result = saas.shop.getAll();
-        result.forEach((r)->{
-            String uuid = Util.randomId();
-            String key = JedisKeyConstant.TASK_JOB_LOCK_ORDER_GROUP_BUY + r.getShopId();
-            saas.getShopApp(r.getShopId()).groupIntegration.updateState();
-            //订单处理时间可能较长，加锁防止重入
-            if( jedisManager.addLock(key,uuid,1000*90) ){
-                saas.getShopApp(r.getShopId()).shopTaskService.groupBuyTaskService.monitorOrder();
-                /** 处理拼团抽奖*/
-                saas.getShopApp(r.getShopId()).groupDraw.groupDrawUser.dealOpenGroupDraw();
-                jedisManager.releaseLock(key,uuid);
-            }
-        });
-    }
+     *Result<ShopRecord> result = saas.shop.getAll();
+     *result.forEach((r) -> {
+     *String uuid = Util.randomId();
+     *String key = JedisKeyConstant.TASK_JOB_LOCK_ORDER_GROUP_BUY + r.getShopId();
+     *saas.getShopApp(r.getShopId()).groupIntegration.updateState();
+     *             //订单处理时间可能较长，加锁防止重入
+     *if (jedisManager.addLock(key, uuid, 1000 * 90)) {
+     *saas.getShopApp(r.getShopId()).shopTaskService.groupBuyTaskService.monitorOrder();
+     *
+     *saas.getShopApp(r.getShopId()).groupDraw.groupDrawUser.dealOpenGroupDraw();
+     *jedisManager.releaseLock(key, uuid);
+     *}
+     *});
+     *}
+      */
+
 
     /**
      * 预售订单自动关闭
