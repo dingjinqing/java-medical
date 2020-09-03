fa64d9fb2d1ac81df8773ddba266a28da22c9592
zhaoxiaodong <zhaoxiaodong@huice.com>
2020-09-03 19:06:48 +0800

    修改埋点表SQL报错

--- jmp/mp-sql/src/main/resources/migration/shop/3.4/V3.4.3__Shop_Modify_Anchor_Points.sql
+++ jmp/mp-sql/src/main/resources/migration/shop/3.4/V3.4.3__Shop_Modify_Anchor_Points.sql
@@ -1,4 +1,4 @@
-ALTER TABLE `b2c_anchor_points` MODIFY COLUMN `platform` varchar(20) NOT NULL DEFAULT '' COMMENT '平台  wxapp admin' AFTER `module`,
+ALTER TABLE `b2c_anchor_points` MODIFY COLUMN `platform` varchar(20) NOT NULL DEFAULT '' COMMENT '平台  wxapp admin' AFTER `module`;
 ALTER TABLE `b2c_anchor_points` MODIFY COLUMN `device` varchar(20) NOT NULL DEFAULT '' COMMENT '设备  android  ios   pc' AFTER `platform`;
 -- 分销员推广中心收藏商品表
 CREATE TABLE IF NOT EXISTS `b2c_distributor_collection` (
