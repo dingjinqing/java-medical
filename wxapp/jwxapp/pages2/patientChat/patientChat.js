056de27aa50a96b025bb469dc56f2d3f1c5e42d0
xuexiangzhong <xuexiangzhong@wangdian.cn>
2020-09-08 09:32:14 +0800

    订单

--- wxapp/jwxapp/pages2/patientChat/patientChat.js
+++ wxapp/jwxapp/pages2/patientChat/patientChat.js
@@ -336,7 +336,7 @@ global.wxPage({
             that.setData({
               sessionId: res.content
             })
-            if (that.data.firstLoad) await that.requestDetail(orderSn)
+            if (that.data.firstLoad) await that.requestDetail(this.data.orderSn)
             resolve(res)
           }
         }, {
