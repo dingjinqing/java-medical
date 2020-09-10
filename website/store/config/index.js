3a6ce9a6256dd9bec8622bc4989f9b7b9a245d08
liuyang <liuyang01@wangdian.cn>
2020-09-10 16:25:26 +0800

    打包路由

--- website/store/config/index.js
+++ website/store/config/index.js
@@ -1,18 +1,18 @@
-"use strict";
+'use strict'
 // Template version: 1.3.1
 // see http://vuejs-templates.github.io/webpack for documentation.
 
-const path = require("path");
+const path = require('path')
 
-const devenv = require("../config/dev.env");
+const devenv = require('../config/dev.env')
 
 module.exports = {
   dev: {
     // Paths
-    assetsSubDirectory: "static",
-    assetsPublicPath: "/admin/",
+    assetsSubDirectory: 'static',
+    assetsPublicPath: '/store/',
     // Various Dev Server settings
-    host: "localhost", // can be overwritten by process.env.HOST
+    host: 'localhost', // can be overwritten by process.env.HOST
     port: 8080, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     autoOpenBrowser: false,
     errorOverlay: true,
@@ -28,11 +28,11 @@ module.exports = {
     showEslintErrorsInOverlay: false,
 
     /**
-     * Source Maps
-     */
+		 * Source Maps
+		 */
 
     // https://webpack.js.org/configuration/devtool/#development
-    devtool: "cheap-module-eval-source-map",
+    devtool: 'cheap-module-eval-source-map',
 
     // If you have problems debugging vue-files in devtools,
     // set this to false - it *may* help
@@ -44,27 +44,27 @@ module.exports = {
 
   build: {
     // Template for index.html
-    index: path.resolve(__dirname, "../dist/index.html"),
+    index: path.resolve(__dirname, '../dist/index.html'),
 
     // Paths
-    assetsRoot: path.resolve(__dirname, "../dist"),
-    assetsSubDirectory: "static",
-    assetsPublicPath: "/admin/",
+    assetsRoot: path.resolve(__dirname, '../dist'),
+    assetsSubDirectory: 'static',
+    assetsPublicPath: '/store/',
 
     /**
-     * Source Maps
-     */
+		 * Source Maps
+		 */
 
     productionSourceMap: false,
     // https://webpack.js.org/configuration/devtool/#production
-    devtool: "#source-map",
+    devtool: '#source-map',
 
     // Gzip off by default as many popular static hosts such as
     // Surge or Netlify already gzip all static assets for you.
     // Before setting to `true`, make sure to:
     // npm install --save-dev compression-webpack-plugin
     productionGzip: false,
-    productionGzipExtensions: ["js", "css"],
+    productionGzipExtensions: ['js', 'css'],
 
     // Run the build command with an extra argument to
     // View the bundle analyzer report after build finishes:
@@ -72,4 +72,4 @@ module.exports = {
     // Set to `true` or `false` to always turn it on or off
     bundleAnalyzerReport: process.env.npm_config_report
   }
-};
+}
