cac86760a9d257defbbf72b04e9a53bf228b8a2c
kongdecheng <kongdecheng@wangdian.cn>
2020-09-11 17:39:54 +0800

    埋点报表修改

--- jmp/mp-dao/src/main/java/com/vpu/mp/dao/shop/anchor/AnchorPointsDao.java
+++ jmp/mp-dao/src/main/java/com/vpu/mp/dao/shop/anchor/AnchorPointsDao.java
@@ -10,9 +10,9 @@ import com.vpu.mp.service.pojo.shop.anchor.AnchorPointsReportVo;
 import org.jooq.Record;
 import org.jooq.SelectJoinStep;
 import org.jooq.impl.DSL;
+import org.jooq.impl.SQLDataType;
 import org.springframework.stereotype.Repository;
 
-import java.math.BigDecimal;
 import java.sql.Date;
 import java.util.List;
 import java.util.Map;
@@ -27,6 +27,12 @@ import static org.jooq.impl.DSL.date;
 @Repository
 public class AnchorPointsDao extends ShopBaseDao {
 
+
+    public final static String  NAME ="name";
+    public final static String  MONEY ="money";
+    public final static String  COUNT ="count";
+    public final static String  DATE ="date";
+    public final static String  VALUE ="value";
 
     public void save(AnchorPointsDo anchorPointsDo) {
         AnchorPointsRecord anchorPointsRecord = db().newRecord(ANCHOR_POINTS, anchorPointsDo);
@@ -68,40 +74,56 @@ public class AnchorPointsDao extends ShopBaseDao {
     }
 
     /**
-     * 计数报表
+     * 计数报表 日期
      * @param param
      * @return
      */
     public Map<Date, List<AnchorPointsReportVo>> countReport(AnchorPointsListParam param) {
-        return db().select(DSL.date(ANCHOR_POINTS.CREATE_TIME).as("date"), ANCHOR_POINTS.EVENT, ANCHOR_POINTS.KEY,
-                ANCHOR_POINTS.VALUE, DSL.count(ANCHOR_POINTS.ID).as("count"))
+        return db().select(DSL.date(ANCHOR_POINTS.CREATE_TIME).as(DATE), ANCHOR_POINTS.EVENT, ANCHOR_POINTS.KEY,
+                ANCHOR_POINTS.VALUE, DSL.count(ANCHOR_POINTS.ID).as(COUNT))
                 .from(ANCHOR_POINTS)
                 .where(ANCHOR_POINTS.EVENT.eq(param.getEvent()))
                 .and(ANCHOR_POINTS.KEY.eq(param.getKey()))
                 .and(ANCHOR_POINTS.CREATE_TIME.between(param.getStartTime(), param.getEndTime()))
                 .groupBy(date(ANCHOR_POINTS.CREATE_TIME), ANCHOR_POINTS.EVENT, ANCHOR_POINTS.KEY, ANCHOR_POINTS.VALUE)
-                .fetchGroups(date(ANCHOR_POINTS.CREATE_TIME).as("date"), AnchorPointsReportVo.class);
+                .fetchGroups(date(ANCHOR_POINTS.CREATE_TIME).as(DATE), AnchorPointsReportVo.class);
     }
 
     /**
-     *金额报表
+     * 计数报表 设备 日期
+     * @param param
+     * @return
      */
-    public Map<Date, List<AnchorPointsReportVo>>   moneyDateReport(AnchorPointsListParam param){
-        return  db().select(date(ANCHOR_POINTS.CREATE_TIME).as("date"), ANCHOR_POINTS.EVENT, ANCHOR_POINTS.KEY,
-                ANCHOR_POINTS.DEVICE,DSL.sum(DSL.cast(ANCHOR_POINTS.VALUE, BigDecimal.class)).as("money"))
+    public Map<Date, List<AnchorPointsReportVo>> countDateDeviceReport(AnchorPointsListParam param) {
+        return db().select(DSL.date(ANCHOR_POINTS.CREATE_TIME).as(DATE), ANCHOR_POINTS.EVENT, ANCHOR_POINTS.KEY,
+                ANCHOR_POINTS.DEVICE, DSL.count(ANCHOR_POINTS.ID).as(COUNT))
                 .from(ANCHOR_POINTS)
                 .where(ANCHOR_POINTS.EVENT.eq(param.getEvent()))
                 .and(ANCHOR_POINTS.KEY.eq(param.getKey()))
                 .and(ANCHOR_POINTS.CREATE_TIME.between(param.getStartTime(), param.getEndTime()))
                 .groupBy(date(ANCHOR_POINTS.CREATE_TIME), ANCHOR_POINTS.EVENT, ANCHOR_POINTS.KEY, ANCHOR_POINTS.DEVICE)
-                .fetchGroups(date(ANCHOR_POINTS.CREATE_TIME).as("date"), AnchorPointsReportVo.class);
+                .fetchGroups(date(ANCHOR_POINTS.CREATE_TIME).as(DATE), AnchorPointsReportVo.class);
+    }
+
+    /**
+     *金额报表 设备 日期
+     */
+    public Map<Date, List<AnchorPointsReportVo>>   moneyDateDeviceReport(AnchorPointsListParam param){
+        return  db().select(date(ANCHOR_POINTS.CREATE_TIME).as(DATE), ANCHOR_POINTS.EVENT, ANCHOR_POINTS.KEY,
+                ANCHOR_POINTS.DEVICE,DSL.sum(ANCHOR_POINTS.VALUE.cast(SQLDataType.DECIMAL(10,2))).as(MONEY))
+                .from(ANCHOR_POINTS)
+                .where(ANCHOR_POINTS.EVENT.eq(param.getEvent()))
+                .and(ANCHOR_POINTS.KEY.eq(param.getKey()))
+                .and(ANCHOR_POINTS.CREATE_TIME.between(param.getStartTime(), param.getEndTime()))
+                .groupBy(date(ANCHOR_POINTS.CREATE_TIME), ANCHOR_POINTS.EVENT, ANCHOR_POINTS.KEY, ANCHOR_POINTS.DEVICE)
+                .fetchGroups(date(ANCHOR_POINTS.CREATE_TIME).as(DATE), AnchorPointsReportVo.class);
     }
     /**
-     *金额报表
+     *金额报表 设备
      */
-    public List<AnchorPointsReportVo>  moneyReport(AnchorPointsListParam param){
-        return  db().select(ANCHOR_POINTS.EVENT, ANCHOR_POINTS.KEY, ANCHOR_POINTS.DEVICE
-                ,DSL.sum(DSL.cast(ANCHOR_POINTS.VALUE, BigDecimal.class)).as("money"))
+    public List<AnchorPointsReportVo>  moneyDeviceReport(AnchorPointsListParam param){
+        return  db().select(ANCHOR_POINTS.EVENT.as(NAME), ANCHOR_POINTS.KEY, ANCHOR_POINTS.DEVICE
+                ,DSL.sum(ANCHOR_POINTS.VALUE.cast(SQLDataType.DECIMAL(10,2))).as(VALUE))
                 .from(ANCHOR_POINTS)
                 .where(ANCHOR_POINTS.EVENT.eq(param.getEvent()))
                 .and(ANCHOR_POINTS.KEY.eq(param.getKey()))
