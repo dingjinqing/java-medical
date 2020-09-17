d3bb9146ba4c75eb5a9a54933b0d35608e6995fd
liuyang <liuyang01@wangdian.cn>
2020-09-17 17:29:41 +0800

    店员订单列表

---
+++ wxapp/jwxapp/pages3/clerkOrderList/clerkOrderList.js
@@ -0,0 +1,77 @@
+let plugin = requirePlugin('routePlan');
+global.wxPage({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+  viewMap(){
+    let key = 'C3JBZ-4UXEJ-PCLFP-KNWNV-UDVUT-IJFES';  //使用在腾讯位置服务申请的key
+    let referer = '旺店精选';   //调用插件的app的名称
+    let endPoint = JSON.stringify({  //终点
+        'name': '吉野家(北京西站北口店)',
+        'latitude': 39.89631551,
+        'longitude': 116.323459711
+    });
+    wx.navigateTo({
+        url: 'plugin://routePlan/index?key=' + key + '&referer=' + referer + '&endPoint=' + endPoint
+    });
+  },
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
