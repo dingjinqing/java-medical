278d20c936ac87f97dcef17c3e0c0773e8fa2afd
kongdecheng <kongdecheng@wangdian.cn>
2020-09-27 14:52:39 +0800

    医师详情页跳转修改

--- website/admin/src/view/admin/index/leftNavComponents/doctor_manger/comment/commentList.vue
+++ website/admin/src/view/admin/index/leftNavComponents/doctor_manger/comment/commentList.vue
@@ -391,7 +391,7 @@ export default {
     handleSeeDoctor (code) {
       console.log(this.$router)
       let newpage = this.$router.resolve({
-        name: 'addDoctor'
+        name: 'doctor_detail'
       })
       newpage.href = newpage.href + '?id=' + code
       console.log(newpage.href)
