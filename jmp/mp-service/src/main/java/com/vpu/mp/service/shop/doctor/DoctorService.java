eaf15a242c8b412dbd4ab3cfb406680d9f8676a2
zhaoxiaodong <zhaoxiaodong@huice.com>
2020-10-15 18:53:10 +0800

    修改医师问诊费用字段更正

--- jmp/mp-service/src/main/java/com/vpu/mp/service/shop/doctor/DoctorService.java
+++ jmp/mp-service/src/main/java/com/vpu/mp/service/shop/doctor/DoctorService.java
@@ -182,7 +182,6 @@ public class DoctorService extends BaseShopConfigService {
             throw MpException.initErrorResult(JsonResultCode.DOCTOR_ID_IS_NULL, "医师id为null");
         }
         DoctorOneParam doctorInfo = doctorDao.getOneInfo(doctorId);
-        doctorInfo.setConsultationPrice(doctorInfo.getConsultationTotalMoney());
         doctorInfo.setAvgAnswerTimeNotSecond(integerTimeToStringTime(doctorInfo.getAvgAnswerTime()));
         List<Integer> departmentIds = doctorDepartmentCoupleDao.getDepartmentIdsByDoctorId(doctorId);
         doctorInfo.setDepartmentIds(departmentIds);
